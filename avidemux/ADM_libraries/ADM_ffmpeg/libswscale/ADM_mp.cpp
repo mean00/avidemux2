61b952baa4c76fe5bd821153a1fba8c77e831dec
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-05-02 13:54:43 +0000

    [ffmpegupdate]
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@4794 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux/ADM_libraries/ADM_ffmpeg/libswscale/ADM_mp.cpp
+++ avidemux/ADM_libraries/ADM_ffmpeg/libswscale/ADM_mp.cpp
@@ -23,9 +23,9 @@
 
 
 extern "C" {
-#include "ADM_libraries/ADM_ffmpeg/ADM_lavcodec/avcodec.h"
+#include "ADM_libraries/ADM_ffmpeg/libavcodec/avcodec.h"
 #include "ADM_libraries/ADM_ffmpeg/libavutil/avutil.h"
-#include "ADM_libraries/ADM_ffmpeg/ADM_libswscale/swscale.h"
+#include "ADM_libraries/ADM_ffmpeg/libswscale/swscale.h"
 }
 
 
