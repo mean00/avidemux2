9ca2f4acadf5ffde2bdc983763403e5ab67830a1
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-05-20 19:31:08 +0000

    [Muxer] Raw video only muxer
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@4847 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ plugins/ADM_muxers/muxerRaw/muxerRawPlugin.cpp
@@ -0,0 +1,32 @@
+/***************************************************************************
+    copyright            : (C) 2007 by mean
+    email                : fixounet@free.fr
+    
+      See lavformat/flv[dec/env].c for detail
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include "ADM_default.h"
+#include "ADM_muxerInternal.h"
+#include "muxerRaw.h"
+
+#include "fourcc.h"
+static bool rawConfigure(void) {return true;};
+
+ADM_MUXER_BEGIN( muxerRaw,
+                    1,0,0,
+                    "RAW",    // Internal name
+                    "RAW muxer plugin (c) Mean 2008",
+                    "Video Only", // DIsplay name
+                    rawConfigure,
+                    NULL,0
+                    
+                );
+
