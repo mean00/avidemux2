d477896961d09b4bc8919764517e80e3e2e4091e
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-05-21 10:01:19 +0000

    [mkvScan] Fix build
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@4852 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ addons/mkvscan/ADM_default.h
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#define ADM_assert(x) if(!(x)){printf("OOPs");}
+#define LU "lu"
+#define LLU "llu"
+#define LD "ld"
+#define LLD "lld"
+#define LX "lx"
+#define LLX "llx"
