81cc83b4ff1b9043f343ccc7d31efabb04100027
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-06-04 17:49:01 +0000

    [xvid] Removed old version
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@4890 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- plugins/ADM_videoEncoder/ADM_vidEnc_xvid/configGuiLoader.cpp
+++
@@ -1,7 +0,0 @@
-#include "ADM_inttype.h"
-#include "configGuiLoader.h"
-
-configGuiLoader::configGuiLoader(const char *file) : ADM_LibWrapper()
-{
-	initialised = (loadLibrary(file) && getSymbols(1, &showXvidConfigDialog, "showXvidConfigDialog"));
-}
--- plugins/ADM_videoEncoder/ADM_vidEnc_xvid/qt4/xvidConfigDialog.h
+++
@@ -1,9 +0,0 @@
-#ifndef xvidConfigDialog_h
-#define xvidConfigDialog_h
-
-extern "C"
-{
-#include "ADM_vidEnc_plugin.h"
-}
-
-#endif	// xvidConfigDialog_h
