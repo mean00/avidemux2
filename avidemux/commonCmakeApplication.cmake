07a6eb3488ac7f40d51352fda3eff7955e789079
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-06-16 17:54:42 +0000

    [build] Factorize
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@4924 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux/commonCmakeApplication.cmake
@@ -0,0 +1,99 @@
+
+CMAKE_MINIMUM_REQUIRED(VERSION 2.4.7 FATAL_ERROR)
+
+if (COMMAND cmake_policy)
+	cmake_policy(VERSION 2.4)
+	cmake_policy(SET CMP0003 NEW)
+endif (COMMAND cmake_policy)
+########################################
+# Definitions and Includes
+########################################
+ADD_DEFINITIONS(-D_FILE_OFFSET_BITS=64 -D_LARGE_FILES)
+ADD_DEFINITIONS(-DHAVE_CONFIG_H)
+ADD_DEFINITIONS(-DADM_UI_TYPE_BUILD=ADM_UI_GTK)
+########################################
+# Where is the config.h dir
+########################################
+IF (NOT AVIDEMUX_CORE_BUILD_DIR)
+	MESSAGE(FATAL_ERROR "Please add -DAVIDEMUX_CORECONFIG_DIR=path_to_avidemux_core_build_dir")
+ELSE (NOT AVIDEMUX_CORE_BUILD_DIR)
+	SET(AVIDEMUX_CORE_BUILD_DIR "${AVIDEMUX_CORE_BUILD_DIR}" CACHE STRING "")
+ENDIF (NOT AVIDEMUX_CORE_BUILD_DIR)
+#
+# Access to our cmake lib
+#
+SET(AVIDEMUX_TOP_SOURCE_DIR ${CMAKE_SOURCE_DIR}/../..)
+SET(CMAKE_MODULE_PATH "${AVIDEMUX_TOP_SOURCE_DIR}/cmake" "${CMAKE_MODULE_PATH}")
+MESSAGE(STATUS "Avidemux cmake scripts are in ${CMAKE_MODULE_PATH}")
+#
+#
+#
+
+IF (ADM_DEBUG)
+	ADD_DEFINITIONS(-DADM_DEBUG)
+ENDIF (ADM_DEBUG)
+###########################################
+#  Basic cmake helper script
+###########################################
+include(admConfigHelper)
+INCLUDE(admCheckRequiredLibs)
+include(admCheckMiscLibs)
+########################################
+# Add include dirs
+########################################
+include(admCoreIncludes)
+
+INCLUDE_DIRECTORIES("${CMAKE_CURRENT_SOURCE_DIR}/ADM_muxerGate/include/")
+
+IF (GETTEXT_FOUND)
+	INCLUDE_DIRECTORIES(${GETTEXT_INCLUDE_DIR})
+ENDIF (GETTEXT_FOUND)
+INCLUDE_DIRECTORIES("${CMAKE_CURRENT_SOURCE_DIR}/../common/")
+INCLUDE_DIRECTORIES("${CMAKE_CURRENT_SOURCE_DIR}/../common/ADM_audioFilter_old")
+INCLUDE_DIRECTORIES("${CMAKE_CURRENT_SOURCE_DIR}/../common/ADM_commonUI")
+#
+# Add main source
+#
+SET(ADM_EXE_SRCS 
+../common/ADM_preview.cpp  
+../common/gtk_gui.cpp  
+../common/gui_autodrive.cpp  
+../common/GUI_jobs.cpp  
+../common/gui_navigate.cpp  
+../common/gui_play.cpp  
+../common/gui_save.cpp  
+../common/gui_savenew.cpp  
+../common/gui_misc.cpp
+../common/main.cpp  
+../common/gui_action.cpp
+../common/gui_blackframes.cpp
+../common/ADM_imageLoader.cpp  
+../common/ADM_gettext.cpp
+)
+
+#############################################
+# Add core libs
+#############################################
+SET(coreLibs
+ADM_core
+ADM_audioParser
+ADM_coreAudio
+ADM_coreAudioFilterAPI
+ADM_coreAudioDevice
+ADM_coreAudioEncoder
+ADM_coreDemuxer
+ADM_coreDemuxerMpeg
+ADM_coreImage
+ADM_coreMuxer
+ADM_coreUI
+ADM_coreUtils
+ADM_coreVideoEncoder
+ADM_coreVideoFilter
+ADM_libavcodec
+ADM_libavformat
+ADM_libavutil
+ADM_libswscale
+ADM_libpostproc
+ADM_smjs
+)
+# END
---
+++ avidemux/gtk/corePackageDebian.cmake
@@ -0,0 +1,32 @@
+##############################
+# DEBIAN
+##############################
+SET(CPACK_DEBIAN_PACKAGE_NAME "avidemux2-gtk")
+SET (CPACK_GENERATOR "DEB")
+# ARCH
+IF (X86_64_SUPPORTED)
+SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64")
+ELSE (X86_64_SUPPORTED)
+SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "i386")
+ENDIF (X86_64_SUPPORTED)
+# Mandatory
+SET(CPACK_DEBIAN_PACKAGE_MAINTAINER "mean")
+SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Simple video editori,main program gtk version ")
+
+# Build our deps list
+# Build deps SET(DEPS "debhelper (>> 4), libfreetype6-dev, libxml2-dev,  libxv-dev, dpatch , cmake, desktop-file-utils")
+SET(DEPS "libc6 (>=2.4),libglib2.0-0 (>=2.14.0),libstdc++6 (>=4.2.1),libx11-6, libext6, libxml2 (>=2.6.27), libxv1, zlib1g (>=1:1.1.4), avidemux2_core (>=2.6.0)")
+# Add optional DEPS here
+SET(CPACK_DEBIAN_PACKAGE_DEPENDS "${DEPS}")
+#
+SET(CPACK_DEBIAN_PACKAGE_SECTION "extra")
+SET(CPACK_DEBIAN_PACKAGE_PRIORITY "optional")
+# Some more infos
+SET(CPACK_PACKAGE_VERSION_MAJOR "2")
+SET(CPACK_PACKAGE_VERSION_MINOR "6")
+SET(CPACK_PACKAGE_VERSION_PATCH "0")
+#
+SET(CPACK_PACKAGE_NAME "avidemux2-gtk")
+#
+
+include(CPack)
