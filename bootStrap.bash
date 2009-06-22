6d4dbf9d0325f2911789e9f77bdff9ff17456942
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-06-22 05:43:20 +0000

    [build] Add simple build all script (no plugins for the moment)
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@4950 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ bootStrap.bash
@@ -0,0 +1,36 @@
+
+
+fail()
+{
+        echo "** Failed at $1**"
+        exit 1
+}
+
+Process()
+{
+        export BUILDDIR=$1
+        export SOURCEDIR=$2
+        echo "Building $BUILDDIR from $SOURCEDIR"
+        rm -Rf $BUILDDIR
+        mkdir $BUILDDIR || fail mkdir
+        cd $BUILDDIR 
+        cmake -DCMAKE_INSTALL_PREFIX=/usr $SOURCEDIR || fail cmake
+        make -j 2 > /tmp/log$BUILDDIR || fail make
+        sudo make install || fail install
+        sudo chown -R `whoami` .
+        fakeroot make package || fail package
+}
+
+export TOP=$PWD
+sudo -v
+echo "**BootStrapping avidemux **"
+echo "** CORE **"
+cd $TOP
+#Process buildCore ../avidemux_core
+echo "** QT4 **"
+cd $TOP
+#Process buildQt4 ../avidemux/qt4
+echo "** GTK **"
+cd $TOP
+Process buildGtk ../avidemux/gtk
+echo "** ALL DONE **"
