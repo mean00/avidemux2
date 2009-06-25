7885ca931f9dca8ae3bd8f248ee1c2f78d7b530d
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-06-25 17:13:02 +0000

    [core/image] Begin to reimplement save as jpeg only depends on core
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@4957 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_core/ADM_coreImage/src/ADM_imageSave.cpp
+++ avidemux_core/ADM_coreImage/src/ADM_imageSave.cpp
@@ -1,5 +1,5 @@
 /**
-      \file gui_misc
+      \file A
       \brief 2nd part of ADMImage, put here to avoid weird dependancies
 */
 #include "ADM_default.h"
@@ -7,6 +7,11 @@
 #include "ADM_bitmap.h"
 #include "DIA_coreToolkit.h"
 #include "ADM_colorspace.h"
+extern "C" 
+{
+#include "ADM_lavcodec.h"
+}
+
 /**
     \fn saveAsBmp
     \brief save current image into filename, into bmp format
@@ -107,39 +112,78 @@ uint8_t  ADMImage::saveAsBmp(const char *filename)
 */
 uint8_t  ADMImage::saveAsJpg(const char *filename)
 {
-    #if 0
- ffmpegEncoderFFMjpeg *codec=NULL;
-  FILE *fd;
-  uint8_t *buffer=NULL;
-  uint32_t sz;
 
+AVCodecContext   *context=NULL;   
+AVFrame          frame;     
+bool             result=false;
+AVCodec          *codec=NULL;
+uint8_t          buffer[_width*_height*4];
+int              sz=0,r=0;
 
-        sz = _width*_height*3;
-        ADMBitstream bitstream(sz);
-        buffer=new uint8_t[sz];
-        bitstream.data=buffer;
-        codec=new  ffmpegEncoderFFMjpeg(_width,_height,FF_MJPEG)  ;
-        codec->init( 95,25000);
-        if(!codec->encode(this,&bitstream))
-        {
-                GUI_Error_HIG(QT_TR_NOOP("Cannot encode the frame"), NULL);
-                delete [] buffer;
-                delete codec;
-                return 0;
-        }
-        delete codec;
-        fd=fopen(filename,"wb");
-        if(!fd)
-        {
-                GUI_Error_HIG(QT_TR_NOOP("File error"),QT_TR_NOOP( "Cannot open \"%s\" for writing."), filename);
-                delete [] buffer;
-                return 0;
-        }
-        fwrite (buffer, bitstream.len, 1, fd);
-        fclose(fd);
-        delete [] buffer;
-        return 1;
-        #endif
-#warning save as jpeg disabled
+    context=avcodec_alloc_context();
+    if(!context) 
+    {
+        printf("[saveAsJpg] Cannot allocate context\n");
         return false;
+    }
+    codec=avcodec_find_encoder(CODEC_ID_MJPEG);
+    if(!codec)
+    {
+        printf("[saveAsJpg] Cannot allocate codec\n");
+        goto jpgCleanup;
+    }
+    context->pix_fmt =PIX_FMT_YUV420P;
+    context->strict_std_compliance = -1;
+    r=avcodec_open(context, codec); 
+    if(r<0)
+    {
+        printf("[saveAsJpg] Cannot mix codec and context\n");
+        ADM_dealloc (context);
+        return false;
+    }
+    // Setup our image & stuff....
+        
+
+        frame.linesize[0] = _width; 
+        frame.linesize[1] = _width>>1; 
+        frame.linesize[2] = _width>>1; 
+        
+        frame.data[0] = GetWritePtr(PLANAR_Y);
+        frame.data[2] = GetWritePtr(PLANAR_U);
+        frame.data[1] = GetWritePtr(PLANAR_V);
+    // Grab a temp buffer
+    
+    // Encode!
+      context->flags |= CODEC_FLAG_QSCALE;
+      frame.quality = (int) floor (FF_QP2LAMBDA * 2+ 0.5);
+        
+        if ((sz = avcodec_encode_video (context, buffer, _width*_height*4, &frame)) < 0)
+        {
+            printf("[jpeg] Error %d encoding video\n",sz);
+            goto  jpgCleanup;
+        }
+        // Ok now write our file...
+        {
+            FILE *f=fopen(filename,"wb");
+            if(f)
+            {
+                fwrite(buffer,sz,1,f);
+                fclose(f);
+                result=true;
+            }else
+            {
+                printf("[saveAsJpeg] Cannot open %s for writing!\n",filename);
+
+            }
+       }
+
+// Cleanup
+jpgCleanup:
+    if(context)
+    {
+        avcodec_close (context);
+        ADM_dealloc (context);
+    }
+    context=NULL;
+    return result;
 }
