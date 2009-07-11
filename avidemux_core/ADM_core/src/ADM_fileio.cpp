f849c94799a2f831d8aa3f8558cde263e423a178
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-07-11 16:09:36 +0000

    [build] gcc 4.4 patch by whaver
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5043 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_core/ADM_core/src/ADM_fileio.cpp
+++ avidemux_core/ADM_core/src/ADM_fileio.cpp
@@ -626,7 +626,7 @@ void ADM_PathStripName(char *str)
 const char *ADM_GetFileName(const char *str)
 {
 	char *filename;
-	char *filename2;
+	const char *filename2;
 
 #ifndef __WIN32
 	filename = strrchr(str, '/');
