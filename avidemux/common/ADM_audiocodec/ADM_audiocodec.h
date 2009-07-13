fbbab982b2d46b1aed6940916eb05393ebacfe8f
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-07-13 08:56:57 +0000

    [audioCodec] more cleanup
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5082 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux/common/ADM_audiocodec/ADM_audiocodec.h
+++ avidemux/common/ADM_audiocodec/ADM_audiocodec.h
@@ -19,7 +19,7 @@
 #include "ADM_audiodef.h"
 #define SCRATCH_PAD_SIZE (100*1000*2)
 extern uint8_t scratchPad[];
-#define  ADMAC_BUFFER (48000*4)
+//#define  ADMAC_BUFFER (48000*4)
 class ADM_Audiocodec
 {
     protected:
