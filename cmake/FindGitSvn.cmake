24f0485fb64ffdb78cc64fd68fd5917eca83e5af
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-07-28 18:33:06 +0000

    [Build] Hack to FindGitSvn so that it works with copies of the git-svn (hudson)
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5164 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- cmake/FindGitSvn.cmake
+++ cmake/FindGitSvn.cmake
@@ -8,14 +8,14 @@ IF(GIT_EXECUTABLE)
         #SET(EXE  "cd ${AVIDEMUX_TOP_SOURCE_DIR}&& ${GIT_EXECUTABLE} svn log | head -2  | grep '^r' | sed 's/ .*$//g'" )
         MESSAGE(STATUS "Getting git-svn version from ${AVIDEMUX_TOP_SOURCE_DIR}")
 
-      EXECUTE_PROCESS(COMMAND /usr/bin/git svn log --oneline --limit=1   
+      EXECUTE_PROCESS(COMMAND /usr/bin/git log -n 1   
         WORKING_DIRECTORY ${AVIDEMUX_TOP_SOURCE_DIR} 
         RESULT_VARIABLE result 
         OUTPUT_VARIABLE output
         )
-        STRING(REGEX REPLACE " .*$" "" ADM_GIT_SVN_REVISION "${output}")
-        STRING(REGEX REPLACE "^r" "" ADM_GIT_SVN_REVISION "${ADM_GIT_SVN_REVISION}")
-        MESSAGE(STATUS "Git Svn Revision : ${ADM_GIT_SVN_REVISION}")
+        STRING(REGEX REPLACE ".*branch_mean@" "" ADM_GIT_SVN_REVISION "${output}")
+        STRING(REGEX REPLACE " .*$" "" ADM_GIT_SVN_REVISION "${ADM_GIT_SVN_REVISION}")
+        MESSAGE(STATUS "Git Svn Revision : <${ADM_GIT_SVN_REVISION}>")
 ELSE(GIT_EXECUTABLE)
         SET(ADM_GIT_SVN_REVISION 0)
 ENDIF(GIT_EXECUTABLE)
