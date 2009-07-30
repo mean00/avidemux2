52bed16e21166430cbc0b5f45796e3b7c16bb54f
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-07-30 18:04:59 +0000

    [build] Use updated getgitrevision from 2.5
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5174 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- cmake/FindGitSvn.cmake
+++ cmake/FindGitSvn.cmake
@@ -1,23 +1,31 @@
 # - Extract information from a subversion working copy
+MACRO(admGetGitRevision _dir _rev)
+
 FIND_PROGRAM(GIT_EXECUTABLE git
   DOC "git command line client")
 MARK_AS_ADVANCED(GIT_EXECUTABLE)
 
 IF(GIT_EXECUTABLE)
         
-        #SET(EXE  "cd ${AVIDEMUX_TOP_SOURCE_DIR}&& ${GIT_EXECUTABLE} svn log | head -2  | grep '^r' | sed 's/ .*$//g'" )
-        MESSAGE(STATUS "Getting git-svn version from ${AVIDEMUX_TOP_SOURCE_DIR}")
+        #SET(EXE  "cd ${_dir}&& ${GIT_EXECUTABLE} svn log | head -2  | grep '^r' | sed 's/ .*$//g'" )
+        MESSAGE(STATUS "Getting git-svn version from ${_dir}")
 
-      EXECUTE_PROCESS(COMMAND /usr/bin/git log -n 1   
-        WORKING_DIRECTORY ${AVIDEMUX_TOP_SOURCE_DIR} 
+      EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} log -n 50 ${_dir}
+        COMMAND grep git-svn-id
+	COMMAND head -1
+        WORKING_DIRECTORY ${_dir} 
         RESULT_VARIABLE result 
         OUTPUT_VARIABLE output
         )
-        STRING(REGEX REPLACE ".*branch_mean@" "" ADM_GIT_SVN_REVISION "${output}")
-        STRING(REGEX REPLACE " .*$" "" ADM_GIT_SVN_REVISION "${ADM_GIT_SVN_REVISION}")
-        MESSAGE(STATUS "Git Svn Revision : <${ADM_GIT_SVN_REVISION}>")
+        #MESSAGE(STATUS "Git Svn Revision : <${output}>")
+        STRING(REGEX REPLACE ".*branch_.*@" "" rev "${output}")
+        STRING(REGEX REPLACE " .*$" "" rev "${rev}")
+        MESSAGE(STATUS "Git Svn Revision : <${rev}>")
+        SET( ${_rev} ${rev})
 ELSE(GIT_EXECUTABLE)
         SET(ADM_GIT_SVN_REVISION 0)
 ENDIF(GIT_EXECUTABLE)
+
+ENDMACRO(admGetGitRevision _dir _rev)
 
 # FindSubversion.cmake ends here.
