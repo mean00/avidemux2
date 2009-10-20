1d576fd4502fef0df16d3e041c1d40d43f56b302
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-10-20 13:26:37 +0000

    [Qt4] window() behaviour has changed on recent qt, hence the root window is used for Xv/SDL display with the old scheme. Directly use the winid() so that the video window is used.
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5387 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux/qt4/ADM_userInterfaces/ADM_gui/T_preview.cpp
+++ avidemux/qt4/ADM_userInterfaces/ADM_gui/T_preview.cpp
@@ -177,7 +177,7 @@ void UI_getWindowInfo(void *draw, GUI_WindowInfo *xinfo)
     const QX11Info &info=videoWindow->x11Info();
     xinfo->display=info.display();
    // xinfo->window=videoWindow->winId();
-    xinfo->window=videoWindow->window()->winId();
+    xinfo->window=videoWindow->winId();
 #endif
 
 	xinfo->x = widget->x();
