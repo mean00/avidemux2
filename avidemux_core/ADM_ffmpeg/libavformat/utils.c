a1f80a8b8ae436bcdb7d6474979530815bec1312
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-10-27 06:44:27 +0000

    [Lavformat] Display value when pts<dts
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5419 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_core/ADM_ffmpeg/libavformat/utils.c
+++ avidemux_core/ADM_ffmpeg/libavformat/utils.c
@@ -2601,8 +2601,10 @@ static int compute_pkt_fields2(AVStream *st, AVPacket *pkt){
         av_log(st->codec, AV_LOG_ERROR, "error, non monotone timestamps %"PRId64" >= %"PRId64"\n", st->cur_dts, pkt->dts);
         return -1;
     }
-    if(pkt->dts != AV_NOPTS_VALUE && pkt->pts != AV_NOPTS_VALUE && pkt->pts < pkt->dts){
-        av_log(st->codec, AV_LOG_ERROR, "error, pts < dts\n");
+    if(pkt->dts != AV_NOPTS_VALUE && pkt->pts != AV_NOPTS_VALUE && pkt->pts < pkt->dts)
+    {
+        av_log(st->codec, AV_LOG_ERROR, "error, pts < dts pts=%llu dts=%llu\n",pkt->pts,pkt->dts);
+        
         return -1;
     }
 
