423d50c4af51085c07f6c06f6db2ca60353e889a
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-11-05 18:25:03 +0000

    [JS] Sample app.video.dumpEditing()
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5446 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ autononreg/videofunc/dumpEditing.js
@@ -0,0 +1,5 @@
+//AD  <- These first 4 characters need to be the first 4 characters to identify the ECMAScript file to Avidemux
+var app = new Avidemux();
+app.video.dumpEditing();
+/* End of test
+*/
