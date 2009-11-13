0637e5374e9acb340a8237a7c094675685d110f3
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-11-13 18:27:18 +0000

    [VideoEncoder/Mjpeg] Switch to new configuration scheme (conf files)
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5490 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux_plugins/ADM_videoEncoder/jpeg/jpeg_encoder.h
@@ -0,0 +1,9 @@
+// Automatically generated, do not edit!
+#ifndef ADM_jpeg_encoder_CONF_H
+#define ADM_jpeg_encoder_CONF_H
+typedef struct {
+   uint32_t colorSpace;
+   uint32_t quantizer;
+}jpeg_encoder;
+#endif //jpeg_encoder
+//EOF
---
+++ avidemux_plugins/ADM_videoEncoder/jpeg/jpeg_encoder_desc.cpp
@@ -0,0 +1,6 @@
+// Automatically generated, do not edit!
+const ADM_paramList jpeg_encoder_param[]={
+ {"colorSpace",offsetof( jpeg_encoder,colorSpace),"uint32_t",ADM_param_uint32_t},
+ {"quantizer",offsetof( jpeg_encoder,quantizer),"uint32_t",ADM_param_uint32_t},
+{NULL,0,NULL}
+};
