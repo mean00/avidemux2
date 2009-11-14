c7e49e226ec633ed58eda4562eb5a9a6872c12a1
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-11-14 08:25:12 +0000

    [MKV/Demux] Add PCM type (most common flavor) to audio track type
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5493 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_plugins/ADM_demuxers/Matroska/ADM_mkvTrackType.cpp
+++ avidemux_plugins/ADM_demuxers/Matroska/ADM_mkvTrackType.cpp
@@ -41,6 +41,7 @@ MKVCC mkvCC[]=
   {"A_AAC/MPEG2/LC",0,WAV_AAC,""},
   {"A_AAC/MPEG4/LC/SBR",0,WAV_AAC,""},
   {"A_AAC/MPEG4/LC",0,WAV_AAC,""},
+  {"A_PCM/INT/LIT",0,WAV_PCM,""},
 
   {"A_AAC",0,WAV_AAC,""},
   {"A_VORBIS",0,WAV_OGG_VORBIS,""},
