15c142e989118412ee3673ea13fe0090ecddc34c
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-11-15 11:22:48 +0000

    [Win32] Bootstrap script for mingw-w32
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5504 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ bootStrapCrossMingw_w32.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# ** Put your config here **
+export PATH=$PATH:/mingw/bin
+export CROSS_PREFIX=i686-w64-mingw32
+export SDLDIR=/mingw
+export MINGW=/mingw
+export QT_HOME=/mingw/Qt/4.5.2
+# ** Put your config here **
+
+fail()
+{
+        echo "** Failed at $1**"
+        exit 1
+}
+
+Process()
+{
+        export BUILDDIR=$1
+        export SCRIPT=$2
+        echo "Building $BUILDDIR from $SOURCEDIR"
+        rm -Rf ./$BUILDDIR
+        mkdir $BUILDDIR || fail mkdir
+        cd $BUILDDIR 
+        sh $TOP/foreignBuilds/$SCRIPT || fail cmake
+        make -j 2 > /tmp/log$BUILDDIR || fail make
+        make install || fail make_install
+}
+
+echo "**BootStrapping avidemux **"
+export TOP=$PWD
+echo "Top dir : $TOP"
+echo "** CORE **"
+cd $TOP
+Process buildMingwCore cross_mingw_core
+echo "** QT4 **"
+cd $TOP
+Process buildMingwQt4 cross_mingw_qt4 
+#echo "** GTK **"
+#cd $TOP
+#Process buildGtk ../avidemux/gtk
+echo "** Plugins **"
+cd $TOP
+Process buildMingwPlugins cross_mingw_plugins 
+echo "** All done **"
+cd $TOP
+echo "** ALL DONE **"
