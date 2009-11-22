2c71d606f53629b1658f433f7156c4cf51836b95
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-11-22 09:14:43 +0000

    [Build] Improved bootstrap
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5538 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- bootStrap.bash
+++ bootStrap.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+do_core=1
+do_gtk=1
+do_qt4=0
+do_plugins=1
 fail()
 {
         echo "** Failed at $1**"
@@ -18,24 +21,106 @@ Process()
         make -j 2 > /tmp/log$BUILDDIR || fail make
         fakeroot make package DESTDIR=debPack || fail package
 }
+printModule()
+{
+        value=$1
+        name=$2
+        if [ "x$value" = "x1" ]; then echo "    $name will be built"
+        else echo "     $name will be skipped"
+        fi
+        
 
+}
+config()
+{
+        echo "Build configuration :"
+        echo "******************* :"
+        printModule $do_core Core
+        printModule $do_gtk Gtk
+        printModule $do_qt4 Qt4
+        printModule $do_plugins Plugins
+}
+usage()
+{
+        echo "Bootstrap avidemux 2.6:"
+        echo "***********************"
+        echo "  --help            : Print usage"
+        echo "  --with-core       : Build core"
+        echo "  --without-core    : Dont build core"
+        echo "  --with-gtk        : Build gtk"
+        echo "  --without-gtk     : Dont build gtk"
+        echo "  --with-core       : Build core"
+        echo "  --without-qt4     : Dont build qt4"
+        echo "  --with-plugins    : Build plugins"
+        echo "  --without-plugins : Dont build plugins"
+        config 
+
+}
+# Could probably do it with getopts...
+while [ $# != 0 ] ;do
+        case "$1" in
+         -h|--help)
+             usage
+             exit 1
+             ;;
+         --without-qt4)
+                do_qt4=0
+             ;;
+         --without-gtk)
+                do_gtk=0
+             ;;
+         --without-plugins)
+                do_plugins=0
+             ;;
+         --without-core)
+                do_core=0
+             ;;
+         --with-qt4)
+                do_qt4=1
+             ;;
+         --with-gtk)
+                do_gtk=1
+             ;;
+         --with-plugins)
+                do_plugins=1
+             ;;
+         --with-core)
+                do_core=1
+             ;;
+        *)
+                echo "unknown parameter $1"
+                usage
+                exit 1
+                ;;
+     esac
+     shift
+done
+config
 echo "**BootStrapping avidemux **"
 export TOP=$PWD
 echo "Top dir : $TOP"
-echo "** CORE **"
-cd $TOP
-Process buildCore ../avidemux_core
-echo " Core needs to be installed, installing through sudo make install ...."
-cd $TOP/buildCore && sudo make install
-echo "** QT4 **"
-cd $TOP
-#Process buildQt4 ../avidemux/qt4
-echo "** GTK **"
-cd $TOP
-Process buildGtk ../avidemux/gtk
-echo "** Plugins **"
-cd $TOP
-Process buildPlugins ../avidemux_plugins
+if [ "x$do_core" = "x1" ] ; then 
+        echo "** CORE **"
+        cd $TOP
+        Process buildCore ../avidemux_core
+        echo " Core needs to be installed, installing through sudo make install ...."
+        cd $TOP/buildCore && sudo make install
+fi
+if [ "x$do_qt4" = "x1" ] ; then 
+        echo "** QT4 **"
+        cd $TOP
+        Process buildQt4 ../avidemux/qt4
+fi
+if [ "x$do_qt4" = "x1" ] ; then 
+        echo "** GTK **"
+        cd $TOP
+        Process buildGtk ../avidemux/gtk
+fi
+if [ "x$do_plugins" = "x1" ] ; then 
+        echo "** Plugins **"
+        cd $TOP
+        Process buildPlugins ../avidemux_plugins
+fi
 echo "** Preparing debs **"
 cd $TOP
 rm -Rf debs
