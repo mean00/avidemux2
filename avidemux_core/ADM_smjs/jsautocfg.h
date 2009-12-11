c25502ae858559e804b171b1debd7a9b60fd2ff8
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-12-11 10:14:25 +0000

    [js] 32/64 bits setup
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5643 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_core/ADM_smjs/jsautocfg.h
+++ avidemux_core/ADM_smjs/jsautocfg.h
@@ -1,18 +1,14 @@
-#ifndef js_autocfg___
-#define js_autocfg___
+#ifndef js_cpucfg___
+#define js_cpucfg___
 
 /* AUTOMATICALLY GENERATED - DO NOT EDIT */
-#include "ADM_coreConfig.h"
-
-
 #ifdef ADM_BIG_ENDIAN
 #undef   IS_LITTLE_ENDIAN 
-#define  IS_BIG_ENDIAN 1
+#define  IS_BIG_ENDIAN   1
 #else
 #define IS_LITTLE_ENDIAN 1
 #undef  IS_BIG_ENDIAN
 #endif
-
 
 #define JS_BYTES_PER_BYTE   1L
 #define JS_BYTES_PER_SHORT  2L
@@ -25,31 +21,9 @@
 #ifdef ADM_CPU_64BIT
 #define JS_BYTES_PER_LONG   8L
 #define JS_BYTES_PER_WORD   8L
-#define JS_BITS_PER_LONG    64L
-#define JS_BITS_PER_WORD    64L
-#define JS_BITS_PER_LONG_LOG2   6L
-#define JS_BITS_PER_WORD_LOG2   6L
-#define JS_ALIGN_OF_LONG    8L
-#define JS_ALIGN_OF_INT64   8L
-#define JS_ALIGN_OF_DOUBLE  8L
-#define JS_ALIGN_OF_POINTER 8L
-#define JS_ALIGN_OF_WORD    8L
-
-
-#else // 32bits
+#else
 #define JS_BYTES_PER_LONG   4L
 #define JS_BYTES_PER_WORD   4L
-#define JS_BITS_PER_LONG    32L
-#define JS_BITS_PER_WORD    32L
-#define JS_BITS_PER_LONG_LOG2   5L
-#define JS_BITS_PER_WORD_LOG2   5L
-
-#define JS_ALIGN_OF_LONG    4L
-#define JS_ALIGN_OF_INT64   4L
-#define JS_ALIGN_OF_DOUBLE  4L
-#define JS_ALIGN_OF_POINTER 4L
-#define JS_ALIGN_OF_WORD    4L
-
 #endif
 
 #define JS_BITS_PER_BYTE    8L
@@ -59,6 +33,14 @@
 #define JS_BITS_PER_FLOAT   32L
 #define JS_BITS_PER_DOUBLE  64L
 
+#ifdef ADM_CPU_64BIT
+#define JS_BITS_PER_WORD    64L
+#define JS_BITS_PER_LONG    64L
+#else
+#define JS_BITS_PER_WORD    32L
+#define JS_BITS_PER_LONG    32L
+#endif
+
 #define JS_BITS_PER_BYTE_LOG2   3L
 #define JS_BITS_PER_SHORT_LOG2  4L
 #define JS_BITS_PER_INT_LOG2    5L
@@ -66,14 +48,39 @@
 #define JS_BITS_PER_FLOAT_LOG2  5L
 #define JS_BITS_PER_DOUBLE_LOG2 6L
 
+#ifdef ADM_CPU_64BIT
+#define JS_BITS_PER_WORD_LOG2   6L
+#define JS_BITS_PER_LONG_LOG2   6L
+#else
+#define JS_BITS_PER_WORD_LOG2   5L
+#define JS_BITS_PER_LONG_LOG2   5L
+#endif
+
+#define JS_ALIGN_OF_FLOAT   4L
 #define JS_ALIGN_OF_SHORT   2L
 #define JS_ALIGN_OF_INT     4L
-#define JS_ALIGN_OF_FLOAT   4L
 
+#ifdef ADM_CPU_64BIT
+#define ADM_ALIGN               8L
+#else
+#define ADM_ALIGN               4L
+#endif
+
+#define JS_ALIGN_OF_LONG    ADM_ALIGN
+#define JS_ALIGN_OF_INT64   ADM_ALIGN
+#define JS_ALIGN_OF_DOUBLE  ADM_ALIGN
+#define JS_ALIGN_OF_POINTER ADM_ALIGN
+#define JS_ALIGN_OF_WORD    ADM_ALIGN
+
+#ifdef ADM_CPU_64BIT
+#define JS_BYTES_PER_WORD_LOG2   3L
+#define JS_BYTES_PER_DWORD_LOG2  3L
+#define JS_WORDS_PER_DWORD_LOG2  0L
+#else
 #define JS_BYTES_PER_WORD_LOG2   2L
 #define JS_BYTES_PER_DWORD_LOG2  3L
 #define JS_WORDS_PER_DWORD_LOG2  1L
-
+#endif
 #define JS_STACK_GROWTH_DIRECTION (-1)
 
 #endif /* js_cpucfg___ */
