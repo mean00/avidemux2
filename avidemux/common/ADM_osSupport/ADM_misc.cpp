021a12c16f708c8b59c4f3364687dd490ae7ce92
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-12-21 13:31:26 +0000

    [Misc] Fix buffer overflow with some languages (greek)
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5683 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux/common/ADM_osSupport/ADM_misc.cpp
+++ avidemux/common/ADM_osSupport/ADM_misc.cpp
@@ -5,17 +5,18 @@
 char* ms2timedisplay(uint32_t ms)
 {
 	uint32_t mm, ss;
-	static char string[20];
+#define ADM_MAX_STRING 256
+	static char string[ADM_MAX_STRING+1];
 
 	mm = (uint32_t)floor(ms / 60000.);
 	
 	if (mm > 1)
 	{
-		sprintf(string, QT_TR_NOOP("%"LU" minutes"), mm);
+		snprintf(string, ADM_MAX_STRING,QT_TR_NOOP("%"LU" minutes"), mm);
 	}
 	else if (mm == 1)
 	{
-		sprintf(string, QT_TR_NOOP("%"LU" minute"), mm);
+		snprintf(string,ADM_MAX_STRING, QT_TR_NOOP("%"LU" minute"), mm);
 	}
 	else
 	{
@@ -23,14 +24,14 @@ char* ms2timedisplay(uint32_t ms)
 
 		if (ss == 1)
 		{
-			sprintf(string, QT_TR_NOOP("%"LU" second"), ss);
+			snprintf(string,ADM_MAX_STRING, QT_TR_NOOP("%"LU" second"), ss);
 		}
 		else
 		{
-			sprintf(string, QT_TR_NOOP("%"LU" seconds"), ss);
+			snprintf(string,ADM_MAX_STRING, QT_TR_NOOP("%"LU" seconds"), ss);
 		}
 	}
-
+    string[ADM_MAX_STRING]=0;
 	return string;
 }
 
