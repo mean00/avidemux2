27aa872113d552af98daa85638093e4ca79ba521
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-12-29 18:51:40 +0000

    [aften] Verbose aften check + add the SVN case
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5768 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- cmake_compile_check/aften_check.cpp
+++ cmake_compile_check/aften_check.cpp
@@ -1,14 +1,17 @@
 #include <string.h>
+#include <stdio.h>
 #include <aften/aften.h>
 
 int main(int argc, char **argv)
 {
 	const char* aftenVersion = aften_get_version();
-
+        fprintf(stderr,"Aften version = %s\n",aftenVersion);
 	if (strcmp(aftenVersion, "0.07") == 0)
 		return 7;
 	else if (strcmp(aftenVersion, "0.0.8") == 0)
 		return 8;
+	else if (strcmp(aftenVersion, "SVN") == 0)
+		return 99;
 
-	return 0;
+	return -1;
 }
