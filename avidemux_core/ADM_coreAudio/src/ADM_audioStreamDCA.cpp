3e6d69e8f2050b2e739e95d0b7bad7ab1c8b56ec
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2009-12-30 19:03:36 +0000

    [DCA] Add and use audioParser for DTS/DCA
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5781 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux_core/ADM_coreAudio/src/ADM_audioStreamDCA.cpp
@@ -0,0 +1,98 @@
+/**
+    \file ADM_audioStreamDCA
+    \brief DCA Handling class
+
+*/
+#include "ADM_default.h"
+#include "ADM_audioStreamDCA.h"
+#include "ADM_dcainfo.h"
+
+/**
+    \fn ADM_audioStreamDCA
+    \brief constructor
+*/
+ADM_audioStreamDCA::ADM_audioStreamDCA(WAVHeader *header,ADM_audioAccess *access) : ADM_audioStreamBuffered(header,access)
+{
+    if(access->canGetDuration()==false)
+    {
+        // We can compute the duration from the length
+        float size=access->getLength();
+              size/=header->byterate; // Result is in second
+              size*=1000;
+              size*=1000; // s->us
+              durationInUs=(uint64_t)size;
+    }
+}
+
+/**
+    \fn ADM_audioStream
+    \brief destructor
+*/
+ADM_audioStreamDCA::~ADM_audioStreamDCA()
+{
+
+}
+/**
+    \fn goToTime
+    \brief goToTime
+*/
+bool         ADM_audioStreamDCA::goToTime(uint64_t nbUs)
+{
+    if(access->canSeekTime()==true)
+    {
+        if( access->goToTime(nbUs)==true)
+        {
+           setDts(nbUs);
+           limit=start=0;
+           refill();
+           return 1;
+        }
+        return 1;
+    }
+    // If CBR we can use the default way
+    return ADM_audioStream::goToTime(nbUs);
+
+}
+/**
+        \fn getPacket
+*/
+uint8_t ADM_audioStreamDCA::getPacket(uint8_t *obuffer,uint32_t *osize, uint32_t sizeMax,uint32_t *nbSample,uint64_t *dts)
+{
+#define ADM_LOOK_AHEAD DTS_HEADER_SIZE // Need 10 bytes...
+uint8_t data[ADM_LOOK_AHEAD];
+uint32_t offset;
+int size;
+ADM_DCA_INFO info;
+    while(1)
+    {
+        // Do we have sync ?
+        if(needBytes(ADM_LOOK_AHEAD)==false) return 0;
+        // Peek
+        peek(ADM_LOOK_AHEAD,data);
+        // Search start seq
+        if(buffer[start]!=0x7F || buffer[start+1]!=0xFE)
+        {
+            read8();
+            continue;
+        }
+        if(false== ADM_DCAGetInfo(buffer+start, limit-start,&info,&offset))
+        {
+            read8();
+            continue;
+        }
+        ADM_assert(info.frameSizeInBytes<=sizeMax);
+        if(needBytes(info.frameSizeInBytes)==false)
+        {
+            return false;
+        }
+        *osize=info.frameSizeInBytes;
+        read(*osize,obuffer);
+        *nbSample=info.samples;
+        *dts=lastDts;
+        advanceDtsBySample(*nbSample);
+        return 1;
+
+    }
+}
+
+// EOF
