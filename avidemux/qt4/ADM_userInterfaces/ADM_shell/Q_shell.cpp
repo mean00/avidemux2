db13337a3eca63685e2a543b6f4efd30417e897b
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-01-05 06:19:34 +0000

    [qShell] Handle small history in shell
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5822 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux/qt4/ADM_userInterfaces/ADM_shell/Q_shell.cpp
+++ avidemux/qt4/ADM_userInterfaces/ADM_shell/Q_shell.cpp
@@ -24,31 +24,57 @@
 #include "Q_shell.h"
 #include "ADM_default.h"
 #include "ui_shell.h"
-
+/**
+    \fn qShell
+*/
 qShell::qShell(jsShellEvaluate *s) : QDialog()
 {
     ADM_info("Setting up JS shell..\n");
     evaluator=s;
+    for(int i=0;i<Q_SHELL_HISTORY;i++)
+                    history[i]=NULL;
+
     ui.setupUi(this);
+    ui.textBrowser_2->installEventFilter(this);
     connect((ui.evalute),SIGNAL(clicked(bool)),this,SLOT(evaluate(bool)));
     connect((ui.clear),SIGNAL(clicked(bool)),this,SLOT(clear(bool)));
-    print(JS_LOG_NORMAL,"Enter your commands then press the evaluate button or CTRL+ENTER\nReady.\n");
+    print(JS_LOG_NORMAL,"Enter your commands then press the evaluate button or CTRL+ENTER.\n");
+    print(JS_LOG_NORMAL,"You can use CTRL+PageUP and CTRL+Page Down to recall previous commands\nReady.\n");
+    indexRead=indexWrite=0;
 }
-
+/**
+    \fn ~qShell
+*/
 qShell::~qShell()
 {
     ADM_info("Destroying JS shell..\n");
+    for(int i=0;i<Q_SHELL_HISTORY;i++)
+    {
+            if(history[i]) delete history[i];
+            history[i]=NULL;
+    }
+
 } 
 bool qShell::run(void)
 {
     this->exec();
     return true;
 }
+/**
+        \fn evaluate
+        \brief callback for the evaluate button
+*/
 bool            qShell::evaluate(bool x)
 {
     ADM_info("Evaluating...\n");
     // 1 Get text from UI
     QString text=ui.textBrowser_2->toPlainText();
+    int dex=indexWrite & (Q_SHELL_HISTORY-1);
+    if(history[dex]) delete history[dex];
+    history[dex]=new QString(text);
+    indexWrite++;
+    indexRead=indexWrite-1; // Points to the last one
+
     ui.textBrowser->setFontItalic(true);
     ui.textBrowser->append(text);
     ui.textBrowser->setFontItalic(false);
@@ -80,7 +106,52 @@ bool qShell::clear(bool x)
     ui.textBrowser->clear();
     return true;
 }
-
+/**
+    \fn eventFilter
+    \brief 
+*/
+bool qShell::eventFilter(QObject* watched, QEvent* event)
+{
+	if(event->type()==QEvent::KeyPress)
+    {
+			QKeyEvent *keyEvent = (QKeyEvent*)event;
+            if(keyEvent->modifiers() == Qt::ControlModifier)
+                switch (keyEvent->key())
+                {
+                    case Qt::Key_PageUp:   previousCommand();return true;break;
+                    case Qt::Key_PageDown: nextCommand();return true;break;
+                    default:break;
+                }
+    }
+    return QObject::eventFilter(watched, event);
+}
+/**
+    \fn previousCommand
+*/
+bool   qShell::previousCommand(void)
+{
+    int dex=indexRead & (Q_SHELL_HISTORY-1);
+    QString *copy=history[dex];
+    if(indexRead) indexRead--;
+    if(!copy) return true;
+    ui.textBrowser_2->clear();
+    ui.textBrowser_2->append(*copy);
+    return true;
+}
+/**
+    \fn nextCommand
+*/
+bool   qShell::nextCommand(void)
+{
+    if(indexRead+1<indexWrite) indexRead++;
+            else return true;
+    int dex=indexRead & (Q_SHELL_HISTORY-1);
+    QString *copy=history[dex];
+    if(!copy) return true;
+    ui.textBrowser_2->clear();
+    ui.textBrowser_2->append(*copy);
+    return true;
+}
 //EOF
 
 
--- avidemux/qt4/ADM_userInterfaces/ADM_shell/Q_shell.h
+++ avidemux/qt4/ADM_userInterfaces/ADM_shell/Q_shell.h
@@ -10,13 +10,19 @@
 /**
     \class ADM_jsQt4Shell
 */
-
+#define Q_SHELL_HISTORY 8 // Must be a power of 2!
 class qShell: public QDialog
 {
 	Q_OBJECT
 protected:
     jsShellEvaluate      *evaluator;
     Ui_SpiderMonkeyShell ui;
+    bool                 eventFilter(QObject* watched, QEvent* event);
+    bool                 previousCommand(void);
+    bool                 nextCommand(void);
+    QString              *history[Q_SHELL_HISTORY];
+    int                  indexWrite;
+    int                  indexRead;
 public:
                     qShell(jsShellEvaluate *s) ;
     virtual         ~qShell() ;
