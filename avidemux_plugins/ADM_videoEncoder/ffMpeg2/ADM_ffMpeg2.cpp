45664bff8589e7e27c825aae806cb7c85d0b127e
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-02-07 08:59:06 +0000

    [mpeg2] Remove unused parameters from mpeg2 dialog
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5910 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_plugins/ADM_videoEncoder/ffMpeg2/ADM_ffMpeg2.cpp
+++ avidemux_plugins/ADM_videoEncoder/ffMpeg2/ADM_ffMpeg2.cpp
@@ -231,10 +231,6 @@ diaMenuEntry meE[]={
   {6,QT_TR_NOOP("X1")}
 };       
 
-diaMenuEntry qzE[]={
-  {0,QT_TR_NOOP("H.263")},
-  {1,QT_TR_NOOP("MPEG")}
-};       
 
 diaMenuEntry rdE[]={
   {0,QT_TR_NOOP("MB comparison")},
@@ -255,21 +251,15 @@ uint32_t me=(uint32_t)conf->lavcSettings.me_method;
 #define PX(x) &(conf->lavcSettings.x)
 
          diaElemBitrate   bitrate(&(Mp2Settings.params),NULL);
-         diaElemMenu      meM(&me,QT_TR_NOOP("Matrices"),4,meE);
+
          diaElemMenu      threadM(PX(MultiThreaded),QT_TR_NOOP("Threading"),4,threads);
          diaElemUInteger  qminM(PX(qmin),QT_TR_NOOP("Mi_n. quantizer:"),1,31);
          diaElemUInteger  qmaxM(PX(qmax),QT_TR_NOOP("Ma_x. quantizer:"),1,31);
          diaElemUInteger  qdiffM(PX(max_qdiff),QT_TR_NOOP("Max. quantizer _difference:"),1,31);
          
-         diaElemToggle    fourMv(PX(_4MV),QT_TR_NOOP("4_MV"));
          diaElemToggle    trellis(PX(_TRELLIS_QUANT),QT_TR_NOOP("_Trellis quantization"));
          
-         diaElemToggle    qpel(PX(_QPEL),QT_TR_NOOP("_Quarter pixel"));
-         diaElemToggle    gmc(PX(_GMC),QT_TR_NOOP("_GMC"));
-
-         
          diaElemUInteger  max_b_frames(PX(max_b_frames),QT_TR_NOOP("_Number of B frames:"),0,32);
-         diaElemMenu     qzM(PX(mpeg_quant),QT_TR_NOOP("_Quantization type:"),2,qzE);
          
          diaElemMenu     rdM(PX(mb_eval),QT_TR_NOOP("_Macroblock decision:"),3,rdE);
          
@@ -278,32 +268,24 @@ uint32_t me=(uint32_t)conf->lavcSettings.me_method;
          diaElemFloat    qzComp(PX(qcompress),QT_TR_NOOP("_Quantizer compression:"),0,1);
          diaElemFloat    qzBlur(PX(qblur),QT_TR_NOOP("Quantizer _blur:"),0,1);
          
-        diaElemUInteger GopSize(PX(gop_size),QT_TR_NOOP("_Gop Size:"),1,500); 
+        diaElemUInteger GopSize(PX(gop_size),QT_TR_NOOP("_Gop Size:"),1,30); 
           /* First Tab : encoding mode */
-        diaElem *diamode[]={&GopSize,&threadM,&bitrate};
-        diaElemTabs tabMode(QT_TR_NOOP("User Interface"),3,diamode);
+        diaElem *diamode[]={&GopSize,&threadM,&max_b_frames,&bitrate};
+        diaElemTabs tabMode(QT_TR_NOOP("User Interface"),4,diamode);
         
         /* 2nd Tab : ME */
-        diaElemFrame frameMe(QT_TR_NOOP("Advanced Simple Profile"));
-        
-        frameMe.swallow(&max_b_frames);
-        frameMe.swallow(&qpel);
-        frameMe.swallow(&gmc);
-        
-        diaElem *diaME[]={&fourMv,&frameMe};
-        diaElemTabs tabME(QT_TR_NOOP("Motion Estimation"),2,diaME);
         /* 3nd Tab : Qz */
         
-         diaElem *diaQze[]={&qzM,&rdM,&qminM,&qmaxM,&qdiffM,&trellis};
-        diaElemTabs tabQz(QT_TR_NOOP("Quantization"),6,diaQze);
+        diaElem *diaQze[]={&rdM,&qminM,&qmaxM,&qdiffM,&trellis};
+        diaElemTabs tabQz(QT_TR_NOOP("Quantization"),5,diaQze);
         
         /* 4th Tab : RControl */
         
          diaElem *diaRC[]={&filetol,&qzComp,&qzBlur};
         diaElemTabs tabRC(QT_TR_NOOP("Rate Control"),3,diaRC);
         
-         diaElemTabs *tabs[]={&tabMode,&tabME,&tabQz,&tabRC};
-        if( diaFactoryRunTabs(QT_TR_NOOP("libavcodec MPEG-4 configuration"),4,tabs))
+         diaElemTabs *tabs[]={&tabMode,&tabQz,&tabRC};
+        if( diaFactoryRunTabs(QT_TR_NOOP("libavcodec MPEG-2 configuration"),3,tabs))
         {
           conf->lavcSettings.me_method=(Motion_Est_ID)me;
           return true;
