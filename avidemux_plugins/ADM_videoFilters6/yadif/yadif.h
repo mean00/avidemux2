0918ee4531e2c4f6f3b96d58027250a5e08d8fac
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-03-04 06:39:08 +0000

    [filter] Add yadif (broken/uncomplete)
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@5971 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux_plugins/ADM_videoFilters6/yadif/yadif.h
@@ -0,0 +1,9 @@
+// Automatically generated, do not edit!
+#ifndef ADM_yadif_CONF_H
+#define ADM_yadif_CONF_H
+typedef struct {
+   uint32_t mode;
+   uint32_t order;
+}yadif;
+#endif //yadif
+//EOF
---
+++ avidemux_plugins/ADM_videoFilters6/yadif/yadif_desc.cpp
@@ -0,0 +1,6 @@
+// Automatically generated, do not edit!
+const ADM_paramList yadif_param[]={
+ {"mode",offsetof( yadif,mode),"uint32_t",ADM_param_uint32_t},
+ {"order",offsetof( yadif,order),"uint32_t",ADM_param_uint32_t},
+{NULL,0,NULL}
+};
