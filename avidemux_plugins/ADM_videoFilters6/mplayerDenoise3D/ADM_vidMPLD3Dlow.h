6f4959cfd172fd6c2df955238e8045e2db0bf673
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-04-01 18:10:09 +0000

    [Mpl3Dlow] Add empty Mplayer3D non hq filter
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@6042 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux_plugins/ADM_videoFilters6/mplayerDenoise3D/ADM_vidMPLD3Dlow.h
@@ -0,0 +1,59 @@
+/***************************************************************************
+                          ADM_vidMPLD3D.h  -  description
+                             -------------------
+Mplayer Denoise3d port to avidemux2
+Original Authors
+Copyright (C) 2003
+Daniel Moreno <comac@comac.darktech.org>
+
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef __D3DLOW__
+#define __D3DLOW__
+
+#include "denoise3d.h"
+
+/**
+    \class ADMVideoMPD3Dlow
+*/
+class ADMVideoMPD3Dlow : public  ADM_coreVideoFilter
+{
+protected:
+                ADMImage        *original;
+                denoise3d       param;
+                uint32_t        last;
+                uint8_t         *Line;
+                int				Coefs[4][512];
+                void 	    PrecalcCoefs(int *Ct, double Dist25);
+				uint8_t  	setup(void);
+				void 	    deNoise(unsigned char *Frame,        // mpi->planes[x]
+                    						unsigned char *FramePrev,    // pmpi->planes[x]
+                    						unsigned char *FrameDest,    // dmpi->planes[x]
+                    						unsigned char *LineAnt,      // vf->priv->Line (width bytes)
+                    						int W, int H, int sStride, int pStride, int dStride,
+                    						int *Horizontal, int *Vertical, int *Temporal);
+
+                    
+public:
+                    ADMVideoMPD3Dlow(ADM_coreVideoFilter *previous,CONFcouple *conf);
+                    ~ADMVideoMPD3Dlow();
+
+        virtual const char   *getConfiguration(void);                 /// Return  current configuration as a human readable string
+        virtual bool         getNextFrame(uint32_t *fn,ADMImage *image);           /// Return the next image
+        virtual bool         getCoupledConf(CONFcouple **couples) ;   /// Return the current filter configuration
+        virtual bool         configure(void) ;                        /// Start graphical user interface
+};
+
+			
+
+#endif
