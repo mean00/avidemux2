1146fad92da135984e2b87c7a09b31c522b1a230
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-04-09 15:23:06 +0000

    [build] Deb is not rpm (gtk)
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@6107 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux/gtk/gtkPackageDebian.cmake
+++ avidemux/gtk/gtkPackageDebian.cmake
@@ -1,24 +1,32 @@
 ##############################
-# RPM
+# DEBIAN
 ##############################
 SET(CPACK_SET_DESTDIR "ON")
-SET(CPACK_RPM_PACKAGE_NAME "avidemux3-gtk")
-SET (CPACK_GENERATOR "RPM")
+SET(CPACK_DEBIAN_PACKAGE_NAME "avidemux3-gtk")
+SET (CPACK_GENERATOR "DEB")
 # ARCH
 IF (X86_64_SUPPORTED)
-SET(CPACK_RPM_PACKAGE_ARCHITECTURE "amd64")
+SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64")
 ELSE (X86_64_SUPPORTED)
-SET(CPACK_RPM_PACKAGE_ARCHITECTURE "i386")
+SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "i386")
 ENDIF (X86_64_SUPPORTED)
 # Mandatory
-SET(CPACK_RPM_PACKAGE_VENDOR "mean")
-SET(CPACK_RPM_PACKAGE_DESCRIPTION "Simple video editor,main program gtk version ")
+SET(CPACK_DEBIAN_PACKAGE_MAINTAINER "mean")
+SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Simple video editor,main program gtk version ")
 
+# Build our deps list
+# Build deps SET(DEPS "debhelper (>> 4), libfreetype6-dev, libxml2-dev,  libxv-dev, dpatch , cmake, desktop-file-utils")
+SET(DEPS "libc6 (>=2.4),libglib2.0-0 (>=2.14.0),libstdc++6 (>=4.2.1),libx11-6,  libxml2 (>=2.6.27), libxv1, zlib1g (>=1:1.1.4), avidemux3-core (>=2.6.0)")
+# Add optional DEPS here
+SET(CPACK_DEBIAN_PACKAGE_DEPENDS "${DEPS}")
+#
+SET(CPACK_DEBIAN_PACKAGE_SECTION "extra")
+SET(CPACK_DEBIAN_PACKAGE_PRIORITY "optional")
 # Some more infos
 SET(CPACK_PACKAGE_VERSION_MAJOR "2")
 SET(CPACK_PACKAGE_VERSION_MINOR "6")
 SET(CPACK_PACKAGE_VERSION_PATCH "0")
-SET(CPACK_PACKAGE_VERSION_PATCH "0_${ADM_SUBVERSION}")
+SET(CPACK_PACKAGE_VERSION_PATCH "0-${ADM_SUBVERSION}")
 #
 SET(CPACK_PACKAGE_NAME "avidemux3-gtk")
 #
