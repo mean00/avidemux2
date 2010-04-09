d71e60397bf6366fba7b68ec92410eda12ba5cc4
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-04-09 15:23:01 +0000

    [build] Prepare to be able to generate rpm,deb and tar.gz
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@6104 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux_core/corePackageTarGz.cmake
@@ -0,0 +1,16 @@
+##############################
+# Tar GZ
+##############################
+SET(CPACK_SET_DESTDIR "ON")
+SET(CPACK_GENERATOR "TGZ")
+# Some more infos
+SET(CPACK_PACKAGE_VERSION_MAJOR "2")
+SET(CPACK_PACKAGE_VERSION_MINOR "6")
+SET(CPACK_PACKAGE_VERSION_PATCH "0")
+
+SET(CPACK_PACKAGE_VERSION_PATCH "0_r${ADM_SUBVERSION}")
+#
+SET(CPACK_PACKAGE_NAME "avidemux3-core")
+#
+
+include(CPack)
---
+++ cmake/admPackager.cmake
@@ -0,0 +1,22 @@
+#
+# Set ADM_SUBVERSION either using subversion or git SVN to 
+#  get the revision
+#
+MACRO(admPackager _prog)
+        IF(NOT AVIDEMUX_PACKAGER)
+                SET(AVIDEMUX_PACKAGER "none" CACHE STRING "")
+        ELSE(NOT AVIDEMUX_PACKAGER)
+                SET(AVIDEMUX_PACKAGER "${AVIDEMUX_PACKAGER}" CACHE STRING "")
+                MESSAGE(STATUS "Packager=${AVIDEMUX_PACKAGER}, valid choices= {deb,rpm,tgz,none}")
+        ENDIF(NOT AVIDEMUX_PACKAGER)
+        
+        if("${AVIDEMUX_PACKAGER}" STREQUAL "rpm")
+                include("./${_prog}Rpm.cmake")
+        elseif ("${AVIDEMUX_PACKAGER}" STREQUAL "deb")
+                include("./${_prog}Debian.cmake")
+        elseif("${AVIDEMUX_PACKAGER}" STREQUAL "tgz")
+                include("./${_prog}TarGz.cmake")
+        else("${AVIDEMUX_PACKAGER}" STREQUAL "rpm")
+                MESSAGE(STATUS "No packaging... (package=${_package})")
+        endif("${AVIDEMUX_PACKAGER}" STREQUAL "rpm")
+ENDMACRO(admPackager _prog )
