f7c5f586a4620fa3f1b3bf9ea467ab9cb146fb29
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-04-25 14:30:40 +0000

    [JS/DF] Import from 2.5
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@6136 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux/common/ADM_script2/src_dialogFactory/ADM_JSDialogFactory_if.cpp
@@ -0,0 +1,44 @@
+/**
+    \file   ADM_JSDialogFactory_if.cpp
+    \brief  JS / DF binding
+    \author gruntster, mean (c) 2010
+
+
+    jsapigen does not like much variable number of arguments
+    In that case, we patch the generated file to go back to native spidermonkey api
+
+
+*/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include "ADM_default.h"
+#include "jsapi.h"
+#include "DIA_factory.h"
+#include "ADM_JSDFMenu.h"
+#include "ADM_JSDialogFactory.h"
+
+/**
+    \fn ADM_JSDialogFactoryInit()
+    \brief Hook classes to spidermonkey
+*/
+bool ADM_JSDialogFactoryInit(JSContext *cx, JSObject *obj)
+{
+    if(NULL==ADM_JSDialogFactory::JSInit(cx,obj))
+    {
+        ADM_error("Cannot register dialogFactory js class\n");
+        return false;
+    }
+    if(NULL==ADM_JSDFMenu::JSInit(cx,obj))
+    {
+        ADM_error("Cannot register dialogFactory js class\n");
+        return false;
+    }
+    ADM_info("Registered DialogFactory classes\n");
+    return true;
+}
