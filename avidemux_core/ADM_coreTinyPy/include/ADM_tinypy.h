3bfea636a6556e1b523c7d2a6e6d96767d1b84f9
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-05-20 15:22:36 +0000

    [tinypy] Base wrapper around tinypy
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@6202 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux_core/ADM_coreTinyPy/include/ADM_tinypy.h
@@ -0,0 +1,32 @@
+/***************************************************************************
+    \file  ADM_tinypy.cpp
+    \brief Wrapper around tinypy
+    \author mean fixounet@free.fr (c) 2010
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef ADM_TINYPY_H
+#define ADM_TINYPY_H
+/**
+    \class tinyPy
+*/
+class tinyPy 
+{
+protected:
+        void *instance;
+        void *script;
+public:
+                tinyPy(void);
+        bool    init(void);
+                ~tinyPy(void);
+        bool    execString(const char *s);
+};
+#endif
---
+++ avidemux_core/ADM_coreTinyPy/src/ADM_tinypy.cpp
@@ -0,0 +1,78 @@
+/***************************************************************************
+    \file  ADM_tinypy.cpp
+    \brief Wrapper around tinypy
+    \author mean fixounet@free.fr (c) 2010
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include "ADM_default.h"
+#include "ADM_tinypy.h"
+#include "tinypy.h"
+#define INSTANCE ((tp_vm *)instance)
+#define SCRIPT   ((tp_obj *)script)
+
+/**
+    \fn tinypy
+    \brief ctor
+*/
+tinyPy::tinyPy(void)
+{
+    instance=NULL;
+    script=NULL;
+}
+/**
+    \fn tinypy
+    \brief dtor
+
+*/
+tinyPy::~tinyPy()
+{
+    if(INSTANCE)
+    {
+        tp_deinit(INSTANCE);
+        instance=NULL;
+    }
+    if(script) // dealloc tp_ob
+    {
+
+    }
+}
+/**
+    \fn tinypy
+    \brief init
+*/
+bool tinyPy::init(void)
+{
+    ADM_assert(!instance);
+    instance=(void *)tp_init(0,NULL);
+    if(instance) return true;   
+    ADM_error("Cannot initialize tinypy\n");
+    return false;
+}
+/**
+    \fn tinypy
+    \brief init
+*/
+bool tinyPy::execString(const char *s)
+{
+    if(!instance)
+    {
+        ADM_warning("No instance\n");
+        return false;
+    }
+    tp_obj name = tp_string("avidemux6");
+    tp_obj program = tp_string(s);
+    tp_obj c = tp_compile(INSTANCE, program, name);
+    tp_exec(INSTANCE, c,INSTANCE->builtins);
+    return true;
+}
+
+// EOF
