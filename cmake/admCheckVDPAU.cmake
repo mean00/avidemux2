e965698f04dfae02eb520d5519d7370e22d4f6c9
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-05-26 17:49:37 +0000

    [vdpau] Check that the version has support for mpeg4 part2 so that libavcodec compiles
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@6257 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- cmake/admCheckVDPAU.cmake
+++ cmake/admCheckVDPAU.cmake
@@ -15,10 +15,17 @@ MACRO(checkVDPAU)
 			FIND_HEADER_AND_LIB(VDPAU vdpau/vdpau.h )
 		
 			IF (VDPAU_FOUND)
-				SET(VDPAU_FOUND 1)
-				SET(USE_VDPAU 1)
-				SET(VDPAU_INCLUDE_DIR "${VDPAU_INCLUDE_DIR}")
-				SET(VDPAU_LIBRARY_DIR "${VDPAU_LIBRARY_DIR}")
+                                # CHECK That it has 
+                                ADM_COMPILE(check_vdpau.cpp "" "${VDPAU_INCLUDE_DIR}" "${VDPAU_LIBRARY_DIR}" VDPAU_COMPILE VDPAU_OUTPUT)
+                                IF(VDPAU_COMPILE)
+				        SET(VDPAU_FOUND 1)
+				        SET(USE_VDPAU 1)
+				        SET(VDPAU_INCLUDE_DIR "${VDPAU_INCLUDE_DIR}")
+				        SET(VDPAU_LIBRARY_DIR "${VDPAU_LIBRARY_DIR}")
+                                ELSE(VDPAU_COMPILE)
+                                        MESSAGE(STATUS "The vdpau found does not compile, probably too old ${VDPAU_OUTPUT}.")
+                                ENDIF(VDPAU_COMPILE)
+                                
 			ENDIF (VDPAU_FOUND)
 
 			PRINT_LIBRARY_INFO("VDPAU" VDPAU_FOUND "${VDPAU_INCLUDE_DIR}" "${VDPAU_LIBRARY_DIR}")
---
+++ cmake_compile_check/check_vdpau.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "vdpau/vdpau.h"
+#include "vdpau/vdpau_x11.h"
+int main(int argc, char **argv)
+{
+        VdpPictureInfoMPEG4Part2 m4;
+        return 0;
+}
