c9cadda4792da5a32da77598dc1f250aee9bd5c8
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-06-05 10:22:07 +0000

    [py] Better saveAudio test
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@6314 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- autononreg/py/avidemux/pySaveAudio.py
+++ autononreg/py/avidemux/pySaveAudio.py
@@ -1,7 +1,9 @@
 adm=Avidemux()
+gui=Gui()
 if(0==adm.loadVideo("/work/samples/avi/2mn.avi")):
    throw("cannot load file")
+#adm.audioCodec("Lame",128)
 if(0==adm.saveAudio("/tmp/audio.mp3")):
    throw("cannot save audio")
-
+gui.displayInfo("Ok","Done")
 
