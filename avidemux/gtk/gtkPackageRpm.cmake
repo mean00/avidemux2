c56621e597cce4df5d01207b18a13b0826c5638a
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-08-23 18:22:09 +0000

    [packaging] Rpm fixup, patch by jan kardell
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@6545 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux/gtk/gtkPackageRpm.cmake
@@ -0,0 +1,26 @@
+##############################                                                                                                                                                     
+# RPM                                                                                                                                                                              
+##############################                                                                                                                                                     
+SET(CPACK_SET_DESTDIR "ON")
+SET(CPACK_RPM_PACKAGE_NAME "avidemux3-gtk")
+SET (CPACK_GENERATOR "RPM")
+# ARCH                                                                                                                                                                             
+IF (X86_64_SUPPORTED)
+SET(CPACK_RPM_PACKAGE_ARCHITECTURE "x86_64")
+ELSE (X86_64_SUPPORTED)
+SET(CPACK_RPM_PACKAGE_ARCHITECTURE "i386")
+ENDIF (X86_64_SUPPORTED)
+# Mandatory                                                                                                                                                                        
+SET(CPACK_RPM_PACKAGE_VENDOR "mean")
+SET(CPACK_RPM_PACKAGE_DESCRIPTION "Simple video editor,main program gtk version ")
+# Some more infos                                                                                                                                                                  
+SET(CPACK_PACKAGE_VERSION_MAJOR "2")
+SET(CPACK_PACKAGE_VERSION_MINOR "6")
+SET(CPACK_PACKAGE_VERSION_PATCH "0")
+SET(CPACK_PACKAGE_VERSION_PATCH "0_${ADM_SUBVERSION}")
+#                                                                                                                                                                                  
+SET(CPACK_PACKAGE_NAME "avidemux3-gtk")
+#                                                                                                                                                                                  
+SET(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}.${CPACK_RPM_PACKAGE_ARCHITECTURE}")
+include(CPack)
+
