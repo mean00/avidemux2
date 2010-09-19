3c6e2e61a690ec8360f0a225b78c1bd7c18948fe
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-09-19 07:35:28 +0000

    [js/py] Add setContainer func
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@6618 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ autononreg/py/avidemux/setContainer.py
@@ -0,0 +1,12 @@
+#PY  <- Needed to identify#
+#--automatically built--
+#--Project: /tmp/foobar.py
+
+adm=Avidemux()
+#** Video **
+# 01 videos source 
+adm.loadVideo("/work/samples/avi/2mn.avi")
+#** Muxer **
+adm.setContainer("AVI")
+
+#End of script
