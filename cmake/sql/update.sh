060cfc2c5a06669db6e2e03c52ad434e8de7d526
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-11-20 08:28:20 +0000

    [sql] Properly generate include/src
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@6767 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- cmake/sql/update.sh
+++ cmake/sql/update.sh
@@ -1,2 +1,3 @@
 sqlite3 ~/.avidemux6/jobs.sql .d > dump
-sql2class -sqlite -prefix -lib /usr dump
+#sql2class -sqlite -global -license -wrapped  -lib $PWD dump
+sql2class -sqlite -build -global -prefix $PWD -lib sqlJobs -namespace db -overwrite dump
