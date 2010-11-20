b1e100744a780de4b2cfee30dff4b796beb1b576
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-11-20 08:35:06 +0000

    [jobs] forgotten file
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@6770 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux/qt4/ADM_jobs/src/ADM_runOneJob.cpp
@@ -0,0 +1,121 @@
+/**
+    \file   ADM_jobControl
+    \author mean fixounet@free.Fr (c) 2010
+
+*/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include "T_jobs.h"
+#include "ADM_default.h"
+#include "ADM_coreJobs.h"
+
+
+/**
+    \fn spawnerBoomerang
+    \brief Allow to do class -> plain C -> class 
+*/
+static void *spawnerBoomerang(void *arg)
+{
+    spawnData    *data=(spawnData *)arg;
+    data->me->runProcess(data);
+    return NULL;
+}
+
+bool spawnProcess(const char *processName, int argc, const string argv[])
+{
+    ADM_info("Starting <%s>\n",processName);
+    string command=string(processName);
+    for(int i=0;i<argc;i++)
+    {
+        ADM_info("%d : %s\n",i,argv[i].c_str());
+        command+=string(" ")+argv[i];
+    }
+    ADM_info("=>%s\n",command.c_str());
+    ADM_info("==================== Start of spawner process job ================\n");
+    system(command.c_str());
+    ADM_info("==================== End of spawner process job ================\n");
+    return true;
+}
+
+/**
+    \fn runProcess
+    \brief Thread can will run a process
+*/
+bool jobWindow::runProcess(spawnData *data)
+{
+    // 3 args in our case...
+    string argv[3];
+    argv[0]=string("--runpy \"")+data->script+string("\" ");
+    argv[1]=string("--save \"")+data->outputFile+string("\" ");
+    argv[2]=string("--quit");
+    return spawnProcess(data->exeName,3,argv);
+}
+/**
+    \fn spawnChild
+    \brief Spawn a child to execute a commande
+*/
+bool jobWindow::spawnChild(const char *exeName, const string &script, const string &outputFile)
+{
+    spawnData data;
+            data.script=script;
+            data.outputFile=outputFile;
+            data.exeName=exeName;
+            data.me=this;
+            pthread_t threadId;
+        // Have to spawn a thread that will handle the exec...
+            if( pthread_create(&threadId,NULL,
+                                spawnerBoomerang, &data))
+            {
+                ADM_error("Spawn failed\n");
+                return false;
+            }
+            // Everything is fine, give process 1 second to use the stack allocated data....
+            ADM_usleep(1000*1000LL);
+            ADM_info("Spawning successfull\n");
+            return true;
+}
+ 
+/**
+    \fn runOneJob
+*/
+bool jobWindow::runOneJob( ADMJob &job)   
+{
+    bool r=false;
+    job.startTime=ADM_getSecondsSinceEpoch();
+    job.status=ADM_JOB_RUNNING;
+    ADM_jobUpdate(job);
+    
+    // 1- Start listening to socket
+
+    // 2- Spawn  child
+    string ScriptFullPath;
+    ScriptFullPath=string(ADM_getJobDir())+string("/")+string(job.scriptName);
+    if(false==spawnChild("avidemux3_qt4",ScriptFullPath,job.outputFileName))
+    {
+        ADM_error("Cannot spawn child\n");
+        r=false;
+        goto done;
+    }
+
+    // 3- Wait for connect...
+
+    // 4- wait for complete and/or success message
+    
+
+    // 5-Done, do the cleanup
+done:
+
+    if(r) job.status=ADM_JOB_OK;
+        else job.status=ADM_JOB_KO;
+    job.endTime=ADM_getSecondsSinceEpoch();
+    ADM_jobUpdate(job);
+    refreshList();
+    ADM_info("Running job id = %d\n",job.id);
+    return r;
+}
