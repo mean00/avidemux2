57d41b82179d04727f4824fdbccc4693bbee9eb6
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2010-12-10 08:06:34 +0000

    [avcodec] add back ADM_lavcodec
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@6827 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux_core/ADM_ffmpeg/libavcodec/ADM_lavcodec.cpp
@@ -0,0 +1,32 @@
+#include "ADM_coreConfig.h"
+#include "ADM_lavcodec.h"
+#include "ADM_default.h"
+/**
+ * 		\fn lavcodec_mm_support
+ * 		\brief Give lavcodec CPU supported ( FF_MM_MMX)
+ */
+extern "C"
+{
+int ADM_lavcodec_mm_support(void);
+}
+int ADM_lavcodec_mm_support(void)
+{
+int rval=0;
+
+#ifdef ADM_CPU_X86
+#undef MATCH
+#define MATCH(x,y) if(CpuCaps::myCpuCaps &  CpuCaps::myCpuMask & ADM_CPUCAP_##x) rval|=FF_MM_##x;
+
+	MATCH(MMX,MMX);
+	MATCH(MMXEXT,MMXEXT);
+	MATCH(SSE,SSE);
+	MATCH(SSE2,SSE2);
+	MATCH(SSE3,SSE3);
+	MATCH(SSSE3,SSSE3);
+	MATCH(3DNOW,3DNOW);
+	MATCH(3DNOWEXT,3DNOWEXT);
+#endif
+
+	return rval;
+}
+// EOF
