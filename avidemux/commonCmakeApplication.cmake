db831a3691bfee12dddeb474da35680d24dddcaa
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2011-01-23 19:54:25 +0000

    [avidemux] Do not apply previous patch on win32
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@6951 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux/commonCmakeApplication.cmake
+++ avidemux/commonCmakeApplication.cmake
@@ -17,9 +17,11 @@ include(admMainChecks)
 # Add include dirs
 ########################################
 include(admCoreIncludes)
-# Add ffmpeg to resolve ADM_libavcodec to the actual name
+# Add ffmpeg to resolve ADM_libavcodec to the actual name, needed for vdpau
+if (NOT WIN32)
 include(admFFmpegUtil)
 registerFFmpeg("${AVIDEMUX_INCLUDE_DIR}/avidemux/2.6" "${AVIDEMUX_LIB_DIR}" 1)
+endif (NOT WIN32)
 # Verify ADM_coreConfig is there
 if(NOT EXISTS "${AVIDEMUX_INCLUDE_DIR}/avidemux/2.6/ADM_coreConfig.h")
         MESSAGE(FATAL_ERROR "CMAKE_INSTALL_PREFIX does not contain include/avidemux/2.6/ADM_coreConfig.h (${AVIDEMUX_INCLUDE_DIR}/avidemux/2.6/ADM_coreConfig.h)")
