691ba7dbc451d8dc92df01b925baef471c468278
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2011-02-20 13:43:00 +0000

    [haiku] stdint definition workaround by diger
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@7010 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_core/ADM_core/include/ADM_default.h
+++ avidemux_core/ADM_core/include/ADM_default.h
@@ -22,10 +22,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#ifndef __HAIKU__
 #include <pthread.h>
-
+#endif
 #include "ADM_coreConfig.h"
 #include "ADM_inttype.h"
+#ifdef __HAIKU__
+#include <pthread.h>
+#endif
 #include "ADM_assert.h"
 #define ADM_NO_PTS 0xffffffffffffffffLL
 #ifdef __cplusplus
