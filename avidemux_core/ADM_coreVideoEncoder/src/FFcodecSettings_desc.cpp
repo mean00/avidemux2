ab016bde177efdc023ff510cc0a16a5fd5ec3008
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2011-03-26 18:50:45 +0000

    [core/build] Switch to admSerialization.py, supports nesting, begin updating conf files
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@7083 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_core/ADM_coreVideoEncoder/src/FFcodecSettings_desc.cpp
+++ avidemux_core/ADM_coreVideoEncoder/src/FFcodecSettings_desc.cpp
@@ -1,6 +1,5 @@
-// Automatically generated, do not edit!
 const ADM_paramList FFcodecSettings_param[]={
- {"params",offsetof( FFcodecSettings,params),"COMPRES_PARAMS",ADM_param_video_encode},
- {"lavcSettings",offsetof( FFcodecSettings,lavcSettings),"FFcodecContext",ADM_param_lavcodec_context},
+ {"params",offsetof(FFcodecSettings,params),"COMPRES_PARAMS",ADM_param_video_encode},
+ {"lavcSettings",offsetof(FFcodecSettings,lavcSettings),"FFcodecContext",ADM_param_lavcodec_context},
 {NULL,0,NULL}
 };
