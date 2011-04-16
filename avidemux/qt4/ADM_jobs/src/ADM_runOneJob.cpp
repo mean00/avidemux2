1d5498ef2bfd5245b7ba71859efd5a3a16a7826d
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2011-04-16 05:28:00 +0000

    [win32] Fix qt4 job build
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@7151 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux/qt4/ADM_jobs/src/ADM_runOneJob.cpp
+++ avidemux/qt4/ADM_jobs/src/ADM_runOneJob.cpp
@@ -13,6 +13,9 @@
  ***************************************************************************/
 #ifdef _WIN32
 #define UNICODE
+#include "ADM_cpp.h"
+
+#define UNICODE
 #include <winbase.h>
 #include <windows.h>
 
