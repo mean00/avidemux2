f1cea51579b47235b101f81554e4440340f1dc52
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2011-05-14 19:47:08 +0000

    [audio core] update aacinfo to read audiospecif decoder config
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@7201 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_core/ADM_coreAudioParser/include/ADM_aacinfo.h
+++ avidemux_core/ADM_coreAudioParser/include/ADM_aacinfo.h
@@ -5,21 +5,16 @@
 
 #ifndef ADM__AAC__INFO
 #define ADM__AAC__INFO
-
+/**
+    \fn AacAudioInfo
+*/
 typedef struct 
 {
-	
-	uint32_t layer;		// 0 mpeg4, 1 mpeg2 
-	uint32_t profile;	// 0 Main/1 LC
-	uint32_t samplerate;	// i.e. Frequency
-	uint32_t channels;	// # channels
-	uint32_t nbBlock;	// Packet size including header
-	uint32_t size;		// size of complete frame
-	uint32_t samples;	// # of sample in this packet
-	uint8_t  extra[2];
-
+	uint32_t frequency;		
+	uint32_t channels;	
+	bool     sbr;	
 }AacAudioInfo;
 
-
-bool getAdtsAacInfo(int size, uint8_t *data, AacAudioInfo *info,int *offset,bool createExtra);
+/// extract fq etc.. from ESDS atom
+bool getAdtsAacInfo(int size, uint8_t *data, AacAudioInfo &info);
 #endif
--- avidemux_core/ADM_coreAudioParser/src/ADM_aacinfo.cpp
+++ avidemux_core/ADM_coreAudioParser/src/ADM_aacinfo.cpp
@@ -22,10 +22,7 @@
 
 #include "ADM_default.h"
 #include <math.h>
-
-
-#include "ADM_default.h"
-//#include "aviaudio.hxx"
+#include "ADM_getbits.h"
 #include "ADM_aacinfo.h"
 
 static const int aacChannels[8]=
@@ -40,155 +37,95 @@ static const int aacChannels[8]=
 8 // 7: 8 channels: front-center, front-left, front-right, side-left, side-right, back-left, back-right, LFE-channel
 };
 
-static 	uint32_t aacBitrate[16]=
+static 	uint32_t aacSampleRate[16]=
 {
 	96000, 88200, 64000, 48000,
 	44100, 32000, 24000, 22050,
 	16000, 12000, 11025,  8000,
 	0,     0,     0,     0 
 };
-/*
-[0]
-	12 bits 111111	0xFC Sync tag
-	
-	1		layer 1 Mpeg2 0 Mpeg4
-	2		00
-	1		prot : 1 absent 0 present
-	
-[2]/16 bits	
-	2		profile  00 main/01 LC
-	4		sampling index
-	1		private
-	
-	1		channel
-[3]/24 bits
-	2               channel (cont'ed)
-	
-	1		original
-	1		Home
--------------------------------------	
-	Sum=28 bits
-	
-	
-	1		copyriht
-	1		copyright id 
-	2               *** AAC frame length (including headers)
-[4]
-        8		*** AAC frame length (including headers)
-[5]
-        3		*** AAC frame length (including headers)
-	5		Buffer fullness 0x7FF = vbr
-[6]
-	6		Buffer fullness 0x7FF = vbr
-	2		nb raw frame
-	
-	28 bits
-	
-	-- total = 56 bits = 8 bytes
-	
-	16		crc
-	
-*/
-#define x_log ADM_info
+
+#define xdebug(...) {}
 /**
     \fn getAdtsAacInfo
 */
 
-bool getAdtsAacInfo(int size, uint8_t *ptr, AacAudioInfo *info,int *offset,bool createExtraData)
+bool getAdtsAacInfo(int size, uint8_t *data, AacAudioInfo &info)
 {
-    uint8_t *org=ptr;
-    uint8_t *limit=ptr+size;
-    int objectType;
-    if(size<8) return false;
-    if(ptr[0]!=0xff || (ptr[1]>>4)!=0xf)
+    if(size<2)
     {
-            x_log("Wrong adts header %02x %02x\n",ptr[0],ptr[1]);
-            return false;
-    }
-    // [1] Mpeg2=0,Mpeg4=1
-    // [2] Layer =0
-    // [1] 1: no CRC, 0:CRC present
-    ptr++;
-    bool crc=!(ptr[0]&1);
-    if(crc)
-        x_log("CRC\n");
-    else
-        x_log("No CRC\n");
-    ptr++;
-    //----
-    // [2] Profile, objectType -1
-    // [4] Sample rate
-    // [1] 0
-    // [1] Channel mapping
-    //
-    //....
-    objectType=1+(ptr[0]>>6);
-    int samplerate=(ptr[0]>>2)&0xf;
-    int fq=aacBitrate[samplerate];
-    ADM_info("Frequency : %d\n",fq);
-    if(!fq)
-    {
-        ADM_warning("Invalid samplerate\n");
         return false;
     }
-    info->samplerate=fq;
-    int channelMapping=((ptr[0]&1)<<2)+(ptr[1]>>6);
-    if(channelMapping>=8)
-    {
-        ADM_warning("Bad channel\n");
-        return false;
-    }
-    ADM_info("Channels %d\n",aacChannels[channelMapping]);
-    info->channels=aacChannels[channelMapping];
-    ptr++;
-    //-------
-    
+    getBits bits(size,data); // get a copy, needed to extract extra data
    
-    // [2] Channel mapping continued
-    // [1] original =0
-    // [1] home =0
-    // [1] Copyright=0
-    // [1] Copyright start=0
-    // [2] Size (including header)
-    //
-    //....
-    
-    uint32_t frameSize=(ptr[0]>>6)&1;
-    frameSize=(frameSize<<8)+ptr[1];
-    frameSize=(frameSize<<3)+(ptr[2]>>5);
-    if(frameSize!=size)
+    int audioObjectType=bits.get(5); // warning
+    if(audioObjectType==31)
     {
-        x_log("Frame size mismatch : computed %d, got %d\n",frameSize,size);
+        ADM_error("Unsupported AAC audioObject Type\n");
         return false;
     }
-    ptr++;
-    // [8] Size (continued)
-    //
-    //....
-    
-    ptr++;
-    // [3] Size (continued)
-    // [5] ??
-    //
-    //....
-
-    ptr++;
-    // [6] ??
-    // [2] Frame count
-    //
-    //....
-    ptr++;
-    // Data
-    int crcOffset=0;
-    if(crc) crcOffset=2;
-    *offset=7+crcOffset;
-    info->samples=1024;
-    info->size=frameSize-7-crcOffset;
-    if(true==createExtraData)
+    int samplingFrequencyIndex=bits.get(4);
+    int extensionAudioObjectType=0;
+    int fq=0;
+    if(samplingFrequencyIndex==0xf)
     {
-        info->extra[0]=(objectType<<3)+(samplerate>>1);
-        info->extra[1]=(samplerate<<7)+(channelMapping<<3);
+            fq=(bits.get(8)<<16)+bits.get(16);
+    }else
+    {
+        fq=aacSampleRate[samplingFrequencyIndex];
     }
+    int channelConfiguration=bits.get(4);
+    int channels=aacChannels[channelConfiguration];
+    bool sbrPresent=false;
+    xdebug("ObjectType=%d\n",audioObjectType);    
+  
+    switch(audioObjectType)
+    {
+        case 2: // GASpecificConfig
+                {
+                bool frameLength=bits.get(1);
+                bool dependsOnCoreCoder=bits.get(1);
+                if(dependsOnCoreCoder) bits.skip(14);
+                bool extensionFlag=bits.get(1);
+                if(!channelConfiguration)
+                {
+                    ADM_error("No channel configuraiton\n");
+                    return false;
+                }
+                if(extensionFlag)
+                {
+                    ADM_error("Extension flag\n");
+                    return false;
+                }
+                }
+                break;
+        default:
+                ADM_error("AudoObjecttype =%d not handled\n",audioObjectType);
+                return false;
+    }
+    int consumed=bits.getConsumedBits();
+    if(size*8-consumed>=16) // extensionAudioObjectType
+    {
+        int syncExtensionType=bits.get(11);
+        if(syncExtensionType==0x2b7)
+        {
+            int extensionAdioObjectType=bits.get(5);
+             if(extensionAdioObjectType==31)
+                {
+                    ADM_error("Unsupported AAC audioObject Type\n");
+                    return false;
+                }
+             if(extensionAdioObjectType==5) 
+             {
+                    sbrPresent=bits.get(1);
+             }
+            
+        }
+    }
+    info.frequency=fq;
+    info.channels=channels;
+    info.sbr=sbrPresent;
+
     return true;
 }
 
