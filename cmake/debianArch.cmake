5559cfe67ddd50b47f711a07ca142b9a6fceb4e6
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2011-06-26 17:14:28 +0000

    [build] Put everything related to architecture in debianArch.cmake, add support for armel
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@7304 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ cmake/debianArch.cmake
@@ -0,0 +1,12 @@
+MACRO (XX4 supp arch)
+        IF(${supp})
+                SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE ${arch})
+        ENDIF(${supp})
+ENDMACRO (XX4 supp arch)
+
+MACRO(SET_DEBIAN_ARCH)
+        XX4(X86_64_SUPPORTED "amd64")
+        XX4(X86_32_SUPPORTED "i386")
+        XX4(ARMEL_SUPPORTED  "armel")
+ENDMACRO(SET_DEBIAN_ARCH)
+
