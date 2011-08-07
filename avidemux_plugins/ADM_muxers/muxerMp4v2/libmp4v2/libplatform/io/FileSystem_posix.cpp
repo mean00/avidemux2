5bd9518f658bfd61aeda5286b11a1876f0be6a31
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2011-08-07 09:09:50 +0000

    [mp4v2] Use our wrapper to deal with non ascii filename on win32
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@7394 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_plugins/ADM_muxers/muxerMp4v2/libmp4v2/libplatform/io/FileSystem_posix.cpp
+++ avidemux_plugins/ADM_muxers/muxerMp4v2/libmp4v2/libplatform/io/FileSystem_posix.cpp
@@ -1,5 +1,12 @@
+#include "ADM_cpp.h"
+
 #include "libplatform/impl.h"
 #include <sys/stat.h>
+
+#define ADM_LEGACY_PROGGY
+#include "ADM_default.h"
+
+// MEANX : Switch to our own wrappers
 
 namespace mp4v2 { namespace platform { namespace io {
 
@@ -8,8 +15,11 @@ namespace mp4v2 { namespace platform { namespace io {
 bool
 FileSystem::exists( string path_ )
 {
+    return ADM_fileExist(path_.c_str());
+/*
     struct stat buf;
     return stat( path_.c_str(), &buf ) == 0;
+*/
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -39,11 +49,15 @@ FileSystem::isFile( string path_ )
 bool
 FileSystem::getFileSize( string path_, File::Size& size_ )
 {
+/*
     size_ = 0;
     struct stat buf;
     if( stat( path_.c_str(), &buf ))
         return true;
     size_ = buf.st_size;
+    return false;
+*/
+    size_=ADM_fileSize(path_.c_str());
     return false;
 }
 
@@ -52,7 +66,17 @@ FileSystem::getFileSize( string path_, File::Size& size_ )
 bool
 FileSystem::rename( string from, string to )
 {
-    return ::rename( from.c_str(), to.c_str() ) != 0;
+    if(false==ADM_copyFile(from.c_str(),to.c_str()))
+    {
+        ADM_error("Cannot copy %s to %s\n",from.c_str(),to.c_str());
+        return true;
+    }
+    if(false==ADM_eraseFile(from.c_str()))
+    {
+        ADM_error("Cannot delete %s\n",from.c_str());
+        return true;
+    }
+    return false;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
--- avidemux_plugins/ADM_muxers/muxerMp4v2/libmp4v2/libplatform/io/File_posix.cpp
+++ avidemux_plugins/ADM_muxers/muxerMp4v2/libmp4v2/libplatform/io/File_posix.cpp
@@ -1,4 +1,9 @@
+#include "ADM_cpp.h"
 #include "libplatform/impl.h"
+#define ADM_LEGACY_PROGGY
+#include "ADM_default.h"
+
+// MEANX : Change fstream based io to FILE based io to be compatible with avidemux wrapper
 
 namespace mp4v2 { namespace platform { namespace io {
 
@@ -18,7 +23,7 @@ public:
 private:
     bool         _seekg;
     bool         _seekp;
-    std::fstream _fstream;
+    FILE         *_file;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -27,73 +32,85 @@ StandardFileProvider::StandardFileProvider()
     : _seekg ( false )
     , _seekp ( false )
 {
+    _file=NULL;
 }
 
 bool
 StandardFileProvider::open( std::string name, Mode mode )
 {
     ios::openmode om = ios::binary;
+    std::string fmode;
     switch( mode ) {
         case MODE_UNDEFINED:
         case MODE_READ:
         default:
-            om |= ios::in;
+            fmode+=std::string("r");
             _seekg = true;
             _seekp = false;
             break;
 
         case MODE_MODIFY:
-            om |= ios::in | ios::out;
+            fmode+=std::string("rw");
             _seekg = true;
             _seekp = true;
             break;
 
         case MODE_CREATE:
-            om |= ios::in | ios::out | ios::trunc;
+            fmode+=std::string("w");
             _seekg = true;
             _seekp = true;
             break;
     }
-
-    _fstream.open( name.c_str(), om );
-    return _fstream.fail();
+    fmode+=std::string("b");
+    _file=fopen(name.c_str(),fmode.c_str());
+    if(!_file) 
+    {
+        ADM_error("Cannot create file %s mode %s\n",name.c_str(),fmode.c_str());
+        return true;
+    }
+    ADM_info("Created file %s mode %s\n",name.c_str(),fmode.c_str());
+    return false;
 }
 
 bool
 StandardFileProvider::seek( Size pos )
 {
+int er=1;
     if( _seekg )
-        _fstream.seekg( pos, ios::beg );
+        er=fseeko(_file,pos,SEEK_SET);
     if( _seekp )
-        _fstream.seekp( pos, ios::beg );
-    return _fstream.fail();
+        er=fseeko(_file,pos,SEEK_SET);
+    if(er)
+    {
+        ADM_error("Seek to %d failed\n",(int)pos);
+        return true;
+    }
+    return false;
 }
 
 bool
 StandardFileProvider::read( void* buffer, Size size, Size& nin, Size maxChunkSize )
 {
-    _fstream.read( (char*)buffer, size );
-    if( _fstream.fail() )
-        return true;
-    nin = _fstream.gcount();
+    nin=fread(buffer,1,size,_file);
+    if(nin<=0) return true;
     return false;
 }
 
 bool
 StandardFileProvider::write( const void* buffer, Size size, Size& nout, Size maxChunkSize )
 {
-    _fstream.write( (const char*)buffer, size );
-    if( _fstream.fail() )
-        return true;
-    nout = size;
+    nout=fwrite(buffer,1,size,_file);
+    if(nout<=0) return true;
     return false;
 }
 
 bool
 StandardFileProvider::close()
 {
-    _fstream.close();
-    return _fstream.fail();
+    if(_file)
+        fclose(_file);
+    _file=NULL;
+    return true;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
