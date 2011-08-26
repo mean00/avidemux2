73e0945eeab6fc40753058a63dc0efb7dcd858b6
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2011-08-26 06:04:43 +0000

    [glYadif] Dummy shader, process one line out of two
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@7446 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_plugins/ADM_videoFilters6_openGl/glYadif/glYadif.h
+++ avidemux_plugins/ADM_videoFilters6_openGl/glYadif/glYadif.h
@@ -1,6 +1,7 @@
 //
 static const char *myShaderY =
 	"#extension GL_ARB_texture_rectangle: enable\n"
+    "#extension GL_EXT_gpu_shader4 : enable\n"
 	"uniform sampler2DRect myTexturePrev;\n" // tex unit 0
     "uniform sampler2DRect myTextureCur;\n" // tex unit 1
     "uniform sampler2DRect myTextureNext;\n" // tex unit 2
@@ -11,6 +12,16 @@ static const char *myShaderY =
 	"void main(void) {\n"
     "  float nx = gl_TexCoord[0].x;\n"
 	"  float ny = gl_TexCoord[0].y;\n"
-	"  gl_FragColor = texture2DRect(myTextureCur,vec2(nx,ny));\n"
+    "  int lineno=ny;\n"
+    "  int odd=lineno&1;\n"
+    "  if(odd)\n"
+    "  {  \n"
+    "           gl_FragColor = texture2DRect(myTextureCur,vec2(nx,ny));\n"
+    "  }  \n"
+    "  else \n"
+    " {\n"
+    "           gl_FragColor = vec4(0,0,0,0);\n"
+    " }\n"
+	
 	"}\n";
 
