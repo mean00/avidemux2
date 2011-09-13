0cc85d2082b68f732209628a3a28a0e018b58d6e
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2011-09-13 12:38:22 +0000

    [gl] Simplify shaders
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@7519 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_plugins/ADM_videoFilters6_openGl/sample_distort/sampleGl.h
+++ avidemux_plugins/ADM_videoFilters6_openGl/sample_distort/sampleGl.h
@@ -4,16 +4,17 @@ static const char *myShaderY =
 	"uniform sampler2DRect myTextureY;\n" // tex unit 0
     "uniform sampler2DRect myTextureU;\n" // tex unit 1
     "uniform sampler2DRect myTextureV;\n" // tex unit 2
+    "const vec2 half_vec=vec2(0.5,0.5);\n"
     "uniform float myWidth;\n"
     "uniform float myHeight;\n"
     "uniform float teta;\n"
 
 	"void main(void) {\n"
-    "  float nx = gl_TexCoord[0].x;\n"
-	"  float ny = gl_TexCoord[0].y;\n"
-    "  vec4 texvalV = texture2DRect(myTextureV, vec2(nx/2,ny/2));\n"
-    "  vec4 texvalU = texture2DRect(myTextureU, vec2(nx/2,ny/2));\n"
-	"  vec4 texvalY = texture2DRect(myTextureY, vec2(nx,ny));\n"
+    "  vec2 full_coord=gl_TexCoord[0];\n"
+    "  vec2 half_coord=full_coord*half_vec;"
+    "  vec4 texvalV = texture2DRect(myTextureV, half_coord);\n"
+    "  vec4 texvalU = texture2DRect(myTextureU, half_coord);\n"
+	"  vec4 texvalY = texture2DRect(myTextureY, full_coord);\n"
 	"  gl_FragColor = vec4(texvalY.r, texvalU.r, texvalV.r, 1.0);\n"
 	"}\n";
 
