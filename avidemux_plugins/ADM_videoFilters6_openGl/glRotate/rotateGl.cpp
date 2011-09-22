abd83a5ff1ced2ca3b0c54fc9d0085c7a8db75bd
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2011-09-22 06:08:17 +0000

    [glFilters] Skeleton for glRotate
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@7569 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux_plugins/ADM_videoFilters6_openGl/glRotate/rotateGl.cpp
@@ -0,0 +1,189 @@
+/** *************************************************************************
+                    \file     rotateGl
+                    \brief    Rotate picture
+
+   
+                    copyright            : (C) 2011 by mean
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include "ADM_openGL.h"
+#define ADM_LEGACY_PROGGY
+#include "ADM_default.h"
+#include "ADM_coreVideoFilterInternal.h"
+#include "T_openGL.h"
+#include "T_openGLFilter.h"
+#include "rotateGl.h"
+#include "ADM_clock.h"
+#include "rotate.h"
+#include "rotate_desc.cpp"
+/**
+    \class rotateGl
+*/
+class rotateGl : public  ADM_coreVideoFilterQtGl
+{
+protected:
+               gl_rotate     params;
+                ADMImage    *original;
+protected:
+                        bool render(ADMImage *image,ADM_PLANE plane,QGLFramebufferObject *fbo);
+public:
+                             rotateGl(ADM_coreVideoFilter *previous,CONFcouple *conf);
+                            ~rotateGl();
+
+        virtual const char   *getConfiguration(void);                   /// Return  current configuration as a human readable string
+        virtual bool         getNextFrame(uint32_t *fn,ADMImage *image);    /// Return the next image
+        virtual bool         getCoupledConf(CONFcouple **couples) ;   /// Return the current filter configuration
+        virtual bool         configure(void) {return true;}             /// Start graphical user interface
+};
+
+// Add the hook to make it valid plugin
+DECLARE_VIDEO_FILTER(   rotateGl,   // Class
+                        1,0,0,              // Version
+                        ADM_UI_QT4+ADM_UI_GL,         // UI
+                        VF_OPENGL,            // Category
+                        "glRotate",            // internal name (must be uniq!)
+                        "OpenGl Rotate",            // Display name
+                        "Rotate image." // Description
+                    );
+
+// Now implements the interesting parts
+/**
+    \fn rotateGl
+    \brief constructor
+*/
+rotateGl::rotateGl(  ADM_coreVideoFilter *in,CONFcouple *setup) : ADM_coreVideoFilterQtGl(in,setup)
+{
+UNUSED_ARG(setup);
+        original=new ADMImageDefault(in->getInfo()->width,in->getInfo()->height);
+        if(!setup || !ADM_paramLoad(setup,gl_rotate_param,&params))
+        {
+            // Default value
+            params.angle=0;
+        }
+        widget->makeCurrent();
+        fboY->bind();
+        printf("Compiling shader \n");
+        glProgramY = new QGLShaderProgram(context);
+        ADM_assert(glProgramY);
+        if ( !glProgramY->addShaderFromSourceCode(QGLShader::Fragment, myShaderY))
+        {
+                ADM_error("[GL Render] Fragment log: %s\n", glProgramY->log().toUtf8().constData());
+                ADM_assert(0);
+        }
+        if ( !glProgramY->link())
+        {
+            ADM_error("[GL Render] Link log: %s\n", glProgramY->log().toUtf8().constData());
+            ADM_assert(0);
+        }
+
+        if ( !glProgramY->bind())
+        {
+                ADM_error("[GL Render] Binding FAILED\n");
+                ADM_assert(0);
+        }
+
+        fboY->release();
+        widget->doneCurrent();
+
+}
+/**
+    \fn rotateGl
+    \brief destructor
+*/
+rotateGl::~rotateGl()
+{
+    if(original) delete original;
+    original=NULL;
+}
+
+/**
+    \fn getFrame
+    \brief Get a processed frame
+*/
+bool rotateGl::getNextFrame(uint32_t *fn,ADMImage *image)
+{
+    // since we do nothing, just get the output of previous filter
+    if(false==previousFilter->getNextFrame(fn,original))
+    {
+        ADM_warning("FlipFilter : Cannot get frame\n");
+        return false;
+    }
+    widget->makeCurrent();
+    glPushMatrix();
+    // size is the last one...
+    fboY->bind();
+
+    glProgramY->setUniformValue("myTextureU", 1); 
+    glProgramY->setUniformValue("myTextureV", 2); 
+    glProgramY->setUniformValue("myTextureY", 0); 
+
+    uploadAllPlanes(original);
+
+    render(image,PLANAR_Y,fboY);
+
+    downloadTextures(image,fboY);
+
+    fboY->release();
+    firstRun=false;
+    glPopMatrix();
+    widget->doneCurrent();
+    
+    return true;
+}
+/**
+    \fn getCoupledConf
+    \brief Return our current configuration as couple name=value
+*/
+bool         rotateGl::getCoupledConf(CONFcouple **couples)
+{
+    return ADM_paramSave(couples, gl_rotate_param,&params);
+}
+/**
+    \fn getConfiguration
+    \brief Return current setting as a string
+*/
+const char *rotateGl::getConfiguration(void)
+{
+    static char st[200];
+    snprintf(st,199,"openGl rotate :%d degrees",params.angle);
+    return st;
+}
+
+
+/**
+    \fn render
+*/
+bool rotateGl::render(ADMImage *image,ADM_PLANE plane,QGLFramebufferObject *fbo)
+{
+    int width=image->GetWidth(plane);
+    int height=image->GetHeight(plane);
+
+    glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+	glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, width, 0, height, -1, 1);
+
+    //
+    glBegin(GL_QUADS);
+	glTexCoord2i(0, 0);
+	glVertex2i(0, 0);
+	glTexCoord2i(width, 0);
+	glVertex2i(width, 0);
+	glTexCoord2i(width, height);
+	glVertex2i(width ,height);
+	glTexCoord2i(0, height);
+	glVertex2i(0, height);
+	glEnd();	// draw cube background
+    return true;
+}
+//EOF
