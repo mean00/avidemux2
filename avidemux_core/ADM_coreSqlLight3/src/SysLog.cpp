21c4a4dd873eaeadcf179dd4f4d8dbc952098065
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2011-11-01 13:30:43 +0000

    [core/sql] Rename syslog to syslog2 to avoid issue
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@7639 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_core/ADM_coreSqlLight3/src/SysLog.cpp
+++ avidemux_core/ADM_coreSqlLight3/src/SysLog.cpp
@@ -38,7 +38,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "Database.h"
 #include "Query.h"
 #include "IError.h"
-#include "SysLog.h"
+#include "SysLog2.h"
 
 
 #ifdef SQLITEW_NAMESPACE
