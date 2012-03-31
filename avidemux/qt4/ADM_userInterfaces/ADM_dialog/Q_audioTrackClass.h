368a52f139171c05b1f96ac20e79e2c895a88858
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2012-03-31 05:59:16 +0000

    [oops] forgotten file
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@7833 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux/qt4/ADM_userInterfaces/ADM_dialog/Q_audioTrackClass.h
@@ -0,0 +1,36 @@
+
+#include "ADM_inttype.h"
+#include "Q_audioTracks.h"
+
+#include "DIA_coreToolkit.h"
+#include "ADM_vidMisc.h"
+#include "ADM_toolkitQt.h"
+#include "ADM_edit.hxx"
+#include "DIA_audioTracks.h"
+#include "ADM_edAudioTrackFromVideo.h"
+extern void UI_purge(void);
+uint32_t audioEncoderGetNumberOfEncoders(void);
+const char  *audioEncoderGetDisplayName(int i);
+/**
+    \fn audioTrackQt4
+*/
+class audioTrackQt4: public QObject,public DIA_audioTrackBase
+{
+            Q_OBJECT
+protected:
+            audioTrackWindow *window;
+            ActiveAudioTracks active;
+            void            setupMenu(int dex);
+            void            enable(int i);
+            void            disable(int i);
+public:
+       
+                            audioTrackQt4( PoolOfAudioTracks *pool, ActiveAudioTracks *xactive );
+            virtual		~audioTrackQt4();
+                          
+                       bool  updateActive(void);
+            virtual   bool  run(void);
+public slots:
+                       bool  filtersClicked(bool a);
+                       bool  codecConfClicked(bool a);
+};
