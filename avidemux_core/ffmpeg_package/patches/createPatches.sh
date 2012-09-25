2e5eccdc7540f56cbd26c061289f40ec058e2a16
gruntster <gruntster@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2012-09-25 06:13:44 +0000

    [ffmpeg] upgrade ffmpeg to 0.11.2
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@8195 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_core/ffmpeg_package/patches/createPatches.sh
+++ avidemux_core/ffmpeg_package/patches/createPatches.sh
@@ -27,9 +27,7 @@ function updatePatch {
 	dos2unix ${1//\//_}_$2.patch
 }
 
-updatePatch . configure
 updatePatch libavcodec avcodec.h
-updatePatch libavcodec dxva2_internal.h
 updatePatch libavcodec golomb.h
 updatePatch libavcodec h263dec.c
 updatePatch libavcodec h264_parser.c
--- cmake/admFFmpegBuild.cmake
+++ cmake/admFFmpegBuild.cmake
@@ -9,7 +9,7 @@ ENDMACRO (xadd)
 
 option(FF_INHERIT_BUILD_ENV "" ON)
 
-set(FFMPEG_VERSION "0.11.1")
+set(FFMPEG_VERSION "0.11.2")
 set(FFMPEG_ROOT_DIR "${AVIDEMUX_TOP_SOURCE_DIR}/avidemux_core/ffmpeg_package")
 set(FFMPEG_PATCH_DIR  "${FFMPEG_ROOT_DIR}/patches/")
 set(FFMPEG_SOURCE_ARCHIVE "ffmpeg-${FFMPEG_VERSION}.tar.bz2")
