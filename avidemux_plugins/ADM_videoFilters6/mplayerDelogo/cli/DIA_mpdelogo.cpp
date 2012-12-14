b639eee4c9cf670c98a2f35e686b556304b6bce3
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2012-12-14 18:19:19 +0000

    [filter/mpdelogo] Add back cli
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@8310 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux_plugins/ADM_videoFilters6/mplayerDelogo/cli/DIA_mpdelogo.cpp
@@ -0,0 +1,33 @@
+
+#include "DIA_flyDialog.h"
+#include "ADM_image.h"
+#include "DIA_flyMpDelogo.h"
+
+//************************
+/**
+    \fn upload
+*/
+uint8_t flyMpDelogo::upload(void)
+{
+
+        return 1;
+}
+/**
+        \fn download
+*/
+uint8_t flyMpDelogo::download(void)
+{
+
+       
+        return true;
+}
+
+/**
+      \fn     DIA_getMpDelogo
+      \brief  Handle delogo dialog
+*/
+bool DIA_getMpDelogo(delogo *param, ADM_coreVideoFilter *in)
+{
+     
+        return true;
+}
