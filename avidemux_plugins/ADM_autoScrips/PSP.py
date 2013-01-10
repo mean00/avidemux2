63252d3f2249beead93be35cf2e52354e26b8f91
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2013-01-10 06:42:49 +0000

    [auto] Add PSP autoscript
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@8378 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux_plugins/ADM_autoScrips/PSP.py
@@ -0,0 +1,82 @@
+###############################################
+#      Convert to SVCD adm tinypy script
+#      Mean 2011
+###############################################
+
+import ADM_imageInfo
+import ADM_image
+finalSizeWidth=480               # Start with DVD target, we'll adjust later
+finalSizeHeight=[272]
+#
+#
+adm=Avidemux()
+##########################
+# Compute resize...
+##########################
+source=ADM_image.image()
+dest=ADM_image.image()
+desc=""
+true_fmt=ADM_imageInfo.get_video_format(desc)
+if(true_fmt is None):
+    raise
+fmt=true_fmt
+if(true_fmt==ADM_image.FMT_FILM):
+    fmt=ADM_image.FMT_NTSC
+source.width=adm.getWidth()
+source.height=adm.getHeight()
+source.fmt=fmt
+dest.fmt=fmt
+print("Format : "+str(fmt))
+dest.width=16
+dest.height=16
+#*****************************
+# Dialog...
+#*****************************
+#
+mnuResolution = DFMenu("Resolution:");
+mnuSourceRatio = DFMenu("Source Aspect Ratio:");
+mnuSourceRatio.addItem("1:1")
+mnuSourceRatio.addItem("4:3")
+mnuSourceRatio.addItem("16:9")
+
+dlgWizard = DialogFactory("auto PSP ("+desc+")");
+dlgWizard.addControl(mnuSourceRatio);
+res=dlgWizard.show()
+if res!=1:
+    exit()
+source.ar=mnuSourceRatio.index
+dest.ar=1
+resizer=source.compute_resize(source,dest,finalSizeWidth,finalSizeHeight,ADM_imageInfo.aspectRatio)
+if(resizer is None):
+    exit()
+print("Resize to "+str(resizer.width)+"x"+str(resizer.height))
+source.apply_resize(resizer)
+############################
+# Handle audio....
+############################
+adm.audioClearTracks()
+adm.audioAddTrack(0)
+adm.audioSetResample(0,48000)
+adm.audioSetMixer(0,"STEREO")
+adm.audioCodec(0,"Faac","bitrate=160")
+##################################
+#  Video
+##################################
+adm.videoCodec("x264", "general.params=CBR=1000", "general.threads=99", "general.fast_first_pass=True", "level=30", "vui.sar_height=1", "vui.sar_width=1", 
+"MaxRefFrames=2", "MinIdr=0", "MaxIdr=250", "i_scenecut_threshold=40", "MaxBFrame=2", "i_bframe_adaptive=0", 
+"i_bframe_bias=0", "i_bframe_pyramid=0", "b_deblocking_filter=False", "i_deblocking_filter_alphac0=0", "i_deblocking_filter_beta=0", "cabac=True", 
+"interlaced=False", "analyze.b_8x8=False", "analyze.b_i4x4=True" , "analyze.b_i8x8=False", "analyze.b_p8x8=True", "analyze.b_p16x16=False", 
+"analyze.b_b16x16=False", "analyze.weighted_pred=2", "analyze.weighted_bipred=True", "analyze.direct_mv_pred=3", 
+"analyze.chroma_offset=0", "analyze.me_method=0", "analyze.mv_range=16", "analyze.subpel_refine=7", "analyze.chroma_me=True", "analyze.mixed_references=True", 
+"analyze.trellis=1", "analyze.psy_rd=0.000000", "analyze.psy_trellis=0.000000", "analyze.fast_pskip=True", "analyze.dct_decimate=True", 
+"analyze.noise_reduction=0", "analyze.psy=True" , "analyze.intra_luma=21", "analyze.inter_luma=21", "ratecontrol.rc_method=0", 
+"ratecontrol.qp_constant=0", "ratecontrol.qp_min=0", "ratecontrol.qp_max=0", "ratecontrol.qp_step=0", "ratecontrol.bitrate=0", "ratecontrol.vbv_max_bitrate=0", 
+"ratecontrol.vbv_buffer_size=0", "ratecontrol.vbv_buffer_init=0", "ratecontrol.ip_factor=0.000000", "ratecontrol.pb_factor=0.000000", "ratecontrol.aq_mode=1", 
+"ratecontrol.aq_strength=1.000000", "ratecontrol.mb_tree=True", "ratecontrol.lookahead=40")
+###################################
+# Container = Mpeg PS/DVD
+###################################
+#
+#
+adm.setContainer("MP4", "muxerType=1", "useAlternateMp3Tag=True")
+    
