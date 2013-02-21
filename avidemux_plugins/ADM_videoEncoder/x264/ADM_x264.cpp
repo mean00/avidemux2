8128bdf7cfa434f09da5d1d4d1cf22dcb141efaf
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2013-02-21 06:24:52 +0000

    [x264] display real file to use for log
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@8481 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_plugins/ADM_videoEncoder/x264/ADM_x264.cpp
+++ avidemux_plugins/ADM_videoEncoder/x264/ADM_x264.cpp
@@ -150,6 +150,7 @@ bool         x264Encoder::setPassAndLogFile(int pass,const char *name)
 #else
     logFile=ADM_strdup(name);
 #endif
+    ADM_info("Creating system file =<%s>\n",logFile.c_str());
     passNumber=pass;
     return true;
 
