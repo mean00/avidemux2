0417f3e8ef2d14388fe1e24a69f821fa31934b5a
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2013-03-26 06:16:19 +0000

    [lav/aac enc] 2 channels max
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@8514 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_plugins/ADM_audioEncoders/lavcodec/ADM_lav_aac.h
+++ avidemux_plugins/ADM_audioEncoders/lavcodec/ADM_lav_aac.h
@@ -5,7 +5,7 @@
 #define ADM_LAV_NAME        "LavAAC" 
 #define ADM_LAV_MENU        "AAC (lav)" 
 #define ADM_LAV_DESC        "AAC LavCodec encoder plugin Mean 2008/2009"
-#define ADM_LAV_MAX_CHANNEL 6
+#define ADM_LAV_MAX_CHANNEL 2
 #define ADM_LAV_SAMPLE_PER_P 1024
 
 #define ADM_LAV_GLOBAL_HEADER 1
