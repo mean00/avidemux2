91d605380803c9733bfdf5098fe89f820e192039
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2013-04-03 05:33:15 +0000

    [audioEncoder/lav] Add dts, inactive for now
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@8534 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux_plugins/ADM_audioEncoders/lavcodec/ADM_lav_dts.h
@@ -0,0 +1,10 @@
+#define Join(x,y) x##_##y
+#define AUDMEncoder_Lavcodec AUDMEncoder_Lavcodec_DTS
+#define makeName(x) Join(x,DTS)
+#define ADM_LAV_VERSION     1,0,0
+#define ADM_LAV_NAME        "LavDTS" 
+#define ADM_LAV_MENU        "DTS (lav)" 
+#define ADM_LAV_DESC        "DTS LavCodec encoder plugin Mean 2008/2009"
+#define ADM_LAV_MAX_CHANNEL 6
+#define ADM_LAV_SAMPLE_PER_P 1536
+
