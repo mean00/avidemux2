7757a389b3fa853128693445d8c9a6b604907e74
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2013-04-11 19:46:15 +0000

    [nsis] Fix iconf fribidi etc..
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@8558 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux/winInstaller/createNsis.sh
+++ avidemux/winInstaller/createNsis.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "#* Preparing 7z svn build *#*"
 echo "** Getting svn revision **"
-rm -Rf install
-mkdir -p install
 export REV=`git log | grep branch_mean | head -1 | sed 's/^.*branch_mean.//g' | sed 's/ .*$//g'`
+cp /mingw/bin/libfribidi.dll /mingw/Release/libfribidi-0.dll
+cp /mingw/bin/libfreetype.dll /mingw/Release/libfreetype-6.dll
+cp /mingw/bin/iconv.dll /mingw/Release/
 echo "Revision : $REV"
 echo "Creating NSIS Package"
 makensis -DSVN_VERSION="${REV}" -DNSIDIR=$PWD avidemux_cross.nsi
