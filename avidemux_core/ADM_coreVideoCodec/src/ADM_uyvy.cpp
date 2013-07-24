0181834057f3674cae743ac98d27918ad88eca2b
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2013-07-24 06:01:48 +0000

    [codec] Fix YUYU (untested)
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@8823 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_core/ADM_coreVideoCodec/src/ADM_uyvy.cpp
+++ avidemux_core/ADM_coreVideoCodec/src/ADM_uyvy.cpp
@@ -16,6 +16,33 @@
 #include "ADM_default.h"
 #include "fourcc.h"
 #include "ADM_uyvy.h"
+
+
+static void doLineYOnlybis(int w,uint8_t *src, uint8_t *y, uint8_t *u, uint8_t *v)
+{
+     for (int x = 0; x < w/2; x++)
+      {
+	    y[0] = src[1];
+            y[1] = src[3];
+            y+=2;
+            src+=4;
+        }    
+}
+static void doLineYUVbis(int w,uint8_t *src, uint8_t *y, uint8_t *u, uint8_t *v,int stride)
+{
+     for (int x = 0; x < w/2; x++)
+      {
+	    y[0] = src[1];
+            y[1] = src[3];
+            v[0] = (src[0]+src[0+stride])>>1;
+            u[0] = (src[2]+src[2+stride])>>1;
+            y+=2;
+            u++;
+            v++;
+            src+=4;
+        }    
+}
+
 /**
     \fn uncompress
 */
@@ -33,22 +60,18 @@ bool   decoderUYVY::uncompress (ADMCompressedImage * in, ADMImage * out)
   ptrY = YPLANE(out);
   ptrU = VPLANE(out);
   ptrV = UPLANE(out);
-
-  for (uint32_t y = 0; y < _h; y++)
+  int sy=out->GetPitch(PLANAR_Y);
+  int su=out->GetPitch(PLANAR_U);
+  int sv=out->GetPitch(PLANAR_V);
+  for (uint32_t y = 0; y < _h/2; y++)
   {
-    for (uint32_t x = 0; x < (_w >> 1); x++)
-      {
-        if (!(y & 1))
-          {
-            *ptrU++ = ((unsigned int)(ptr[0])+(unsigned int)(ptr2[0]))>>1;
-            *ptrV++ = ((unsigned int)(ptr[2])+(unsigned int)(ptr2[2]))>>1;
-         }
-            *ptrY++ = ptr[1];
-            *ptrY++ = ptr[3];
-            ptr+=4;
-            ptr2+=4;
-      }
-    }
+    doLineYUVbis(_w,ptr,ptrY,ptrU,ptrV,_w*2)  ;
+    doLineYOnlybis(_w,ptr+_w*2,ptrY+sy,NULL,NULL)  ;
+    ptr+=4*_w;
+    ptrY+=2*sy;
+    ptrU+=su;
+    ptrV+=sv;
+  }
     out->Pts=in->demuxerPts;
     out->flags = AVI_KEY_FRAME;
     return 1;
