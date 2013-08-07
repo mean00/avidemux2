0814ff82146537b0871f5123b7895ac71e188a9c
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2013-08-07 06:28:00 +0000

    [x264/qt4] Dont use volatile string, make a copy before using it
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@8856 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_plugins/ADM_videoEncoder/x264/qt4/Q_x264.cpp
+++ avidemux_plugins/ADM_videoEncoder/x264/qt4/Q_x264.cpp
@@ -605,7 +605,7 @@ void x264Dialog::configurationComboBox_currentIndexChanged(int index)
     ADM_pluginGetPath("x264",pluginVersion,rootPath);
     QString text=QString(ADM_SEPARATOR)+ui.configurationComboBox->itemText(n);
     text=QString(rootPath.c_str())+text+QString(".json");
-    const char *t=text.toUtf8().constData();
+    const char *t=ADM_strdup(text.toUtf8().constData());
     ADM_info("Loading preset %s\n",t);
     if(false==x264_encoder_jdeserialize(t,x264_encoder_param,&myCopy))
     {
@@ -615,6 +615,7 @@ void x264Dialog::configurationComboBox_currentIndexChanged(int index)
     {
         upload();
     }
+    ADM_dezalloc(t);
 }
 /**
     \fn getProfileName  
