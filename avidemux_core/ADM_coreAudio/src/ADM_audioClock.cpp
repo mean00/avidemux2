4b01cc24ac9aa32e4f3a3e494ed1e5f2dbe974a1
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2013-11-07 06:20:02 +0000

    [AudioCore] Move audioClock to audioCore
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@8955 30592fff-cfd4-0310-bf0e-8235a3c7be26

---
+++ avidemux_core/ADM_coreAudio/src/ADM_audioClock.cpp
@@ -0,0 +1,62 @@
+/***************************************************************************
+                          ADM_audioClock.cpp  -  description
+                             -------------------
+    copyright            : (C) 2008 by mean
+    email                : fixounet@free.fr
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include "ADM_default.h"
+#include "ADM_audioClock.h"
+/**
+ *
+ * */
+audioClock::audioClock(uint32_t fq)
+{
+        _nbSamples=0;
+        _frequency=fq;
+        _baseClock=0;
+
+}
+/**
+ *
+ * */
+bool           audioClock::advanceBySample(uint32_t samples)
+{
+        _nbSamples+=samples;
+        return true;
+}
+/**
+ *
+ * */
+uint64_t       audioClock::getTimeUs(void)
+{
+        float f=_nbSamples;
+                f=f*1000*1000;
+                f/=_frequency;
+                return _baseClock+(uint64_t)(f+0.5);
+}
+/**
+ *
+ * */
+bool           audioClock::setTimeUs(uint64_t clk)
+{
+                uint64_t curTime=getTimeUs();
+                int64_t delta=(int64_t)clk-(int64_t)curTime;
+                if(abs(delta)<2000) return true;
+                printf("[audioClock] Drift detected :%"PRIu64" vs %"PRIu64", delta=%"PRId64"\n",curTime,clk,delta);
+                _nbSamples=0;
+                _baseClock=clk;
+                return true; 
+
+
+}
+
+// EOF
