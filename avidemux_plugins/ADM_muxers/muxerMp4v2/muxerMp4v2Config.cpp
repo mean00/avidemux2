a1d9836468ab91e0b91f5569e59fd173185089d0
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2014-02-23 14:35:28 +0000

    [mp4] re-enable optimize
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@9026 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_plugins/ADM_muxers/muxerMp4v2/muxerMp4v2Config.cpp
+++ avidemux_plugins/ADM_muxers/muxerMp4v2/muxerMp4v2Config.cpp
@@ -27,16 +27,16 @@ bool mp4v2Configure(void)
 {
         bool optimize=muxerConfig.optimize;
         bool addItuneMetaData=muxerConfig.add_itunes_metadata;
-
-        //diaElemToggle   wOptimize(&optimize,"Optimize for streaming");
+        
+        diaElemToggle   wOptimize(&optimize,"Optimize for streaming");
         diaElemToggle   wItunes(&addItuneMetaData,"Add ipod metadata");
 
         //diaElem *tabs[]={&wOptimize,&wItunes};
         diaElem *tabs[]={&wItunes};
         if( diaFactoryRun(("MP4V2 Settings"),1,tabs))
         {
-            //muxerConfig.optimize=optimize;
-            muxerConfig.optimize=false;
+            muxerConfig.optimize=optimize;
+            //muxerConfig.optimize=false;
             muxerConfig.add_itunes_metadata=addItuneMetaData;
             return true;
         }
