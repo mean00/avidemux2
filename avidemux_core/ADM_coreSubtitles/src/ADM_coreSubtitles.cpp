8e09f880285442614aa8042dc81425d7c3a3e2e9
mean <mean@30592fff-cfd4-0310-bf0e-8235a3c7be26>
2014-02-23 14:35:51 +0000

    [subcore] some preliminary code
    git-svn-id: svn://svn.berlios.de/avidemux/branches/avidemux_2.6_branch_mean@9028 30592fff-cfd4-0310-bf0e-8235a3c7be26

--- avidemux_core/ADM_coreSubtitles/src/ADM_coreSubtitles.cpp
+++ avidemux_core/ADM_coreSubtitles/src/ADM_coreSubtitles.cpp
@@ -1,8 +1,37 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include "ADM_default.h"
 #include "ADM_coreSubtitles.h"
-
-void foobar(void )
+/**
+ * 
+ * @param subtitleFile
+ * @return 
+ */
+ADM_subtitle:: ADM_subtitle()
 {
-    printf("Hi!\n");
+    _type=SUB_TYPE_NONE;
+}
+/**
+ * 
+ * @param subtitleFile
+ * @return 
+ */
+ADM_subtitle::~ADM_subtitle()
+{
+    
+}
+/**
+ * 
+ * @param subtitleFile
+ * @return 
+ */        
+bool      ADM_subtitle::load(const char *subtitleFile)
+{
     
 }
