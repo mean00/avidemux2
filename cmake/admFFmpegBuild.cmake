a826272e658c6d786d438d6121970926c1785bf1
kap <me@localhost.localdomain>
2014-11-25 21:35:10 -0500

    Adding vp9 decoding support

--- cmake/admFFmpegBuild.cmake
+++ cmake/admFFmpegBuild.cmake
@@ -22,7 +22,7 @@ set(FFMPEG_BINARY_DIR "${FFMPEG_BASE_DIR}/build")
 set(FFMPEG_DECODERS  aac ac3 eac3 adpcm_ima_amv  amv  bmp  cinepak  cyuv  dca  dvbsub  dvvideo  ffv1  ffvhuff  flv  fraps  h263  h264  hevc  huffyuv  mjpeg
 					 mjpegb  mpeg2video  mpeg4  msmpeg4v2  msmpeg4v3  msvideo1  nellymoser  png  qdm2  rawvideo  snow
 					 svq3  theora  tscc  mp2 mp3 mp2_float mp3_float
-					 vc1  vp3  vp6  vp6a  vp6f  vp8 wmapro wmav2  wmv1  wmv2  wmv3 cscd)
+					 vc1  vp3  vp6  vp6a  vp6f  vp8 vp9 wmapro wmav2  wmv1  wmv2  wmv3 cscd)
 set(FFMPEG_ENCODERS  ac3  ac3_float dvvideo  ffv1  ffvhuff  flv  h263  huffyuv  mjpeg  mp2  mpeg1video  mpeg2video  mpeg4  snow aac dca)
 set(FFMPEG_MUXERS  flv  matroska  mpeg1vcd  mpeg2dvd  mpeg2svcd  mpegts  mov  mp4  psp)
 set(FFMPEG_PARSERS  ac3  h263  h264  hevc  mpeg4video)
