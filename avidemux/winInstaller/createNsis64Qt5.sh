b798106bccd645aae63f7e0614ac110c9a92f0c2
mean <fixounet@free.fr>
2015-03-16 08:28:38 +0100

    [build] update installer (64 bits)

---
+++ avidemux/winInstaller/createNsis64Qt5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir -p install
+rm -f install/*
+echo "#* Preparing 7z svn build *#*"
+echo "** Getting svn revision **"
+export REV=`git log | grep branch_mean | head -1 | sed 's/^.*branch_mean.//g' | sed 's/ .*$//g'`
+export REV="00"
+#cp /mingw/bin/iconv.dll /mingw/Release/
+echo "Revision : $REV"
+echo "Creating NSIS Package"
+mkdir -p install
+rm -f install/*
+makensis -DSVN_VERSION="${REV}" -DNSIDIR=$PWD avidemux_cross64Qt5.nsi
+echo "Done"
