945ed23e7eb0ef1659bab6a02be98ef6c782a27d
mean <fixounet@free.fr>
2015-05-02 09:29:21 +0200

    [genlog] Better changelog generation

--- avidemux/winInstaller/genlog.sh
+++ avidemux/winInstaller/genlog.sh
@@ -1,1 +1,5 @@
-git log --sparse --no-decorate | grep -v "^commit" | grep -v "^Author" | grep -v "^Date" | grep -v "git-svn" | sed '/^[\t ]*$/d' | head -10 > "Change Log.html"
+export output="Change Log.html"
+echo "<html><head></head><body><h1>Changelog:</h1><br>" > $output
+git log  --pretty=format:'<li> %s <a href="https://github.com/mean00/avidemux2/commit/%H"> *;</a> </li> ' | head -20   >> $output
+
+echo "</body>" >> $output
