901c03d743151b226b43dd3f1b795ec7edcdcc8e
mean <fixounet@free.fr>
2015-05-18 15:51:49 +0200

    [nsi] Put date in nsi file

--- avidemux/winInstaller/createNsis64Qt5.sh
+++ avidemux/winInstaller/createNsis64Qt5.sh
@@ -4,12 +4,10 @@ rm -f install/*
 sh genlog.sh
 echo "#* Preparing 7z svn build *#*"
 echo "** Getting svn revision **"
-export REV=`git log | grep branch_mean | head -1 | sed 's/^.*branch_mean.//g' | sed 's/ .*$//g'`
-export REV="00"
 #cp /mingw/bin/iconv.dll /mingw/Release/
 echo "Revision : $REV"
 echo "Creating NSIS Package"
 mkdir -p install
 rm -f install/*
-makensis -DSVN_VERSION="${REV}" -DNSIDIR=$PWD avidemux_cross64Qt5.nsi
+makensis  -DNSIDIR=$PWD avidemux_cross64Qt5.nsi
 echo "Done"
