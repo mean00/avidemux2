d55e85c67f7682cc807dfdf81d85bf8aa5872dbd
mean <fixounet@free.fr>
2015-06-09 07:32:28 +0200

    [RPM] Fix rpm provide for core

--- avidemux_core/corePackageRpm.cmake
+++ avidemux_core/corePackageRpm.cmake
@@ -1,7 +1,7 @@
 ##############################
 # RPM
 ##############################
-SET(CPACK_RPM_PACKAGE_PROVIDES "avidemux3-core = ${CPACK_RPM_PACKAGE_VERSION}")
+SET(CPACK_RPM_PACKAGE_PROVIDES "avidemux3-core = ${AVIDEMUX_VERSION}")
 SET(CPACK_RPM_PACKAGE_NAME "avidemux3-core")
 SET(CPACK_RPM_PACKAGE_DESCRIPTION "Simple video editor,core libraries and development files.")
 SET(CPACK_RPM_PACKAGE_SUMMARY "Graphical video editing and transcoding tool and its development files.")
