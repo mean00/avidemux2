a6d0d431bfe79fdc00cc985f4309cb6c33b5e7b7
mean <fixounet@free.fr>
2015-06-09 07:47:11 +0200

    [build] More RPM tidying

--- avidemux/qt4/qt4PackageRpm.cmake
+++ avidemux/qt4/qt4PackageRpm.cmake
@@ -1,7 +1,7 @@
 ##############################
 # RPM
 ##############################
-SET(CPACK_RPM_PACKAGE_PROVIDES "avidemux3-${QT_EXTENSION} = ${CPACK_RPM_PACKAGE_VERSION}")
+SET(CPACK_RPM_PACKAGE_PROVIDES "avidemux3-${QT_EXTENSION} = ${AVIDEMUX_VERSION}")
 SET(CPACK_RPM_PACKAGE_NAME "avidemux3-${QT_EXTENSION}")
 SET(CPACK_RPM_PACKAGE_DESCRIPTION "Simple video editor,main program ${QT_EXTENSION} version ")
 SET(CPACK_RPM_PACKAGE_SUMMARY "Qt interface for avidemux")
