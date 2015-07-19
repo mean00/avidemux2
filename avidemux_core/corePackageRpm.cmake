953f6f049de4adc23873850eeb685e1965f06518
Andrew J. Schultz <ajschult@verizon.net>
2015-07-19 11:13:10 -0400

    explicitly disable CPACK_PACKAGE_RELOCATABLE
    see http://avidemux.org/smif/index.php/topic,16444.0.html

--- avidemux_core/corePackageRpm.cmake
+++ avidemux_core/corePackageRpm.cmake
@@ -5,5 +5,6 @@ SET(CPACK_RPM_PACKAGE_PROVIDES "avidemux3-core = ${AVIDEMUX_VERSION}")
 SET(CPACK_RPM_PACKAGE_NAME "avidemux3-core")
 SET(CPACK_RPM_PACKAGE_DESCRIPTION "Simple video editor,core libraries and development files.")
 SET(CPACK_RPM_PACKAGE_SUMMARY "Graphical video editing and transcoding tool and its development files.")
+SET(CPACK_PACKAGE_RELOCATABLE "false")
 
 include(admCPackRpm)
