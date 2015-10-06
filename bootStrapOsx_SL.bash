a5cd3ed8211c199bb6e5c31554298dadb382a7ee
mean <fixounet@free.fr>
2015-10-06 07:08:31 +0200

    [Build] Snow leopard build scripts

---
+++ bootStrapOsx_SL.bash
@@ -0,0 +1,205 @@
+#!/bin/bash
+# Bootstrapper to semi-automatically build avidemux from source on OSX
+# (c) Mean 2009
+
+# Specify the the directory where you want to install avidemux (a.k.a. the cmake_install_prefix)
+# like export BASE_INSTALL_DIR="<full_path_to_installation>". This can be /usr/local or /opt/local (macports) or /sw (Fink)
+export DAT=`date +"%Y_%m_%d"`
+export gt=`git log --format=oneline -1 | head -c 11`
+export REV="${DAT}_$gt"
+#
+export BASE_INSTALL_DIR="/";
+export BASE_APP="$HOME/Avidemux2.6.app/"
+export PREFIX="${BASE_APP}/Contents/Resources/"
+rm ~/A*.dmg
+rm -Rf $BASE_APP/*
+mkdir -p $BASE_APP
+mkdir -p $BASE_APP/Contents
+mkdir -p $BASE_APP/Contents/Resources
+echo "Revision : $REV"
+packages_ext=""
+do_core=1
+do_cli=0
+do_gtk=0   # Note that gtk is no fully longer supported on OSX. You are on your own here
+do_qt4=1
+do_plugins=1
+debug=0
+fail()
+{
+        echo "** Failed at $1**"
+        exit 1
+}
+
+rm -Rf $PREFIX/*
+Process()
+{
+        export BUILDDIR=$1
+        export SOURCEDIR=$2
+        export FAKEROOT="-DFAKEROOT=$FAKEROOT_DIR"
+        export EXTRA="$3"
+        export DEBUG=""
+        BUILDER="Unix Makefiles"
+	echo "**************** $1 *******************"
+        if [ "x$debug" = "x1" ] ; then 
+                DEBUG="-DVERBOSE=1 -DCMAKE_BUILD_TYPE=Debug  "
+                BUILDDIR="${BUILDDIR}_debug"
+                BUILDER="CodeBlocks - Unix Makefiles"
+        fi
+        
+        echo "Building $BUILDDIR from $SOURCEDIR with EXTRA=<$EXTRA>, DEBUG=<$DEBUG>"
+        rm -Rf ./$BUILDDIR
+        mkdir $BUILDDIR || fail mkdir
+        cd $BUILDDIR 
+        cmake $PKG $FAKEROOT -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_EDIT_COMMAND=vim -DAVIDEMUX_SOURCE_DIR=$TOP  $EXTRA $DEBUG -G "$BUILDER" $SOURCEDIR || fail cmakeZ
+        make -j 2 > /tmp/log$BUILDDIR || fail make
+	echo "** installing at $FAKEROOT_DIR **"
+	make install DESTDIR=$FAKEROOT_DIR || fail install
+}
+printModule()
+{
+        value=$1
+        name=$2
+        if [ "x$value" = "x1" ]; then echo "    $name will be built"
+        else echo "     $name will be skipped"
+        fi
+        
+
+}
+config()
+{
+        echo "Build configuration :"
+        echo "******************* :"
+        echo "Build type :"
+        if [ "x$debug" = "x1" ] ; then echo   "Debug build"
+        else echo   "Release build"
+        fi
+        printModule $do_core Core
+        printModule $do_gtk Gtk
+        printModule $do_qt4 Qt4
+        printModule $do_cli Cli
+        printModule $do_plugins Plugins
+}
+usage()
+{
+        echo "Bootstrap avidemux 2.6:"
+        echo "***********************"
+        echo "  --help            : Print usage"
+        echo "  --tgz             : Build tgz packages"
+        echo "  --debug           : Switch debugging on"
+        echo "  --with-core       : Build core"
+        echo "  --without-core    : Dont build core"
+        echo "  --with-cli        : Build cli"
+        echo "  --without-cli     : Dont build cli"
+        echo "  --with-core       : Build core"
+        echo "  --without-qt4     : Dont build qt4"
+        echo "  --with-plugins    : Build plugins"
+        echo "  --without-plugins : Dont build plugins"
+        config 
+
+}
+# Could probably do it with getopts...
+while [ $# != 0 ] ;do
+        case "$1" in
+         -h|--help)
+             usage
+             exit 1
+             ;;
+         --debug)
+                debug=1
+                ;;
+         --pause)
+                pause_script=true
+                ;;
+         --tgz)
+                packages_ext=tar.gz
+                PKG="$PKG -DAVIDEMUX_PACKAGER=tgz"
+                ;;
+         --without-qt4)
+                do_qt4=0
+             ;;
+         --without-cli)
+                do_cli=0
+             ;;
+         --without-plugins)
+                do_plugins=0
+             ;;
+         --without-core)
+                do_core=0
+             ;;
+         --with-qt4)
+                do_qt4=1
+             ;;
+         --with-cli)
+                do_cli=1
+             ;;
+         --with-plugins)
+                do_plugins=1
+             ;;
+         --with-core)
+                do_core=1
+             ;;
+        *)
+                echo "unknown parameter $1"
+                usage
+                exit 1
+                ;;
+     esac
+     shift
+done
+config
+echo "**BootStrapping avidemux **"
+export TOP=$PWD
+export POSTFIX=""
+echo "Top dir : $TOP"
+if [ "x$debug" = "x1" ] ; then echo   
+POSTFIX="_debug"
+fi
+
+if [ "x$do_core" = "x1" ] ; then 
+        echo "** CORE **"
+        cd $TOP
+        Process buildCore ../avidemux_core
+fi
+if [ "x$do_qt4" = "x1" ] ; then 
+        echo "** QT4 **"
+        cd $TOP
+        Process buildQt4 ../avidemux/qt4
+fi
+if [ "x$do_cli" = "x1" ] ; then 
+        echo "** CLI **"
+        cd $TOP
+        Process buildCli ../avidemux/cli
+fi
+if [ "x$do_plugins" = "x1" ] ; then 
+        echo "** Plugins **"
+        cd $TOP
+        Process buildPluginsCommon ../avidemux_plugins -DPLUGIN_UI=COMMON 
+fi
+if [ "x$do_plugins" = "x1" -a "x$do_qt4" = "x1" ] ; then 
+        echo "** Plugins Qt4 **"
+        cd $TOP
+        Process buildPluginsQt4 ../avidemux_plugins -DPLUGIN_UI=QT4 
+fi
+if [ "x$do_plugins" = "x1" -a "x$do_cli" = "x1" ] ; then 
+        echo "** Plugins CLI **"
+        cd $TOP
+        Process buildPluginsCLI ../avidemux_plugins -DPLUGIN_UI=CLI 
+fi
+echo "** Copying Qt nib files**"
+cp -Rap /opt/local/Library/Frameworks/QtGui.framework/Resources/qt_menu.nib $PREFIX/bin/
+echo "**  Changing link path**"
+python $TOP/cmake/osx_libs_qt4.py
+echo "* Copying make files **"
+cp $TOP/cmake/osx/Info.plist $PREFIX/../
+mkdir -p $PREFIX/../MacOS
+cp $TOP/cmake/osx/Avidemux2.6 $PREFIX/../MacOS/Avidemux2.6.app
+chmod +x $PREFIX/../MacOS/Avidemux2.6.app
+# Copy icons
+echo "Copying icons"
+cp $TOP/cmake/osx/*.icns $PREFIX/
+# creating dmg file
+cd $TOP
+rm -f *.dmg
+hdiutil create Avidemux2.6_r${REV}.dmg -srcfolder $HOME/Avidemux2.6.app/ -ov
+echo "** Preparing packaging **"
+echo "** ALL DONE **"
---
+++ cmake/osx_libs_qt4.py
@@ -0,0 +1,308 @@
+#
+#
+import shutil
+import re
+import subprocess
+import os
+home=os.getenv("HOME")
+print "Preparing independant bundle, home="+home+"..."
+rootFolder=home+"/Avidemux2.6.app/Contents/Resources"
+libFolder=rootFolder+"/lib"
+binFolder=rootFolder+"/bin"
+frameWorkFolder=rootFolder+"/../Frameworks"
+qtPluginFolder=rootFolder+"/../plugins"
+qts = ['QtCore', 'QtGui', 'QtOpenGL','QtScript']
+localPath = ['/opt/local','/usr/local/opt','/usr/local/Cellar/','/usr/local/lib']
+#
+# /usr/local/Cellar/qt/4.8.7_1/lib/QtCore.framework/Versions/4/QtCore
+#
+allSymbols=[]
+#
+#
+#
+def log(s):
+    print "Log:<"+str(s)+">"
+    pass
+#
+#
+#
+def getShortName(fl):
+    shortName=re.sub('^.*\/','',fl)
+    return shortName
+#
+# Returns the list of libs as dependencies in /opt (macport)
+#
+def getGlobalDeps(target):
+    cmd = ["/usr/bin/otool","-L", target]
+    log(cmd)
+    q = []
+    cmd = subprocess.Popen(" ".join(cmd), shell=True, stdout=subprocess.PIPE)
+    for line in cmd.stdout:
+       line = re.sub('[ \t\r\n]*', '', line)
+       line = re.sub('\(.*$', '', line)
+       isLocal = False
+       for l in localPath:
+           if(line.startswith(l)):
+              isLocal=True
+       if(isLocal is False):
+         continue
+       if(":" in line):
+              continue
+       log(line)
+       q.append(line)
+    log("\n"+target + "Found deps to adjust:"+str(q))
+    return q;
+#
+#
+#
+def getGlobalDepsNoQt(target):
+    q= []
+    p = getGlobalDeps(target)
+    for line in p:
+       if not getShortName(line).startswith('Qt'):
+          q.append(line)
+    return q;
+#
+def getGlobalDepsQtOnly(target):
+    q= []
+    p = getGlobalDeps(target)
+    for line in p:
+       if getShortName(line).startswith('Qt'):
+	  log("\n"+target+" has deps : "+line)
+          q.append(line)
+    return q;
+#
+# Returns the list of libs as dependencies  to local installation folder (
+#
+def getLocalDeps(target):
+    cmd = ["/usr/bin/otool","-L", target]
+    log(cmd)
+    q = []
+    cmd = subprocess.Popen(" ".join(cmd), shell=True, stdout=subprocess.PIPE)
+    for line in cmd.stdout:
+       line = re.sub('[ \t\r\n]*', '', line)
+       line = re.sub('\(.*$', '', line)
+       if(not line.startswith(libFolder)):
+              continue
+       if(":" in line):
+              continue
+       log(line)
+       q.append(line)
+    log(str(q))
+    return q;
+#
+# 
+# Copy used library coming from /opt/local
+#
+def copyFiles(folder,libFolder):
+    copied=0
+    log("Copy files"+folder)
+    for dirname, dirnames, filenames in os.walk(folder):
+       for filename in filenames:
+           absPath=os.path.join(dirname, filename)
+           deps=getGlobalDepsNoQt(absPath)
+           for dep in deps:
+               shortName=re.sub('^.*\/','',dep)
+               if(os.path.exists(libFolder+'/'+shortName)):
+                               log(shortName+" already copied")
+               else:
+                   log("Copying"+shortName)
+                   shutil.copy(dep,libFolder)
+                   copied+=1
+    return copied
+#
+# 
+# Copy used library coming from /opt/local
+#
+def copyQtDeps(components,libFolder):
+    copied=0
+    for modul in components:
+        absPath='/usr/local/opt/qt/lib/'+modul+'.framework/Versions/4/'+modul
+        print("Copy deps for "+modul+" ("+absPath+")")
+        deps=getGlobalDepsNoQt(absPath)
+        for dep in deps:
+                   shortName=re.sub('^.*\/','',dep)
+                   if(os.path.exists(libFolder+'/'+shortName)):
+                               print(shortName+" already copied")
+                   else:
+                       print("Copying:"+shortName)
+                       shutil.copy(dep,libFolder)
+                       copied+=1
+    # copy plugins deps too
+    copyFiles(qtPluginFolder+'/imageformats',libFolder)
+    return copied
+##
+def changeSymbol(target,oldName,newName):
+        cmd="/usr/bin/install_name_tool -change "+oldName+" "+newName+" "+target
+        log(cmd)
+        subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+#
+def changeId(target,newId):
+           cmd="/usr/bin/install_name_tool -id "+newId+" "+target
+           log(cmd)
+           subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+#
+#
+def renameSymbols(libs):
+   for qt in libs:
+      print "Processing "+qt
+      # Change binding
+      for otherQt in allSymbols:
+          newname=re.sub(r'^.*\/',r'@executable_path/../lib/',otherQt)
+          exc="install_name_tool -change "+otherQt+" "+newname+" libToProcess"
+          print(exc)
+#
+#
+#
+def changeGlobalLinkPathForOne(f):
+    deps=getGlobalDepsNoQt(f)
+    for d in deps:
+        shortName=getShortName(d)
+        shortName="@executable_path/../lib/"+shortName
+        changeSymbol(f,d,shortName)
+def changeLocalLinkPathForOne(f):
+    deps=getLocalDeps(f)
+    for d in deps:
+        shortName="@executable_path/../lib/"+re.sub("^.*\/","",d)
+        changeSymbol(f,d,shortName)
+def changeLibLinkPath(folder):
+    for dirname, dirnames, filenames in os.walk(folder):
+       for filename in filenames:
+           absPath=os.path.join(dirname, filename)
+           changeGlobalLinkPathForOne(absPath)
+           changeLocalLinkPathForOne(absPath)
+           changeQtLinkPathForOne(absPath)
+           shortName="@executable_path/../lib/"+re.sub("^.*\/","",absPath)
+           changeId(absPath,shortName)
+#
+def changeQtPlatformLinkPath(folder):
+    for dirname, dirnames, filenames in os.walk(folder):
+       for filename in filenames:
+           absPath=os.path.join(dirname, filename)
+           changeGlobalLinkPathForOne(absPath)
+           changeLocalLinkPathForOne(absPath)
+           changeQtLinkPathForOne(absPath)
+           shortName="@executable_path/../../plugins/platforms/"+re.sub("^.*\/","",absPath)
+           changeId(absPath,shortName)
+def changeQtPluginLinkPath(folder):
+    for dirname, dirnames, filenames in os.walk(folder):
+       for filename in filenames:
+           absPath=os.path.join(dirname, filename)
+           changeGlobalLinkPathForOne(absPath)
+           changeLocalLinkPathForOne(absPath)
+           changeQtLinkPathForOne(absPath)
+           shortName="@executable_path/../../plugins/imageformats/"+re.sub("^.*\/","",absPath)
+           changeId(absPath,shortName)
+def changeBinLinkPath(folder):
+    for dirname, dirnames, filenames in os.walk(folder):
+       for filename in filenames:
+           absPath=os.path.join(dirname, filename)
+           changeGlobalLinkPathForOne(absPath)
+           changeLocalLinkPathForOne(absPath)
+           changeQtLinkPathForOne(absPath)
+           shortName="@executable_path/bin/"+re.sub("^.*\/","",absPath)
+           changeId(absPath,shortName)
+            
+def changePluginLinkPath(folder,relFolder):
+    for dirname, dirnames, filenames in os.walk(folder):
+       for filename in filenames:
+           absPath=os.path.join(dirname, filename)
+           changeGlobalLinkPathForOne(absPath)
+           changeQtLinkPathForOne(absPath)
+           shortName="@executable_path/lib/"+relFolder+re.sub("^.*\/","",absPath)
+           changeId(absPath,shortName)
+#
+#
+#
+def myChmod(dst):
+        log("chmod <"+dst+">")
+        cmd="/bin/chmod" + " -R 755  "+dst
+        log("Exec <"+cmd+">")
+        subprocess.call(cmd, shell=True)
+
+def changeQtFileSelfLinkPath(fl,modu):
+           shortName="@executable_path/../../Frameworks/"+modu+".framework/Versions/4/"+modu
+           cmd="/usr/bin/install_name_tool -id "+shortName+" "+fl
+           log(cmd)
+           subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+def changeQtLinkPathForOne(f):
+    deps=getGlobalDepsQtOnly(f)
+    for d in deps:
+        shortName=getShortName(d)
+        shortName="@executable_path/../../Frameworks/"+shortName+".framework/Versions/4/"+shortName
+        changeSymbol(f,d,shortName)
+
+#
+def myMkDir(target):
+        if(os.path.exists(target)):
+                return
+        os.makedirs(target)   
+#         
+def myCopyTree(src,dst):
+	log(src+"->"+dst)
+        if(not os.path.exists(src)):
+                return
+        shutil.copytree(src,dst)            
+        myChmod(dst)
+def myCopyFile(src,dst):
+        if(not os.path.exists(src)):
+                return
+        shutil.copy(src,dst)            
+        myChmod(dst)
+##
+def copyQtFiles(targetFolder):
+        myMkDir(targetFolder)
+        for q in qts:
+               print q
+               if(os.path.exists(targetFolder+'/'+q+'.framework')):
+                               log(q+" already copied")
+               else:
+                   log("Copying"+q+' to '+targetFolder)
+                   src='/usr/local/opt/qt/lib/'+q+'.framework'
+                   dst=targetFolder+'/'+q+'.framework'
+                   myMkDir(dst)
+                   src=src+'/Versions/4/'
+                   dst=dst+'/Versions/'
+                   myMkDir(dst)
+                   dst=dst+'/4/'
+                   myMkDir(dst)
+                   print "Copying "+dst+'Resources'
+                   myCopyTree(src+'Resources',dst+'Resources')
+                   print "Copying "+dst+q
+                   myCopyFile(src+q,dst+q)
+                   changeQtFileSelfLinkPath(dst+q,q)
+                   changeQtLinkPathForOne(dst+q) 
+                   changeGlobalLinkPathForOne(dst+q)
+        # Also copy plugins
+        myMkDir(qtPluginFolder)
+	myCopyTree('/usr/local/opt/qt/plugins/imageformats',qtPluginFolder+'/imageformats')
+	myCopyTree('/usr/local/opt/qt/plugins/platforms',qtPluginFolder+'/platforms')
+#################################################################
+# Step 1 : Copy system files so we have a standalone package
+#
+#################################################################
+print "Copying Qt framework"
+copyQtFiles(frameWorkFolder)
+print "Copying Qt framework dependencies"
+copyQtDeps(qts,libFolder)
+print "Copying system files"
+#copyFiles(binFolder,libFolder)
+processed=1
+# Copy file until all of them are there
+while not processed == 0:
+        processed=copyFiles(libFolder,libFolder)
+#################################################################
+# Step 2 :  Change link name so that they are all executable_path
+#               relative
+#################################################################
+print "Adjusting dependencies"
+changeBinLinkPath(binFolder)
+changeLibLinkPath(libFolder)
+changeQtPluginLinkPath(qtPluginFolder)
+subFolders=["audioDecoder",    "audioEncoders",   "autoScripts",     "demuxers",        "muxers",          "scriptEngines",   "videoEncoders",   "videoFilters"]
+for s in subFolders:
+        relFolder="ADM_plugins6/"+s
+        changePluginLinkPath(libFolder+"/"+relFolder,relFolder)
+changeQtPlatformLinkPath(binFolder+'/platforms')
+        
+
