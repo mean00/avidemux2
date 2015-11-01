d24993379f954a6296945d70cfb9549ebec381ae
mean <fixounet@free.fr>
2015-11-01 16:31:25 +0100

    [yv12/encoder] Fix image mapping

--- avidemux_plugins/ADM_videoEncoder/yv12/ADM_yv12Encoder.cpp
+++ avidemux_plugins/ADM_videoEncoder/yv12/ADM_yv12Encoder.cpp
@@ -23,7 +23,7 @@
 */
 ADM_yv12Encoder::ADM_yv12Encoder(ADM_coreVideoFilter *src,bool globalHeader) : ADM_coreVideoEncoder(src)
 {
-    printf("[YV12Encoder] Creating.\n");
+    ADM_info("[YV12Encoder] Creating.\n");
     int w,h;
     FilterInfo *info=src->getInfo();
     w=info->width;
@@ -36,7 +36,7 @@ ADM_yv12Encoder::ADM_yv12Encoder(ADM_coreVideoFilter *src,bool globalHeader) : A
 */
 ADM_yv12Encoder::~ADM_yv12Encoder()
 {
-    printf("[YV12Encoder] Destroying.\n");
+    ADM_info("[YV12Encoder] Destroying.\n");
 }
 
 
@@ -48,11 +48,24 @@ bool         ADM_yv12Encoder::encode (ADMBitstream * out)
     uint32_t fn;
     if(source->getNextFrame(&fn,image)==false)
     {
-        printf("[YV12] Cannot get next image\n");
+        ADM_info("[YV12] Cannot get next image\n");
         return false;
     }
     ADM_assert(out->bufferSize>plane);
-    memcpy(out->data,image->GetReadPtr(PLANAR_Y),plane); // We KNOW the y,u,v planes are contiguous and width=stride!
+    //--
+    int w=image->GetWidth(PLANAR_Y);
+    int h=image->GetHeight(PLANAR_Y);
+    
+    ADMImageRefWrittable packed(w,h);
+    uint32_t square=w*h;
+    packed._planes[0]=out->data;
+    packed._planes[1]=out->data+square;
+    packed._planes[2]=out->data+((square*5)>>2);
+    
+    packed._planeStride[0]=w;
+    packed._planeStride[1]=packed._planeStride[2]=w>>1;
+    
+    packed.duplicate(image);
     out->len=plane;
     out->pts=out->dts=image->Pts;
     out->flags=AVI_KEY_FRAME;
