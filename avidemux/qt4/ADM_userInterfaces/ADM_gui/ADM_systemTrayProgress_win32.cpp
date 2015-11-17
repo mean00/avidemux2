f97e832247ae2bc09c5937c23fd576b340a9b861
mean <fixounet@free.fr>
2015-11-17 08:23:16 +0100

    [Qt/Tray] Blind win32 version

--- avidemux/qt4/ADM_userInterfaces/ADM_gui/ADM_systemTrayProgress_win32.cpp
+++ avidemux/qt4/ADM_userInterfaces/ADM_gui/ADM_systemTrayProgress_win32.cpp
@@ -3,19 +3,46 @@
 #include "ADM_cpp.h"
 #include "ADM_default.h"
 #include "ADM_systemTrayProgress.h"
+#include <QtWinExtras>
 /**
  */
-class defaultTaskBarProgress : public admUITaskBarProgress
+class winTaskBarProgress : public admUITaskBarProgress
 {
-    virtual bool enable() {return true;}
-    virtual bool disable() {return true;}
-    virtual bool setProgress(int percent) {return true;} 
+    public:
+        winTaskBarProgress(QMainWindow *w)
+        {
+              button = new QWinTaskbarButton(w);
+        }
+        virtual ~winTaskBarProgress()
+        {
+            delete button;
+            button=NULL;
+        }
+    virtual bool enable() 
+    {
+        QWinTaskbarProgress *progress = button->progress();
+        progress->setVisible(true);
+        return true;
+    }
+    virtual bool disable() 
+    {
+        QWinTaskbarProgress *progress = button->progress();
+        progress->setVisible(false);
+        return true;
+    }
+    virtual bool setProgress(int percent) 
+    {
+        QWinTaskbarProgress *progress = button->progress();
+        progress->setValue(percent);
+    } 
+    QWinTaskbarButton *button;
 };
 
 /**
  */
 admUITaskBarProgress *createADMTaskBarProgress(void *parent)
 {
-    return new defaultTaskBarProgress;
+        QMainWindow *win=( QMainWindow *)parent;
+        return new winTaskBarProgress(win);
 }
 // EOF
