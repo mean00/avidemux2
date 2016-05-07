ed18a9b3a56b714ac57588818344a4e61775576f
mean <fixounet@free.fr>
2016-05-07 17:24:57 +0200

    [cosmetic] reindent

--- avidemux_core/ADM_coreVideoFilter/include/ADM_videoFilterCache.h
+++ avidemux_core/ADM_coreVideoFilter/include/ADM_videoFilterCache.h
@@ -21,10 +21,10 @@ class ADM_coreVideoFilter;
 */
 typedef struct vidCacheEntry
 {
-		uint32_t 	frameNum;
-		ADMImage 	*image;
-		uint8_t		frameLock;		
-		uint32_t	lastUse;
+                uint32_t 	frameNum;
+                ADMImage 	*image;
+                uint8_t		frameLock;		
+                uint32_t	lastUse;
         bool        freeEntry;
 
 }vidCacheEntry;
@@ -33,26 +33,26 @@ typedef struct vidCacheEntry
 */
 class ADM_COREVIDEOFILTER6_EXPORT VideoCache
 {
-	private:
-		vidCacheEntry	    *entry;
-		uint32_t	        counter;
-		uint32_t 	        nbEntry;
-		ADM_coreVideoFilter *incoming;
-		
-		
-		int32_t 	        searchFrame( uint32_t frame);
-		int32_t 	        searchPtr( ADMImage *ptr);
-        int                 searchFreeEntry(void);
-		ADMImage            *getImageBase(uint32_t frame);
-	public:
-		VideoCache(uint32_t nb,ADM_coreVideoFilter *in);
-		~VideoCache(void);
-		
-		ADMImage *getImage(uint32_t frame);
-        ADMImage *getImageAs(ADM_HW_IMAGE type,uint32_t frame);
-		uint8_t unlockAll(void);
-		uint8_t unlock(ADMImage  *frame);
-		uint8_t flush(void);
-        void    dump(void);
+        private:
+                vidCacheEntry       *entry;
+                uint32_t            counter;
+                uint32_t            nbEntry;
+                ADM_coreVideoFilter *incoming;
+
+
+                int32_t             searchFrame( uint32_t frame);
+                int32_t             searchPtr( ADMImage *ptr);
+                int                 searchFreeEntry(void);
+                ADMImage            *getImageBase(uint32_t frame);
+        public:
+                                    VideoCache(uint32_t nb,ADM_coreVideoFilter *in);
+                                    ~VideoCache(void);
+
+                ADMImage            *getImage(uint32_t frame);
+                ADMImage            *getImageAs(ADM_HW_IMAGE type,uint32_t frame);
+                uint8_t             unlockAll(void);
+                uint8_t             unlock(ADMImage  *frame);
+                uint8_t             flush(void);
+                void                dump(void);
 };
 #endif
