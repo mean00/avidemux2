7b8361f29d12296da36dbb618c261b43704eede5
mean <fixounet@free.fr>
2016-05-31 15:49:02 +0200

    [logo] Cli version

---
+++ avidemux_plugins/ADM_videoFilters6/logo/cli/DIA_flyLogo.cpp
@@ -0,0 +1,17 @@
+#include "DIA_flyDialog.h"
+#include "ADM_default.h"
+#include "ADM_image.h"
+
+
+#include "logo.h"
+#include "DIA_flyLogo.h"
+#include "ADM_vidLogo.h"
+
+
+/**
+    \fn process
+*/
+uint8_t    flyLogo::processYuv(ADMImage* in, ADMImage *out)
+{
+    return true;
+}
