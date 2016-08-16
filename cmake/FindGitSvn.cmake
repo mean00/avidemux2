3ddbed4fac329737cdfdb260a53b7ece2f1b0081
mean <fixounet@free.fr>
2016-08-16 21:00:57 +0200

    [Build] Try to fix the versioning for good

--- cmake/FindGitSvn.cmake
+++ cmake/FindGitSvn.cmake
@@ -6,22 +6,22 @@ include(admTimeStamp)
 #
 ###################################################################
 MACRO(admIsGitSvn _dir _isSvn)
-	#MESSAGE(STATUS "Git on folder ${_dir}")
-	EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} log -n 50 ${_dir}
-			COMMAND grep "git-svn-id"
-			COMMAND wc -l
-			WORKING_DIRECTORY ${_dir}
-        		RESULT_VARIABLE result 
-        		OUTPUT_VARIABLE output
-        	)
-	if(${output} EQUAL 0)
-		MESSAGE(STATUS "This is not git-svn")
-		SET(${_isSvn} 0)
-	else(${output} EQUAL 0)
-		MESSAGE(STATUS "This is git-svn")
-		SET(${_isSvn} 1)
-	endif(${output} EQUAL 0)
-	#MESSAGE(STATUS "Dir : ${_dir} Output : ${output} result:${result} svn:${${_isSvn}}")	
+    #MESSAGE(STATUS "Git on folder ${_dir}")
+    EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} log -n 50 ${_dir}
+            COMMAND grep "git-svn-id"
+            COMMAND wc -l
+            WORKING_DIRECTORY ${_dir}
+                RESULT_VARIABLE result 
+                OUTPUT_VARIABLE output
+            )
+    if(${output} EQUAL 0)
+        MESSAGE(STATUS "This is not git-svn")
+        SET(${_isSvn} 0)
+    else(${output} EQUAL 0)
+        MESSAGE(STATUS "This is git-svn")
+        SET(${_isSvn} 1)
+    endif(${output} EQUAL 0)
+    #MESSAGE(STATUS "Dir : ${_dir} Output : ${output} result:${result} svn:${${_isSvn}}")    
 ENDMACRO(admIsGitSvn _dir _svn)
 #
 #
@@ -35,19 +35,31 @@ MACRO(admGetGitRevision _dir _rev)
         IF(GIT_EXECUTABLE)
         
         #SET(EXE  "cd ${_dir}&& ${GIT_EXECUTABLE} svn log | head -2  | grep '^r' | sed 's/ .*$//g'" )
-	set(svn 0)
-        	MESSAGE(STATUS "Getting git-svn version from ${_dir}")
-      		EXECUTE_PROCESS(
-                        COMMAND ${GIT_EXECUTABLE} log --format=oneline -1  ${_dir}
-                        COMMAND head -c 11
-                        WORKING_DIRECTORY ${_dir} 
-                        RESULT_VARIABLE result 
-                        OUTPUT_VARIABLE output
-                        )
-                ADM_TIMESTAMP(date)
-                STRING(STRIP "${output}" output)
-                SET( ${_rev} "${date}_${output}")
-                MESSAGE(STATUS "<${output}><${date}>==> ${${_rev}}")
+        set(svn 0)
+       MESSAGE(STATUS "Getting git-svn version from ${_dir}")
+       EXECUTE_PROCESS(
+               COMMAND ${GIT_EXECUTABLE} rev-parse  --show-toplevel ${_dir}
+               WORKING_DIRECTORY ${_dir} 
+               RESULT_VARIABLE result 
+               OUTPUT_VARIABLE topdir
+               )
+       STRING(STRIP "${topdir}" topdir)
+       MESSAGE(STATUS "Top dir is <${topdir}>")
+       #MESSAGE(STATUS "COMMAND  ${GIT_EXECUTABLE} log --format=oneline -1   ${topdir}")
+       EXECUTE_PROCESS(
+               COMMAND echo  log --format=oneline -1   ${topdir}
+               COMMAND xargs ${GIT_EXECUTABLE} 
+               COMMAND head -c 11
+               WORKING_DIRECTORY ${_dir} 
+               RESULT_VARIABLE result 
+               OUTPUT_VARIABLE output
+               )
+
+       #MESSAGE(STATUS "git last entry is ${output} -- ${result}")
+       ADM_TIMESTAMP(date)
+       STRING(STRIP "${output}" output)
+       SET( ${_rev} "${date}_${output}")
+       MESSAGE(STATUS "<${output}><${date}>==> ${${_rev}}")
                 
 ELSE(GIT_EXECUTABLE)
                 SET(ADM_GIT_SVN_REVISION 0)
