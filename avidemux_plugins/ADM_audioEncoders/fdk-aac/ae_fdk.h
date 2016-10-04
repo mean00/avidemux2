d790df2d000927c0a037df5bb6cf6200cc033f53
mean <fixounet@free.fr>
2016-10-04 07:26:51 +0200

    [AudioEncoder] Skeleton for fdk aac

---
+++ avidemux_plugins/ADM_audioEncoders/fdk-aac/ae_fdk.h
@@ -0,0 +1,35 @@
+
+/***************************************************************************
+    copyright            : (C) 2002-6 by mean
+    email                : fixounet@free.fr
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef AUDMaudioAAC
+#define AUDMaudioAAC
+#include "faac_encoder.h"
+ //_____________________________________________
+class AUDMEncoder_Faac : public ADM_AudioEncoder
+{
+protected:
+         void           *_handle;
+         uint32_t        _chunk;
+         uint8_t        refillBuffer(int minimum);
+         bool           _globalHeader;
+         faac_encoder   _config;
+         float          *ordered;
+public:
+            bool        initialize(void);
+    virtual             ~AUDMEncoder_Faac();
+                        AUDMEncoder_Faac(AUDMAudioFilter *instream,bool globalHeader,CONFcouple *setup);
+    virtual bool	encode(uint8_t *dest, uint32_t *len, uint32_t *samples);
+};
+
+#endif
