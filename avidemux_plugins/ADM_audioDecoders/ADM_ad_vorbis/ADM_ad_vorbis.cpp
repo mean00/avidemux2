8e260dc170f898225d9161724e61ae92bd95c5e2
mean <fixounet@free.fr>
2016-10-15 08:46:21 +0200

    [Decode/vorbis] simplify

--- avidemux_plugins/ADM_audioDecoders/ADM_ad_vorbis/ADM_ad_vorbis.cpp
+++ avidemux_plugins/ADM_audioDecoders/ADM_ad_vorbis/ADM_ad_vorbis.cpp
@@ -5,14 +5,14 @@
     copyright            : (C) 2002 by mean
     email                : fixounet@free.fr
 
-	Strongly inspired by mplayer ogg vorbis decoder
-	(ripp off should be more appropriate)
+    Strongly inspired by mplayer ogg vorbis decoder
+    (ripp off should be more appropriate)
 
-	We expect the first packet as extraData. It contains needed info such
-	as frequency/channels etc...
+    We expect the first packet as extraData. It contains needed info such
+    as frequency/channels etc...
 
-	How to deal with comment and codebook ?
-	Mmmmm
+    How to deal with comment and codebook ?
+    Mmmmm
 
  ***************************************************************************/
 
@@ -32,20 +32,22 @@
 #include "ADM_ad_vorbis.h"
 #include "ADM_coreUtils.h"
 
-#define STRUCT ((oggVorbis *)_contextVoid)
 
+/**
+ * \class ADM_vorbis
+ */
 class ADM_vorbis : public     ADM_Audiocodec
 {
-	protected:
-		void *_contextVoid;
+    protected:
+        oggVorbis           _context;
 
-	public:
-		ADM_vorbis(uint32_t fourcc, WAVHeader *info, uint32_t l, uint8_t *d);
-		virtual	~ADM_vorbis();
-		virtual	uint8_t run(uint8_t *inptr, uint32_t nbIn, float *outptr, uint32_t *nbOut);
-		virtual	uint8_t isCompressed(void) {return 1;}
-		virtual	uint8_t isDecompressable(void) {return 1;}
-        virtual	bool    resetAfterSeek(void) ;
+    public:
+                            ADM_vorbis(uint32_t fourcc, WAVHeader *info, uint32_t l, uint8_t *d);
+        virtual             ~ADM_vorbis();
+        virtual    uint8_t run(uint8_t *inptr, uint32_t nbIn, float *outptr, uint32_t *nbOut);
+        virtual    uint8_t isCompressed(void) {return 1;}
+        virtual    uint8_t isDecompressable(void) {return 1;}
+        virtual    bool    resetAfterSeek(void) ;
 };
 
 // Supported formats + declare our plugin
@@ -53,26 +55,21 @@ class ADM_vorbis : public     ADM_Audiocodec
 static  ad_supportedFormat Formats[]={
         {WAV_OGG_VORBIS,AD_MEDIUM_QUAL},
 };
-DECLARE_AUDIO_DECODER(ADM_vorbis,						// Class
-			0,0,1, 												// Major, minor,patch
-			Formats, 											// Supported formats
-			"libVorbis decoder plugin for avidemux (c) Mean\n"); 	// Desc
+DECLARE_AUDIO_DECODER(ADM_vorbis,                        // Class
+            0,0,1,                                                 // Major, minor,patch
+            Formats,                                             // Supported formats
+            "libVorbis decoder plugin for avidemux (c) Mean\n");     // Desc
 //********************************************************
 
 
  ADM_vorbis::~ADM_vorbis()
  {
- 	oggVorbis *o;
-	o=(oggVorbis *)_contextVoid;
-	if(o)
-	{
-		vorbis_block_clear(&o->vblock);
-		vorbis_info_clear(&o->vinfo);
-		delete o;
-	}
-	_contextVoid=NULL;
-	_init=0;
-
+    if(_init)
+    {
+        vorbis_block_clear(&(_context.vblock));
+        vorbis_info_clear(&(_context.vinfo));
+    }
+    _init=false;
  }
  /**
   * 
@@ -123,7 +120,7 @@ VERR(OV_ENOSEEK    )
   * @param extraData
   */
  ADM_vorbis::ADM_vorbis(uint32_t fcc, WAVHeader *info, uint32_t extra, uint8_t *extraData)
- 	: ADM_Audiocodec(fcc,*info)
+     : ADM_Audiocodec(fcc,*info)
  {
  ogg_packet packet;
  vorbis_comment comment;
@@ -133,78 +130,76 @@ VERR(OV_ENOSEEK    )
  uint32_t *ptr;
  int error;
 #define MANAGE_ERROR(st,er) {if(er<0) {printError(st,er); return ;}}
- 	_init=0;
- 	ADM_info("Trying to initialize vorbis codec with %d bytes of header data\n",(int)extra);
+     _init=0;
+     ADM_info("Trying to initialize vorbis codec with %d bytes of header data\n",(int)extra);
 
-	vrbis=new oggVorbis;
- 	_contextVoid=(void *)vrbis;
+     _init=false;
+    memset(&(_context),0,sizeof(_context));
 
-	memset(vrbis,0,sizeof(oggVorbis));
+    // init everything
+    vorbis_info_init(&(_context.vinfo));
+    vorbis_comment_init(&(_context.vcomment));
 
-	// init everything
-	vorbis_info_init(&STRUCT->vinfo);
-	vorbis_comment_init(&STRUCT->vcomment);
+    // split extradata as header/comment/code
+    ptr=(uint32_t *)extraData;
+    size_hdr=*ptr++;
+    size_cmt=*ptr++;
+    size_code=*ptr++;
 
-	// split extradata as header/comment/code
-	ptr=(uint32_t *)extraData;
-	size_hdr=*ptr++;
-	size_cmt=*ptr++;
-	size_code=*ptr++;
-
-	hdr=extraData;
-        hdr+=3*sizeof(uint32_t);
-	cmt=hdr+size_hdr;
-	code=cmt+size_cmt;
+    hdr=extraData;
+    hdr+=3*sizeof(uint32_t);
+    cmt=hdr+size_hdr;
+    code=cmt+size_cmt;
 
         
         
- 	// Feed header passed as extraData
-	packet.bytes=size_hdr;
-	packet.packet=hdr;
-	packet.b_o_s=1; // yes, it is a new stream
-        printPacket("1st packet",&packet);
-	error=vorbis_synthesis_headerin(&STRUCT->vinfo,&comment,&packet);
-        MANAGE_ERROR("1st packet",error);
-	// update some info in header this is the only place to get them
-	// especially frequency.
+     // Feed header passed as extraData
+    packet.bytes=size_hdr;
+    packet.packet=hdr;
+    packet.b_o_s=1; // yes, it is a new stream
+    printPacket("1st packet",&packet);
+    error=vorbis_synthesis_headerin(&(_context.vinfo),&comment,&packet);
+    MANAGE_ERROR("1st packet",error);
+    // update some info in header this is the only place to get them
+    // especially frequency.
 /*
-	info->channels=STRUCT->vinfo.channels;
-	info->frequency=STRUCT->vinfo.rate;
+    info->channels=STRUCT->vinfo.channels;
+    info->frequency=STRUCT->vinfo.rate;
 */
-	info->byterate=STRUCT->vinfo.bitrate_nominal>>3;
+    info->byterate=_context.vinfo.bitrate_nominal>>3;
 
-	if(!info->byterate)
-	{
-		ADM_warning("Mmm, no nominal bitrate...\n");
-		info->byterate=16000;
-	}
-	// now unpack comment
-	packet.bytes=size_cmt;
-	packet.packet=cmt;
-	packet.b_o_s=0; // Not new
-        printPacket("2nd packet",&packet);
-        
-        error=vorbis_synthesis_headerin(&STRUCT->vinfo,&comment,&packet);        
-        MANAGE_ERROR("2nd packet",error);
+    if(!info->byterate)
+    {
+        ADM_warning("Mmm, no nominal bitrate...\n");
+        info->byterate=16000;
+    }
+    // now unpack comment
+    packet.bytes=size_cmt;
+    packet.packet=cmt;
+    packet.b_o_s=0; // Not new
+    printPacket("2nd packet",&packet);
 
-	// and codebook
-	packet.bytes=size_code;
-	packet.packet=code;
-	packet.b_o_s=0; // Not new
-        printPacket("3rd packet",&packet);
+    error=vorbis_synthesis_headerin(&(_context.vinfo),&comment,&packet);        
+    MANAGE_ERROR("2nd packet",error);
+
+    // and codebook
+    packet.bytes=size_code;
+    packet.packet=code;
+    packet.b_o_s=0; // Not new
+    printPacket("3rd packet",&packet);
         
-	error=vorbis_synthesis_headerin(&STRUCT->vinfo,&comment,&packet);
-        MANAGE_ERROR("3rd packet",error);
+    error=vorbis_synthesis_headerin(&(_context.vinfo),&comment,&packet);
+    MANAGE_ERROR("3rd packet",error);
         
-	vorbis_comment_clear(&comment);
-	vorbis_synthesis_init(&STRUCT->vdsp,&STRUCT->vinfo);
-	vorbis_block_init(&STRUCT->vdsp,&STRUCT->vblock);
-	ADM_info("Vorbis init successfull\n");
-	STRUCT->ampscale=1;
-	_init=1;
+    vorbis_comment_clear(&comment);
+    vorbis_synthesis_init(&(_context.vdsp),&(_context.vinfo));
+    vorbis_block_init(&(_context.vdsp),&(_context.vblock));
+    ADM_info("Vorbis init successfull\n");
+    _context.ampscale=1;
+    _init=1;
   CHANNEL_TYPE *p_ch_type = channelMapping;
 #define DOIT(y) *(p_ch_type++)=ADM_CH_##y;
-    switch(STRUCT->vinfo.channels)
+    switch(_context.vinfo.channels)
     {
     case 1:
     case 2:
@@ -228,42 +223,49 @@ VERR(OV_ENOSEEK    )
     }
     }
  }
- // This codec expects more or less one packet at a time !
+ /**
+  * 
+  * @param inptr
+  * @param nbIn
+  * @param outptr
+  * @param nbOut
+  * @return 
+  */
 
  uint8_t ADM_vorbis::run(uint8_t *inptr, uint32_t nbIn, float *outptr, uint32_t *nbOut)
 {
 ogg_packet packet;
 float **sample_pcm;
-int	nb_synth;
+int    nb_synth;
 
-	*nbOut=0;
-	if(!_init) return 0;
-	packet.b_o_s=0;
-	packet.e_o_s=0;
-	packet.bytes=nbIn;
-	packet.packet=inptr;
+    *nbOut=0;
+    if(!_init) return 0;
+    packet.b_o_s=0;
+    packet.e_o_s=0;
+    packet.bytes=nbIn;
+    packet.packet=inptr;
         packet.granulepos=-1;
-	if(!vorbis_synthesis(&STRUCT->vblock,&packet))
-	{
-	      vorbis_synthesis_blockin(&STRUCT->vdsp,&STRUCT->vblock);
-	 }
-	 nb_synth=vorbis_synthesis_pcmout(&STRUCT->vdsp,&sample_pcm);
-	 if(nb_synth<0)
-	 {
-	 	printf("error decoding vorbis %d\n",nb_synth);
-		return 0;
-	 }
+    if(!vorbis_synthesis(&(_context.vblock),&packet))
+    {
+          vorbis_synthesis_blockin(&(_context.vdsp),&(_context.vblock));
+     }
+     nb_synth=vorbis_synthesis_pcmout(&(_context.vdsp),&sample_pcm);
+     if(nb_synth<0)
+     {
+         printf("error decoding vorbis %d\n",nb_synth);
+        return 0;
+     }
 
-	 for (uint32_t samp = 0; samp < nb_synth; samp++)
-	 	for (uint8_t chan = 0; chan < STRUCT->vinfo.channels; chan++)
-			*outptr++ = sample_pcm[chan][samp] * STRUCT->ampscale;
+     for (uint32_t samp = 0; samp < nb_synth; samp++)
+         for (uint8_t chan = 0; chan < _context.vinfo.channels; chan++)
+            *outptr++ = sample_pcm[chan][samp] * _context.ampscale;
 
-	 *nbOut = STRUCT->vinfo.channels * nb_synth;
+     *nbOut = _context.vinfo.channels * nb_synth;
 
-	// Puge them
-	 vorbis_synthesis_read(&STRUCT->vdsp,nb_synth);
-	 //printf("This round : in %d bytes, out %d bytes synthetized:%d\n",nbIn,*nbOut,nb_synth);
-	return 1;
+    // Puge them
+     vorbis_synthesis_read(&_context.vdsp,nb_synth);
+     //printf("This round : in %d bytes, out %d bytes synthetized:%d\n",nbIn,*nbOut,nb_synth);
+    return 1;
 
 }
 /**
@@ -277,10 +279,10 @@ int	nb_synth;
   float **sample_pcm;
   ogg_packet packet;
 
-  	//vorbis_synthesis_blockin(&STRUCT->vdsp,&STRUCT->vblock);
-  	vorbis_synthesis_pcmout(&STRUCT->vdsp,&sample_pcm);
-    vorbis_synthesis_restart(&STRUCT->vdsp);
-  	return 1;
+    ADM_info("Vorbis-Resetting\n");
+    vorbis_synthesis_pcmout(&(_context.vdsp),&sample_pcm);
+    vorbis_synthesis_restart(&(_context.vdsp));
+    return true;
   }
 
 
