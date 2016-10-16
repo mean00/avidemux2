ff92a4e1c16646b1bb1dd06d80c1c61481559dea
mean <fixounet@free.fr>
2016-10-16 09:30:41 +0200

    [libva] Better detection of system libva (h265, vp9,...)

---
+++ cmake/cmake_compile_check/libva_dummy.cpp
@@ -0,0 +1,10 @@
+#include <string.h>
+#include <stdio.h>
+#include <va/va.h>
+#error fail safe
+int main(int argc, char **argv)
+{
+        lfdkmlfml,f
+        int attr=VAProfileHEVCMain;      
+        return 0;
+}
