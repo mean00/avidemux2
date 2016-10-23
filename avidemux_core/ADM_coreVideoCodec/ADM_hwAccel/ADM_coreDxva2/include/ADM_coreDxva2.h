0313d233a6a02574994c6f47ccb1b83f93365876
mean <fixounet@free.fr>
2016-10-23 15:41:41 +0200

    [DXVA2] Add skeleton

---
+++ avidemux_core/ADM_coreVideoCodec/ADM_hwAccel/ADM_coreDxva2/include/ADM_coreDxva2.h
@@ -0,0 +1,27 @@
+/***************************************************************************
+    \file             : ADM_coreDxva2.cpp
+    \brief            : Wrapper around dxva functions
+    \author           : (C) 2016 by mean fixounet@free.fr
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#pragma once
+#include "ADM_windowInfo.h"
+/**
+    \class admVdpau
+*/
+class admDxva2
+{
+public:
+        static bool init(GUI_WindowInfo *x);
+        static bool isOperationnal(void);
+        static bool cleanup(void);
+};
---
+++ avidemux_core/ADM_coreVideoCodec/ADM_hwAccel/ADM_coreDxva2/src/ADM_coreDxva2.cpp
@@ -0,0 +1,41 @@
+/***************************************************************************
+    \file             : ADM_coreDXVA2.cpp
+    \brief            : Wrapper around DXVA2 functions
+    \author           : (C) 2016 by mean fixounet@free.fr
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include "ADM_default.h"
+#include "../include/ADM_coreDxva2.h"
+#ifdef USE_DXVA2
+#include "../include/ADM_coreDxva2Internal.h"
+#include "ADM_dynamicLoading.h"
+#include <map>
+static bool                  coreDxva2Working=false;
+bool admDxva2::init(GUI_WindowInfo *x)
+{
+          return false;
+}
+  
+/**
+    \fn isOperationnal
+*/
+bool admDxva2::isOperationnal(void)
+{
+    ADM_warning("This binary has no DXVA2 support\n");
+    return coreDxva2Working;
+}
+bool admDxva2::cleanup(void)
+{
+    ADM_warning("This binary has no DXVA2 support\n");
+    return true;
+}
+#endif
+// EOF
