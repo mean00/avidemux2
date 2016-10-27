fde5fbb2ff88b4e9a86e153909383b4e3a80e6bb
mean <fixounet@free.fr>
2016-10-27 07:31:54 +0200

    [dxva2] missing probe prototype

--- avidemux/common/adm_main.h
+++ avidemux/common/adm_main.h
@@ -14,6 +14,7 @@ extern uint8_t GUI_close(void);
 static bool setPrefsDefault(void);
 extern bool  vdpauProbe(void);
 extern bool  libvaProbe(void);
+extern bool  dxva2Probe(void);
 
 extern void registerVideoFilters( void );
 extern void filterCleanUp( void );
