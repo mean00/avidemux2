162189a9888cc11ad7ebdcf00b0375f46e8c716f
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2016-10-31 18:44:56 +0100

    [x264/Qt, x265/Qt] Add comment to clarify why we substitute an empty string with 'none'

--- avidemux_plugins/ADM_videoEncoder/x264/qt4/Q_x264.cpp
+++ avidemux_plugins/ADM_videoEncoder/x264/qt4/Q_x264.cpp
@@ -172,6 +172,8 @@ x264Dialog::x264Dialog(QWidget *parent, void *param) : QDialog(parent)
         for(int i=0;i<NB_TUNE;i++)
         {
             const char* _tn=listOfTunings[i];
+            // we pass an empty string to the encoder in order to disable tuning,
+            // but want to show a descriptive label to the user
             if(_tn=="")
                 _tn=QT_TRANSLATE_NOOP("x264","none");
             tunings->addItem(QString(_tn));
