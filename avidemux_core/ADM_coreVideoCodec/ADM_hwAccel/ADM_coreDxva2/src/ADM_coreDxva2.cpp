133e90f98f0b97566a8b715e35a941d8cdb951aa
mean <fixounet@free.fr>
2016-11-01 12:05:13 +0100

    [Dxva2] look up supported codec

--- avidemux_core/ADM_coreVideoCodec/ADM_hwAccel/ADM_coreDxva2/src/ADM_coreDxva2.cpp
+++ avidemux_core/ADM_coreVideoCodec/ADM_hwAccel/ADM_coreDxva2/src/ADM_coreDxva2.cpp
@@ -17,7 +17,10 @@
  ***************************************************************************/
 #include "ADM_default.h"
 #include "../include/ADM_coreDxva2.h"
-
+extern "C" 
+{
+ #include "libavcodec/avcodec.h"
+}
 #ifdef USE_DXVA2
 #include "../include/ADM_coreDxva2Internal.h"
 #include "ADM_dynamicLoading.h"
@@ -53,6 +56,122 @@ static HANDLE                       deviceHandle=INVALID_HANDLE_VALUE;
     
 #include <initguid.h>
 DEFINE_GUID(IID_IDirectXVideoDecoderService, 0xfc51a551,0xd5e7,0x11d9,0xaf,0x55,0x00,0x05,0x4e,0x43,0xff,0x02);
+
+DEFINE_GUID(DXVA2_ModeMPEG2_VLD,      0xee27417f, 0x5e28,0x4e65,0xbe,0xea,0x1d,0x26,0xb5,0x08,0xad,0xc9);
+DEFINE_GUID(DXVA2_ModeMPEG2and1_VLD,  0x86695f12, 0x340e,0x4f04,0x9f,0xd3,0x92,0x53,0xdd,0x32,0x74,0x60);
+DEFINE_GUID(DXVA2_ModeH264_E,         0x1b81be68, 0xa0c7,0x11d3,0xb9,0x84,0x00,0xc0,0x4f,0x2e,0x73,0xc5);
+DEFINE_GUID(DXVA2_ModeH264_F,         0x1b81be69, 0xa0c7,0x11d3,0xb9,0x84,0x00,0xc0,0x4f,0x2e,0x73,0xc5);
+DEFINE_GUID(DXVADDI_Intel_ModeH264_E, 0x604F8E68, 0x4951,0x4C54,0x88,0xFE,0xAB,0xD2,0x5C,0x15,0xB3,0xD6);
+DEFINE_GUID(DXVA2_ModeVC1_D,          0x1b81beA3, 0xa0c7,0x11d3,0xb9,0x84,0x00,0xc0,0x4f,0x2e,0x73,0xc5);
+DEFINE_GUID(DXVA2_ModeVC1_D2010,      0x1b81beA4, 0xa0c7,0x11d3,0xb9,0x84,0x00,0xc0,0x4f,0x2e,0x73,0xc5);
+DEFINE_GUID(DXVA2_ModeHEVC_VLD_Main,  0x5b11d51b, 0x2f4c,0x4452,0xbc,0xc3,0x09,0xf2,0xa1,0x16,0x0c,0xc0);
+DEFINE_GUID(DXVA2_ModeVP9_VLD_Profile0, 0x463707f8, 0xa1d0,0x4585,0x87,0x6d,0x83,0xaa,0x6d,0x60,0xb8,0x9e);
+DEFINE_GUID(DXVA2_NoEncrypt,          0x1b81beD0, 0xa0c7,0x11d3,0xb9,0x84,0x00,0xc0,0x4f,0x2e,0x73,0xc5);
+DEFINE_GUID(GUID_NULL,                0x00000000, 0x0000,0x0000,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00);
+/**
+ */
+typedef struct dxva2_mode 
+{
+  const GUID     *guid;
+  enum AVCodecID codec;
+} dxva2_mode;
+/**
+ */
+static const dxva2_mode dxva2_modes[] = 
+{
+    /* MPEG-2 */
+    { &DXVA2_ModeMPEG2_VLD,      AV_CODEC_ID_MPEG2VIDEO },
+    { &DXVA2_ModeMPEG2and1_VLD,  AV_CODEC_ID_MPEG2VIDEO },
+
+    /* H.264 */
+    { &DXVA2_ModeH264_F,         AV_CODEC_ID_H264 },
+    { &DXVA2_ModeH264_E,         AV_CODEC_ID_H264 },
+    /* Intel specific H.264 mode */
+    { &DXVADDI_Intel_ModeH264_E, AV_CODEC_ID_H264 },
+
+    /* VC-1 / WMV3 */
+    { &DXVA2_ModeVC1_D2010,      AV_CODEC_ID_VC1  },
+    { &DXVA2_ModeVC1_D2010,      AV_CODEC_ID_WMV3 },
+    { &DXVA2_ModeVC1_D,          AV_CODEC_ID_VC1  },
+    { &DXVA2_ModeVC1_D,          AV_CODEC_ID_WMV3 },
+
+    /* HEVC/H.265 */
+    { &DXVA2_ModeHEVC_VLD_Main,  AV_CODEC_ID_HEVC },
+
+    /* VP8/9 */
+    { &DXVA2_ModeVP9_VLD_Profile0, AV_CODEC_ID_VP9 },
+
+    { NULL,                      (AVCodecID)0 },
+};
+typedef struct 
+{
+     enum AVCodecID  codec;
+     bool            enabled;
+     DXVA2_VideoDesc desc;
+           GUID      device_guid;
+     const GUID      guid;
+}Dxv2SupportMap;
+
+static Dxv2SupportMap dxva2H265={AV_CODEC_ID_HEVC,false};
+static Dxv2SupportMap dxva2H264={AV_CODEC_ID_H264,false};
+
+/**
+ * \fn lookupCodec
+ */
+static bool lookupCodec(const char *codecName,Dxv2SupportMap *context,unsigned int guid_count,GUID *guid_list)
+{
+    HRESULT hr;
+    int surface_alignment;
+    D3DFORMAT target_format = (D3DFORMAT) 0;
+
+    for (int i = 0; dxva2_modes[i].guid; i++) 
+    {
+        D3DFORMAT *target_list = NULL;
+        unsigned target_count = 0;
+        const dxva2_mode *mode = &dxva2_modes[i];
+        if (mode->codec != context->codec)
+            continue;
+        int j;
+        for (j = 0; j < guid_count; j++) 
+        {
+            if (IsEqualGUID( *(mode->guid), guid_list[j]))
+                break;
+        }
+        if (j == guid_count)
+            continue;
+
+        hr = IDirectXVideoDecoderService_GetDecoderRenderTargets(decoder_service,*( mode->guid), &target_count, &target_list);
+        if (FAILED(hr)) 
+        {
+            continue;
+        }
+        for (j = 0; j < target_count; j++) 
+        {
+            const D3DFORMAT format = target_list[j];
+            if (format == MKTAG('N','V','1','2')) 
+            {
+                target_format = format;
+                break;
+            }
+        }
+        CoTaskMemFree(target_list);
+        if (target_format) 
+        {
+            context->device_guid = *(mode->guid);
+            break;
+        }
+    }
+    
+
+    if (IsEqualGUID(context->device_guid, GUID_NULL))
+    {
+        ADM_info("No decoder device for codec %s found\n",codecName);
+        return false;
+    }
+    ADM_info("DXVA2 support for  %s found\n",codecName);
+    context->enabled=true;
+    return true;
+}
 
 /**
  * \fn init
@@ -142,6 +261,24 @@ bool admDxva2::init(GUI_WindowInfo *x)
 
     coreDxva2Working=true;
     ADM_info("Dxva2 core successfully initialized\n");
+    // Look up what is supported
+    {
+        unsigned guid_count = 0, i, j;
+        GUID device_guid = GUID_NULL;
+        GUID *guid_list = NULL;
+        HRESULT hr;
+        int surface_alignment;
+
+        hr = IDirectXVideoDecoderService_GetDecoderDeviceGuids(decoder_service, &guid_count, &guid_list);
+        if (FAILED(hr)) {
+            ADM_warning("Failed to retrieve decoder device GUIDs\n");
+            return true;
+        }
+        lookupCodec("H264",&dxva2H264,guid_count,guid_list);
+        lookupCodec("H265",&dxva2H265,guid_count,guid_list);
+        CoTaskMemFree(guid_list);
+    }
+    ADM_info("Scanning supported format done\n");
     return true;
 failInit:
     // cleanup
@@ -205,8 +342,6 @@ bool admDxva2_exitCleanup()
     ADM_info("Dxva2 cleanup end\n");
     return true;
 }
-
-
 
 
 #endif
