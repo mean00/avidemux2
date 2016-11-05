3f30c58364b5d66202d1861be5c91a01bcbc4a92
mean <fixounet@free.fr>
2016-11-05 18:27:48 +0100

    [Dxva2] Proper management of dont copy for dxv2 (slow) + fully copy image (3)

--- avidemux/common/ADM_videoCodec/private_inc/ADM_codecDxva2.h
+++ avidemux/common/ADM_videoCodec/private_inc/ADM_codecDxva2.h
@@ -53,7 +53,7 @@ public:
                     int         getNumSurfaces() {return num_surfaces;}
 
     virtual const   char        *getName(void)        {return "DXVA2";}
-    virtual         bool        dontcopy() {return true;} // copy the frame for the moment
+    virtual         bool        dontcopy() {return false;} // copy the frame for the moment
                     
 public:
             // public API
