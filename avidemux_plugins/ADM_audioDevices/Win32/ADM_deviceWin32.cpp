6b28b9f16e93b75617372fb2c67a99b176fd4e62
mean <fixounet@free.fr>
2016-11-14 17:38:41 +0100

    [win32/audio] simplify + dont do busy looping

--- avidemux_plugins/ADM_audioDevices/Win32/ADM_deviceWin32.cpp
+++ avidemux_plugins/ADM_audioDevices/Win32/ADM_deviceWin32.cpp
@@ -20,15 +20,7 @@
 
 #define aprintf(...) {}
 
-#define NB_BUCKET 64
-
-static uint32_t bucketSize;
-static HWAVEOUT myDevice;
-static MMRESULT myError;
-
-static void handleMM(MMRESULT err);
-
-WAVEHDR waveHdr[NB_BUCKET];	
+  
 ADM_DECLARE_AUDIODEVICE(Win32,win32AudioDevice,1,0,0,"Win32 audio device (c) mean");
 /**
     \fn ctor
@@ -36,8 +28,8 @@ ADM_DECLARE_AUDIODEVICE(Win32,win32AudioDevice,1,0,0,"Win32 audio device (c) mea
 
 win32AudioDevice::win32AudioDevice(void) 
 {
-	printf("[Win32] Creating audio device\n");
-	_inUse=0;
+    ADM_info("[Win32] Creating audio device\n");
+    _inUse=0;
 }
 /**
     \fn localStop
@@ -45,138 +37,153 @@ win32AudioDevice::win32AudioDevice(void)
 
 bool win32AudioDevice::localStop(void) 
 {
-	if (!_inUse)
-		return false;
+    if (!_inUse)
+        return false;
 
-	printf("[Win32] Closing audio\n");
+    ADM_info("[Win32] Closing audio\n");
 
-	waveOutReset(myDevice);		
+    waveOutReset(myDevice);        
 
-	for (uint32_t i = 0; i < NB_BUCKET; i++)
-		waveOutUnprepareHeader(myDevice, &waveHdr[i], sizeof(WAVEHDR));
+    for (uint32_t i = 0; i < NB_BUCKET; i++)
+        waveOutUnprepareHeader(myDevice, &waveHdr[i], sizeof(WAVEHDR));
 
-	myError = waveOutClose(myDevice);
+    myError = waveOutClose(myDevice);
 
-	if (myError != MMSYSERR_NOERROR)
-	{
-		printf("[Win32] Close failed %d\n", myError);
-		handleMM(myError);
-		return 0;
-	}
+    if (myError != MMSYSERR_NOERROR)
+    {
+        ADM_info("[Win32] Close failed %d\n", myError);
+        handleMM(myError);
+        return 0;
+    }
 
-	for (uint32_t i = 0; i < NB_BUCKET; i++)
-		delete[] waveHdr[i].lpData;
+    for (uint32_t i = 0; i < NB_BUCKET; i++)
+        delete[] waveHdr[i].lpData;
 
-	_inUse=0;
-	myDevice = NULL;
+    _inUse=0;
+    myDevice = NULL;
 
-	return true;
+    return true;
 }
 /**
     \fn localInit
 */
 bool win32AudioDevice::localInit(void) 
 {
-	printf("[Win32] Opening Audio, channels=%u freq=%u\n",_channels, _frequency);
+    ADM_info("[Win32] Opening Audio, channels=%u freq=%u\n",_channels, _frequency);
 
-	if (_inUse) 
-	{
-		printf("[Win32] Already running?\n");
-		return false;
-	}
+    if (_inUse) 
+    {
+        ADM_warning("[Win32] Already running?\n");
+        return false;
+    }
 
-	_inUse = 1;
-	
-	bucketSize = (_channels * _frequency*2*4)/100; // 40 ms bucket * 64 => 3 sec buffer
+    _inUse = 1;
+    
+    bucketSize = (_channels * _frequency*2*4)/100; // 40 ms bucket * 64 => 3 sec buffer
 
-	WAVEFORMATEX wav;
+    WAVEFORMATEX wav;
 
-	memset(&wav, 0, sizeof(WAVEFORMATEX));
+    memset(&wav, 0, sizeof(WAVEFORMATEX));
 
-	wav.wFormatTag = WAVE_FORMAT_PCM;
-	wav.nSamplesPerSec = _frequency;
-	wav.nChannels = _channels;
-	wav.nBlockAlign = 2 * _channels;
-	wav.nAvgBytesPerSec = 2 * _channels * _frequency;
-	wav.wBitsPerSample = 16;
+    wav.wFormatTag = WAVE_FORMAT_PCM;
+    wav.nSamplesPerSec = _frequency;
+    wav.nChannels = _channels;
+    wav.nBlockAlign = 2 * _channels;
+    wav.nAvgBytesPerSec = 2 * _channels * _frequency;
+    wav.wBitsPerSample = 16;
 
-	myError = waveOutOpen(&myDevice, WAVE_MAPPER, &wav, NULL, NULL, CALLBACK_NULL);
+    myError = waveOutOpen(&myDevice, WAVE_MAPPER, &wav, NULL, NULL, CALLBACK_NULL);
 
-	if (MMSYSERR_NOERROR != myError)
-	{
-		printf("[Win32] waveOutOpen failed\n");
-		handleMM(myError);
-		return 0;
-	}
+    if (MMSYSERR_NOERROR != myError)
+    {
+        ADM_warning("[Win32] waveOutOpen failed\n");
+        handleMM(myError);
+        return 0;
+    }
 
-	for (uint32_t i = 0; i < NB_BUCKET; i++)
-	{
-		memset(&waveHdr[i], 0, sizeof(WAVEHDR));
+    for (uint32_t i = 0; i < NB_BUCKET; i++)
+    {
+        memset(&waveHdr[i], 0, sizeof(WAVEHDR));
 
-		waveHdr[i].dwBufferLength = bucketSize;
-		waveHdr[i].lpData = (char*)new uint8_t[bucketSize];
+        waveHdr[i].dwBufferLength = bucketSize;
+        waveHdr[i].lpData = (char*)new uint8_t[bucketSize];
 
-		if (waveOutPrepareHeader(myDevice, &waveHdr[i], sizeof(WAVEHDR)) != MMSYSERR_NOERROR)
-			printf("[Win32] waveOutPrepareHeader error\n");
+        if (waveOutPrepareHeader(myDevice, &waveHdr[i], sizeof(WAVEHDR)) != MMSYSERR_NOERROR)
+            ADM_warning("[Win32] waveOutPrepareHeader error\n");
 
-		waveHdr[i].dwBufferLength = 0;
-		waveHdr[i].dwFlags |= WHDR_DONE;
-	}
+        waveHdr[i].dwBufferLength = 0;
+        waveHdr[i].dwFlags |= WHDR_DONE;
+    }
 
-	return 1;
+    return 1;
 }
 /**
     \fn setVolume
 */
 uint8_t  win32AudioDevice::setVolume(int volume) 
 {
-	uint32_t value;
+    uint32_t value;
 
-	value = (0xffff * volume) / 100;
-	value = value + (value << 16);
+    value = (0xffff * volume) / 100;
+    value = value + (value << 16);
 
-	waveOutSetVolume(myDevice, value);
+    waveOutSetVolume(myDevice, value);
 
-	return 1;
+    return 1;
 }
 /**
     \fn sendData
 */
 
+int win32AudioDevice::findFreeBucket()
+{
+    for (uint32_t i = 0; i < NB_BUCKET; i++)
+    {
+        if (waveHdr[i].dwFlags & WHDR_DONE) // Free ?
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
 void win32AudioDevice::sendData(void)
 {
-	uint8_t success = 0;
+    uint8_t success = 0;
     uint32_t len=wrIndex-rdIndex;
-
-	for (uint32_t i = 0; i < NB_BUCKET; i++)
-	{
-		if (waveHdr[i].dwFlags & WHDR_DONE)
-		{
-			waveHdr[i].dwFlags &= ~WHDR_DONE;
-            mutex.lock();
-			if (len > bucketSize)
-				waveHdr[i].dwBufferLength = bucketSize;
-			else
-				waveHdr[i].dwBufferLength = len;
-
-			memcpy(waveHdr[i].lpData, audioBuffer.at(rdIndex), waveHdr[i].dwBufferLength);
-			rdIndex += waveHdr[i].dwBufferLength;
-			len -= waveHdr[i].dwBufferLength;
-            mutex.unlock();
-            MMRESULT er=waveOutWrite(myDevice, &waveHdr[i], sizeof(WAVEHDR));
-			if (er == MMSYSERR_NOERROR)
-				success = 1;
-			else
-            {
-                handleMM(er);
-				break;
-            }
-		}
-
-		if (len == 0)
-			break;
-	}
-	return ;
+    MMRESULT er;
+    while(len)
+    {
+         if (!_inUse)
+             return;
+        int bucket=findFreeBucket();
+        if(bucket==-1)
+        {
+            ADM_usleep(10); // Wait for a bucket , it failed, wait a bit
+            continue;
+        }
+        WAVEHDR *b=&(waveHdr[bucket]);
+        b->dwFlags &=~WHDR_DONE; // mark as used
+        mutex.lock();
+            if (len > bucketSize)
+                  b->dwBufferLength = bucketSize;
+            else
+                  b->dwBufferLength = len;
+            memcpy(b->lpData, audioBuffer.at(rdIndex), b->dwBufferLength);                    
+        mutex.unlock();
+        er=waveOutWrite(myDevice, b, sizeof(WAVEHDR));
+        if (er != MMSYSERR_NOERROR)
+        {
+            handleMM(er);
+            b->dwFlags|=WHDR_DONE;
+            ADM_warning("Audio out : Error !\n");
+            break;
+        }
+        // consume data
+        rdIndex += b->dwBufferLength;
+        len-= b->dwBufferLength ;
+    }
 }
 
 /**
@@ -206,13 +213,13 @@ const CHANNEL_TYPE *win32AudioDevice::getWantedChannelMapping(uint32_t channels)
 */
 void handleMM(MMRESULT err)
 {
-#define ERMM(x) if(err==x) printf("[Win32] "#x"\n");
+#define ERMM(x) if(err==x)  ADM_info("[Win32] "#x"\n");
 
-	ERMM(MMSYSERR_ALLOCATED);
-	ERMM(MMSYSERR_BADDEVICEID);
-	ERMM(MMSYSERR_NODRIVER);
-	ERMM(WAVERR_BADFORMAT);
-	ERMM(WAVERR_SYNC);
+    ERMM(MMSYSERR_ALLOCATED);
+    ERMM(MMSYSERR_BADDEVICEID);
+    ERMM(MMSYSERR_NODRIVER);
+    ERMM(WAVERR_BADFORMAT);
+    ERMM(WAVERR_SYNC);
     ERMM(WAVERR_STILLPLAYING);
 }
 // EOF
--- avidemux_plugins/ADM_audioDevices/Win32/ADM_deviceWin32.h
+++ avidemux_plugins/ADM_audioDevices/Win32/ADM_deviceWin32.h
@@ -9,22 +9,35 @@
 // Copyright: See COPYING file that comes with this distribution
 //
 //
+#pragma once
+#define NB_BUCKET 64
 
-#ifndef ADM_DEVICEWIN32_H
-#define ADM_DEVICEWIN32_H
+
 class win32AudioDevice : public audioDeviceThreaded
 {
 protected:
-	uint8_t	    _inUse;
-	virtual     bool     localInit(void);
+                uint8_t  _inUse;
+    virtual     bool     localInit(void);
     virtual     bool     localStop(void);
     virtual     void     sendData(void); 
     virtual     const CHANNEL_TYPE *getWantedChannelMapping(uint32_t channels);
 public:
-    uint8_t setVolume(int volume);
-	win32AudioDevice(void);
+                uint8_t setVolume(int volume);
+                        win32AudioDevice(void);
+    
+protected:    
+                uint32_t bucketSize;
+                HWAVEOUT myDevice;
+                MMRESULT myError;
+                WAVEHDR  waveHdr[NB_BUCKET];  
+protected:
+                 void handleMM(MMRESULT err);
+    
+protected:
+                int     findFreeBucket();
+                          
 
 };
-#endif
+
 
 
