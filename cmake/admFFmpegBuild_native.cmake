6df6ec6d4e0c1d951df3b1748f5efd85efcbb1ef
mean <fixounet@free.fr>
2016-11-20 07:34:37 +0100

    [core/ffmpeg] fix patching

--- cmake/admFFmpegBuild_native.cmake
+++ cmake/admFFmpegBuild_native.cmake
@@ -15,20 +15,20 @@ ENDIF(USE_NVENC)
 #@@
 ADM_FF_PATCH_IF_NEEDED()
 
-
 MESSAGE(STATUS "Patching Linux common.mak")
 patch_file("${FFMPEG_SOURCE_DIR}" "${FFMPEG_PATCH_DIR}/common.mak.diff")
+
 
 xadd(--enable-pthreads)
 
 if (USE_VDPAU)
-	xadd(--enable-vdpau)
-	set(FFMPEG_DECODERS ${FFMPEG_DECODERS} h264_vdpau  vc1_vdpau  mpeg1_vdpau  mpeg_vdpau  wmv3_vdpau)
+    xadd(--enable-vdpau)
+    set(FFMPEG_DECODERS ${FFMPEG_DECODERS} h264_vdpau  vc1_vdpau  mpeg1_vdpau  mpeg_vdpau  wmv3_vdpau)
 endif (USE_VDPAU)
 
 if (USE_LIBVA)
-	xadd(--enable-vaapi)
-	set(FFMPEG_DECODERS ${FFMPEG_DECODERS} h264_vaapi hevc_vaapi)
+    xadd(--enable-vaapi)
+    set(FFMPEG_DECODERS ${FFMPEG_DECODERS} h264_vaapi hevc_vaapi)
 endif (USE_LIBVA)
 
 #@@
@@ -44,17 +44,25 @@ ADM_FF_SET_EXTRA_FLAGS()
 #@@
 ADM_FF_BUILD_UNIX_STYLE()
 #
+
+IF(APPLE)
+        MESSAGE(STATUS "Patching config.mak - mac")
+        patch_file("${FFMPEG_BINARY_DIR}" "${FFMPEG_PATCH_DIR}/config.mak.mac.diff")
+ELSE(APPLE)
+        MESSAGE(STATUS "Patching Linux config.mak")
+        patch_file("${FFMPEG_BINARY_DIR}" "${FFMPEG_PATCH_DIR}/config.mak.diff")
+ENDIF(APPLE)
 #
 MACRO(FF_ADD_SUBLIB lib)
-        	add_custom_command(
-				   OUTPUT       "${lib}"
-           DEPENDS 	libavutil_dummy
-				   COMMAND ${BASH_EXECUTABLE} -c echo "placeHolder")
+            add_custom_command(
+                   OUTPUT       "${lib}"
+           DEPENDS     libavutil_dummy
+                   COMMAND ${BASH_EXECUTABLE} -c echo "placeHolder")
 
 ENDMACRO(FF_ADD_SUBLIB lib)
 
-FF_ADD_SUBLIB(     	"${FFMPEG_BINARY_DIR}/libavutil/${LIBAVUTIL_LIB}"       )
-FF_ADD_SUBLIB(     	"${FFMPEG_BINARY_DIR}/libavcodec/${LIBAVCODEC_LIB}"       )
+FF_ADD_SUBLIB(         "${FFMPEG_BINARY_DIR}/libavutil/${LIBAVUTIL_LIB}"       )
+FF_ADD_SUBLIB(         "${FFMPEG_BINARY_DIR}/libavcodec/${LIBAVCODEC_LIB}"       )
 FF_ADD_SUBLIB(      "${FFMPEG_BINARY_DIR}/libavformat/${LIBAVFORMAT_LIB}"   )
 FF_ADD_SUBLIB(      "${FFMPEG_BINARY_DIR}/libpostproc/${LIBPOSTPROC_LIB}"   )
 FF_ADD_SUBLIB(      "${FFMPEG_BINARY_DIR}/libswscale/${LIBSWSCALE_LIB}"     )
@@ -62,7 +70,7 @@ FF_ADD_SUBLIB(      "${FFMPEG_BINARY_DIR}/libswscale/${LIBSWSCALE_LIB}"     )
 ADM_FF_INSTALL_LIBS_AND_HEADERS()
 
 IF(USE_LIBVA)
-				INSTALL(FILES "${FFMPEG_SOURCE_DIR}/libavcodec/vaapi.h" DESTINATION "${AVIDEMUX_INCLUDE_DIR}/avidemux/2.6/libavcodec" COMPONENT dev)
-				INSTALL(FILES "${FFMPEG_SOURCE_DIR}/libavcodec/vaapi_internal.h" DESTINATION "${AVIDEMUX_INCLUDE_DIR}/avidemux/2.6/libavcodec" COMPONENT dev)
+                INSTALL(FILES "${FFMPEG_SOURCE_DIR}/libavcodec/vaapi.h" DESTINATION "${AVIDEMUX_INCLUDE_DIR}/avidemux/2.6/libavcodec" COMPONENT dev)
+                INSTALL(FILES "${FFMPEG_SOURCE_DIR}/libavcodec/vaapi_internal.h" DESTINATION "${AVIDEMUX_INCLUDE_DIR}/avidemux/2.6/libavcodec" COMPONENT dev)
 ENDIF(USE_LIBVA)
 # EOF
