a7416223398e2eefca0e35c8487d8df8ae8852c2
mean <fixounet@free.fr>
2016-11-21 18:40:56 +0100

    [win32] Remove duplicate functions

--- avidemux_core/ADM_core/src/ADM_folder_mingw.cpp
+++ avidemux_core/ADM_core/src/ADM_folder_mingw.cpp
@@ -35,19 +35,6 @@ static char *ADM_systemPluginSettings=NULL;
 #undef fwrite
 #undef fopen
 #undef fclose
-
-/**
-    \fn ADM_getAutoDir
-    \brief  Get the  directory where auto script are stored. No need to free the string.
-******************************************************/
-const char *ADM_getAutoDir(void)
-{
-    if (ADM_autodir )
-        return ADM_autodir;
-    const char *startDir=ADM_RELATIVE_LIB_DIR;
-    ADM_autodir = ADM_getInstallRelativePath(startDir, ADM_PLUGIN_DIR, "autoScripts");
-	return ADM_autodir;
-}
 /**
     \fn ADM_getPluginSettingsDir
     \brief Get the folder containing the plugin settings (presets etc..)
@@ -100,91 +87,6 @@ char *ADM_getInstallRelativePath(const char *base1, const char *base2, const cha
 	return relativePath;
 }
 
-/*
-      Get the root directory for .avidemux stuff
-******************************************************/
-const char *ADM_getBaseDir(void)
-{
-	return ADM_basedir;
-}
-
-/**
- * \fn ADM_initBaseDir
- * \brief ADM_initBaseDir
- */
-void ADM_initBaseDir(int argc, char *argv[])
-{
-	char *home = NULL;
-
-    bool portableMode=isPortableMode(argc,argv);
-	// Get the base directory
-
-    if (portableMode)
-    {
-        // Portable mode...
-        home = ADM_getInstallRelativePath(NULL, NULL, NULL);
-    }
-	else
-    {
-        wchar_t wcHome[MAX_PATH];
-
-        if (SHGetFolderPathW(NULL, CSIDL_APPDATA, NULL, 0, wcHome) == S_OK)
-        {
-            int len = wideCharStringToUtf8(wcHome, -1, NULL);
-            home = new char[len];
-
-            wideCharStringToUtf8(wcHome, -1, home);
-        }
-        else
-        {
-            printf("Oops: can't determine the Application Data folder.");
-            home=new char[10];
-            strcpy(home,"c:\\");
-        }
-    }
-
-
-	// Try to open the .avidemux directory
-
-	if (home)
-	{
-		strcpy(ADM_basedir, home);
-		AddSeparator(ADM_basedir);
-
-
-		const char *ADM_DIR_NAME;
-
-		if (portableMode)
-			ADM_DIR_NAME = "settings";
-		else
-			ADM_DIR_NAME = "avidemux";
-
-		strcat(ADM_basedir, ADM_DIR_NAME);
-		strcat(ADM_basedir, ADM_SEPARATOR);
-
-		delete [] home;
-
-		if (ADM_mkdir(ADM_basedir))
-		{
-			printf("Using %s as base directory for prefs, jobs, etc.\n", ADM_basedir);
-		}
-		else
-		{
-			ADM_error("Oops: cannot create the .avidemux directoryi (%s)\n", ADM_basedir);
-		}
-	}
-}
-/**
- * \fn ADM_getI8NDir
- */
-const std::string ADM_getI8NDir(const std::string &flavor)
-{
-    //
-    char *home = ADM_getInstallRelativePath(flavor.c_str(), "i18n", NULL);
-    std::string r=std::string(home);
-    delete [] home;
-    return r;
-}
 //--- 8< 8< 8<
 
 #	include <io.h>
