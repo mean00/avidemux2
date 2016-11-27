d4e9def3e732bbf671bbbc6e81e66478e9fa34e3
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2016-11-27 14:50:07 +0100

    [Build] Re-add forgotten libva-dev to createDebFromSourceUbuntu.bash script

--- createDebFromSourceUbuntu.bash
+++ createDebFromSourceUbuntu.bash
@@ -3,7 +3,7 @@ echo "This will install all the necessary packages and build avidemux, it will t
 echo "You will be asked to enter your password because installing build dependencies requires root permissions"
 # gcc, g++ and make get installed as dependencies of build-essential
 sudo apt-get update && sudo apt-get install build-essential cmake pkg-config yasm \
-libsqlite3-dev libfontconfig1-dev libfribidi-dev libxv-dev libvdpau-dev libpulse-dev \
+libsqlite3-dev libfontconfig1-dev libfribidi-dev libxv-dev libvdpau-dev libva-dev libpulse-dev \
 qttools5-dev-tools qtbase5-dev libqt5opengl5-dev \
 libpng12-dev libaften-dev libmp3lame-dev libx264-dev libfaad-dev libfaac-dev libopus-dev libvorbis-dev libogg-dev libdca-dev \
  || { echo "The installation at least of some of the build dependencies failed. Aborting." && exit 2; }
