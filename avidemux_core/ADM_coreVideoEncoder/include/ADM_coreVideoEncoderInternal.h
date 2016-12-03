65ee389cb6d4e4a86e006034a96ee9e5a6e11ad4
mean <fixounet@free.fr>
2016-12-03 18:12:14 +0100

    [build] Fix video encoder plugin for vs

--- avidemux_core/ADM_coreVideoEncoder/include/ADM_coreVideoEncoderInternal.h
+++ avidemux_core/ADM_coreVideoEncoder/include/ADM_coreVideoEncoderInternal.h
@@ -88,8 +88,8 @@ extern ADM_COREVIDEOENCODER6_EXPORT BVector <ADM_videoEncoder6 *> ListOfEncoders
 // Macros to declare audio encoder
 /**************************************************************************/
 #define ADM_DECLARE_VIDEO_ENCODER_PREAMBLE(Class) \
-static bool getConfigurationData (CONFcouple **c); \
-static bool setConfigurationData (CONFcouple *c,bool full=true);\
+static bool xgetConfigurationData (CONFcouple **c); \
+static bool xsetConfigurationData (CONFcouple *c,bool full=true);\
 \
 static ADM_coreVideoEncoder * create (ADM_coreVideoFilter * head,bool globalHeader) \
 { \
@@ -113,20 +113,20 @@ static ADM_videoEncoderDesc encoderDesc={\
     configure,\
     setProfile,\
     getProfile, \
-    getConfigurationData,\
-    setConfigurationData,\
+    xgetConfigurationData,\
+    xsetConfigurationData,\
     resetConfigurationData,\
     probe,\
     uiType,\
     maj,minV,patch,\
     NULL\
 };\
-ADM_PLUGIN_EXPORT bool getConfigurationData (CONFcouple **c)\
+bool xgetConfigurationData (CONFcouple **c)\
 {\
          if(confTemplate==NULL) {*c=NULL;return true;} \
          return ADM_paramSave(c,confTemplate,confVar); \
 }\
-ADM_PLUGIN_EXPORT bool setConfigurationData (CONFcouple *c,bool full)\
+bool xsetConfigurationData (CONFcouple *c,bool full)\
 {\
 	if(full) return ADM_paramLoad(c,confTemplate,confVar); \
 	return ADM_paramLoadPartial(c,confTemplate,confVar); \
