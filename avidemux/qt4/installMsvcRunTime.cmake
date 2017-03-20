a546b1bf7e4a8a4f50d566e98b8dba6ba01082a9
mean <fixounet@free.fr>
2017-03-20 20:52:37 +0100

    [msvc] Install msvcRuntime and Qt runtime (mostly complete)

---
+++ avidemux/qt4/installMsvcRunTime.cmake
@@ -0,0 +1,24 @@
+#
+# Runtime
+#
+include(InstallRequiredSystemLibraries)
+MESSAGE(STATUS "MSVC Runtime files = ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS}")
+ADM_INSTALL_LIB_FILES( ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS})
+#
+# Qt5
+#
+macro(COPY_QT5_DLL module)
+    #get_target_property( Qt5_${module}Location ${Qt5${module}_LIBRARIES} LOCATION)
+    get_target_property(Qt5_${module}Location Qt5::${module} LOCATION) 
+    MESSAGE(STATUS "Adding ${Qt5_${module}Location} as lib to install")
+    ADM_INSTALL_LIB_FILES( ${Qt5_${module}Location})
+endmacro(COPY_QT5_DLL module)
+
+COPY_QT5_DLL(Core)
+COPY_QT5_DLL(Gui)
+COPY_QT5_DLL(OpenGL)
+COPY_QT5_DLL(Widgets)
+COPY_QT5_DLL(Network)
+COPY_QT5_DLL(WinExtras)
+
+# Need the platform one too => TODO
