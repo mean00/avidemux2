7596b48a58f354afb10344bebc9dd7710267ec6d
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2017-03-27 15:30:59 +0200

    [build] Fix deb creation on Ubuntu 16.10, add alsa and xvid to deps in the simple build script

--- avidemux/qt4/qt4PackageDebian.cmake
+++ avidemux/qt4/qt4PackageDebian.cmake
@@ -5,7 +5,7 @@ set(CPACK_COMPONENTS_ALL             runtime dev )
 
 SET(CPACK_DEBIAN_PACKAGE_NAME        "avidemux3-${QT_EXTENSION}")
 SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Simple video editor,main program qt4 version ")
-SET(DEPS "libc6 (>=2.4),libstdc++6 (>=4.2.1),libx11-6,  libxv1, zlib1g (>=1:1.1.4), libglib2.0-0, libpng12-0, avidemux3-core-runtime (>=${AVIDEMUX_VERSION})")
+SET(DEPS "libc6 (>=2.4), libstdc++6 (>=4.2.1), libx11-6, zlib1g (>=1:1.1.4), libglib2.0-0, libpng12-0 | libpng16-16, avidemux3-core-runtime (>=${AVIDEMUX_VERSION})")
 # QT5
 SET(DEPS "${DEPS}, libqt5opengl5, libqt5widgets5, libqt5gui5, libqt5core5a")
 IF(USE_XV)
