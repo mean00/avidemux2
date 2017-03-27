3b75855eea8a725958b9ec861f8198de903c60f7
mean <fixounet@free.fr>
2017-03-27 18:25:26 +0200

    [mkv/demux] dont stretch max dts/pts to the max when there is no valid dts

--- avidemux_plugins/ADM_demuxers/Matroska/ADM_mkv.cpp
+++ avidemux_plugins/ADM_demuxers/Matroska/ADM_mkv.cpp
@@ -20,6 +20,7 @@
 #include "ADM_codecType.h"
 #include "mkv_tags.h"
 #include "ADM_audioXiphUtils.h"
+#include "ADM_vidMisc.h"
 
 /**
     \fn open
@@ -216,7 +217,6 @@ uint8_t mkvHeader::open(const char *name)
         _audioStreams[i]=ADM_audioCreateStream(&(_tracks[1+i].wavHeader), _access[i]);;
         _audioStreams[i]->setLanguage(_tracks[1+i].language);
     }
-  //dumpVideoIndex(200);
   printf("[MKV]Matroska successfully read\n");
   return 1;
 }
@@ -250,6 +250,10 @@ bool mkvHeader::ComputeDeltaAndCheckBFrames(uint32_t *minDeltaX, uint32_t *maxDe
     int64_t delta,maxDelta=0;
     int64_t minDelta=100000000;
     *bFramePresent=false;
+    int nbValidDts=0;
+   
+    
+    
     if(nb>1)
     {
         bool monotone=true;
@@ -273,7 +277,7 @@ bool mkvHeader::ComputeDeltaAndCheckBFrames(uint32_t *minDeltaX, uint32_t *maxDe
             *bFramePresent=true;
         }
     }
-
+    
     if(nb>1)
     {
         // Search minimum and maximum between 2 frames
@@ -281,17 +285,30 @@ bool mkvHeader::ComputeDeltaAndCheckBFrames(uint32_t *minDeltaX, uint32_t *maxDe
         // the maximum will give us the max PTS-DTS delta so that we can compute DTS
         for(int i=0;i<nb-1;i++) 
         {
+            if(track->index[i].Dts!=ADM_NO_PTS)
+                nbValidDts++;
             if(track->index[i].flags==AVI_B_FRAME) nbBFrame++;
             if(track->index[i+1].Pts==ADM_NO_PTS || track->index[i].Pts==ADM_NO_PTS)
                 continue;
             delta=(int64_t)track->index[i+1].Pts-(int64_t)track->index[i].Pts;
             if(delta<0) delta=-delta;
+            
             if(delta<minDelta) minDelta=delta;
             if(delta>maxDelta) maxDelta=delta;
             //printf("\/=%" PRId64" Min %" PRId64" MAX %" PRId64"\n",delta,minDelta,maxDelta);
         }
     }
     if(nbBFrame) *bFramePresent=true;
+    
+    if(nbValidDts<3)
+    {
+            ADM_warning("Not enough valid DTS\n");
+            *minDeltaX=0;
+            *maxDeltaX=0;
+            return false;
+    }
+    
+    
     ADM_info("Minimum delta found %" PRId64" us\n",minDelta);
     ADM_info("Maximum delta found %" PRId64" us\n",maxDelta);
     if(minDelta)
