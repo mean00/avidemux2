abc15d92ad174a88ead89c75dd728114650ca71a
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2017-04-10 18:47:42 +0200

    [Qt/props] Add copy to clipboard

--- avidemux/qt4/ADM_userInterfaces/ADM_dialog/Q_props.h
+++ avidemux/qt4/ADM_userInterfaces/ADM_dialog/Q_props.h
@@ -1,14 +1,19 @@
 #ifndef Q_props_h
 #define Q_props_h
 
+#include <vector>
 #include "ui_props.h"
 
 class propWindow : public QDialog
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	propWindow(QWidget *parent);
-	Ui_propsDialog ui;
+    propWindow(QWidget *parent);
+    Ui_propsDialog ui;
+private:
+    std::vector <QString> listOfValues;
+public slots:
+    void propsCopyToClipboard(void);
 };
 #endif	// Q_props_h
