3e045060727af4e32c900ca34e029a980986afe3
mean <fixounet@free.fr>
2017-05-22 19:26:06 +0200

    [vp9] naive use of parser, not sure this is correct

--- avidemux_core/ADM_coreVideoCodec/include/ADM_codecFFVP9.h
+++ avidemux_core/ADM_coreVideoCodec/include/ADM_codecFFVP9.h
@@ -1,3 +1,18 @@
+/***************************************************************************
+    \file ADM_codecFFVP9
+    \brief Simple wrapper around vp9, including parser
+    \author mean fixounet@free.fr (c) 2017
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #pragma once
 
 #include "ADM_codecFFsimple.h"
@@ -5,6 +20,9 @@
     \class decoderFF
     \brief Base class for lavcodec based decoder
 */
+
+
+
 class ADM_COREVIDEOCODEC6_EXPORT decoderFFVP9:public decoderFFSimple
 {
 protected:
@@ -21,4 +39,7 @@ public:
                                   return hwDecoder->getName();
                               return "Lavcodec VP9";
                           }
+                            
+protected:
+                AVCodecParserContext *_parserContext;
 };
--- avidemux_core/ADM_coreVideoCodec/src/ADM_ffVp9.cpp
+++ avidemux_core/ADM_coreVideoCodec/src/ADM_ffVp9.cpp
@@ -20,13 +20,17 @@
 #include "DIA_coreToolkit.h"
 #include "ADM_hwAccel.h"
 #include "ADM_codecFFVP9.h"
+
+#define xlog(...) {}
+
 /**
  * \fn ctor
  */
 decoderFFVP9::decoderFFVP9 (uint32_t w, uint32_t h,uint32_t fcc, uint32_t extraDataLen, uint8_t *extraData,uint32_t bpp)
     : decoderFFSimple(w,h,fcc,extraDataLen,extraData,bpp)
 {
-    
+    _parserContext=av_parser_init(AV_CODEC_ID_VP9);
+    ADM_assert(_parserContext);
 }
 /**
  * \fn dtor
@@ -37,9 +41,33 @@ decoderFFVP9::~decoderFFVP9 ()
 }
 /**
  * \fn uncompress
+ * \brief not sure this is correct, we run the parser and feed everything to libavcodec
  */
 bool    decoderFFVP9::uncompress (ADMCompressedImage * in, ADMImage * out)
 {
-    return decoderFFSimple::uncompress(in,out);
+    uint8_t *parsedPointer=NULL;
+    int      parsedLen=0;
+    bool result;
+    // run parser on it..
+    int offset=0;
+    xlog("Parse %d\n",in->dataLength);
+    while(offset<in->dataLength)
+    {
+        int r=av_parser_parse2( _parserContext, _context,
+                                        &parsedPointer, &parsedLen, 
+                                        in->data+offset, in->dataLength-offset,
+                                        in->demuxerPts, in->demuxerDts, -1);
+        if(r<=0 || !parsedPointer)
+            break;
+        offset+=r;
+        xlog("Got pic\n");
+        ADMCompressedImage dummy;
+        dummy=*in;
+        dummy.data=parsedPointer;
+        dummy.dataLength=parsedLen;
+        result=decoderFFSimple::uncompress(&dummy,out);
+    }
+    xlog("/Parse\n");
+    return result;
 }
 // eof
