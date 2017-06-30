b1cbca9a93519b586e135c3a0e8ed2829ae9dc89
mean <fixounet@free.fr>
2017-06-30 07:31:19 +0200

    [qt/build] Split platform specific stuff into separate files

---
+++ avidemux/qt4/adm_osx.cmake
@@ -0,0 +1,18 @@
+#
+MACRO(ADM_PREBUILD)
+        LIST(APPEND PlatformLibs   "-lm -lstdc++")
+        LIST(APPEND PlatformLibs  "-framework CoreServices -framework CoreAudio -framework AudioUnit -framework Carbon")
+        LIST(APPEND PlatformLibs  "-Wl,-dylib_file,/System/Library/Frameworks/OpenGL.framework/Versions/A/Libraries/libGL.dylib:/System/Library/Frameworks/OpenGL.framework/Versions/A/Libraries/libGL.dylib")
+ENDMACRO(ADM_PREBUILD)
+#
+MACRO(ADM_MAIN_APP)
+        ADD_EXECUTABLE(avidemux3_${QT_EXTENSION} BUNDLE ${ADM_EXE_SRCS})
+        set_property(TARGET avidemux3_${QT_EXTENSION} PROPERTY OUTPUT_NAME avidemux)
+	IF (USE_SDL)
+		TARGET_LINK_LIBRARIES(avidemux3_${QT_EXTENSION} ADM_libsdl)
+	ENDIF(USE_SDL)
+ENDMACRO(ADM_MAIN_APP)
+#
+MACRO(ADM_POSTBUILD)
+ENDMACRO(ADM_POSTBUILD)
+
---
+++ avidemux/qt4/adm_win32_vs.cmake
@@ -0,0 +1,27 @@
+#
+MACRO(ADM_PREBUILD)
+      	LIST(APPEND PlatformLibs  Qt5::WinMain)
+ENDMACRO(ADM_PREBUILD)
+#
+MACRO(ADM_MAIN_APP)
+        ADD_EXECUTABLE(avidemux3_${QT_EXTENSION} WIN32 ${ADM_EXE_SRCS})
+        set_target_properties(avidemux3_${QT_EXTENSION} PROPERTIES LINK_FLAGS_DEBUG "/SUBSYSTEM:WINDOWS")
+	set_target_properties(avidemux3_${QT_EXTENSION} PROPERTIES WIN32_EXECUTABLE True)
+        set_property(TARGET avidemux3_${QT_EXTENSION} PROPERTY OUTPUT_NAME avidemux)
+ENDMACRO(ADM_MAIN_APP)
+#
+MACRO(ADM_POSTBUILD)
+        include(./installMsvcRunTime.cmake)
+        include(FindBourne)
+        IF(RELEASE)
+                configure_file(
+                                ${CMAKE_CURRENT_SOURCE_DIR}/../winInstaller/ChangeLog.release 
+                                ${CMAKE_CURRENT_SOURCE_DIR}/../winInstaller/ChangeLog.html
+                                COPYONLY)
+        ELSE(RELEASE)
+                execute_process(
+                        COMMAND ${BASH_EXECUTABLE} genlog.sh
+                        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/../winInstaller/
+                        )
+        ENDIF(RELEASE)
+ENDMACRO(ADM_POSTBUILD)
