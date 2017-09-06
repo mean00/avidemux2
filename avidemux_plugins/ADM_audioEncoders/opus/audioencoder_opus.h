aa6c4b850c668264dbb75d358d2fcfa98e5057cb
mean <fixounet@free.fr>
2017-09-06 10:25:16 +0200

    [opus]

---
+++ avidemux_plugins/ADM_audioEncoders/opus/audioencoder_opus.h
@@ -0,0 +1,33 @@
+
+/***************************************************************************
+    copyright            : (C) 2002-6 by mean
+    email                : fixounet@free.fr
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#pragma once
+#include ".h"
+ //_____________________________________________
+class AUDMEncoder_Opus : public ADM_AudioEncoder
+{
+protected:
+         void           *_handle;
+         uint32_t        _chunk;
+         uint8_t        refillBuffer(int minimum);
+         bool           _globalHeader;
+         faac_encoder   _config;
+         float          *ordered;
+public:
+            bool        initialize(void);
+    virtual             ~AUDMEncoder_Opus();
+                        AUDMEncoder_Opus(AUDMAudioFilter *instream,bool globalHeader,CONFcouple *setup);
+    virtual bool	encode(uint8_t *dest, uint32_t *len, uint32_t *samples);
+};
+
