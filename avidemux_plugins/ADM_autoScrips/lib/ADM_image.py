cb0da58b6182385143d1b59d28a7b093ecf0f8eb
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2017-12-11 08:51:01 +0100

    [autoScripts] Update to the new swscale filter API

--- avidemux_plugins/ADM_autoScrips/lib/ADM_image.py
+++ avidemux_plugins/ADM_autoScrips/lib/ADM_image.py
@@ -8,7 +8,7 @@ FMT_UNKNOWN=-3
 
 AR_1_1=0
 AR_4_3=1
-AR_16_9=2
+AR_16_9=3
 #
 #
 #
@@ -92,7 +92,7 @@ class image:
     def apply_resize(self,resize):
          adm=Avidemux()
          if(self.width!=resize.width or self.height!=resize.height): 
-             adm.addVideoFilter("swscale","width="+str(resize.width),"height="+str(resize.height),"algo=1","sourceAR="+str(self.ar),"targetAR="+str(resize.ar))
+             adm.addVideoFilter("swscale","width="+str(resize.width),"height="+str(resize.height),"algo=1","sourceAR="+str(self.ar),"targetAR="+str(resize.ar),"lockAR=True","roundup=False")
          if(resize.topbottom!=0 or resize.leftright!=0):
              l=str(resize.leftright)
              t=str(resize.topbottom)
