f2a30f88763b5697c8aa2fe0eb94d27bf5664108
mean <fixounet@free.fr>
2018-01-02 08:28:48 +0100

    [win32] make the rc file compatible with vc++ (incomplete)

--- cmake/admWindRes.cmake
+++ cmake/admWindRes.cmake
@@ -5,8 +5,7 @@
         ENDIF(CROSS)
 include(admTimeStamp)
 #
-MACRO(WINDRESIFY src)
-   IF(NOT MSVC)
+MACRO(WINDRESIFY input src)
         # add icon and version info
         SET(FILEVERSION_STRING "${AVIDEMUX_VERSION}")
         SET(PRODUCTVERSION_STRING "${AVIDEMUX_VERSION}")
@@ -23,15 +22,13 @@ MACRO(WINDRESIFY src)
         ENDIF (ADM_CPU_X86_64)
 
         SET(AVIDEMUX_ICON "adm.ico")
-        CONFIGURE_FILE("${AVIDEMUX_TOP_SOURCE_DIR}/cmake/admWin32.rc.in" ${CMAKE_CURRENT_BINARY_DIR}/admWin.rc IMMEDIATE)
+        CONFIGURE_FILE(${input} ${CMAKE_CURRENT_BINARY_DIR}/admWin.rc IMMEDIATE)
 
         if (MINGW)
 	        SET(ADM_WIN_RES "adm.obj")
 	        SET( ${src} ${ADM_WIN_RES})
 	        ADD_CUSTOM_COMMAND(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${ADM_WIN_RES} COMMAND ${WINDRES} -F ${WIN_RES_TARGET} -i ${CMAKE_CURRENT_BINARY_DIR}/admWin.rc -o ${CMAKE_CURRENT_BINARY_DIR}/${ADM_WIN_RES} -O coff --define VS_VERSION_INFO=1)
+        else (MINGW) # MSVC
+            SET(${src}  ${CMAKE_CURRENT_BINARY_DIR}/admWin.rc )
         endif (MINGW)
-	ELSE (NOT MSVC)
-		#SET(${src}  ${CMAKE_CURRENT_BINARY_DIR}/admWin.rc)
-	endif (NOT MSVC)
-
-ENDMACRO(WINDRESIFY src)
+ENDMACRO(WINDRESIFY input src)
