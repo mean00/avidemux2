8f25eb91d790f7f9680ca5440022ba73b1d801c1
mean <fixounet@free.fr>
2018-01-07 17:26:10 +0100

    [NSIS] More cleanup

--- avidemux/winInstaller/createNsis64Qt5.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-t=`dirname $0`
-export TOP_FOLDER="$t/../../"
-echo "** Getting svn revision **"
-echo "Revision : $REV"
-echo "Creating NSIS Package (64)"
-makensis -DBINARY_FOLDER=/mingw/avidemux_64 -DDEV_FOLDER=/mingw_dev/mingw/Release -DSOURCE_FOLDER=$TOP_FOLDER -DAPI_VERSION=2.7 -DNSIDIR=$PWD avidemux_cross64Qt5.nsi
-echo "Done"
---
+++ avidemux/winInstaller/createNsisQt5.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+BITS=$1
+echo " Creating cross Nsis package"
+if [ "$BITS" == "32" ] || [  "$BITS" == "64" ]  
+then
+    echo "Variant $BITS bits"
+else
+    echo "Failed : 32 or 64" 
+    exit 1
+fi
+t=`dirname $0`
+export TOP_FOLDER="$t/../../"
+echo "Revision : $REV"
+echo "Version  : $Bits"
+echo "Creating NSIS Package"
+makensis -DBINARY_FOLDER=/mingw/avidemux_${BITS} -DDEV_FOLDER=/mingw_dev/mingw/Release -DSOURCE_FOLDER=$TOP_FOLDER -DAPI_VERSION=2.7 -DNSIDIR=$PWD avidemux_cross${BITS}Qt5.nsi
+echo "Done"
