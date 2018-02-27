mean00/avidemux2

commit f40175dad60813503e489f63319f797c8bcb6f43

[changeFps,avsfilter] Implement getTimeRange to propagate updated time range down the filter chain

avidemux_plugins/ADM_videoFilters6/avsfilter/avsfilter.cpp
@@ -651,6 +651,16 @@ std::string pAvsScript=std::string(param.avs_script);
  return 0;
 }
 
+/**
+    \fn getTimeRange
+*/
+bool getTimeRange(uint64_t *startTime, uint64_t *endTime)
+{
+    *startTime = 0;
+    *endTime = info.totalDuration;
+    return true;
+}
+
 //#define SET_AVS(i,x,y,z) {wine_loader->avs_pipes[i].pipename = x; wine_loader->avs_pipes[i].hpipe = y; wine_loader->avs_pipes[i].flags = z;}
 
 bool avsfilter::SetParameters(avsfilter_config *newparam)

avidemux_plugins/ADM_videoFilters6/avsfilter/avsfilter.h
@@ -76,6 +76,7 @@ public:
   virtual ~avsfilter();
   virtual bool getNextFrame(uint32_t *fn, ADMImage *image);
   virtual bool configure(void);
+  virtual bool getTimeRange(uint64_t *startTime, uint64_t *endTime);
   virtual bool getCoupledConf( CONFcouple **couples);
   virtual void setCoupledConf( CONFcouple *couples);
   virtual const char   *getConfiguration(void);                   /// Return  current configuration as a human readable string

avidemux_plugins/ADM_videoFilters6/changeFps/changeFps.cpp
@@ -60,6 +60,7 @@ public:
                             changeFps(ADM_coreVideoFilter *previous,CONFcouple *conf);
                             ~changeFps();
         bool                goToTime(uint64_t usSeek);
+        virtual bool        getTimeRange(uint64_t *start, uint64_t *end);
         virtual const char   *getConfiguration(void);                   /// Return  current configuration as a human readable string
         virtual bool         getNextFrame(uint32_t *fn,ADMImage *image);    /// Return the next image
         virtual bool         getCoupledConf(CONFcouple **couples) ;   /// Return the current filter configuration
@@ -126,6 +127,17 @@ bool         changeFps::goToTime(uint64_t usSeek)
     timing*=configuration.newFpsNum;
     timing*=configuration.oldFpsDen;
     return  previousFilter->goToTime((uint64_t)timing);
+}
+
+/**
+    \fn getTimeRange
+    \brief Report updated time range to the next filter
+*/
+bool changeFps::getTimeRange(uint64_t *start, uint64_t *end)
+{
+    *start=0;
+    *end=info.totalDuration;
+    return true;
 }
 
 /**

commit 17634e48baf2a6eeb232c5325e23805aa20abc7a

[avsfilter] Fix build broken by missing scope in f40175d, oops

avidemux_plugins/ADM_videoFilters6/avsfilter/avsfilter.cpp
@@ -654,7 +654,7 @@ std::string pAvsScript=std::string(param.avs_script);
 /**
     \fn getTimeRange
 */
-bool getTimeRange(uint64_t *startTime, uint64_t *endTime)
+bool avsfilter::getTimeRange(uint64_t *startTime, uint64_t *endTime)
 {
     *startTime = 0;
     *endTime = info.totalDuration;