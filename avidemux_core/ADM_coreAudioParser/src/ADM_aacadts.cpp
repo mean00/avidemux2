mean00/avidemux2

commit 1265b63f8ecb8958770bbefc42b8cbbcdc3653ad

[coreAudio/AAC] Handle corrupted or misdetected audio files more gracefully by refusing to overflow buffer instead of asserting

avidemux_core/ADM_coreAudioParser/src/ADM_aacadts.cpp
@@ -87,8 +87,8 @@ int ADM_adts2aac::getFrequency(void)
 {
     if(!hasExtra)
     {
-            ADM_error("No extradata in aac! using default of 48 kHz");
-            return 48000;
+        ADM_error("No extradata in aac! using default of 48 kHz\n");
+        return 48000;
     }
     uint8_t *p=extra;
     int dex=((p[0]&7)<<1)+(p[1]>>7);
@@ -102,8 +102,8 @@ int ADM_adts2aac::getChannels(void)
 {
     if(!hasExtra)
     {
-            ADM_error("No extradata in aac! using default of 2 channels");
-            return 2;
+        ADM_error("No extradata in aac! using default of 2 channels\n");
+        return 2;
     }
     uint8_t *p=extra;
     int dex=((p[1]>>3)&0xf);
@@ -144,7 +144,7 @@ bool  ADM_adts2aac::addData(int incomingLen,const uint8_t *inData)
     {
         ADM_error("Head=%d tail=%d bufferSize=%d\n",head,tail,ADTS_BUFFER_SIZE*2);
         ADM_error("Adts buffer overflow\n");
-        ADM_assert(0);
+        return false;
     }
     memcpy(buffer.at(head),inData,incomingLen);
     head+=incomingLen;
@@ -276,8 +276,11 @@ again:
 ADM_adts2aac::ADTS_STATE ADM_adts2aac::convert2(int incomingLen,const uint8_t *inData,int *outLen,uint8_t *out)
 {
     *outLen=0;
+    bool r=false;
     if(incomingLen)
-        addData(incomingLen,inData);
+        r=addData(incomingLen,inData);
+    if(!r)
+        return ADTS_ERROR;
     return getAACFrame(outLen,out);
 }
 //EOF

avidemux_core/ADM_coreAudio/src/ADM_audioAccessFileAACADTS.cpp
@@ -32,7 +32,7 @@ bool ADM_audioAccessFileAACADTS::init(void)
     if(n<=0) return false;
     fseek(_fd,0,SEEK_SET);
     ADM_info("Probing AAC/ADTS with %d bytes\n",n);
-    aac->addData(n,buffer);
+    if(!aac->addData(n,buffer)) return false;
     if(ADM_adts2aac::ADTS_OK!=aac->getAACFrame(NULL,NULL))
     {
         ADM_warning("Cannot sync\n");
@@ -125,7 +125,8 @@ bool ADM_audioAccessFileAACADTS::refill(void)
     int n=fread(tmp,1,4024,_fd);
     if(n<1) return false;
     // feed
-    aac->addData(n,tmp);
+    if(!aac->addData(n,tmp))
+        return false;
     return true;
 }
 /**

avidemux_core/ADM_coreAudio/src/ADM_audioAccessFileAACADTS_indexer.cpp
@@ -66,9 +66,6 @@ bool adtsIndexer::index(std::vector<aacAdtsSeek>& seekPoints)
    aacAdtsSeek  start;
    int offset;
 
-   int      beginningOfBuffer=0;
-   int      endOfBuffer=0;
-   
    start.dts=0;
    start.position=0;
    seekPoints.push_back(start); // initial start
@@ -106,9 +103,8 @@ bool adtsIndexer::index(std::vector<aacAdtsSeek>& seekPoints)
                int n=fread(buffer,1,CHUNK_SIZE,f);
                if(n<=0)
                    return true;
-               beginningOfBuffer=endOfBuffer;
-               endOfBuffer+=n;
-               aac.addData(n,buffer);
+               if(!aac.addData(n,buffer))
+                   return true;
                break;
            }
            default:

commit 6ceefbea8f9946914d36a9285a58de8bd8179bed

[coreAudioParser/aacadts] Don't refuse to consume data if we can't refill buffer
This fixes DVB-T2 samples with AAC audio broken by commit 1265b63.

avidemux_core/ADM_coreAudioParser/src/ADM_aacadts.cpp
@@ -279,8 +279,9 @@ ADM_adts2aac::ADTS_STATE ADM_adts2aac::convert2(int incomingLen,const uint8_t *i
     bool r=false;
     if(incomingLen)
         r=addData(incomingLen,inData);
-    if(!r)
+    ADTS_STATE state=getAACFrame(outLen,out);
+    if(!r && state==ADTS_MORE_DATA_NEEDED)
         return ADTS_ERROR;
-    return getAACFrame(outLen,out);
+    return state;
 }
 //EOF