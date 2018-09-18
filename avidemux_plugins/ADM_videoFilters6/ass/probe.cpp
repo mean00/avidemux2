455418ac22bb663fee800f4a4982c5395d4b2d16
mean <fixounet@free.fr>
2018-09-18 07:47:55 +0200

    [fontconfig/freetype] Also check fontconfing version

--- avidemux_plugins/ADM_videoFilters6/ass/probe.cpp
+++ avidemux_plugins/ADM_videoFilters6/ass/probe.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
-#include <ft2build.h>
+#include "ft2build.h"
 #include FT_FREETYPE_H
+#include <fontconfig/fontconfig.h>
+#include <fontconfig/fcfreetype.h>
 
 
 #define CHECK(x) if((x)) {printf( #x " failed \n");return -1;}
@@ -17,7 +19,17 @@ int main(int a, char **b)
   CHECK(FT_Init_FreeType(   &alibrary ));
   FT_Library_Version(   alibrary,&maj,&min,&patch );
 
+ // build with FT 17.1.11
   printf("Freetype version %d.%d.%d\n",maj,min,patch);
 
+  CHECK(!FcInit());
+
+  int fcVersion=FcGetVersion();
+
+    int fcMaj=fcVersion/10000;
+    int fcMin=(fcVersion-fcMaj*10000)/100;
+
+  printf("Fontconfig version %d :%d.%d\n",fcVersion,fcMaj,fcMin);
+    // Add check here
   return 0;
 }
