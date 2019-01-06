a3bd49d379d91dd28befd16d1c76a64c2924c516
mean <fixounet@free.fr>
2019-01-06 16:05:43 +0100

    [qtifw] framework

---
+++ avidemux/winInstaller/qtifw/split.bash
@@ -0,0 +1,25 @@
+#/bin/bash
+export src_path=$1
+export run_path="runtime"
+export dev_path="dev" 
+
+if [ ! -d "$src_path" ]; then
+	echo "Source path does not exist"
+	exit 1
+fi
+
+echo "Cleaning"
+rm -Rf $run_path
+rm -Rf $dev_path
+mkdir -p $run_path $dev_path
+
+echo "Copying"
+
+cp -Rap $src_path/* $run_path/
+
+echo "Moving"
+mv $run_path/include $dev_path
+mv $run_path/*.lib $dev_path
+rm $run_path/plugins/*/*.lib
+
+echo "Done"
