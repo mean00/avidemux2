84dc2444c5dfa76ae9ecc15e08a7f3a6bd202def
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2019-01-14 02:18:19 +0100

    [commonUI/editor] Default to post-processing disabled, apply default post-processing settings, add a deinterlacing option, remove redundant UV swap from dialog

--- avidemux/common/ADM_editor/src/ADM_edit.cpp
+++ avidemux/common/ADM_editor/src/ADM_edit.cpp
@@ -294,10 +294,9 @@ uint8_t ADM_Composer::addFile (const char *name)
     if(!_segments.getNbRefVideos())
     {
         uint32_t type=0,value=0;
-#if 0
-        if(!prefs->get(DEFAULT_POSTPROC_TYPE,&type)) type=3;
-        if(!prefs->get(DEFAULT_POSTPROC_VALUE,&value)) value=3;
-#endif
+        prefs->get(DEFAULT_POSTPROC_TYPE,&type);
+        prefs->get(DEFAULT_POSTPROC_VALUE,&value);
+
         if(_pp) delete _pp;
         _pp=new ADM_PP(info.width,info.height);
         _pp->postProcType=type;
