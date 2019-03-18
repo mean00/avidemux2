04ca69c2a4f32d1abca0a3aecd7a8fd41287b7e2
mean <fixounet@free.fr>
2019-03-18 16:39:14 +0100

    [VC++] Quick and dirty crash handler, incomplete

--- avidemux_core/ADM_core/include/ADM_crashdump_other.h
+++ avidemux_core/ADM_core/include/ADM_crashdump_other.h
@@ -1,4 +1,4 @@
-#if !defined(__APPLE__) && !defined(__unix__) && !defined(__MINGW32__) && !defined ADM_CRASHDUMP_OTHER_H
+#if !defined(__APPLE__) && !defined(__unix__) && !defined(__MINGW32__) && !defined ADM_CRASHDUMP_OTHER_H &&!defined _MSC_VER
 #define ADM_CRASHDUMP_OTHER_H
 
 #define installSigHandler(...) {}
