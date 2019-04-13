97a4a2ed8803a7d95b951d89fef45f4234d4bea1
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2019-04-14 00:45:49 +0200

    [automation] Remove unused option, update descriptions

--- avidemux/common/ADM_toolkit/automation.cpp
+++ avidemux/common/ADM_toolkit/automation.cpp
@@ -77,27 +77,25 @@ typedef struct
 #define avs_port_change "avisynth-port"
 AUTOMATON reaction_table[]=
 {
-    
     {"append",                 1, "append video",                                                              (one_arg_type)A_appendVideo},
-    {"audio-codec",            1, "set audio codec (MP2/MP3/AC3/NONE (WAV PCM)/TWOLAME/COPY)",                 (one_arg_type)call_audiocodec},
-    {avs_port_change,          1, "set avsproxy port accordingly",                                             (one_arg_type)A_set_avisynth_port},    
-    {"help",                   0, "print this",                                                                (one_arg_type)call_help},    
-    {"info",                   0, "show information about loaded video and audio streams",                     (one_arg_type)show_info},    
-    {"list-audio-languages",   0, "list all available audio langues",                                          (one_arg_type)list_audio_languages},        
+    {"audio-codec",            1, "set audio codec (copy|Lame|FDK_AAC|LavAC3|Opus|TwoLame|...)",               (one_arg_type)call_audiocodec},
+    {avs_port_change,          1, "set avsproxy port accordingly",                                             (one_arg_type)A_set_avisynth_port},
+    {"help",                   0, "print this",                                                                (one_arg_type)call_help},
+    {"info",                   0, "show information about loaded video and audio streams",                     (one_arg_type)show_info},
+    {"list-audio-languages",   0, "list all available audio langues",                                          (one_arg_type)list_audio_languages},
     {"load",                   1, "load video or workbench",                                                   (one_arg_type)loadCB},
     {"nogui",                  0, "Run in silent mode",                                                        (one_arg_type)GUI_Quiet},
-    {"output-format",          1, "set output format (AVI|OGM|ES|PS|AVI_DUAL|AVI_UNP|...)",                    (one_arg_type)set_output_format},
+    {"output-format",          1, "set output format (MKV|MP4|ffTS|ffPS|AVI|RAW|...)",                         (one_arg_type)set_output_format},
     {"quit",                   0, "exit avidemux",                                                             (one_arg_type)call_quit},
-    {"slave",                  1, "run as slave, master is on port arg",                                       (one_arg_type)call_slave},    
-    {"reuse-2pass-log",        0, "reuse 2pass logfile if it exists",                                          (one_arg_type)set_reuse_2pass_log},        
+    {"slave",                  1, "run as slave, master is on port arg",                                       (one_arg_type)call_slave},
     {"run",                    1, "load and run a script",                                                     (one_arg_type)call_scriptEngine},
     {"save",                   1, "save video",                                                                (one_arg_type)saveCB},
     {"save-jpg",               1, "save a jpeg",                                                               (one_arg_type)A_saveJpg},
     {"save-raw-audio",         1, "save audio as-is ",                                                         (one_arg_type)A_saveAudioCopy},
     {"save-uncompressed-audio",1, "save uncompressed audio",                                                   (one_arg_type)A_saveAudioProcessed},
-    {"set-audio-language",     2, "Set language of an active audio track {track_index} {language_short_name}", (one_arg_type)A_setAudioLang},    
-    {"var",                    1, "set var (--var myvar=3)",                                                   (one_arg_type)setVar},    
-    {"video-codec",            1, "set video codec (x264/...)",                                                (one_arg_type)call_videocodec},    
+    {"set-audio-language",     2, "Set language of an active audio track {track_index} {language_short_name}", (one_arg_type)A_setAudioLang},
+    {"var",                    1, "set var (--var myvar=3)",                                                   (one_arg_type)setVar},
+    {"video-codec",            1, "set video codec (Copy|x264|x265|xvid4|ffMpeg2|ffNvEnc|...)",                (one_arg_type)call_videocodec},
 };
 #define NB_AUTO (sizeof(reaction_table)/sizeof(AUTOMATON))
 
@@ -493,15 +491,6 @@ uint8_t scriptAddVar(char *var,char *value)
 {
     video_body->setVar(var,value);
     return 1;
-}
-
-/**
- * 
- * @param p
- */
-void set_reuse_2pass_log(char *p)
-{
-   prefs->set(FEATURES_REUSE_2PASS_LOG,true);
 }
 
 /**
