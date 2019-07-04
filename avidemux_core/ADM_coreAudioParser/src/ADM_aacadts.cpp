bc6239e048e511c33565585a8e042a4f033999e0
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2019-07-04 11:53:41 +0200

    [coreAudioParser/aacadts] Check layer bits too to avoid false positives with padded DTS streams

--- avidemux_core/ADM_coreAudioParser/src/ADM_aacadts.cpp
+++ avidemux_core/ADM_coreAudioParser/src/ADM_aacadts.cpp
@@ -178,9 +178,9 @@ again:
     int match;
     for( p=(buffer.at(tail));p<(buffer.at(head-6));p++)
     {
-        if(p[0]!=0xff || ((p[1]&0xf0)!=0xf0))
+        if(p[0]!=0xff || p[1]&0xf0!=0xf0 || p[1]&6)
             continue;
-        
+
         match=p-buffer.at(0); // offset of syncword
         packetLen=((p[3]&0x3)<<11)+(p[4]<<3)+(p[5]>>5);
         nbFrames=1+(p[6]&3);
@@ -204,9 +204,9 @@ again:
             return ADTS_MORE_DATA_NEEDED;
         }
         // do we have sync at the end ?
-        if(p[packetLen]!=0xff || (p[packetLen+1]&0xf0)!=0xf0)
+        if(p[packetLen]!=0xff || p[packetLen+1]&0xf0!=0xf0 || p[packetLen+1]&6)
         {
-            aprintf("[ADTS] no ff marker at the end\n");
+            aprintf("[ADTS] no syncword at the end or layer!=0\n");
             continue;
         }
         aprintf("[ADTS] End marker found\n");
