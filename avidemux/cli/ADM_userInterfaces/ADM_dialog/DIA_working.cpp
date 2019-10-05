4263ffb2c0215362f9a92d73afe5fa5c60e18e39
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2019-10-05 10:55:01 +0200

    [cli/DIA_working] Unify indentation, improve progress reporting for time elapsed < 1s

--- avidemux/cli/ADM_userInterfaces/ADM_dialog/DIA_working.cpp
+++ avidemux/cli/ADM_userInterfaces/ADM_dialog/DIA_working.cpp
@@ -31,109 +31,105 @@
 class DIA_workingNone : public DIA_workingBase
 {
 protected:
-        virtual void 	    postCtor( void );
+    virtual void        postCtor( void );
 public:
-                            DIA_workingNone( const char *title=NULL );
-        virtual		        ~DIA_workingNone();
-            // If returns 1 -> Means aborted
-        virtual uint8_t  	update(uint32_t percent);
-        virtual uint8_t 	update(uint32_t current,uint32_t total);
-        virtual uint8_t  	isAlive (void );
-        
+                        DIA_workingNone( const char *title=NULL );
+    virtual             ~DIA_workingNone();
+    // If returns 1 -> Means aborted
+    virtual uint8_t     update(uint32_t percent);
+    virtual uint8_t     update(uint32_t current,uint32_t total);
+    virtual uint8_t     isAlive (void );
+
 };
 //**********************************
 DIA_workingNone::DIA_workingNone( const char *title ) : DIA_workingBase(title)
 {
-	this->postCtor();
+    this->postCtor();
 }
 
 void DIA_workingNone :: postCtor( void )
 {
-	lastper=0;
-	_nextUpdate=0;
+    lastper=0;
+    _nextUpdate=0;
 }
 
 uint8_t DIA_workingNone::update(uint32_t percent)
 {
-                if(!percent) return 0;
+    if(!percent || percent==lastper)
+        return 0;
 
-                if(percent==lastper)
-                {
-                   return 0;
-                }
+    lastper=percent;
+    //aprintf("DIA_working::update(%lu) called\n", percent);
+    elapsed=_clock.getElapsedMS();
+    if(elapsed==_nextUpdate)
+        return 0;
 
-				lastper=percent;
-                aprintf("DIA_working::update(%lu) called\n", percent);
-                elapsed=_clock.getElapsedMS();
-                if(elapsed<_nextUpdate) 
-                {
+    if(elapsed<1000)
+    {
+        printf("%u%% done, elapsed: %u ms\n",percent,elapsed);
+        _nextUpdate=elapsed;
+        return 0;
+    }
+    if(elapsed<_nextUpdate)
+        return 0;
+    _nextUpdate=elapsed+1000;
 
-                  return 0;
-                }
-                _nextUpdate=elapsed+1000;
-        
-		//
 		// 100/totalMS=percent/elapsed
 		// totalM=100*elapsed/percent
 
-		double f;
-		f=100.;
-		f*=elapsed;
-		f/=percent;
+    double f;
+    f=100.;
+    f*=elapsed;
+    f/=percent;
 
-		f-=elapsed;
-		f/=1000;
+    f-=elapsed;
+    f/=1000;
 
-		uint32_t sectogo=(uint32_t)floor(f);
+    uint32_t sectogo=(uint32_t)floor(f);
 
-	char b[300];
-   		int  mm,ss;
-    			mm=sectogo/60;
-      			ss=sectogo%60;
-    			printf(" %d m %d s left ", mm,ss);
+    char b[300];
+    int  mm,ss;
+    mm=sectogo/60;
+    ss=sectogo%60;
+    printf("%u%% done, %d m %d s left\n",percent,mm,ss);
 
-		double p;
-			p=percent;
-			p=p/100.;
-                        printf("%% done : %f\n",p);
-		return 0;
-
+    return 0;
 
 }
+
 uint8_t DIA_workingNone::update(uint32_t cur, uint32_t total)
 {
-		double d,n;
-		uint32_t percent;
+    double d,n;
+    uint32_t percent;
 
-		//aprintf("DIA_working::update(uint32_t %lu,uint32_t %lu) called\n", cur, total);
-		if(!total) return 0;
+    //aprintf("DIA_working::update(uint32_t %lu,uint32_t %lu) called\n", cur, total);
+    if(!total) return 0;
 
-		d=total;
-		n=cur;
-		n=n*100.;
+    d=total;
+    n=cur;
+    n=n*100.;
 
-		n=n/d;
+    n=n/d;
 
-		percent=(uint32_t )floor(n);
-		return update(percent);
+    percent=(uint32_t )floor(n);
+    return update(percent);
 
 }
 
 uint8_t DIA_workingNone::isAlive (void )
 {
-	return 1;
+    return 1;
 }
 
 DIA_workingNone::~DIA_workingNone()
 {
-	
-        
+
 }
 namespace ADM_CliCoreUIToolkit
 {
 DIA_workingBase *createWorking(const char *title)
 {
-        return new DIA_workingNone(title);
+    return new DIA_workingNone(title);
 
 }
 }
