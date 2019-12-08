d98a13167869684931bb623c78bebddbc7a881e8
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2019-12-08 01:15:15 +0100

    [coreVideoFilter] Add timebase fields to FilterInfo

--- avidemux_core/ADM_coreVideoFilter/src/ADM_videoFilterBridge.cpp
+++ avidemux_core/ADM_coreVideoFilter/src/ADM_videoFilterBridge.cpp
@@ -43,8 +43,9 @@ ADM_videoFilterBridge::ADM_videoFilterBridge(IEditor *editor, uint64_t startTime
     bridgeInfo.width = fo.width;
     bridgeInfo.height = fo.height;
     bridgeInfo.frameIncrement = editor->getFrameIncrement();
+    bridgeInfo.timeBaseDen = fo.timebase_den;
+    bridgeInfo.timeBaseNum = fo.timebase_num;
     bridgeInfo.totalDuration = endTime - startTime;
-    bridgeInfo.frameIncrement = editor->getFrameIncrement();
     rewind();
 }
 
