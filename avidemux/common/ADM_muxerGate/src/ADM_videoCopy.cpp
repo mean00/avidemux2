cf916ecaea4682ecff7a5fc87615a186dfae2b3b
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2019-12-16 18:15:03 +0100

    [muxerGate] Query common timebase from the editor

--- avidemux/common/ADM_muxerGate/src/ADM_videoCopy.cpp
+++ avidemux/common/ADM_muxerGate/src/ADM_videoCopy.cpp
@@ -44,8 +44,7 @@ ADM_videoStreamCopy::ADM_videoStreamCopy(uint64_t startTime,uint64_t endTime)
     height=info.height;
     fourCC=info.fcc;
     averageFps1000=info.fps1000;
-    timeBaseDen=info.timebase_den;
-    timeBaseNum=info.timebase_num;
+    video_body->getTimeBase(&timeBaseNum,&timeBaseDen);
     frameIncrement=video_body->getFrameIncrement();
     isCFR=false;
     // Estimate start frame
