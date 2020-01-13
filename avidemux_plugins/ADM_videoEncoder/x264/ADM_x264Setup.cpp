mean00/avidemux2

commit 5368ebafd9fb59681616830f3e6b06c1067239b6

[videoEncoder/x264] Set timebase from the filter info, guess variable frame rate input from frame increment and timebase and configure encoder accordingly

avidemux_plugins/ADM_videoEncoder/x264/ADM_x264Setup.cpp
@@ -98,11 +98,18 @@ bool x264Encoder::setup(void)
   param.i_log_level=X264_LOG_INFO; //DEBUG; //INFO;
 
   //Framerate
-  int n,d;    
+  int n,d;
   uint64_t f=source->getInfo()->frameIncrement;
   usSecondsToFrac(f,&n,&d);
   param.i_fps_num = d;
   param.i_fps_den = n;
+  n = source->getInfo()->timeBaseNum & 0x7FFFFFFF;
+  d = source->getInfo()->timeBaseDen & 0x7FFFFFFF;
+  ADM_assert(d);
+  param.i_timebase_num = n;
+  param.i_timebase_den = d;
+  bool constantFps = (n * 1000 * 1000 + (d / 2))/ d >= f;
+  param.b_vfr_input = !constantFps;
 
   // -------------- vui------------
   #undef MKPARAM
@@ -193,9 +200,10 @@ bool x264Encoder::setup(void)
       param.b_repeat_headers=0;
   else
       param.b_repeat_headers=1;
-
+#if 0
   // We do pseudo cfr ...
   param.b_vfr_input=0;
+#endif
 
   if(x264Settings.useAdvancedConfiguration)
   {  
@@ -481,7 +489,7 @@ static void dumpx264Setup(x264_param_t *param)
 
     PI(i_timebase_num);
     PI(i_timebase_den);
-
+    PI(b_vfr_input);
         
     PI(i_frame_reference);
     PI(i_keyint_max);
@@ -540,7 +548,6 @@ static void dumpx264Setup(x264_param_t *param)
     PI(b_aud);
     PI(b_repeat_headers);
     PI(b_annexb);
-    PI(b_vfr_input);
     
     AI(i_luma_deadzone[0]);
     AI(i_luma_deadzone[1]);

commit 1f53ee05faf14db7162a6f3c371a3987ccf28639

[videoEncoder/x264] Fix ratecontrol in 2pass mode, broken by 5368eba
Do not ask libx264 to use timebase and timestamps for ratecontrol purposes, this cannot work right at least as long as our timestamps are not in timebase units. Back to setting b_vfr_input always to false like FFmpeg does.

avidemux_plugins/ADM_videoEncoder/x264/ADM_x264Setup.cpp
@@ -100,7 +100,7 @@ bool x264Encoder::setup(void)
   //Framerate
   int n,d;
   uint64_t f=source->getInfo()->frameIncrement;
-  usSecondsToFrac(f,&n,&d);
+  usSecondsToFrac(f,&n,&d,INT_MAX);
   param.i_fps_num = d;
   param.i_fps_den = n;
   n = source->getInfo()->timeBaseNum & 0x7FFFFFFF;
@@ -108,8 +108,13 @@ bool x264Encoder::setup(void)
   ADM_assert(d);
   param.i_timebase_num = n;
   param.i_timebase_den = d;
+#if 0
   bool constantFps = (n * 1000 * 1000 + (d / 2))/ d >= f;
   param.b_vfr_input = !constantFps;
+#else
+// We do pseudo cfr ...
+  param.b_vfr_input=0;
+#endif
 
   // -------------- vui------------
   #undef MKPARAM
@@ -200,10 +205,6 @@ bool x264Encoder::setup(void)
       param.b_repeat_headers=0;
   else
       param.b_repeat_headers=1;
-#if 0
-  // We do pseudo cfr ...
-  param.b_vfr_input=0;
-#endif
 
   if(x264Settings.useAdvancedConfiguration)
   {  