a882a5bc8e9173681aca3884850e316ae9443ade
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2020-02-27 00:35:18 +0100

    [coreDemuxerMpeg] Bump index file version to force re-indexing

--- avidemux_core/ADM_coreDemuxerMpeg/include/ADM_indexFile.h
+++ avidemux_core/ADM_coreDemuxerMpeg/include/ADM_indexFile.h
@@ -24,7 +24,7 @@
     \class indexFile
     \brief Read a index file easily
 */
-#define ADM_INDEX_FILE_VERSION 6
+#define ADM_INDEX_FILE_VERSION 7
 #define ADM_INDEX_BUFFER (20*1024)
 /**
     \class dmxToken
