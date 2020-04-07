3a89c9302f8284629a8bddfe0336cf1dc3f50ca8
mean <fixounet@free.fr>
2020-04-07 10:22:53 +0200

    appImage for buster, incomplete

---
+++ makeAppImageBuster.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+fail()
+{
+       echo "FAIL $@"
+       exit 1
+}
+export CXXFLAGS="$CXXFLAGS -std=c++11"
+bash bootStrap.bash --rebuild --deb --without-cli || fail main
+bash appImage/deployBuster.sh
