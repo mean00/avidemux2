mean00/avidemux2

commit 4e69af151372cc9a39fc28345ac50408a26b8b9f

[ffmpeg,coreUtils,editor,muxerGate,demuxers/Matroska,demuxers/Mp4] Use NALU length size for H.264 and HEVC ISO/IEC type streams as specified by extradata whenever possible, remove nal_length_size field from ADM_SPSinfoH265 to align the handling for HEVC with H.264

avidemux/common/ADM_muxerGate/src/ADM_videoCopySeiInjector.cpp
@@ -28,6 +28,20 @@ ADM_videoStreamCopySeiInjector::ADM_videoStreamCopySeiInjector(uint64_t startTim
 {
     ADM_info("SEI injector created\n");
     uint32_t length=0;
+    seiLen=0;
+    nalSize=0;
+    {
+        uint8_t *extra;
+        uint32_t extraLen=0;
+        getExtraData(&extraLen,&extra);
+        if(extraLen)
+            nalSize=ADM_getNalSizeH264(extra,extraLen);
+        if(!nalSize)
+        {
+            ADM_warning("Cannot get valid extradata, passthrough operation.\n");
+            return;
+        }
+    }
     if(video_body->getUserDataUnregistered(startTime, seiBuf, ADM_H264_MAX_SEI_LENGTH, &length))
     {
         ADM_info("SEI message with x264 build info present, NAL unit length: %u\n",length);
@@ -36,7 +50,6 @@ ADM_videoStreamCopySeiInjector::ADM_videoStreamCopySeiInjector(uint64_t startTim
     }else
     {
         ADM_info("No x264 build info found, purely passthrough operation.\n");
-        seiLen=0;
     }
 }
 
@@ -61,7 +74,7 @@ bool  ADM_videoStreamCopySeiInjector::getPacket(ADMBitstream *out)
         return true;
     // Check whether this particular SEI message is already present.
     uint32_t len=0;
-    if(extractH264SEI(out->data,out->len,NULL,ADM_H264_MAX_SEI_LENGTH,&len))
+    if(extractH264SEI(out->data, out->len, nalSize, NULL, ADM_H264_MAX_SEI_LENGTH, &len))
     {
         if(len!=seiLen)
             ADM_warning("SEI message present, but the length is different?\n");
@@ -80,16 +93,12 @@ bool  ADM_videoStreamCopySeiInjector::getPacket(ADMBitstream *out)
     uint8_t *tail=out->data;
     uint8_t *head=tail+out->len;
     uint8_t stream;
-    uint32_t nalSize=3;
 
     while(tail + nalSize < head)
     {
-        nalSize=4;
-        uint32_t length=(tail[0]<<16)+(tail[1]<<8)+tail[2];
-        if((length<<8)+tail[3] > out->len)
-            nalSize=3;
-        else
-            length=(length<<8)+tail[3];
+        uint32_t i,length=0;
+        for(i=0; i<length; i++)
+            length=(length<<8)+tail[i];
         stream=*(tail+nalSize)&0x1F;
         switch(stream)
         {

avidemux/common/ADM_editor/src/ADM_edVideoCopy.cpp
@@ -266,9 +266,12 @@ ADM_cutPointType ADM_Composer::checkSegmentStartsOnIntra(uint32_t segNo)
         bool AnnexB=false;
         uint8_t *extra;
         uint32_t extraLen=0;
+        uint32_t nalSize=0;
         demuxer->getExtraHeaderData(&extraLen,&extra);
         if(!extraLen)
             AnnexB=true;
+        else
+            nalSize=ADM_getNalSizeH264(extra,extraLen);
         // Allocate memory to hold compressed frame
         ADMCompressedImage img;
         buffer=new uint8_t[MAX_FRAME_LENGTH];
@@ -286,7 +289,7 @@ ADM_cutPointType ADM_Composer::checkSegmentStartsOnIntra(uint32_t segNo)
         if(AnnexB)
             outcome=extractH264FrameType_startCode(img.data, img.dataLength, &(img.flags), &poc, &sps, &recoveryDistance);
         else
-            outcome=extractH264FrameType(img.data, img.dataLength, &(img.flags), &poc, &sps, &recoveryDistance);
+            outcome=extractH264FrameType(img.data, img.dataLength, nalSize, &(img.flags), &poc, &sps, &recoveryDistance);
         if(!outcome)
         {
             ADM_warning("Cannot get H.264 frame type and Picture Order Count Least Significant Bits value.\n");
@@ -344,6 +347,17 @@ ADM_cutPointType ADM_Composer::checkSegmentStartsOnIntra(uint32_t segNo)
                 ADM_warning("Combining AVCC and AnnexB type H.264 streams is currently not supported.\n");
                 cut=ADM_EDITOR_CUT_POINT_MISMATCH;
                 BOWOUT
+            }
+            // For AVCC, check that NALU length is coded on the same number of bytes
+            if(extraLen)
+            {
+                uint32_t nalSize2=ADM_getNalSizeH264(extra,extraLen);
+                if(nalSize2 != nalSize)
+                {
+                    ADM_warning("NAL unit length size mismatch: %u vs %u\n",nalSize2,nalSize);
+                    cut=ADM_EDITOR_CUT_POINT_MISMATCH;
+                    BOWOUT
+                }
             }
             // Get SPS info
             ADM_SPSInfo sps2;
@@ -426,7 +440,7 @@ ADM_cutPointType ADM_Composer::checkSegmentStartsOnIntra(uint32_t segNo)
             if(AnnexB)
                 outcome=extractH264FrameType_startCode(img.data, img.dataLength, &(img.flags), &poc2, &sps, NULL);
             else
-                outcome=extractH264FrameType(img.data, img.dataLength, &(img.flags), &poc2, &sps, NULL);
+                outcome=extractH264FrameType(img.data, img.dataLength, nalSize, &(img.flags), &poc2, &sps, NULL);
             if(!outcome)
             {
                 ADM_warning("Cannot get H.264 frame type and Picture Order Count Least Significant Bits value.\n");
@@ -473,9 +487,12 @@ ADM_cutPointType ADM_Composer::checkSegmentStartsOnIntra(uint32_t segNo)
         bool AnnexB=false;
         uint8_t *extra;
         uint32_t extraLen=0;
+        uint32_t nalSize=0;
         demuxer->getExtraHeaderData(&extraLen,&extra);
         if(!extraLen)
             AnnexB=true;
+        else
+            nalSize=ADM_getNalSizeH265(extra,extraLen);
         // Allocate memory to hold compressed frame
         ADMCompressedImage img;
         buffer=new uint8_t[MAX_FRAME_LENGTH];
@@ -516,6 +533,17 @@ ADM_cutPointType ADM_Composer::checkSegmentStartsOnIntra(uint32_t segNo)
                 cut=ADM_EDITOR_CUT_POINT_MISMATCH;
                 BOWOUT
             }
+            // For HVCC, check that NALU length is coded on the same number of bytes
+            if(extraLen)
+            {
+                uint32_t nalSize2=ADM_getNalSizeH265(extra,extraLen);
+                if(nalSize2 != nalSize)
+                {
+                    ADM_warning("NAL unit length size mismatch: %u vs %u\n",nalSize2,nalSize);
+                    cut=ADM_EDITOR_CUT_POINT_MISMATCH;
+                    BOWOUT
+                }
+            }
             // Get SPS info
             ADM_SPSinfoH265 sps2;
             if(!getH265SPSInfo(vid,&sps2))
@@ -534,7 +562,6 @@ ADM_cutPointType ADM_Composer::checkSegmentStartsOnIntra(uint32_t segNo)
             MATCH(output_flag_present_flag)
             MATCH(field_info_present)
             MATCH(address_coding_length)
-            MATCH(nal_length_size)
             if(!match)
             {
                 ADM_warning("SPS mismatch, saved video will be broken.\n");
@@ -583,7 +610,7 @@ ADM_cutPointType ADM_Composer::checkSegmentStartsOnIntra(uint32_t segNo)
         if(AnnexB)
             outcome=extractH265FrameType_startCode(img.data, img.dataLength, &sps, &(img.flags), &poc2);
         else
-            outcome=extractH265FrameType(img.data, img.dataLength, &sps, &(img.flags), &poc2);
+            outcome=extractH265FrameType(img.data, img.dataLength, nalSize, &sps, &(img.flags), &poc2);
         if(!outcome)
         {
             ADM_warning("Cannot get HEVC frame type and Picture Order Count value.\n");
@@ -612,7 +639,7 @@ ADM_cutPointType ADM_Composer::checkSegmentStartsOnIntra(uint32_t segNo)
         if(AnnexB)
             outcome=extractH265FrameType_startCode(img.data, img.dataLength, &sps, &(img.flags), &poc);
         else
-            outcome=extractH265FrameType(img.data, img.dataLength, &sps, &(img.flags), &poc);
+            outcome=extractH265FrameType(img.data, img.dataLength, nalSize, &sps, &(img.flags), &poc);
         if(!outcome)
         {
             ADM_warning("Cannot get HEVC frame type and Picture Order Count value.\n");
@@ -726,6 +753,7 @@ static bool bFrameDroppable(uint32_t fcc,_VIDEOS *vid,ADMCompressedImage *img=NU
         uint32_t maxSize=img->dataLength;
         maxSize+=512; // arbitrary safety margin
         uint32_t size=maxSize;
+        uint32_t nalSize=0;
         notStackAllocator buf(maxSize);
         bool AnnexB=false;
         if(vid && vid->_aviheader)
@@ -735,6 +763,8 @@ static bool bFrameDroppable(uint32_t fcc,_VIDEOS *vid,ADMCompressedImage *img=NU
             vid->_aviheader->getExtraHeaderData(&extraLen,&extra);
             if(!extraLen)
                 AnnexB=true;
+            else
+                nalSize=ADM_getNalSizeH264(extra,extraLen);
         }
         if(AnnexB)
             size=ADM_convertFromAnnexBToMP4(img->data,img->dataLength,buf.data,maxSize);
@@ -746,7 +776,7 @@ static bool bFrameDroppable(uint32_t fcc,_VIDEOS *vid,ADMCompressedImage *img=NU
             bool gotSps=getH264SPSInfo(vid,&sps);
             if(!gotSps)
                 ADM_warning("No SPS info available to check fields.\n");
-            if(extractH264FrameType(buf.data,size,&(img->flags),NULL, gotSps? &sps : NULL,NULL) &&
+            if(extractH264FrameType(buf.data, size, nalSize, &(img->flags), NULL, gotSps? &sps : NULL,NULL) &&
                     (img->flags & AVI_NON_REF_FRAME) &&
                     !(img->flags & (AVI_STRUCTURE_TYPE_MASK+AVI_FIELD_STRUCTURE)))
                 return true;
@@ -1140,13 +1170,19 @@ bool ADM_Composer::getUserDataUnregistered(uint64_t start, uint8_t *buffer, uint
     demuxer->getVideoInfo(&info);
     if(!isH264Compatible(info.fcc))
         return false;
+    uint8_t *extra;
+    uint32_t extraLen=0;
+    demuxer->getExtraHeaderData(&extraLen,&extra);
+    if(!extraLen) // AnnexB
+        return false;
+    uint32_t nalSize=ADM_getNalSizeH264(extra,extraLen);
     ADMCompressedImage img;
     uint8_t *space=new uint8_t[MAX_FRAME_LENGTH];
     img.data=space;
     if(!demuxer->getFrame(0,&img))
         return false;
 
-    bool r=extractH264SEI(img.data,img.dataLength,buffer,max,length);
+    bool r=extractH264SEI(img.data,img.dataLength,nalSize,buffer,max,length);
 
     delete [] space;
     space=NULL;

avidemux/common/ADM_muxerGate/include/ADM_videoCopy.h
@@ -110,6 +110,7 @@ protected:
 #define ADM_H264_MAX_SEI_LENGTH 2048
         uint8_t         seiBuf[ADM_H264_MAX_SEI_LENGTH];
         uint32_t        seiLen;
+        uint32_t        nalSize;
 public:
                         ADM_videoStreamCopySeiInjector(uint64_t startTime,uint64_t endTime);
         virtual         ~ADM_videoStreamCopySeiInjector();

avidemux_core/ADM_coreUtils/include/ADM_videoInfoExtractor.h
@@ -58,7 +58,6 @@ public:
       output_flag_present_flag=false; // from pps
       field_info_present=false;
       address_coding_length=0;
-      nal_length_size=0;
     }
     int     width;
     int     height;
@@ -70,7 +69,6 @@ public:
     bool    output_flag_present_flag;
     bool    field_info_present;
     int     address_coding_length;
-    int     nal_length_size;
 };
 
 
@@ -79,20 +77,22 @@ ADM_COREUTILS6_EXPORT bool    ADM_SPSannexBToMP4(uint32_t dataLen,uint8_t *incom
 
 ADM_COREUTILS6_EXPORT bool    extractSPSInfo(uint8_t *data, uint32_t len,ADM_SPSInfo *info);
 ADM_COREUTILS6_EXPORT bool    extractSPSInfo_mp4Header(uint8_t *data, uint32_t len,ADM_SPSInfo *info);
-ADM_COREUTILS6_EXPORT uint32_t getRawH264SPS(uint8_t *data, uint32_t len, uint8_t *dest, uint32_t maxsize);
+ADM_COREUTILS6_EXPORT uint32_t getRawH264SPS(uint8_t *data, uint32_t len, uint32_t nalSize, uint8_t *dest, uint32_t maxsize);
+ADM_COREUTILS6_EXPORT uint32_t ADM_getNalSizeH264(uint8_t *extra, uint32_t len);
 
-ADM_COREUTILS6_EXPORT uint8_t extractH264FrameType(uint8_t *buffer,uint32_t len,uint32_t *flags,int *pocLsb,ADM_SPSInfo *sps,uint32_t *recovery=NULL);
+ADM_COREUTILS6_EXPORT uint8_t extractH264FrameType(uint8_t *buffer, uint32_t len, uint32_t nalSize, uint32_t *flags, int *pocLsb, ADM_SPSInfo *sps, uint32_t *recovery=NULL);
 ADM_COREUTILS6_EXPORT uint8_t extractH264FrameType_startCode(uint8_t *buffer,uint32_t len,uint32_t *flags,int *pocLsb,ADM_SPSInfo *sps,uint32_t *recovery=NULL);
-ADM_COREUTILS6_EXPORT bool    extractH264SEI(uint8_t *src, uint32_t inlen, uint8_t *dest, uint32_t bufsize, uint32_t *outlen); // dest may be NULL
+ADM_COREUTILS6_EXPORT bool    extractH264SEI(uint8_t *src, uint32_t inlen, uint32_t nalSize, uint8_t *dest, uint32_t bufsize, uint32_t *outlen); // dest may be NULL
 
 ADM_COREUTILS6_EXPORT bool    ADM_getH264SpsPpsFromExtraData(uint32_t extraLen,uint8_t *extra,
                                     uint32_t *spsLen,uint8_t **spsData,
                                     uint32_t *ppsLen,uint8_t **ppsData); // return a copy of pps/sps extracted
 
+ADM_COREUTILS6_EXPORT uint32_t ADM_getNalSizeH265(uint8_t *extra, uint32_t len);
 
 ADM_COREUTILS6_EXPORT bool    extractSPSInfoH265_mp4Header(uint8_t *data, uint32_t len,ADM_SPSinfoH265 *info); 
-ADM_COREUTILS6_EXPORT bool    extractSPSInfoH265(uint8_t *data, uint32_t len,ADM_SPSinfoH265 *info); 
-ADM_COREUTILS6_EXPORT bool    extractH265FrameType(uint8_t *buffer, uint32_t len, ADM_SPSinfoH265 *info, uint32_t *flags, int *poc);
+ADM_COREUTILS6_EXPORT bool    extractSPSInfoH265(uint8_t *data, uint32_t len, ADM_SPSinfoH265 *info);
+ADM_COREUTILS6_EXPORT bool    extractH265FrameType(uint8_t *buffer, uint32_t len, uint32_t nalSize, ADM_SPSinfoH265 *info, uint32_t *flags, int *poc);
 ADM_COREUTILS6_EXPORT bool    extractH265FrameType_startCode(uint8_t *buffer, uint32_t len, ADM_SPSinfoH265 *info, uint32_t *flags, int *poc);
 
 #define MAX_NALU_PER_CHUNK 60

avidemux_core/ADM_coreUtils/src/ADM_infoExtractorH264.cpp
@@ -53,6 +53,9 @@ extern int ff_h264_info(AVCodecParserContext *parser, int ticksPerFrame, ffSpsIn
 #define seiprintf(...) {}
 #endif
 
+extern bool ADM_findAnnexBStartCode(uint8_t *start, uint8_t *end, uint8_t *outstartcode,
+                uint32_t *offset, bool *fivebytes);
+
 /**
     \fn ADM_getH264SpsPpsFromExtraData
     \brief Returns a copy of PPS/SPS extracted from extrdata
@@ -93,8 +96,25 @@ bool ADM_SPSannexBToMP4(uint32_t dataLen,uint8_t *incoming,
     return true;
 }
 
-extern bool ADM_findAnnexBStartCode(uint8_t *start, uint8_t *end, uint8_t *outstartcode,
-                uint32_t *offset, bool *fivebytes);
+/**
+    \fn ADM_getNalSizeH264
+    \brief extract NALU length size from avcC header
+*/
+uint32_t ADM_getNalSizeH264(uint8_t *extra, uint32_t len)
+{
+    if(len < 9)
+    {
+        ADM_warning("Invalid H.264 extradata length %u\n",len);
+        return 0;
+    }
+    if(extra[0] != 1)
+    {
+        ADM_warning("Invalid H.264 extradata\n");
+        return 0;
+    }
+    return (extra[4] & 3) + 1;
+}
+
 /**
     \fn ADM_escapeH264
     \brief Add escape stuff
@@ -641,27 +661,30 @@ static bool getNalType (uint8_t *head, uint8_t *tail, uint32_t *flags, ADM_SPSIn
 }
 
 /**
-      \fn extractH264FrameType
-      \brief return frametype in flags (KEY_FRAME or 0).
-             To be used only with  mkv/mp4 nal type (i.e. no startcode)
-                    but 4 bytes NALU
-
+    \fn extractH264FrameType
+    \brief Parse access unit in buffer of size len, return frametype in flags (KEY/P/B).
+           nalSize should be either the NALU length size value retrieved from extradata or 0 for autodetect.
+           If ADM_SPSInfo is provided, return POC LSB or -1 if implicit or on error.
+           Return value: 1 on success, 0 on failure.
+           To be used only with AVCC (mkv/mp4) nal type (i.e. no startcode)
 */
-uint8_t extractH264FrameType(uint8_t *buffer, uint32_t len, uint32_t *flags, int *pocLsb, ADM_SPSInfo *sps, uint32_t *extRecovery)
+uint8_t extractH264FrameType(uint8_t *buffer, uint32_t len, uint32_t nalSize, uint32_t *flags, int *pocLsb, ADM_SPSInfo *sps, uint32_t *extRecovery)
 {
     uint8_t *head = buffer, *tail = buffer + len;
     uint8_t stream;
     uint32_t i, length = 0;
 
-    // Try to detect number of bytes used to code NAL length. Shaky.
-    uint32_t nalSize = 4;
-    for(i = 0; i < nalSize; i++)
-    {
-        length = (length << 8) + head[i];
-        if(i && length > len)
+    if(!nalSize || nalSize > 4)
+    { // Try to detect number of bytes used to code NAL length. Shaky.
+        nalSize = 4;
+        for(i = 0; i < nalSize; i++)
         {
-            nalSize = i;
-            break;
+            length = (length << 8) + head[i];
+            if(i && length > len)
+            {
+                nalSize = i;
+                break;
+            }
         }
     }
     uint32_t recovery=0xff;
@@ -836,21 +859,23 @@ uint8_t extractH264FrameType_startCode(uint8_t *buffer, uint32_t len, uint32_t *
  *  \fn extractH264SEI
  *  \brief If present, copy SEI containing x264 version info from access unit src to dest
  */
-bool extractH264SEI(uint8_t *src, uint32_t inlen, uint8_t *dest, uint32_t bufsize, uint32_t *outlen)
+bool extractH264SEI(uint8_t *src, uint32_t inlen, uint32_t nalSize, uint8_t *dest, uint32_t bufsize, uint32_t *outlen)
 {
     uint8_t *tail = src, *head = src + inlen;
     uint8_t stream;
     uint32_t i, length = 0;
 
-    // Try to detect NAL length size.
-    uint32_t nalSize = 4;
-    for(i = 0; i < nalSize; i++)
-    {
-        length = (length << 8) + tail[i];
-        if(i && length > inlen)
+    if(!nalSize || nalSize > 4)
+    { // Try to detect NAL length size.
+        nalSize = 4;
+        for(i = 0; i < nalSize; i++)
         {
-            nalSize = i;
-            break;
+            length = (length << 8) + tail[i];
+            if(i && length > inlen)
+            {
+                nalSize = i;
+                break;
+            }
         }
     }
     uint32_t unregistered = 0;
@@ -1000,7 +1025,7 @@ theEnd:
     \fn getRawH264SPS
     \brief Find the first SPS in mp4 style buffer and copy it to dest, return SPS length.
 */
-uint32_t getRawH264SPS(uint8_t *data, uint32_t len, uint8_t *dest, uint32_t maxsize)
+uint32_t getRawH264SPS(uint8_t *data, uint32_t len, uint32_t nalSize, uint8_t *dest, uint32_t maxsize)
 {
     if(!dest || !maxsize)
         return 0;
@@ -1008,15 +1033,18 @@ uint32_t getRawH264SPS(uint8_t *data, uint32_t len, uint8_t *dest, uint32_t maxs
     uint8_t *head=data, *tail=data+len;
     uint8_t stream;
 
-    uint32_t i, length=0, nalSize=4;
-    // Try to detect NAL length size.
-    for(i = 0; i < nalSize; i++)
-    {
-        length = (length << 8) + head[i];
-        if(i && length > len)
+    uint32_t i, length=0;
+    if(!nalSize || nalSize > 4)
+    { // Try to detect NAL length size.
+        nalSize = 4;
+        for(i = 0; i < nalSize; i++)
         {
-            nalSize = i;
-            break;
+            length = (length << 8) + head[i];
+            if(i && length > len)
+            {
+                nalSize = i;
+                break;
+            }
         }
     }
 

avidemux_core/ADM_coreUtils/src/ADM_infoExtractorH265.cpp
@@ -42,7 +42,6 @@ extern "C"
 extern const HEVCSPS *ff_hevc_parser_get_sps(AVCodecParserContext *parser);
 extern const HEVCPPS *ff_hevc_parser_get_pps(AVCodecParserContext *parser);
 extern const HEVCVPS *ff_hevc_parser_get_vps(AVCodecParserContext *parser);
-extern int ff_hevc_parser_get_nal_length_size(AVCodecParserContext *parser);
 }
 
 #include "../include/ADM_h265_tag.h"
@@ -151,7 +150,6 @@ static bool spsInfoFromParserContext(AVCodecParserContext *parser, ADM_SPSinfoH2
     const HEVCVPS *vps = ff_hevc_parser_get_vps(parser);
     const HEVCPPS *pps = ff_hevc_parser_get_pps(parser);
     spsinfo-> num_extra_slice_header_bits=0;
-    spsinfo->nal_length_size = ff_hevc_parser_get_nal_length_size(parser);
     spsinfo->output_flag_present_flag=false;
     if(sps)
     {
@@ -493,10 +491,10 @@ static bool decodeSliceHeaderH265(uint8_t *head, uint8_t *tail, uint32_t *flags,
 
 /**
  *  \fn extractH265FrameType
- *  \brief Parse access unit, find a slice and decode the header
+ *  \brief Parse access unit using given NALU length size nalSize (0: autodetect), find a slice and decode the header.
  *         The caller must set *poc to previous POC if applicable, else to INT_MIN
  */
-bool extractH265FrameType(uint8_t *buffer, uint32_t len, ADM_SPSinfoH265 *spsinfo, uint32_t *flags, int *poc)
+bool extractH265FrameType(uint8_t *buffer, uint32_t len, uint32_t nalSize, ADM_SPSinfoH265 *spsinfo, uint32_t *flags, int *poc)
 {
     if(!spsinfo || !flags || !poc)
         return false;
@@ -506,24 +504,19 @@ bool extractH265FrameType(uint8_t *buffer, uint32_t len, ADM_SPSinfoH265 *spsinf
     uint8_t nalType;
 
     uint32_t i, length = 0;
-    uint32_t nalSize = spsinfo->nal_length_size;
     if(!nalSize || nalSize > 4)
-    {
-        ADM_warning("Invalid NAL length size %d from parser, trying max (4)\n",spsinfo->nal_length_size);
+    { // Try to detect number of bytes used to code NAL length. Shaky.
         nalSize = 4;
-    }
-    // Check for short nalSize to be sure
-    for(i = 0; i < nalSize; i++)
-    {
-        length = (length << 8) + head[i];
-        if(i && length > len)
+        for(i = 0; i < nalSize; i++)
         {
-            nalSize = i;
-            ADM_warning("Reducing NAL length size to %u\n",nalSize);
-            break;
+            length = (length << 8) + head[i];
+            if(i && length > len)
+            {
+                nalSize = i;
+                break;
+            }
         }
     }
-    spsinfo->nal_length_size = (int)nalSize;
     *flags = 0;
 
     while(head + nalSize < tail)
@@ -664,5 +657,24 @@ bool extractH265FrameType_startCode(uint8_t *buffer, uint32_t len, ADM_SPSinfoH2
     }
     ADM_warning("No picture slice found in the buffer.\n");
     return false;
+}
+
+/**
+ *  \fn ADM_getNalSizeH265
+ *  \brief extract NALU length size from hvcC header
+ */
+uint32_t ADM_getNalSizeH265(uint8_t *extra, uint32_t len)
+{
+    if(len < 24)
+    {
+        ADM_warning("Invalid HEVC extradata length %u\n",len);
+        return 0;
+    }
+    if(extra[0] != 1)
+    {
+        ADM_warning("Invalid HEVC extradata.\n");
+        return 0;
+    }
+    return (extra[22] & 3) + 1;
 }
 // EOF

avidemux_core/ffmpeg_package/patches/libavcodec_hevc_parser.c.patch
@@ -1,6 +1,6 @@
 --- libavcodec/hevc_parser.c.orig	2019-12-31 22:35:23.000000000 +0100
-+++ libavcodec/hevc_parser.c	2020-04-01 15:21:20.711929323 +0200
-@@ -31,6 +31,14 @@
++++ libavcodec/hevc_parser.c	2020-04-16 23:51:04.726124182 +0200
+@@ -31,6 +31,13 @@
  #include "internal.h"
  #include "parser.h"
  
@@ -8,14 +8,13 @@
 +const HEVCSPS *ff_hevc_parser_get_sps(AVCodecParserContext *s);
 +const HEVCPPS *ff_hevc_parser_get_pps(AVCodecParserContext *s);
 +const HEVCVPS *ff_hevc_parser_get_vps(AVCodecParserContext *s);
-+int ff_hevc_parser_get_nal_length_size(AVCodecParserContext *s);
 +/* /MEANX */
 +
 +
  #define START_CODE 0x000001 ///< start_code_prefix_one_3bytes
  
  #define IS_IRAP_NAL(nal) (nal->type >= 16 && nal->type <= 23)
-@@ -171,6 +179,166 @@
+@@ -171,6 +178,156 @@
  }
  
  /**
@@ -165,16 +164,6 @@
 +
 +    vps = (HEVCVPS *)ref->data;
 +    return vps;
-+}
-+/**
-+    \fn ff_hevc_parser_get_nal_size_length
-+    \brief eum: get number of bytes used to code nal length
-+*/
-+int ff_hevc_parser_get_nal_length_size(AVCodecParserContext *s)
-+{
-+    HEVCParserContext *ctx = s->priv_data;
-+    if (!ctx) return 0;
-+    return ctx->nal_length_size;
 +}
 +// /MEANX
 +

avidemux_plugins/ADM_demuxers/Matroska/ADM_mkvIndexer.cpp
@@ -242,7 +242,7 @@ uint8_t mkvHeader::addIndexEntry(uint32_t track,ADM_ebml_file *parser,uint64_t w
             if(rpt)
                 memcpy(readBuffer,_tracks[0].headerRepeat,rpt);
             parser->readBin(readBuffer+rpt,size-3);
-            extractH264FrameType(readBuffer,rpt+size-3,&flags,NULL,NULL,&_H264Recovery);
+            extractH264FrameType(readBuffer,rpt+size-3,0,&flags,NULL,NULL,&_H264Recovery);
             if(flags & AVI_KEY_FRAME)
             {
                 printf("[MKV/H264] Frame %" PRIu32" is a keyframe\n",(uint32_t)Track->index.size());

commit 2169adc5b32443c6cbbbbe6e136ca4c93caa9769

[muxerGate/videoCopySeiInjector] Fix typo of doom which broke copy mode for H.264 streams with SEI unregistered data present when we actually injected SEI message
This was a regression from 4e69af151372cc9a39fc28345ac50408a26b8b9f.

avidemux/common/ADM_muxerGate/src/ADM_videoCopySeiInjector.cpp
@@ -97,7 +97,7 @@ bool  ADM_videoStreamCopySeiInjector::getPacket(ADMBitstream *out)
     while(tail + nalSize < head)
     {
         uint32_t i,length=0;
-        for(i=0; i<length; i++)
+        for(i=0; i<nalSize; i++)
             length=(length<<8)+tail[i];
         stream=*(tail+nalSize)&0x1F;
         switch(stream)