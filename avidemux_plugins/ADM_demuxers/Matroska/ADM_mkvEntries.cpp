mean00/avidemux2

commit 5f645f61adf9249ff2ac8c020b83a4ab9d7392cc

[demuxers/Matroska] Calculate approximate average bitrate for audio tracks where we cannot obtain the exact value, assume duration = video duration for now, set language for A_MS/ACM tracks too

avidemux_plugins/ADM_demuxers/Matroska/ADM_mkvEntries.cpp
@@ -246,6 +246,10 @@ uint8_t mkvHeader::analyzeOneTrack(void *head,uint32_t headlen)
       if(entry.trackType==2 && _nbAudioTrack<ADM_MKV_MAX_TRACKS)
       {
         mkvTrak *t=&(_tracks[1+_nbAudioTrack]);
+        t->language=entry.language;
+        t->wavHeader.bitspersample=16;
+        t->wavHeader.byterate=0; // to be set later
+        t->extraData=entry.extraData;
         t->extraDataLen=entry.extraDataLen;
         ADM_info("This track has %d bytes of  extradata\n",t->extraDataLen);
         // MS/ACM : ACMX
@@ -272,7 +276,7 @@ uint8_t mkvHeader::analyzeOneTrack(void *head,uint32_t headlen)
                         t->wavHeader.encoding=MKV_MUX_LATM;
                 }
                 if(t->wavHeader.encoding==MKV_MUX_LATM)
-                    t->wavHeader.byterate=(128000)>>3; // dummy value, FIXME
+                    t->wavHeader.byterate=0; // to be set later
                 delete [] entry.extraData;
                 t->streamIndex=entry.trackNo;
                 if(entry.defaultDuration)
@@ -307,17 +311,15 @@ uint8_t mkvHeader::analyzeOneTrack(void *head,uint32_t headlen)
                 }
             }
         }
+        if(entry.fcc==WAV_PCM || entry.fcc==WAV_LPCM)
+        {
+            t->wavHeader.byterate = t->wavHeader.bitspersample * entry.fq * entry.chan >> 3;
+        }
         //**
-         t->language=entry.language;
          t->wavHeader.encoding=entry.fcc;
          t->wavHeader.channels=entry.chan;
          t->wavHeader.frequency=entry.fq;
-         t->wavHeader.bitspersample=16;
-         t->wavHeader.byterate=(128000)>>3; //FIXME
          t->streamIndex=entry.trackNo;
-         t->extraData=entry.extraData;
-         
-         t->extraDataLen=entry.extraDataLen;
          if(entry.defaultDuration)
           t->_defaultFrameDuration=entry.defaultDuration;
          else

avidemux_plugins/ADM_demuxers/Matroska/ADM_mkv.cpp
@@ -310,6 +310,8 @@ _again:
           for(int i=0;i<_nbAudioTrack;i++)
           {
             rescaleTrack(&(_tracks[1+i]),duration32);
+            if(!_tracks[1+i].duration)
+                _tracks[1+i].duration=_tracks[0].duration; // FIXME
             switch(_tracks[1+i].wavHeader.encoding)
             {
                 case WAV_OGG_VORBIS:            

avidemux_plugins/ADM_demuxers/Matroska/ADM_mkvIndexer.cpp
@@ -324,6 +324,7 @@ uint8_t mkvHeader::addIndexEntry(uint32_t track,ADM_ebml_file *parser,uint64_t w
             }
     }
   }
+  Track->_sizeInBytes+=size;
   Track->index.append(ix);
 
   return 1;

avidemux_plugins/ADM_demuxers/Matroska/ADM_mkv_audio.cpp
@@ -47,6 +47,9 @@ mkvAccess::mkvAccess(const char *name,mkvTrak *track)
   _currentLace=_maxLace=0;
   goToBlock(0);
 
+  if(track->duration && !track->wavHeader.byterate)
+    track->wavHeader.byterate=(uint32_t)(track->_sizeInBytes*1000000LL/track->duration);
+
   /* Check that it is really EAC3 */
   if(_track->wavHeader.encoding==WAV_EAC3)
   {

commit 3666b466d344ac5b2d09dbae22b1764d78f13a14

[demuxers/Matroska] Fix crash from double delete [] if an audio track has an ACM compatibility header but no extradata, caused again by 5f645f61adf

avidemux_plugins/ADM_demuxers/Matroska/ADM_mkvEntries.cpp
@@ -280,9 +280,6 @@ uint8_t mkvHeader::analyzeOneTrack(void *head,uint32_t headlen)
         t->language=entry.language;
         t->wavHeader.bitspersample=16;
         t->wavHeader.byterate=0; // to be set later
-        t->extraData=entry.extraData;
-        t->extraDataLen=entry.extraDataLen;
-        ADM_info("This track has %d bytes of  extradata\n",t->extraDataLen);
         // MS/ACM : ACMX
         if(0x100001==entry.fcc)
         {
@@ -326,8 +323,6 @@ uint8_t mkvHeader::analyzeOneTrack(void *head,uint32_t headlen)
                 ADM_info("Recreating aac extradata..\n");
                 entry.extraData = new uint8_t[5];
                 createAACExtraData(entry.codecId.c_str(),&entry);
-                t->extraData=entry.extraData;
-                t->extraDataLen=entry.extraDataLen;
             }else
             {
                 // check AAC infoata
@@ -361,6 +356,9 @@ uint8_t mkvHeader::analyzeOneTrack(void *head,uint32_t headlen)
             t->headerRepeatSize=entry.headerRepeatSize;
             memcpy(t->headerRepeat,entry.headerRepeat,hdr);
         }
+        t->extraData=entry.extraData;
+        t->extraDataLen=entry.extraDataLen;
+        ADM_info("This track has %d bytes of extradata\n",t->extraDataLen);
 
         _nbAudioTrack++;
         return 1;