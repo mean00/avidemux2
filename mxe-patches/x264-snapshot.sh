e347dff8bb7c5a8f8a5c99b5177d6b0c3a21bdf3
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2021-01-20 00:24:54 +0100

    [build/cross] Automate MXE patch generation for x264 snapshot

--- mxe-patches/x264-snapshot.sh
+++ mxe-patches/x264-snapshot.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # Create a snapshot of the current head of the stable branch in the current
-# directory, e.g. as x264-0.159-20200409git296494a.tar.bz2
-# Adapted from RPM Fusion x264 Fedora package.
+# directory, e.g. as x264-0.159-20200409git296494a.tar.bz2 and generate
+# the corresponding patch for MXE.
+# Derived from RPM Fusion x264 Fedora package.
+failMsg()
+{
+    echo "** $1 **"
+    exit 1
+}
 
 set -e
 
@@ -20,7 +26,7 @@ branch=stable
 commit=HEAD
 
 pushd "$tmp"
-git clone https://code.videolan.org/videolan/x264.git -b ${branch}
+git clone https://code.videolan.org/videolan/x264.git -b ${branch} || failMsg "Cannot download source code"
 
 cd ${package}
 tag=$(git rev-list HEAD -n 1 | cut -c 1-7)
@@ -28,5 +34,45 @@ git checkout ${commit}
 ./version.sh > version.h
 API="$(grep '#define X264_BUILD' < x264.h | sed 's/^.* \([1-9][0-9]*\).*$/\1/')"
 date=$(git log -1 --format=%cd --date=short | tr -d \-)
-git archive --prefix="${package}-0.$API-${date}git${tag}/" --format=tar stable | bzip2 > "$pwd"/${package}-0.$API-${date}git${tag}.tar.bz2
+version="0.$API-${date}git${tag}"
+outfile="${package}-${version}.tar.bz2"
+git archive --prefix="${package}-${version}/" --format=tar stable | bzip2 > "$pwd/${outfile}" || failMsg "Cannot create archive"
 popd >/dev/null
+
+# test run
+sha256sum "${outfile}" >/dev/null || failMsg "Cannot calculate checksum"
+# now for real
+checksum=$(sha256sum "${outfile}" | cut -d \  -f 1)
+
+cat > x264.patch <<EOF
+diff --git a/src/x264.mk b/src/x264.mk
+index 0170301b..1189ccd0 100644
+--- a/src/x264.mk
++++ b/src/x264.mk
+@@ -3,20 +3,13 @@
+ PKG             := x264
+ \$(PKG)_WEBSITE  := https://www.videolan.org/developers/x264.html
+ \$(PKG)_IGNORE   :=
+-\$(PKG)_VERSION  := 20180806-2245
+-\$(PKG)_CHECKSUM := 9f876c88aeb21fa9315e4a078931faf6fc0d3c3f47e05a306d2fdc62ea0afea2
+-\$(PKG)_SUBDIR   := \$(PKG)-snapshot-\$(\$(PKG)_VERSION)
+-\$(PKG)_FILE     := \$(PKG)-snapshot-\$(\$(PKG)_VERSION).tar.bz2
+-\$(PKG)_URL      := https://download.videolan.org/pub/videolan/\$(PKG)/snapshots/\$(\$(PKG)_FILE)
++\$(PKG)_VERSION  := ${version}
++\$(PKG)_CHECKSUM := ${checksum}
++\$(PKG)_SUBDIR   := \$(PKG)-\$(\$(PKG)_VERSION)
++\$(PKG)_FILE     := \$(PKG)-\$(\$(PKG)_VERSION).tar.bz2
++\$(PKG)_URL      :=
+ \$(PKG)_DEPS     := cc liblsmash \$(BUILD)~nasm
+ 
+-define \$(PKG)_UPDATE
+-    \$(WGET) -q -O- 'https://git.videolan.org/?p=x264.git;a=shortlog' | \\
+-    \$(SED) -n 's,.*\([0-9]\{4\}\)-\([0-9]\{2\}\)-\([0-9]\{2\}\).*,\1\2\3-2245,p' | \\
+-    \$(SORT) | \\
+-    tail -1
+-endef
+-
+ define \$(PKG)_BUILD
+     cd '\$(BUILD_DIR)' && AS='\$(PREFIX)/\$(BUILD)/bin/nasm' '\$(SOURCE_DIR)/configure'\\
+         \$(MXE_CONFIGURE_OPTS) \\
+EOF
