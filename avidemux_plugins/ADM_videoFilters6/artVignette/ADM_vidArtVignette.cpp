b5ea4dc29b8afd5b9cb839f23b94fd5bb84af2a8
Adam <szullo.adam@gmail.com>
2021-01-26 22:04:46 +0100

    [vf/artVignette] Filter for lens vignetting effect

---
+++ avidemux_plugins/ADM_videoFilters6/artVignette/ADM_vidArtVignette.cpp
@@ -0,0 +1,276 @@
+/***************************************************************************
+                          Vignette filter 
+    Algorithm:
+        Copyright (C) 2011 Simon Andreas Eugster (simon.eu@gmail.com)
+    Ported to Avidemux:
+        Copyright 2021 szlldm
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#define _USE_MATH_DEFINES // some compilers do not export M_PI etc.. if GNU_SOURCE or that is defined, let's do that
+#include <cmath>
+#include "ADM_default.h"
+#include "ADM_coreVideoFilter.h"
+#include "ADM_coreVideoFilterInternal.h"
+#include "DIA_factory.h"
+#include "artVignette.h"
+#include "artVignette_desc.cpp"
+
+#ifndef M_PI
+#define M_PI   3.14159265358979323846
+#endif
+#ifndef M_PI_2
+#define M_PI_2 1.57079632679489661923
+#endif
+
+extern uint8_t DIA_getArtVignette(artVignette *param, ADM_coreVideoFilter *in);
+/**
+    \class ADMVideoArtVignette
+*/
+class  ADMVideoArtVignette:public ADM_coreVideoFilter
+{
+
+  protected:
+    void            update(void);
+    artVignette     _param;
+    float           _aspect;
+    float           _center;
+    float           _soft;
+    float *         _filterMask;
+  public:
+    ADMVideoArtVignette(ADM_coreVideoFilter *in,CONFcouple *couples);
+    ~ADMVideoArtVignette();
+
+    virtual const char   *getConfiguration(void);          /// Return  current configuration as a human readable string
+    virtual bool         getNextFrame(uint32_t *fn,ADMImage *image);    /// Return the next image
+    virtual bool         getCoupledConf(CONFcouple **couples) ;   /// Return the current filter configuration
+    virtual void         setCoupledConf(CONFcouple *couples);
+    virtual bool         configure(void) ;                 /// Start graphical user interface
+
+};
+
+
+
+// Add the hook to make it valid plugin
+//DECLARE_VIDEO_FILTER(   ADMVideoArtVignette,   // Class
+DECLARE_VIDEO_FILTER_PARTIALIZABLE(   ADMVideoArtVignette,   // Class
+                                      1,0,0,              // Version
+                                      ADM_UI_TYPE_BUILD,         // UI
+                                      VF_ART,            // Category
+                                      "artVignette",            // internal name (must be uniq!)
+                                      QT_TRANSLATE_NOOP("artVignette","Vignette"),            // Display name
+                                      QT_TRANSLATE_NOOP("artVignette","Lens vignetting effect, ported from frei0r.") // Description
+                                  );
+/**
+    \fn ArtVignetteCreateMask
+*/
+void ArtVignetteCreateMask(float * mask, int w, int h, float aspect, float center, float soft)
+{
+    if (!mask) return;
+    float scaleX = 1;
+    float scaleY = 1;
+    float scale = std::fabs(aspect-.5)*2;
+    scale = 1 + 4*std::pow(scale, 3);
+    soft = 5*std::pow(float(1)-soft,2)+.01;
+    if (aspect > 0.5) {
+        scaleX = scale;
+    } else {
+        scaleY = scale;
+    }
+    int cx = w/2;
+    int cy = h/2;
+    float rmax = std::sqrt(std::pow(float(cx), 2) + std::pow(float(cy), 2));
+    float r,xsq,ysq,cos4;
+    //for (int y = 0; y < h; y++) {
+    //    for (int x = 0; x < w; x++) {
+    // speed up 4x by exploiting H-W symmetries (by szlldm)
+    for (int y = 0; y < cy; y++) {
+        for (int x = 0; x < cx; x++) {
+
+            // Euclidian distance to the center, normalized to [0,1]
+            xsq = scaleX*(x-cx);
+            xsq = xsq * xsq;
+            ysq = scaleY*(y-cy);
+            ysq = ysq * ysq;
+            r = std::sqrt(xsq + ysq)/rmax;
+
+            // Subtract the clear center
+            r -= center;
+
+            if (r <= 0) {
+                // Clear center: Do not modify the brightness here
+                mask[y*w+x] = 1;
+            } else {
+                r *= soft;
+                if (r > M_PI_2) {
+                    mask[y*w+x] = 0;
+                } else {
+                    cos4 = std::cos(r);
+                    cos4 = cos4 * cos4;
+                    cos4 = cos4 * cos4;	// power of 4
+                    mask[y*w+x] = cos4; //std::pow(std::cos(r), 4);
+                }
+            }
+
+        }
+    }
+
+    // make top half
+    for (int y = 0; y < cy; y++) {
+        for (int x = 0; x < cx; x++) {
+             mask[y*w+((w-1) - x)] = mask[y*w+x];
+        }
+    }
+
+    // mirror down to bottom
+    for (int y = 0; y < cy; y++) {
+        memcpy(&mask[((h-1) - y)*w],&mask[y*w],sizeof(float)*w);
+    }
+}
+/**
+    \fn ArtVignetteProcess_C
+*/
+void ArtVignetteProcess_C(ADMImage *img, float * mask)
+{
+    int width=img->GetWidth(PLANAR_Y); 
+    int height=img->GetHeight(PLANAR_Y);
+    int stride;
+    uint8_t * ptr;
+    float pixel;
+
+    if(img->_range == ADM_COL_RANGE_MPEG)
+        img->expandColorRange();
+
+    // Y plane
+    stride=img->GetPitch(PLANAR_Y);
+    ptr=img->GetWritePtr(PLANAR_Y);
+    for(int y=0;y<height;y++)
+    {
+        for (int x=0;x<width;x++)
+        {
+            pixel = ptr[x];
+            ptr[x] = (uint8_t)std::round(pixel * mask[x+y*width]);
+        }
+        ptr+=stride;
+    }
+
+    // UV planes
+    for (int p=1; p<3; p++)
+    {
+        stride=img->GetPitch((ADM_PLANE)p);
+        ptr=img->GetWritePtr((ADM_PLANE)p);
+        for(int y=0;y<height/2;y++)	// 4:2:0
+        {
+            for (int x=0;x<width/2;x++)
+            {
+                pixel = ptr[x];
+                pixel -= 128;
+                ptr[x] = (uint8_t)std::round( (pixel * mask[x*2+y*2*width]) + 128);
+            }
+            ptr+=stride;
+        }
+    }
+}
+
+/**
+    \fn configure
+*/
+bool ADMVideoArtVignette::configure()
+{
+    uint8_t r=0;
+
+    r=  DIA_getArtVignette(&_param, previousFilter);
+    if(r) update();
+    return r;
+}
+/**
+    \fn getConfiguration
+*/
+const char   *ADMVideoArtVignette::getConfiguration(void)
+{
+    static char s[256];
+    snprintf(s,255," Aspect ratio: %.2f, Size of the unaffected center: %.2f, Softness: %.2f",_param.aspect,_param.center,_param.soft);
+    return s;
+}
+/**
+    \fn ctor
+*/
+ADMVideoArtVignette::ADMVideoArtVignette(  ADM_coreVideoFilter *in,CONFcouple *couples)  :ADM_coreVideoFilter(in,couples)
+{
+    if(!couples || !ADM_paramLoad(couples,artVignette_param,&_param))
+    {
+        _param.aspect =0.5;
+        _param.center =0.0;
+        _param.soft   =0.6;
+    }
+    _filterMask = new float[info.width * info.height];
+    update();
+}
+/**
+    \fn update
+*/
+void ADMVideoArtVignette::update(void)
+{
+    _aspect=_param.aspect;
+    _center=_param.center;
+    _soft=_param.soft;
+    ArtVignetteCreateMask(_filterMask, info.width, info.height, _aspect, _center, _soft);
+}
+/**
+    \fn dtor
+*/
+ADMVideoArtVignette::~ADMVideoArtVignette()
+{
+    if (_filterMask) delete _filterMask;
+}
+/**
+    \fn getCoupledConf
+*/
+bool ADMVideoArtVignette::getCoupledConf(CONFcouple **couples)
+{
+    return ADM_paramSave(couples, artVignette_param,&_param);
+}
+
+void ADMVideoArtVignette::setCoupledConf(CONFcouple *couples)
+{
+    ADM_paramLoad(couples, artVignette_param, &_param);
+}
+
+/**
+    
+*/
+
+/**
+    \fn getNextFrame
+    \brief
+*/
+bool ADMVideoArtVignette::getNextFrame(uint32_t *fn,ADMImage *image)
+{
+    /*
+    ADMImage *src;
+    src=vidCache->getImage(nextFrame);
+    if(!src)
+        return false; // EOF
+    *fn=nextFrame++;
+    image->copyInfo(src);
+    image->copyPlane(src,image,PLANAR_Y); // Luma is untouched
+    src = image;
+
+    DoFilter(...);
+
+    vidCache->unlockAll();
+    */
+    if(!previousFilter->getNextFrame(fn,image)) return false;
+
+    ArtVignetteProcess_C(image, _filterMask);
+
+    return 1;
+}
+
---
+++ avidemux_plugins/ADM_videoFilters6/artVignette/qt4/Q_artVignette.cpp
@@ -0,0 +1,154 @@
+/***************************************************************************
+                          DIA_crop.cpp  -  description
+                             -------------------
+
+			    GUI for cropping including autocrop
+			    +Revisted the Gtk2 way
+			     +Autocrop now in RGB space (more accurate)
+
+    begin                : Fri May 3 2002
+    copyright            : (C) 2002/2007 by mean
+    email                : fixounet@free.fr
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "Q_artVignette.h"
+#include "ADM_toolkitQt.h"
+#include "math.h"
+
+//
+//	Video is in YV12 Colorspace
+//
+//
+Ui_artVignetteWindow::Ui_artVignetteWindow(QWidget *parent, artVignette *param,ADM_coreVideoFilter *in) : QDialog(parent)
+{
+    uint32_t width,height;
+        ui.setupUi(this);
+        lock=0;
+        // Allocate space for green-ised video
+        width=in->getInfo()->width;
+        height=in->getInfo()->height;
+
+        canvas=new ADM_QCanvas(ui.graphicsView,width,height);
+
+        myFly=new flyArtVignette( this,width, height,in,canvas,ui.horizontalSlider);
+        memcpy(&(myFly->param),param,sizeof(artVignette));
+        myFly->filterW = width;
+        myFly->filterH = height;
+        myFly->filterMask = new float[width*height];
+        myFly->_cookie=&ui;
+        myFly->addControl(ui.toolboxLayout);
+        myFly->upload();
+        myFly->sliderChanged();
+
+        connect( ui.horizontalSlider,SIGNAL(valueChanged(int)),this,SLOT(sliderUpdate(int)));
+#define SPINNER(x) connect( ui.horizontalSlider##x,SIGNAL(valueChanged(int)),this,SLOT(valueChanged(int))); 
+        SPINNER(Aspect);
+        SPINNER(Center);
+        SPINNER(Soft);
+
+        setModal(true);
+}
+void Ui_artVignetteWindow::sliderUpdate(int foo)
+{
+    myFly->sliderChanged();
+}
+void Ui_artVignetteWindow::gather(artVignette *param)
+{
+    myFly->download();
+    memcpy(param,&(myFly->param),sizeof(artVignette));
+}
+Ui_artVignetteWindow::~Ui_artVignetteWindow()
+{
+    if(myFly) {
+        if (myFly->filterMask) delete myFly->filterMask;
+        delete myFly;
+    }
+    myFly=NULL; 
+    if(canvas) delete canvas;
+    canvas=NULL;
+}
+void Ui_artVignetteWindow::valueChanged( int f )
+{
+    if(lock) return;
+    lock++;
+    myFly->download();
+    myFly->sameImage();
+    lock--;
+}
+
+void Ui_artVignetteWindow::resizeEvent(QResizeEvent *event)
+{
+    if(!canvas->height())
+        return;
+    uint32_t graphicsViewWidth = canvas->parentWidget()->width();
+    uint32_t graphicsViewHeight = canvas->parentWidget()->height();
+    myFly->fitCanvasIntoView(graphicsViewWidth,graphicsViewHeight);
+    myFly->adjustCanvasPosition();
+}
+
+void Ui_artVignetteWindow::showEvent(QShowEvent *event)
+{
+    QDialog::showEvent(event);
+    myFly->adjustCanvasPosition();
+    canvas->parentWidget()->setMinimumSize(30,30); // allow resizing after the dialog has settled
+}
+
+#define MYSPIN(x) w->horizontalSlider##x
+#define MYCHECK(x) w->checkBox##x
+//************************
+
+extern void ArtVignetteCreateMask(float * mask, int w, int h, float aspect, float center, float soft);
+
+uint8_t flyArtVignette::upload(void)
+{
+    Ui_artVignetteDialog *w=(Ui_artVignetteDialog *)_cookie;
+    MYSPIN(Aspect)->setValue((int)round(param.aspect * 100.0));
+    MYSPIN(Center)->setValue((int)round(param.center * 100.0));
+    MYSPIN(Soft)->setValue((int)round(param.soft * 100.0));
+    ArtVignetteCreateMask(filterMask, filterW, filterH, param.aspect, param.center, param.soft);
+    return 1;
+}
+uint8_t flyArtVignette::download(void)
+{
+    Ui_artVignetteDialog *w=(Ui_artVignetteDialog *)_cookie;
+    param.aspect=(float)MYSPIN(Aspect)->value() / 100.0;
+    param.center=(float)MYSPIN(Center)->value() / 100.0;
+    param.soft=(float)MYSPIN(Soft)->value() / 100.0;
+    ArtVignetteCreateMask(filterMask, filterW, filterH, param.aspect, param.center, param.soft);
+    return 1;
+}
+
+/**
+    \fn     DIA_getCropParams
+    \brief  Handle crop dialog
+*/
+uint8_t DIA_getArtVignette(artVignette *param,ADM_coreVideoFilter *in)
+{
+    uint8_t ret=0;
+    Ui_artVignetteWindow dialog(qtLastRegisteredDialog(), param,in);
+
+    qtRegisterDialog(&dialog);
+
+    if(dialog.exec()==QDialog::Accepted)
+    {
+        dialog.gather(param); 
+        ret=1;
+    }
+
+    qtUnregisterDialog(&dialog);
+
+    return ret;
+}
+//____________________________________
+// EOF
+
+
