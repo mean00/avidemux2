74c1dc42143f080f2520fed01da73b559d2e0841
Adam <szullo.adam@gmail.com>
2021-01-27 01:03:33 +0100

    [vf/artPixelize] Pixeliser filter with free PAR

---
+++ avidemux_plugins/ADM_videoFilters6/artPixelize/cli/DIA_artPixelize.cpp
@@ -0,0 +1,11 @@
+#include "ADM_image.h"
+#include "ADM_default.h"
+#include "ADM_coreVideoFilter.h"
+
+#include "../artPixelize.h"
+#include "DIA_factory.h"
+uint8_t DIA_getArtPixelize(artPixelize *param, ADM_coreVideoFilter *in)
+{
+    
+    return 0;
+}
---
+++ avidemux_plugins/ADM_videoFilters6/artPixelize/qt4/DIA_flyArtPixelize.h
@@ -0,0 +1,34 @@
+/***************************************************************************
+  \fn     DIA_flyArtPixelize
+  \author (C) 2004/2012 by mean/fixounet@free.fr
+  \brief  Ui for artPixelize
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef FLY_ARTPixelize_H
+#define FLY_ARTPixelize_H
+#include "artPixelize.h"
+/**
+    \class flyArtPixelize
+*/
+class flyArtPixelize : public ADM_flyDialogYuv
+{
+
+  public:
+    artPixelize  param;
+  public:
+    uint8_t    processYuv(ADMImage* in, ADMImage *out);
+    uint8_t    download(void);
+    uint8_t    upload(void);
+    uint8_t    update(void);
+    flyArtPixelize (QDialog *parent,uint32_t width,uint32_t height,ADM_coreVideoFilter *in,
+                                    ADM_QCanvas *canvas, ADM_QSlider *slider) : ADM_flyDialogYuv(parent, width, height, in, canvas, slider, RESIZE_AUTO) {};
+};
+#endif
