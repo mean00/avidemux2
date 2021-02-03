04b88e343fe5d0f676d017ff64df8982bd60fc06
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2021-02-03 13:06:14 +0100

    [core] Fix build failure on Ubuntu 18.04.x

--- avidemux_core/ADM_core/include/ADM_files.h
+++ avidemux_core/ADM_core/include/ADM_files.h
@@ -47,8 +47,9 @@ ADM_CORE6_EXPORT uint8_t ADM_renameFile(const char *source, const char *target);
 ADM_CORE6_EXPORT char *ADM_getInstallRelativePath(const char *base1, const char *base2=NULL,const char *base3=NULL);
 /* Returns the full path relative to .avidemux dir i.e. /home/fx/... +base1/base2 needs to be deleted []*/
 ADM_CORE6_EXPORT char *ADM_getHomeRelativePath(const char *base1, const char *base2=NULL,const char *base3=NULL);
-#endif
+/* Populate list with a sorted list of files in directory base matching extension ext, return 1 on success, else 0 */
 ADM_CORE6_EXPORT uint8_t buildDirectoryContent(const char *base, std::vector<std::string> *list, const char *ext);
+#endif
 
 #ifdef _WIN32
 #define ADM_SEPARATOR "\\"
