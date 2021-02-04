mean00/avidemux2

commit 8cb1d7cb804b3424d5a6497fa4272a79671b5cdc

[muxers/Mp4] Add option for a custom display aspect ratio, calculated from display width

avidemux_plugins/ADM_muxers/muxerMp4/muxerMP4Config.cpp
@@ -27,6 +27,7 @@ bool movConfigure(void)
 {
         uint32_t fmt = muxerConfig.muxerType;
         uint32_t dar = muxerConfig.aspectRatio;
+        uint32_t diw = muxerConfig.displayWidth;
         uint32_t rot = muxerConfig.rotation;
         uint32_t opt = muxerConfig.optimize;
         uint32_t clk = muxerConfig.clockfreq;
@@ -57,9 +58,17 @@ bool movConfigure(void)
 #endif
         diaElemMenu  menuOptimize(&opt,QT_TRANSLATE_NOOP("mp4muxer","Optimize for Streaming"),NB_OPTIMIZE,streamOpt,"");
         diaElemToggle forceAR(&force,QT_TRANSLATE_NOOP("mp4muxer","Force aspect ratio"));
-        diaMenuEntry aspect[]={{STANDARD,"4:3"},{WIDE,"16:9"},{UNI,"18:9"},{CINEMA,"64:27"}};
-        diaElemMenu  menuAspect(&dar,QT_TRANSLATE_NOOP("mp4muxer","Aspect Ratio (DAR)"),4,aspect,"");
+        diaMenuEntry aspect[]={
+            {STANDARD,"4:3"},
+            {WIDE,"16:9"},
+            {UNI,"18:9"},
+            {CINEMA,"64:27"},
+            {CUSTOM,QT_TRANSLATE_NOOP("mp4muxer","Derived from display width")}
+        };
+        diaElemMenu  menuAspect(&dar,QT_TRANSLATE_NOOP("mp4muxer","Aspect Ratio (DAR)"),5,aspect,"");
+        diaElemUInteger dWidth(&diw,QT_TRANSLATE_NOOP("mp4muxer","Display Width"),16,65535);
         forceAR.link(1,&menuAspect);
+        menuAspect.link(aspect+4,1,&dWidth);
         diaMenuEntry rotation[]={
             {MP4_MUXER_ROTATE_0,QT_TRANSLATE_NOOP("mp4muxer","Do not rotate")},
             {MP4_MUXER_ROTATE_90,QT_TRANSLATE_NOOP("mp4muxer","90°")},
@@ -81,9 +90,9 @@ bool movConfigure(void)
         diaElemMenu menuClock(&clk,QT_TRANSLATE_NOOP("mp4muxer","Time scale"),8,clockFrequencies,NULL);
 
 #ifndef MUXER_IS_MOV
-        diaElem *tabs[]={&menuFormat,&menuOptimize,&forceAR,&menuAspect,&menuRotation,&menuClock};
+        diaElem *tabs[]={&menuFormat,&menuOptimize,&forceAR,&menuAspect,&dWidth,&menuRotation,&menuClock};
 #else
-        diaElem *tabs[]={            &menuOptimize,&forceAR,&menuAspect,&menuRotation,&menuClock};
+        diaElem *tabs[]={            &menuOptimize,&forceAR,&menuAspect,&dWidth,&menuRotation,&menuClock};
 #endif
 
         if( diaFactoryRun(QT_TRANSLATE_NOOP("mp4muxer","MP4 Muxer"),NB_TABS,tabs))
@@ -92,6 +101,7 @@ bool movConfigure(void)
             muxerConfig.optimize=(MP4_MUXER_OPTIMIZE)opt;
             muxerConfig.forceAspectRatio=force;
             muxerConfig.aspectRatio=(MP4_MUXER_DAR)dar;
+            muxerConfig.displayWidth=diw;
             muxerConfig.rotation=(MP4_MUXER_ROTATION)rot;
             muxerConfig.clockfreq=(MP4_MUXER_CLOCK_FREQUENCIES)clk;
             return true;

avidemux_plugins/ADM_muxers/muxerMp4/mp4_muxer.conf
@@ -3,6 +3,7 @@ uint32_t:muxerType
 uint32_t:optimize
 bool:forceAspectRatio
 uint32_t:aspectRatio
+uint32_t:displayWidth
 uint32_t:rotation
 uint32_t:clockfreq
 }

avidemux_plugins/ADM_muxers/muxerMp4/mp4_muxer.h
@@ -6,6 +6,7 @@ uint32_t muxerType;
 uint32_t optimize;
 bool forceAspectRatio;
 uint32_t aspectRatio;
+uint32_t displayWidth;
 uint32_t rotation;
 uint32_t clockfreq;
 }mp4_muxer;

avidemux_plugins/ADM_muxers/muxerMp4/mp4_muxer_desc.cpp
@@ -4,6 +4,7 @@ extern const ADM_paramList mp4_muxer_param[]={
  {"optimize",offsetof(mp4_muxer,optimize),"uint32_t",ADM_param_uint32_t},
  {"forceAspectRatio",offsetof(mp4_muxer,forceAspectRatio),"bool",ADM_param_bool},
  {"aspectRatio",offsetof(mp4_muxer,aspectRatio),"uint32_t",ADM_param_uint32_t},
+ {"displayWidth",offsetof(mp4_muxer,displayWidth),"uint32_t",ADM_param_uint32_t},
  {"rotation",offsetof(mp4_muxer,rotation),"uint32_t",ADM_param_uint32_t},
  {"clockfreq",offsetof(mp4_muxer,clockfreq),"uint32_t",ADM_param_uint32_t},
 {NULL,0,NULL}

avidemux_plugins/ADM_muxers/muxerMp4/muxerMP4.cpp
@@ -35,6 +35,7 @@ mp4_muxer muxerConfig=
     MP4_MUXER_OPT_FASTSTART,
     false,
     WIDE,
+    1280,
     MP4_MUXER_ROTATE_0,
     MP4_MUXER_CLOCK_FREQ_AUTO
 };
@@ -202,24 +203,28 @@ bool MOVCLASS::open(const char *file, ADM_videoStream *s, uint32_t nbAudioTrack,
             float w=h;
             switch (muxerConfig.aspectRatio)
             {
-                case 0:
+                case STANDARD:
                     w*=4.;
                     w/=3.;
                     break;
-                case 1:
+                case WIDE:
                     w*=16.;
                     w/=9.;
                     break;
-                case 2:
+                case UNI:
                     w*=2.;
                     break;
-                case 3:
+                case CINEMA:
                     w*=64.;
                     w/=27.;
                     break;
+                case CUSTOM:
+                default:
+                    break;
             }
             int num=1,den=1;
-            av_reduce(&num, &den, (uint32_t)w, s->getWidth(),65535);
+            int64_t iw64=(muxerConfig.aspectRatio==CUSTOM)? muxerConfig.displayWidth : w;
+            av_reduce(&num, &den, iw64, s->getWidth(),65535);
             par->sample_aspect_ratio.num=num;
             par->sample_aspect_ratio.den=den;
             video_st->sample_aspect_ratio.num=num;

avidemux_plugins/ADM_muxers/muxerMp4/muxerMP4.h
@@ -36,10 +36,11 @@ typedef enum
 
 typedef enum
 {
-    STANDARD,
-    WIDE,
-    UNI,
-    CINEMA
+    STANDARD=0,
+    WIDE=1,
+    UNI=2,
+    CINEMA=3,
+    CUSTOM=4
 }MP4_MUXER_DAR;
 
 typedef enum

commit 25ac84cdfbd98d9bfddfe947b7676acc255767a7

[muxerMp4] Fix timebase combobox missing, regressed by 8cb1d7cb804

avidemux_plugins/ADM_muxers/muxerMp4/muxerMP4Config.cpp
@@ -51,9 +51,9 @@ bool movConfigure(void)
 
 #ifndef MUXER_IS_MOV
 #   define NB_OPTIMIZE 3
-#   define NB_TABS 6
+#   define NB_TABS 7
 #else
-#   define NB_TABS 5
+#   define NB_TABS 6
 #   define NB_OPTIMIZE 2
 #endif
         diaElemMenu  menuOptimize(&opt,QT_TRANSLATE_NOOP("mp4muxer","Optimize for Streaming"),NB_OPTIMIZE,streamOpt,"");