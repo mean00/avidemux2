e8e0ad79fc889c2d468252fb4448aaef673ab238
Adam <szullo.adam@gmail.com>
2021-02-11 03:05:38 +0100

    [videoFilter] New filter: Cartoon

---
+++ avidemux_plugins/ADM_videoFilters6/artCartoon/qt4/DIA_flyArtCartoon.h
@@ -0,0 +1,38 @@
+/***************************************************************************
+  \fn     DIA_flyArtCartoon
+  \author (C) 2004/2012 by mean/fixounet@free.fr
+  \brief  Ui for artCartoon
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef FLY_ARTCartoon_H
+#define FLY_ARTCartoon_H
+#include "artCartoon.h"
+/**
+    \class flyArtCartoon
+*/
+class flyArtCartoon : public ADM_flyDialogYuv
+{
+  public:
+    artCartoon         param;
+    int                    rgbBufStride;
+    ADM_byteBuffer *       rgbBufRaw;
+    ADMImageRef *          rgbBufImage;
+    ADMColorScalerFull *   convertYuvToRgb;
+    ADMColorScalerFull *   convertRgbToYuv;
+  public:
+    uint8_t    processYuv(ADMImage* in, ADMImage *out);
+    uint8_t    download(void);
+    uint8_t    upload(void);
+    uint8_t    update(void);
+    flyArtCartoon (QDialog *parent,uint32_t width,uint32_t height,ADM_coreVideoFilter *in,
+                                    ADM_QCanvas *canvas, ADM_QSlider *slider) : ADM_flyDialogYuv(parent, width, height, in, canvas, slider, RESIZE_AUTO) {};
+};
+#endif
