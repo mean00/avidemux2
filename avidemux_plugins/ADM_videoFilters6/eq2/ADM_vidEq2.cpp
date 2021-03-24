mean00/avidemux2

commit 57a5b6b9096b611abca075e4fa18327c0f80bbd6

[eq2] Fix lookup tables repopulated for each picture during preview, fix encapsulation, draw limited range marks on the histogram red, fix histogram alignment

avidemux_plugins/ADM_videoFilters6/eq2/ADM_vidEq2.cpp
@@ -27,34 +27,13 @@
 #include "eq2_desc.cpp"
 #include <math.h>
 
-#if defined( ADM_CPU_X86) && !defined(_MSC_VER)
-        #define CAN_DO_INLINE_X86_ASM
-        void affine_1d_MMX (oneSetting *par, ADMImage *srcImage, ADMImage *destImage,ADM_PLANE plane);
+#ifdef CAN_DO_INLINE_X86_ASM
+static void affine_1d_MMX (oneSetting *par, ADMImage *srcImage, ADMImage *destImage, ADM_PLANE plane);
 #endif
+static void create_lut(oneSetting *par);
+static void apply_lut (oneSetting *par, ADMImage *srcImage, ADMImage *destImage, ADM_PLANE plane);
 
-
-/**
-    \class ADMVideoEq2
-*/
-class  ADMVideoEq2:public ADM_coreVideoFilter
-{
-
-  protected:
-            eq2             _param;    
-            Eq2Settings     settings;   
-            ADMImage        *mysrc;
-            void            update(void);
-  public:
-                
-                            ADMVideoEq2(ADM_coreVideoFilter *in,CONFcouple *couples)   ;
-                            ~ADMVideoEq2();
-       virtual const char   *getConfiguration(void);          /// Return  current configuration as a human readable string
-       virtual bool         getNextFrame(uint32_t *fn,ADMImage *image);    /// Return the next image
-	   virtual bool         getCoupledConf(CONFcouple **couples) ;   /// Return the current filter configuration
-	   virtual void setCoupledConf(CONFcouple *couples);
-       virtual bool         configure(void) ;                 /// Start graphical user interface        
-
-}     ;
+extern uint8_t DIA_getEQ2Param(eq2 *param,ADM_coreVideoFilter *in);
 
 // Add the hook to make it valid plugin
 DECLARE_VIDEO_FILTER_PARTIALIZABLE( ADMVideoEq2, // Class
@@ -65,6 +44,21 @@ DECLARE_VIDEO_FILTER_PARTIALIZABLE( ADMVideoEq2, // Class
                         QT_TRANSLATE_NOOP("eq2","MPlayer eq2"),            // Display name
                         QT_TRANSLATE_NOOP("eq2","Adjust contrast, brightness, saturation and gamma.") // Description
                     );
+/**
+    \fn processPlane
+*/
+bool ADMVideoEq2::processPlane(oneSetting *par, ADMImage *srcImage, ADMImage *destImage, ADM_PLANE plane)
+{
+#ifdef CAN_DO_INLINE_X86_ASM
+    if(CpuCaps::hasMMX())
+    {
+        affine_1d_MMX(par,srcImage,destImage,plane);
+        return true;
+    }
+#endif
+    apply_lut(par,srcImage,destImage,plane);
+    return true;
+}
 /**
     \fn configure
 */
@@ -143,71 +137,60 @@ void ADMVideoEq2::setCoupledConf(CONFcouple *couples)
 */
  bool         ADMVideoEq2::getNextFrame(uint32_t *fn,ADMImage *image)
 {
-  if(!previousFilter->getNextFrame(fn,mysrc)) return false;
-  image->copyInfo(mysrc);
+    if(!previousFilter->getNextFrame(fn,mysrc))
+        return false;
+    image->copyInfo(mysrc);
 
-#ifdef CAN_DO_INLINE_X86_ASM
-  if(CpuCaps::hasMMX())
-  {
-        affine_1d_MMX(&(settings.param[0]),mysrc,image,PLANAR_Y);
-        affine_1d_MMX(&(settings.param[1]),mysrc,image,PLANAR_U);
-        affine_1d_MMX(&(settings.param[2]),mysrc,image,PLANAR_V);
-   }
-   else
-#endif
-   {
-        apply_lut(&(settings.param[0]),mysrc,image,PLANAR_Y);
-        apply_lut(&(settings.param[1]),mysrc,image,PLANAR_U);
-        apply_lut(&(settings.param[2]),mysrc,image,PLANAR_V);
-    }
+    for(int i=0; i < 3; i++)
+        processPlane(&(settings.param[i]),mysrc,image,(ADM_PLANE)i);
 
-  return 1;
+    return true;
 }
 /**
     \fn update_lut
 */
-void update_lut(Eq2Settings *settings,eq2 *_param)
+bool ADMVideoEq2::update_lut(Eq2Settings *settings, eq2 *cfg)
 {
      memset(settings,0,sizeof(*settings));
 
     settings->param[0].lut_clean=0;
     settings->param[1].lut_clean=0;
     settings->param[2].lut_clean=0;
-    settings->contrast=_param->contrast;
-    settings->brightness=_param->brightness;
-    settings->saturation=_param->saturation;
-    
-    settings->ggamma=_param->ggamma;
-    settings->bgamma=_param->bgamma;
-    settings->rgamma=_param->rgamma;
-    settings->gamma=_param->gamma;
+    settings->contrast=cfg->contrast;
+    settings->brightness=cfg->brightness;
+    settings->saturation=cfg->saturation;
 
-    settings->gamma_weight=_param->gamma_weight;
-    
+    settings->ggamma=cfg->ggamma;
+    settings->bgamma=cfg->bgamma;
+    settings->rgamma=cfg->rgamma;
+    settings->gamma=cfg->gamma;
+
+    settings->gamma_weight=cfg->gamma_weight;
+
     if(settings->ggamma<0.1) settings->ggamma=0.1;
-    
-    settings->param[0].c=_param->contrast;  
-    settings->param[0].b=_param->brightness;;
+
+    settings->param[0].c=cfg->contrast;
+    settings->param[0].b=cfg->brightness;
     settings->param[0].g=settings->gamma*settings->ggamma;
     settings->param[0].w=settings->gamma_weight;
-    
-    
-    	
-    settings->param[1].c=_param->saturation;
+
+    settings->param[1].c=cfg->saturation;
     settings->param[1].b=0;
     settings->param[1].g=sqrt(settings->bgamma/settings->ggamma);
     settings->param[1].w=settings->gamma_weight;
-    
-    settings->param[2].c=_param->saturation;
+
+    settings->param[2].c=cfg->saturation;
     settings->param[2].b=0;
     settings->param[2].g=sqrt(settings->rgamma/settings->ggamma);
     settings->param[2].w=settings->gamma_weight;
-    
+
     //printf("GGamma:%f\n",settings->ggamma);
     create_lut(&(settings->param[0]));
     create_lut(&(settings->param[1]));
-    create_lut(&(settings->param[2])); 
-}          
+    create_lut(&(settings->param[2]));
+
+    return true;
+}
 /**
     \fn create_lut
 */

avidemux_plugins/ADM_videoFilters6/eq2/ADM_vidEq2.h
@@ -3,6 +3,10 @@
 
 #include "eq2.h"
 
+#if defined( ADM_CPU_X86) && !defined(_MSC_VER)
+#   define CAN_DO_INLINE_X86_ASM
+#endif
+
 typedef struct oneSetting {
   unsigned char lut[256];
 #ifdef LUT16
@@ -31,16 +35,28 @@ typedef struct Eq2Settings {
 
  
 } Eq2Settings;
-//*************************
-uint8_t DIA_getEQ2Param(eq2 *param,ADM_coreVideoFilter *in);
 
-void update_lut(Eq2Settings *settings,eq2 *_param);
-void apply_lut (oneSetting *par, ADMImage *srcImage, ADMImage *destImage,ADM_PLANE plane);
-void create_lut (oneSetting *par);
+/**
+    \class ADMVideoEq2
+*/
+class ADMVideoEq2:public ADM_coreVideoFilter
+{
+  protected:
+            eq2         _param;
+            Eq2Settings settings;
+            ADMImage    *mysrc;
 
-#if defined( ADM_CPU_X86) && !defined(_MSC_VER)
-        #define CAN_DO_INLINE_X86_ASM
-        void affine_1d_MMX (oneSetting *par, ADMImage *srcImage, ADMImage *destImage,ADM_PLANE plane);
-#endif
+            void        update(void);
+  public:
+                        ADMVideoEq2(ADM_coreVideoFilter *in,CONFcouple *couples);
+                        ~ADMVideoEq2();
+    virtual const char  *getConfiguration(void); /// Return current configuration as a human readable string
+    virtual bool        getNextFrame(uint32_t *fn,ADMImage *image); /// Return the next image
+    virtual bool        getCoupledConf(CONFcouple **couples); /// Return the current filter configuration
+    virtual void        setCoupledConf(CONFcouple *couples);
+    virtual bool        configure(void); /// Start graphical user interface
 
+            static bool processPlane(oneSetting *par, ADMImage *srcImage, ADMImage *destImage, ADM_PLANE plane);
+            static bool update_lut(Eq2Settings *settings, eq2 *cfg);
 
+};

avidemux_plugins/ADM_videoFilters6/eq2/qt4/DIA_flyEq2.cpp
@@ -17,15 +17,25 @@
 #include <math.h>
 #include "DIA_flyDialogQt4.h"
 #include "ADM_default.h"
-#include "ADM_image.h"
 
 #include "ADM_vidEq2.h"
-
 #include "DIA_flyEq2.h"
-#include "QGraphicsScene"
-typedef void lutMeType(oneSetting *par, ADMImage *i,ADMImage *o,ADM_PLANE plane);
 
 /************* COMMON PART *********************/
+
+/**
+    \fn ctor
+*/
+flyEq2::flyEq2(QDialog *parent, uint32_t width, uint32_t height, ADM_coreVideoFilter *in,
+    ADM_QCanvas *canvas, ADM_QSlider *slider, QGraphicsScene *sc)
+    : ADM_flyDialogYuv(parent, width, height, in, canvas, slider, RESIZE_AUTO)
+{
+    scene = sc;
+    tablesDone=false;
+}
+/**
+    \fn update
+*/
 uint8_t  flyEq2::update(void)
 {
     return 1;
@@ -35,8 +45,6 @@ uint8_t  flyEq2::update(void)
 */
 uint8_t    flyEq2::processYuv(ADMImage* in, ADMImage *out)
 {
-	Eq2Settings mySettings;
-
 #if 0
 	printf("Contrast   :%f\n",param.contrast);
 	printf("brightness :%f\n",param.brightness);
@@ -52,25 +60,13 @@ uint8_t    flyEq2::processYuv(ADMImage* in, ADMImage *out)
 	printf("ggamma :%f\n",param.ggamma);
 	printf("******************\n");
 #endif	
-	
-	        update_lut(&mySettings,&param);
-	        
-
-
-			lutMeType *lutMe=apply_lut;
-			
-
-#ifdef CAN_DO_INLINE_X86_ASM
-	        if(CpuCaps::hasMMX())
-	        {
-	        		lutMe=affine_1d_MMX;
-	        }
-#endif	
-	        lutMe(&(mySettings.param[0]),in,out,PLANAR_Y);
-            lutMe(&(mySettings.param[1]),in,out,PLANAR_U);
-            lutMe(&(mySettings.param[2]),in,out,PLANAR_V);
-	        
-	        	
+    if(!tablesDone)
+    {
+        ADMVideoEq2::update_lut(&mySettings,&param);
+        tablesDone = true;
+    }
+    for(int i=0; i < 3; i++)
+        ADMVideoEq2::processPlane(&(mySettings.param[i]),in,out,(ADM_PLANE)i);
 
 	if(scene)
 	{
@@ -99,8 +95,6 @@ uint8_t    flyEq2::processYuv(ADMImage* in, ADMImage *out)
 			sumsum[i]=(10*sumsum[i]*(127))/totalSum;
 			if(sumsum[i]>127) sumsum[i]=127;
 		}
-		int toggle=0;
-
 		scene->clear();
 		for(int i=0;i<256;i++)
 		{
@@ -108,15 +102,19 @@ uint8_t    flyEq2::processYuv(ADMImage* in, ADMImage *out)
 			scene->addLine(qline);
 		}
 		// Draw 16 and 235 line
+		QColor color(Qt::red);
+		QPen pen(color);
 		QLineF qline(16,100,16,126);
-		scene->addLine(qline);
+		scene->addLine(qline,pen);
 		QLineF qline2(235,100,235,126);
-		scene->addLine(qline2);
+		scene->addLine(qline2,pen);
 	}
 
 	if (!fullpreview)
 	{
 		in->copyLeftSideTo(out);
+		out->printString(1,1,"Original"); // printString can't handle non-ascii input, do not translate this!
+		out->printString(in->GetWidth(PLANAR_Y)/24+1,1,"Processed"); // as above, don't try to translate
 	}
 
 		return 1;

avidemux_plugins/ADM_videoFilters6/eq2/qt4/DIA_flyEq2.h
@@ -12,28 +12,27 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
-#ifndef FLY_EQ2_H
-#define FLY_EQ2_H
+#pragma once
+#include <QGraphicsScene>
 #include "ADM_vidEq2.h"
-class QGraphicsScene;
+
 class flyEq2 : public ADM_flyDialogYuv
 {
-  
+  private:
+    Eq2Settings mySettings;
+    bool        tablesDone;
   public:
-   eq2         param;
-   QGraphicsScene *scene;
-   bool        fullpreview;
+    eq2         param;
+    QGraphicsScene *scene;
+    bool        fullpreview;
   public:
-   uint8_t    processYuv(ADMImage* in, ADMImage *out);
-   uint8_t    download(void);
-   uint8_t    upload(void);
-   uint8_t    update(void);
-              flyEq2 (QDialog *parent, uint32_t width, uint32_t height, ADM_coreVideoFilter *in, ADM_QCanvas *canvas, ADM_QSlider *slider,QGraphicsScene*sc) :
-                  ADM_flyDialogYuv(parent, width, height, in, canvas, slider, RESIZE_AUTO)
-                    {
-                      scene=sc;
-                    };
-    void      setTabOrder(void);
+    uint8_t     processYuv(ADMImage* in, ADMImage *out);
+    uint8_t     download(void);
+    uint8_t     upload(void);
+    uint8_t     update(void);
+    void        setTabOrder(void);
+
+                flyEq2 (QDialog *parent, uint32_t width, uint32_t height, ADM_coreVideoFilter *in,
+                        ADM_QCanvas *canvas, ADM_QSlider *slider, QGraphicsScene *sc);
 };
 
-#endif

avidemux_plugins/ADM_videoFilters6/eq2/qt4/Q_eq2.cpp
@@ -39,6 +39,7 @@ Ui_eq2Window::Ui_eq2Window(QWidget *parent, eq2 *param,ADM_coreVideoFilter *in) 
     canvas=new ADM_QCanvas(ui.graphicsView,width,height);
 
     scene=new QGraphicsScene(this);
+    scene->setSceneRect(0,0,256,128);
     ui.graphicsViewHistogram->setScene(scene);
     ui.graphicsViewHistogram->scale(1.0,1.0);
 
@@ -243,6 +244,8 @@ uint8_t flyEq2::upload(void)
     sliderSet(Green,ggamma);
     sliderSet(Blue,bgamma);
 
+    tablesDone = false;
+
     return 1;
 }
 
@@ -263,6 +266,8 @@ uint8_t flyEq2::download(void)
     sliderGet(Red,rgamma);
     sliderGet(Green,ggamma);
     sliderGet(Blue,bgamma);
+
+    tablesDone = false;
 
     return 1;
 }

commit 3f63d1404b6de5a722500be4ed441a490d383fa9

[eq2] Fix swapped red and blue gamma, regressed by 57a5b6b9096b611abca075e4fa18327c0f80bbd6

avidemux_plugins/ADM_videoFilters6/eq2/ADM_vidEq2.cpp
@@ -176,12 +176,12 @@ bool ADMVideoEq2::update_lut(Eq2Settings *settings, eq2 *cfg)
 
     settings->param[1].c=cfg->saturation;
     settings->param[1].b=0;
-    settings->param[1].g=sqrt(settings->bgamma/settings->ggamma);
+    settings->param[1].g=sqrt(settings->rgamma/settings->ggamma);
     settings->param[1].w=settings->gamma_weight;
 
     settings->param[2].c=cfg->saturation;
     settings->param[2].b=0;
-    settings->param[2].g=sqrt(settings->rgamma/settings->ggamma);
+    settings->param[2].g=sqrt(settings->bgamma/settings->ggamma);
     settings->param[2].w=settings->gamma_weight;
 
     //printf("GGamma:%f\n",settings->ggamma);