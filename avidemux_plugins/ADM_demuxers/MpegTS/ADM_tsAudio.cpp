mean00/avidemux2

commit 7bdd8373ccb159b6b5856b9f6fecbbd19416004c

[demuxers/MpegTS] Try to recover from encountering a damaged LATM frame instead of giving up immediately

avidemux_plugins/ADM_demuxers/MpegTS/ADM_tsAudio.cpp
@@ -310,7 +310,7 @@ bool      ADM_tsAccess::getPacket(uint8_t *buffer, uint32_t *size, uint32_t maxS
         case ADM_TS_MUX_LATM:
             {
                 // Try to get one...
-                int retries=20;
+                int retries=40; // arbitrary
                 bool gotPacket=false;
                 uint64_t time=ADM_NO_PTS;
                 while(latm.empty()) // fetch next LOAS frame, it will contain several frames
@@ -321,8 +321,14 @@ bool      ADM_tsAccess::getPacket(uint8_t *buffer, uint32_t *size, uint32_t maxS
                         return false;
                     }
                     if(gotPacket) time=packet->pts;
-                    if(ADM_latm2aac::LATM_MORE_DATA_NEEDED==latm.convert(time))
+                    ADM_latm2aac::LATM_STATE outcome = latm.convert(time);
+                    if(outcome != ADM_latm2aac::LATM_OK)
                     {
+                        if(outcome == ADM_latm2aac::LATM_ERROR)
+                        {
+                            latm.flush();
+                            ADM_warning("Error demuxing LATM frame, %d attempts remaining.\n",retries);
+                        }
                         if(false==demuxer.getNextPES(packet)) return false;
                         int avail=packet->payloadSize-packet->offset;
                         if(avail>maxSize) ADM_assert(0);

commit aee15452f894ca515068e0480a8e960b4d296ca5

[demuxers/MpegTS] Consume AAC LATM data on LATM demuxer error before retrying with the next PES packet
This fixes a regression from 7bdd8373ccb159b6b5856b9f6fecbbd19416004c
which resulted in a failure to extract or play AAC LATM audio tracks
when the very first LATM frame in a sufficiently high number of
consecutive PES packets didn't contain extradata.

avidemux_plugins/ADM_demuxers/MpegTS/ADM_tsAudio.cpp
@@ -326,8 +326,9 @@ bool      ADM_tsAccess::getPacket(uint8_t *buffer, uint32_t *size, uint32_t maxS
                     {
                         if(outcome == ADM_latm2aac::LATM_ERROR)
                         {
-                            latm.flush();
                             ADM_warning("Error demuxing LATM frame, %d attempts remaining.\n",retries);
+                            if(--retries > 10 /* arbitrary cap */) continue; // consume pushed data first
+                            latm.flush();
                         }
                         if(false==demuxer.getNextPES(packet)) return false;
                         int avail=packet->payloadSize-packet->offset;