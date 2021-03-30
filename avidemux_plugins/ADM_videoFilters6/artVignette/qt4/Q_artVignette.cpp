bb8a11e12327636e74065d842aa4ea3e47b92fd2
Adam <szullo.adam@gmail.com>
2021-03-30 18:45:40 +0200

    [artVignette] add coupled spinboxes

--- avidemux_plugins/ADM_videoFilters6/artVignette/qt4/Q_artVignette.cpp
+++ avidemux_plugins/ADM_videoFilters6/artVignette/qt4/Q_artVignette.cpp
@@ -53,7 +53,9 @@ Ui_artVignetteWindow::Ui_artVignetteWindow(QWidget *parent, artVignette *param,A
         myFly->sliderChanged();
 
         connect( ui.horizontalSlider,SIGNAL(valueChanged(int)),this,SLOT(sliderUpdate(int)));
-#define SPINNER(x) connect( ui.horizontalSlider##x,SIGNAL(valueChanged(int)),this,SLOT(valueChanged(int))); 
+#define SPINNER(x) \
+        connect( ui.horizontalSlider##x,SIGNAL(valueChanged(int)),this,SLOT(valueChanged(int))); \
+        connect( ui.doubleSpinBox##x,SIGNAL(valueChanged(double)),this,SLOT(valueChangedSpinBox(double)));
         SPINNER(Aspect);
         SPINNER(Center);
         SPINNER(Soft);
@@ -83,10 +85,32 @@ Ui_artVignetteWindow::~Ui_artVignetteWindow()
     if(canvas) delete canvas;
     canvas=NULL;
 }
+#define COPY_VALUE_TO_SPINBOX(x) \
+        ui.doubleSpinBox##x->blockSignals(true); \
+        ui.doubleSpinBox##x->setValue((float)ui.horizontalSlider##x->value() / 100.0); \
+        ui.doubleSpinBox##x->blockSignals(false);
 void Ui_artVignetteWindow::valueChanged( int f )
 {
     if(lock) return;
     lock++;
+    COPY_VALUE_TO_SPINBOX(Aspect);
+    COPY_VALUE_TO_SPINBOX(Center);
+    COPY_VALUE_TO_SPINBOX(Soft);
+    myFly->download();
+    myFly->sameImage();
+    lock--;
+}
+#define COPY_VALUE_TO_SLIDER(x) \
+        ui.horizontalSlider##x->blockSignals(true); \
+        ui.horizontalSlider##x->setValue((int)round(ui.doubleSpinBox##x->value() * 100.0)); \
+        ui.horizontalSlider##x->blockSignals(false);
+void Ui_artVignetteWindow::valueChangedSpinBox(double foo)
+{
+    if(lock) return;
+    lock++;
+    COPY_VALUE_TO_SLIDER(Aspect);
+    COPY_VALUE_TO_SLIDER(Center);
+    COPY_VALUE_TO_SLIDER(Soft);
     myFly->download();
     myFly->sameImage();
     lock--;
@@ -118,15 +142,23 @@ void Ui_artVignetteWindow::showEvent(QShowEvent *event)
 }
 
 #define MYSPIN(x) w->horizontalSlider##x
+#define MYDBLSPIN(x) w->doubleSpinBox##x
 #define MYCHECK(x) w->checkBox##x
+#define UPLOADDBLSPIN(x, value) \
+        w->doubleSpinBox##x->blockSignals(true); \
+        w->doubleSpinBox##x->setValue(value); \
+        w->doubleSpinBox##x->blockSignals(false);
 //************************
 
 uint8_t flyArtVignette::upload(void)
 {
     Ui_artVignetteDialog *w=(Ui_artVignetteDialog *)_cookie;
     MYSPIN(Aspect)->setValue((int)round(param.aspect * 100.0));
+    UPLOADDBLSPIN(Aspect, param.aspect);
     MYSPIN(Center)->setValue((int)round(param.center * 100.0));
+    UPLOADDBLSPIN(Center, param.center);
     MYSPIN(Soft)->setValue((int)round(param.soft * 100.0));
+    UPLOADDBLSPIN(Soft, param.soft);
     ADMVideoArtVignette::ArtVignetteCreateMask(filterMask, filterW, filterH, param.aspect, param.center, param.soft);
     return 1;
 }
@@ -143,10 +175,14 @@ void flyArtVignette::setTabOrder(void)
 {
     Ui_artVignetteDialog *w=(Ui_artVignetteDialog *)_cookie;
     std::vector<QWidget *> controls;
-#define PUSHME(x) controls.push_back(MYSPIN(x));
-    PUSHME(Aspect)
-    PUSHME(Center)
-    PUSHME(Soft)
+#define PUSHSLIDER(x) controls.push_back(MYSPIN(x));
+#define PUSHDBLSPIN(x) controls.push_back(MYDBLSPIN(x));
+    PUSHSLIDER(Aspect)
+    PUSHDBLSPIN(Aspect)
+    PUSHSLIDER(Center)
+    PUSHDBLSPIN(Center)
+    PUSHSLIDER(Soft)
+    PUSHDBLSPIN(Soft)
 
     controls.insert(controls.end(), buttonList.begin(), buttonList.end());
     controls.push_back(w->horizontalSlider);
