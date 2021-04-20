b2ed48d39269c10ffa712bc64265b00fb45ce399
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2021-04-21 01:26:31 +0200

    [coreAudio,editor] Feed external PCM audio tracks in chunks max. 10 ms in duration to consumers
    This fixes the issue of broken files created when muxing audio tracks from
    WAV files into MKV or AVI containers in copy mode for audio.

--- avidemux_core/ADM_coreAudio/src/ADM_audioAccessFile.cpp
+++ avidemux_core/ADM_coreAudio/src/ADM_audioAccessFile.cpp
@@ -34,7 +34,8 @@ ADM_audioAccessFile::~ADM_audioAccessFile()
 */
 bool      ADM_audioAccessFile::setPos(uint64_t pos)
 {
-    fseeko(_fd,_offset+pos,SEEK_SET);
+    if(fseeko(_fd,_offset+pos,SEEK_SET))
+        return false;
     return true;
 }
 /**
@@ -62,5 +63,77 @@ bool    ADM_audioAccessFile::getPacket(uint8_t *buffer, uint32_t *size, uint32_t
         return false;
 }
 
+/* ################################################################## */
+
+/**
+    \fn ctor
+*/
+ADM_audioAccessFilePCM::ADM_audioAccessFilePCM(const char *fileName, int offset, WAVHeader *info)
+    : ADM_audioAccessFile(fileName,offset)
+{
+    ADM_assert(info);
+    ADM_assert(info->channels && info->channels <= MAX_CHANNELS);
+    ADM_assert(info->frequency >= MIN_SAMPLING_RATE && info->frequency <= MAX_SAMPLING_RATE);
+    switch(info->bitspersample)
+    {
+        case 8: case 16: case 24:
+            break;
+        default:
+            ADM_error("Unsupported bit depth %u\n",info->bitspersample);
+            ADM_assert(0);
+            break;
+    }
+    uint16_t alignment = (info->bitspersample >> 3) * info->channels;
+    if(info->blockalign != alignment)
+    {
+        ADM_warning("Block alignment mismatch: %u vs %u, using the latter.\n",info->blockalign,alignment);
+        info->blockalign = alignment;
+    }
+    memcpy(&_hdr,info,sizeof(WAVHeader));
+    double d = (double)fileLength / _hdr.blockalign;
+    d *= 1000.;
+    d /= _hdr.frequency;
+    d *= 1000.;
+    _duration = d;
+}
+/**
+    \fn goToTime
+*/
+bool ADM_audioAccessFilePCM::goToTime(uint64_t usecs)
+{
+    double d = usecs;
+    d /= 1000. * 1000.;
+    d *= _hdr.frequency;
+    uint64_t pos = d;
+    pos *= _hdr.blockalign;
+    return setPos(pos);
+}
+/**
+    \fn getPacket
+*/
+bool ADM_audioAccessFilePCM::getPacket(uint8_t *buffer, uint32_t *size, uint32_t maxSize, uint64_t *dts)
+{
+    uint32_t maxSamplesPerPacket = _hdr.frequency / 100;
+    uint64_t pos = getPos();
+    if(pos % _hdr.blockalign)
+    {
+        ADM_warning("Unaligned access by %u bytes.\n",pos % _hdr.blockalign);
+        pos /= _hdr.blockalign;
+        pos++;
+        pos *= _hdr.blockalign;
+        if(!setPos(pos))
+            return false;
+    }
+    double d = pos;
+    d /= _hdr.blockalign;
+    d *= 1000. * 1000.;
+    d /= _hdr.frequency;
+    *dts = d;
+    if(maxSize / _hdr.blockalign < maxSamplesPerPacket)
+        maxSamplesPerPacket = maxSize / _hdr.blockalign;
+    uint32_t blocks = fread(buffer, _hdr.blockalign, maxSamplesPerPacket, _fd);
+    *size = blocks * _hdr.blockalign;
+    return !!blocks;
+}
 // EOF
 
