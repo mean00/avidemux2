c14b8354607b2cf39992f62824fc9d5efe71b359
Adam <szullo.adam@gmail.com>
2021-05-26 14:01:56 +0200

    [DIA_flyDialog] make HiDPI scaling QT4 aware

--- avidemux/qt4/ADM_UIs/src/T_QCanvas.cpp
+++ avidemux/qt4/ADM_UIs/src/T_QCanvas.cpp
@@ -21,8 +21,12 @@
 
 void ADM_QCanvas::changeSize(uint32_t w,uint32_t h)
 {
-	_w=(w*devicePixelRatioF() + 0.5);
-	_h=(h*devicePixelRatioF() + 0.5);
+	_w=w;
+	_h=h;
+#if QT_VERSION >= QT_VERSION_CHECK(5,5,0)
+	_w=(_w*devicePixelRatioF() + 0.5);
+	_h=(_h*devicePixelRatioF() + 0.5);
+#endif
 	_l=ADM_IMAGE_ALIGN(_w*4);
 	dataBuffer=NULL;
 	resize(w,h);
@@ -53,7 +57,9 @@ void ADM_QCanvas::paintEvent(QPaintEvent *ev)
 		return ;
 
 	QImage image(dataBuffer,_w,_h,_l,QImage::Format_RGB32);
+#if QT_VERSION >= QT_VERSION_CHECK(5,5,0)
 	image.setDevicePixelRatio(devicePixelRatioF());
+#endif
 	QPainter painter(this);
 	painter.drawImage(QPoint(0,0),image);
 	painter.end();
