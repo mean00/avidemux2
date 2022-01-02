bd66ce6b66b29cb89e5589a441613d462321ecf1
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2022-01-02 19:46:49 +0100

    [ffVaH264,ffVaHEVC] Fix buffer alignment
    This patch fixes a crash in av_frame_free() when input video width is not
    a multiple of 64.

--- avidemux_plugins/ADM_videoEncoder/ffVaH264/ADM_ffVAEncH264.cpp
+++ avidemux_plugins/ADM_videoEncoder/ffVaH264/ADM_ffVAEncH264.cpp
@@ -211,7 +211,7 @@ bool             ADM_ffVAEncH264Encoder::preEncode(void)
     swFrame->height=source->getInfo()->height;
     swFrame->format=AV_PIX_FMT_NV12;
 
-    int err=av_frame_get_buffer(swFrame, 32);
+    int err=av_frame_get_buffer(swFrame, 64);
     if(err<0)
     {
         CLEARTEXT(err)
