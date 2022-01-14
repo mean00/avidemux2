mean00/avidemux2

commit ea533ab04e25f2cf903d2538685047b64eb4fcd7

[editor/segment] Exclude time matching segment duration from segment
This effectively reverts 097f9abc781916fdd3aa82d650bb94cf3872314d.

avidemux/common/ADM_editor/src/ADM_segment.cpp
@@ -660,6 +660,7 @@ bool        ADM_EditorSegment::convertLinearTimeToSeg(  uint64_t frameTime, uint
             return true;
         }
     }
+#if 0 /* time matching the total duration doesn't belong to a segment */
     int max=segments.size();
     if(max)
     {
@@ -672,6 +673,7 @@ bool        ADM_EditorSegment::convertLinearTimeToSeg(  uint64_t frameTime, uint
             return true;
         }
     }
+#endif
     ADM_warning("Cannot find segment matching time %" PRIu64"ms \n",frameTime/1000);
     dump();
     return false;

commit ea3739ea6f862eb20bf6acc0aae41fdf5519426e

[editor/segment] Fix crash copying to clipboard when selection extends to the end of video
Also fix another case where the return value of convertLinearTimeToSeg()
was not evaluated.

The crash was a regression from ea533ab04e25f2cf903d2538685047b64eb4fcd7.

avidemux/common/ADM_editor/src/ADM_segment.cpp
@@ -1094,12 +1094,21 @@ bool        ADM_EditorSegment::LinearToRefTime(int segNo,uint64_t linear,uint64_
  */
 bool        ADM_EditorSegment::copyToClipBoard(uint64_t startTime, uint64_t endTime)
 {
-    ADM_info("Copy to clipboard from %s",ADM_us2plain(startTime));
-    ADM_info("to %s\n",ADM_us2plain(endTime));
+    char *plainStart = ADM_strdup(ADM_us2plain(startTime));
+    ADM_info("Copying from %s to %s\n",plainStart,ADM_us2plain(endTime));
+    ADM_dealloc(plainStart);
+    plainStart = NULL;
     uint32_t startSeg,endSeg;
     uint64_t startSegTime,endSegTime;
-    convertLinearTimeToSeg(  startTime, &startSeg,&startSegTime);
-    convertLinearTimeToSeg(  endTime, &endSeg,&endSegTime);
+    if(false == convertLinearTimeToSeg(startTime, &startSeg, &startSegTime))
+        return false;
+    uint64_t total = getTotalDuration();
+    if(endTime >= total)
+    {
+        endSeg = getNbSegments() - 1;
+        endTime = total;
+    }else if(false == convertLinearTimeToSeg(endTime, &endSeg, &endSegTime))
+        return false;
     dump();
     clipboard.clear();
     for(int seg=startSeg;seg<=endSeg;seg++)
@@ -1154,8 +1163,13 @@ bool        ADM_EditorSegment::pasteFromClipBoard(uint64_t currentTime)
     }
     ADM_info("Pasting from clipboard to %s\n",ADM_us2plain(currentTime));
     uint32_t startSeg;
-    uint64_t startSegTime;
-    convertLinearTimeToSeg(  currentTime, &startSeg,&startSegTime);    
+    uint64_t startSegTime, total = getTotalDuration();
+    if(currentTime >= total)
+    {
+        startSeg = getNbSegments() - 1;
+        currentTime = total;
+    }else if(false == convertLinearTimeToSeg(currentTime, &startSeg, &startSegTime))
+        return false;
     ListOfSegments tmp=segments;
     ListOfSegments newSegs;
     int n=segments.size();