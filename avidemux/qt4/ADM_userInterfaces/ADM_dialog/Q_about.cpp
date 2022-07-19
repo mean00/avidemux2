4f109f8681a28921140248286c4fa680af3a971e
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2022-07-19 02:00:19 +0200

    [Qt/about] Automate copyright year update, make URL clickable

--- avidemux/qt4/ADM_userInterfaces/ADM_dialog/Q_about.cpp
+++ avidemux/qt4/ADM_userInterfaces/ADM_dialog/Q_about.cpp
@@ -30,7 +30,8 @@ Ui_aboutWindow::Ui_aboutWindow(QWidget* parent) : QDialog(parent)
 
     connect(ui.licenseButton, SIGNAL(clicked(bool)), this, SLOT(licenseButton_clicked(bool)));
 
-    char subversion[128]={0};
+#define SBUFLEN 128
+    char subversion[SBUFLEN] = {0};
     const char *compiler="";
 #ifdef WIN32
         #ifdef _MSC_VER
@@ -40,13 +41,20 @@ Ui_aboutWindow::Ui_aboutWindow(QWidget* parent) : QDialog(parent)
         #endif
 #endif
 #if defined(ADM_SUBVERSION)
-        sprintf(subversion,"%s <br><small>(%s-fflibs %s%s)</small>", ADM_VERSION, ADM_SUBVERSION, ADM_FFMPEG_VERSION,compiler);
+    snprintf(subversion, SBUFLEN, " %s <br><small>(%s-fflibs %s%s)</small>", ADM_VERSION, ADM_SUBVERSION, ADM_FFMPEG_VERSION, compiler);
 #else
-        sprintf(subversion,"%s - Release",ADM_VERSION);
+    snprintf(subversion, SBUFLEN, " %s - Release", ADM_VERSION);
 #endif
-        QString  sv(subversion);
-    ui.versionLabel->setTextFormat(Qt::RichText);
-    ui.versionLabel->setText(ui.versionLabel->text() + sv);
+    QString sv(subversion);
+    ui.labelVersion->setTextFormat(Qt::RichText);
+    ui.labelVersion->setText(ui.labelVersion->text() + sv);
+#if !defined(COPYRIGHT_YEAR)
+    #define COPYRIGHT_YEAR 2022
+#endif
+    subversion[0] = '\0';
+    snprintf(subversion, SBUFLEN, "© 2001 - %d  Mean / eumagga0x2a", COPYRIGHT_YEAR); // doesn't need to be translatable
+    ui.labelCopy->setText(QString::fromUtf8(subversion));
+    ui.labelUrl->setText("<a href=\"http://www.avidemux.org\">http://www.avidemux.org</a>");
 }
 
 void Ui_aboutWindow::licenseButton_clicked(bool)
