mean00/avidemux2

commit 3fb6ed2baf15bb10ea97efe07c373a0c37c2df95

[coreVideoCodec] Tear down codec context on hw decoder failure, factorize code

avidemux_core/ADM_coreVideoCodec/src/ADM_codecFFsimple.cpp
@@ -14,7 +14,6 @@
  *                                                                         *
  ***************************************************************************/
 #include "ADM_codecFFsimple.h"
-#include "fourcc.h"
 #include "DIA_coreToolkit.h"
 #include "ADM_coreCodecMapping.h"
 
@@ -26,77 +25,33 @@ decoderFFSimple::decoderFFSimple (uint32_t w, uint32_t h, uint32_t fcc, uint32_t
         : decoderFF(w,h,fcc,extraDataLen,extraData,bpp)
 {
     hasBFrame=false;
-    codec = NULL;
-    if(!_frame)
+    if(!_frame || !_packet)
         return;
     const ffVideoCodec *c=getCodecIdFromFourcc(fcc);
     if(!c)
         return;
 
-    AVCodecID id=c->codecId;
-    codec=avcodec_find_decoder(id);
-    if(!codec)
-    {
-        GUI_Error_HIG(QT_TRANSLATE_NOOP("adm","Codec"),QT_TRANSLATE_NOOP("adm","Internal error finding codec 0x%x"),fcc);
-        return;
-    }
-    codecId=id;
-    if(id==AV_CODEC_ID_NONE)
-        return;
+    hasBFrame = c->hasBFrame;
+    if(c->refCopy)
+        _refCopy = 1;
 
-    _context = avcodec_alloc_context3(codec);
-    if(!_context)
+    if(false == lavSetupPrepare(c->codecId))
         return;
-
-    if(true==c->extraData)
-    {
-         _context->extradata = (uint8_t *) _extraDataCopy;
-         _context->extradata_size = (int) extraDataLen;
-    }
-    if(true==c->refCopy)
-        _refCopy=1;
-    if(true==c->hasBFrame)
-        hasBFrame=true;
-
-    _context->width = _w;
-    _context->height = _h;
-    _context->pix_fmt = AV_PIX_FMT_YUV420P;
-    _context->codec_tag = fcc;
-    _context->workaround_bugs=1*FF_BUG_AUTODETECT +0*FF_BUG_NO_PADDING;
-    _context->error_concealment=3;
-    _context->get_format=ADM_FFgetFormat;
-    _context->opaque=this;
 
     if(!staged)
     {
-        applyQuirks(id);
-        _initCompleted = finish();
+        applyQuirks();
+        _initCompleted = lavSetupFinish();
     }
 }
-/**
-    \fn finish
-*/
-bool decoderFFSimple::finish(void)
-{
-    if (!codec || !_context)
-        return false;
-    if (avcodec_open2(_context, codec, NULL) < 0)
-    {
-        printf("[lavc] Decoder init: %x video decoder failed!\n",_fcc);
-        GUI_Error_HIG(QT_TRANSLATE_NOOP("adm","Codec"),QT_TRANSLATE_NOOP("adm","Internal error opening 0x%x"),_fcc);
-        return false;
-    }
-    printf("[lavc] Decoder init: %x video decoder initialized with %d thread(s)! (%s)\n",_fcc,_context->thread_count,codec->long_name);
-    _initCompleted=true;
-    return true;
-}
+
 /**
     \fn applyQuirks
     \brief All the codec-specific stuff which cannot be handled elsewhere
 */
-void decoderFFSimple::applyQuirks(AVCodecID id)
+void decoderFFSimple::applyQuirks(void)
 {
-    switch(id)
+    switch(codecId)
     {
         case AV_CODEC_ID_TSCC:
         case AV_CODEC_ID_CSCD:
@@ -109,7 +64,7 @@ void decoderFFSimple::applyQuirks(AVCodecID id)
             decoderMultiThread();
             if(_usingMT)
             {
-                if(codec->capabilities & AV_CODEC_CAP_SLICE_THREADS)
+                if(_codec->capabilities & AV_CODEC_CAP_SLICE_THREADS)
                 {
                     _context->thread_count = _threads;
                     _context->thread_type = FF_THREAD_SLICE;

avidemux_core/ADM_coreVideoCodec/include/ADM_codecFFsimple.h
@@ -20,11 +20,6 @@
 #ifndef ADM_CODEC_FF_SIMPLE
 #define ADM_CODEC_FF_SIMPLE
 #include "ADM_default.h"
-
-extern "C" {
-#include "libavcodec/avcodec.h"
-}
-
 #include "ADM_codec.h"
 #include "ADM_ffmp43.h"
 
@@ -35,10 +30,7 @@ class decoderFFSimple:public decoderFF
 {
 protected:
     bool        hasBFrame;
-    const AVCodec   * codec;
-
-    bool        finish(void);
-    void        applyQuirks(AVCodecID id);
+    void        applyQuirks(void);
 public:
                 decoderFFSimple (uint32_t w, uint32_t h,uint32_t fcc, uint32_t extraDataLen, uint8_t *extraData, uint32_t bpp, bool staged = false);
    virtual bool bFramePossible (void) {return hasBFrame; }

avidemux_core/ADM_coreVideoCodec/include/ADM_ffmp43.h
@@ -68,7 +68,8 @@ protected:
            bool         _endOfStream;
            bool         _setBpp;
            bool         _setFcc;
-           int          codecId;
+           AVCodecID    codecId;
+           const AVCodec *_codec;
            uint8_t      _refCopy;
            uint32_t     _bpp;
            AVCodecContext *_context;
@@ -88,11 +89,14 @@ protected:
            uint32_t     frameType (void);
            void         decoderMultiThread ();
            uint32_t     admFrameTypeFromLav (AVFrame *pic);
+           bool         lavSetupPrepare(AVCodecID cid);
+           bool         lavSetupFinish(void);
+           void         lavFree(void);
 
 public:
                         decoderFF (uint32_t w, uint32_t h,uint32_t fcc, uint32_t extraDataLen, uint8_t *extraData,uint32_t bpp);
         virtual         ~ decoderFF ();
-        virtual bool initialized(void);
+        virtual bool    initialized(void) { return _initCompleted; }
             bool        setHwDecoder(ADM_acceleratedDecoderFF *h)
                         {
                             if(h)
@@ -181,7 +185,7 @@ FF_SIMPLE_DECLARE(decoderFFH265,
                                  virtual bool bFramePossible (void)   {   return true;    }
                                 )
 
-
+/*
 #define WRAP_Open_Template(funcz,argz,display,codecid,extra) \
 {\
   const AVCodec *codec=funcz(argz);\
@@ -228,7 +232,9 @@ FF_SIMPLE_DECLARE(decoderFFH265,
 
 #define WRAP_Open(x)            {WRAP_Open_Template(avcodec_find_decoder,x,#x,x,;)}
 #define WRAP_OpenByName(x,y)    {WRAP_Open_Template(avcodec_find_decoder_by_name,#x,#x,y,;)}
+*/
 
+#define WRAP_Open(x) if(lavSetupPrepare(x)) { _initCompleted = lavSetupFinish(); }
 
 // EOF
 

avidemux_core/ADM_coreVideoCodec/src/ADM_ffHwAccel.cpp
@@ -63,12 +63,18 @@ extern enum AVPixelFormat ADM_FFgetFormat(struct AVCodecContext *avctx,  const e
         ADM_info("Multithreading enabled, skipping hw accel lookup.\n");
         return avcodec_default_get_format(avctx,fmt);
     }
+    decoderFF *ff = (decoderFF *)avctx->opaque;
+    ADM_assert(ff);
+    if(ff->hwDecoderIsBlacklisted())
+    {
+        ADM_info("Hw accel blacklisted, skipping lookup.\n");
+        return avcodec_default_get_format(avctx,fmt);
+    }
 
     enum AVPixelFormat outFmt;
     ADM_hwAccelEntry    *accel=ADM_hwAccelManager::lookup(avctx,fmt,outFmt);
     if(accel)
     {
-        decoderFF *ff=(decoderFF *)avctx->opaque;
         if(ff->getHwDecoder())
         {
             ADM_info("Reusing existing setup\n");        

avidemux_core/ADM_coreVideoCodec/src/ADM_ffVp9.cpp
@@ -32,14 +32,16 @@ decoderFFVP9::decoderFFVP9 (uint32_t w, uint32_t h,uint32_t fcc, uint32_t extraD
     _parserContext=NULL;
     decoderMultiThread();
 
-    if (_context && _usingMT && (codec->capabilities & AV_CODEC_CAP_SLICE_THREADS))
+    if (_context && _usingMT && (_codec->capabilities & AV_CODEC_CAP_SLICE_THREADS))
     {
         _context->thread_count = _threads;
         _context->thread_type = FF_THREAD_SLICE;    // this is important! the default FF_THREAD_FRAME wont work with Avidemux
     }
 
-    if(!finish())
+    if (!lavSetupFinish())
         return;
+
+    _initCompleted = true;
 
     _parserContext=av_parser_init(AV_CODEC_ID_VP9);
     if(!_parserContext)

avidemux_core/ADM_coreVideoCodec/src/ADM_ffmp43.cpp
@@ -18,8 +18,8 @@
 #include "ADM_default.h"
 #include "ADM_ffmp43.h"
 #include "DIA_coreToolkit.h"
-#include "ADM_hwAccel.h"
 #include "prefs.h"
+#include "fourcc.h"
 
 #ifdef ADM_DEBUG
     #define LAV_VERBOSITY_LEVEL AV_LOG_DEBUG
@@ -315,11 +315,11 @@ decoderFF::decoderFF (uint32_t w, uint32_t h,uint32_t fcc, uint32_t extraDataLen
   _endOfStream=false;
   _setBpp=false;
   _setFcc=false;
-  codecId = 0;
-//                              memset(&_context,0,sizeof(_context));
+  codecId = AV_CODEC_ID_NONE;
   _allowNull = 0;
   _gmc = 0;
   _context = NULL;
+  _codec = NULL;
   _frame = NULL;
   _refCopy = 0;
   _usingMT = 0;
@@ -357,13 +357,7 @@ decoderFF::~decoderFF ()
       printf ("[lavc] Killing decoding threads\n");
       _usingMT = 0;
     }
-  if(_context)
-  {
-        avcodec_close (_context);
-        av_free(_context);
-        _context=NULL;
-        printf ("[lavc] Destroyed\n");
-    }
+  lavFree();
   // NULL-safe and will set the pointer to NULL
   av_frame_free(&_frame);
   av_packet_free(&_packet);
@@ -377,13 +371,81 @@ decoderFF::~decoderFF ()
   hwDecoder=NULL;
 }
 /**
- * \fn initialized
+ * \fn lavSetupPrepare
  */
-bool decoderFF::initialized(void)
+bool decoderFF::lavSetupPrepare(AVCodecID cid)
 {
-    return _initCompleted;
+    _codec = avcodec_find_decoder(cid);
+    if (!_codec)
+    {
+        GUI_Error_HIG(
+            QT_TRANSLATE_NOOP("adm","Codec"),
+            QT_TRANSLATE_NOOP("adm","Internal error finding decoder for %s"), avcodec_get_name(cid));
+        return false;
+    }
+    codecId = cid;
+    ADM_assert(_context == NULL);
+    _context = avcodec_alloc_context3(_codec);
+    if(!_context)
+    {
+        ADM_error("Could not allocate AVCodecContext.\n");
+        return false;
+    }
+    _context->width = _w;
+    _context->height = _h;
+    _context->pix_fmt = AV_PIX_FMT_YUV420P; // will be overridden by lavc if necessary
+    _context->workaround_bugs = FF_BUG_AUTODETECT; // + FF_BUG_NO_PADDING;
+    _context->error_concealment = FF_EC_GUESS_MVS + FF_EC_DEBLOCK;
+    _context->opaque = this;
+    _context->get_format = ADM_FFgetFormat;
+    if (_setBpp)
+        _context->bits_per_coded_sample = _bpp;
+    if (_setFcc)
+        _context->codec_tag = _fcc;
+    if (_extraDataCopy)
+    {
+        _context->extradata = _extraDataCopy;
+        _context->extradata_size = _extraDataLen;
+    }
+    if (_usingMT)
+        _context->thread_count = _threads;
+
+    return true;
 }
 
+/**
+ * \fn lavSetupFinish
+ */
+bool decoderFF::lavSetupFinish(void)
+{
+    if (!_context || !_codec)
+        return false;
+    if (avcodec_open2(_context, _codec, NULL) < 0)
+    {
+        ADM_error("%s video decoder init has failed, fcc: %s = 0x%08x\n", _codec->long_name, fourCC::tostring(_fcc), _fcc);
+        GUI_Error_HIG(
+            QT_TRANSLATE_NOOP("adm","Codec"),
+            QT_TRANSLATE_NOOP("adm","Internal error opening libavcodec %s decoder"),
+            _codec->long_name);
+        return false;
+    }
+     ADM_info("%s video decoder for %s initialized with %d thread(s)\n",
+         _codec->long_name, fourCC::tostring(_fcc), _context->thread_count);
+     return true;
+}
+
+/**
+ * \fn lavFree
+ */
+void decoderFF::lavFree(void)
+{
+    if (!_context) return;
+
+    avcodec_close (_context);
+    av_free(_context);
+    _context = NULL;
+    ADM_info("Codec context freed\n");
+}
 
 ADM_pixelFormat decoderFF::admPixFrmtFromLav(AVPixelFormat pix_fmt, bool * swap)
 {
@@ -721,22 +783,20 @@ bool   decoderFF::uncompress (ADMCompressedImage * in, ADMImage * out)
   int ret = 0;
   out->_noPicture = 0;
   out->_Qp = ADM_IMAGE_UNKNOWN_QP;
-  if(hwDecoder && !_usingMT)
+  if(hwDecoder && !_usingMT && !_blacklistHwDecoder)
   {
         if(!hwDecoder->isAlive())
         {
+            ADM_warning("Hw decoder is not functional!\n");
             delete hwDecoder;
             hwDecoder = NULL;
             _blacklistHwDecoder = true;
+            lavFree();
+            ADM_assert(lavSetupPrepare(codecId) && lavSetupFinish());
             return uncompress(in,out);
         }
         if(hwDecoder)
-        {
-            if(!hwDecoder->uncompress(in,out))
-                return false;
-            _blacklistHwDecoder = false;
-            return true;
-        }
+            return hwDecoder->uncompress(in,out);
   }
   //printf("Frame size : %d\n",in->dataLength);
 
@@ -783,7 +843,7 @@ bool   decoderFF::uncompress (ADMCompressedImage * in, ADMImage * out)
 
         // HW accel may be setup by now if this has been the very first packet. In this case
         // ask the hw decoder to collect the decoded picture.
-        if(hwDecoder)
+        if(hwDecoder && hwDecoder->isAlive() && !_blacklistHwDecoder)
         {
             hwDecoder->skipSendFrame();
             if(hwDecoder->uncompress(in,out))
@@ -794,7 +854,8 @@ bool   decoderFF::uncompress (ADMCompressedImage * in, ADMImage * out)
             delete hwDecoder;
             hwDecoder = NULL;
             _blacklistHwDecoder = true;
-            flush();
+            lavFree();
+            ADM_assert(lavSetupPrepare(codecId) && lavSetupFinish());
             return uncompress(in,out);
         }
     }
@@ -965,14 +1026,16 @@ decoderFF_ffhuff::decoderFF_ffhuff (uint32_t w, uint32_t h,uint32_t fcc, uint32_
 decoderFFH264::decoderFFH264 (uint32_t w, uint32_t h,uint32_t fcc, uint32_t extraDataLen, uint8_t *extraData,uint32_t bpp)
         :decoderFF (w, h,fcc,extraDataLen,extraData,bpp)
 {
-  _refCopy = 1;			// YUV420 only
-  decoderMultiThread ();
-  ADM_info ("[lavc] Initializing H264 decoder with %d extradata\n", (int)extraDataLen);
-  WRAP_Open(AV_CODEC_ID_H264);
+    _refCopy = 1; // YUV420 only
+    decoderMultiThread();
+    ADM_info ("Initializing H264 decoder with %d bytes of extradata\n", (int)extraDataLen);
+    if(false == lavSetupPrepare(AV_CODEC_ID_H264))
+        return;
 #ifdef ADM_DEBUG
   //_context->debug |= FF_DEBUG_MMCO;
   //_context->debug |= FF_DEBUG_PICT_INFO;
 #endif
+    _initCompleted = lavSetupFinish();
 }
 //*********************
 extern "C" {int av_getAVCStreamInfo(AVCodecContext *avctx, uint32_t  *nalSize, uint32_t *isAvc);}

commit 7186a874c2ec8d2069a961acd0f176f0d45ddccb

[decoderFFSimple] Fix codec_tag in AVCodecContext not set appropriately
This regression from 3fb6ed2 has broken a number of decoders which require
FourCC to work.

avidemux_core/ADM_coreVideoCodec/src/ADM_codecFFsimple.cpp
@@ -35,6 +35,8 @@ decoderFFSimple::decoderFFSimple (uint32_t w, uint32_t h, uint32_t fcc, uint32_t
     if(c->refCopy)
         _refCopy = 1;
 
+    _setFcc = true;
+
     if(false == lavSetupPrepare(c->codecId))
         return;
 