a2459d13c67d5f10f8cdaf78419f856c2e94b1fa
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2023-05-31 23:25:47 +0200

    [muxerffTS] Use convenience macro to declare plugin, capitalize suggested filename extensions when env var ADM_TS_MUXER_EXT_ALL_CAPS is set

--- avidemux_plugins/ADM_muxers/muxerffTS/muxerffTSPlugin.cpp
+++ avidemux_plugins/ADM_muxers/muxerffTS/muxerffTSPlugin.cpp
@@ -21,79 +21,23 @@
 #include "fourcc.h"
  bool ffTSConfigure(void);
 
-extern "C" {
-static void *defaultConfig = NULL;
-
-static void snapshotDefaultConfiguration(void)
+static const char *getTsExt(void)
 {
-    if (defaultConfig)
-        return;
-    size_t confSize = sizeof(ts_muxer);
-    defaultConfig = ADM_alloc(confSize);
-    memcpy(defaultConfig, &tsMuxerConfig, confSize);
-}
-
-ADM_PLUGIN_EXPORT ADM_muxer *create(void)
-{
-    return new muxerffTS;
-}
-ADM_PLUGIN_EXPORT void destroy(ADM_muxer *m)
-{
-    muxerffTS *z = (muxerffTS *)m;
-    delete z;
-}
-ADM_PLUGIN_EXPORT uint8_t getVersion(uint32_t *major, uint32_t *minor, uint32_t *patch)
-{
-    *major = 1;
-    *minor = 0;
-    *patch = 1;
-    return 1;
-}
-ADM_PLUGIN_EXPORT uint32_t getApiVersion(void)
-{
-    return ADM_MUXER_API_VERSION;
-}
-ADM_PLUGIN_EXPORT const char *getName(void)
-{
-    return "ffTS";
-}
-ADM_PLUGIN_EXPORT const char *getDescriptor(void)
-{
-    return "ffMpeg TS muxer plugin (c) Mean 2009";
-}
-ADM_PLUGIN_EXPORT const char *getDisplayName(void)
-{
-    return "Mpeg TS Muxer (ff)";
-}
-ADM_PLUGIN_EXPORT const char *getDefaultExtension(void)
-{
+    if (getenv("ADM_TS_MUXER_EXT_ALL_CAPS"))
+        return tsMuxerConfig.m2TsMode ? "M2TS" : "TS";
     return tsMuxerConfig.m2TsMode ? "m2ts" : "ts";
 }
-ADM_PLUGIN_EXPORT void clearDefaultConfig(void)
-{
-    ADM_dealloc(defaultConfig);
-    defaultConfig = NULL;
-}
-ADM_PLUGIN_EXPORT bool getConfiguration(CONFcouple **conf)
-{
-    return ADM_paramSave(conf, ts_muxer_param, &tsMuxerConfig);
-}
-ADM_PLUGIN_EXPORT bool setConfiguration(CONFcouple *conf)
-{
-    snapshotDefaultConfiguration();
-    return ADM_paramLoad(conf, ts_muxer_param, &tsMuxerConfig);
-}
-ADM_PLUGIN_EXPORT bool resetConfiguration(void)
-{
-    snapshotDefaultConfiguration();
-    if (defaultConfig)
-        memcpy(&tsMuxerConfig, defaultConfig, sizeof(ts_muxer));
-    return true;
-}
-ADM_PLUGIN_EXPORT bool configure(void)
-{
-    snapshotDefaultConfiguration();
-    return ffTSConfigure();
-}
-}
+
+ADM_MUXER_DYN_EXT(
+    getTsExt,
+    muxerffTS,
+    1,0,1,
+    "ffTS", // Internal name
+    "ffMpeg TS muxer plugin (c) Mean 2009",
+    "Mpeg TS Muxer (ff)", // Display name
+    ffTSConfigure,
+    ts_muxer_param, // template
+    &tsMuxerConfig, // config
+    sizeof(ts_muxer)
+)
 
