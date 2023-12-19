35efcfdd1d7d5c276d477778b202b11331c68be9
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2023-12-19 22:41:45 +0100

    [tinyPy] Implement ADM_pyTools functions left as empty stubs with MSVC

--- avidemux_plugins/ADM_scriptEngines/tinyPy/src/ADM_pyTools_win32.cpp
+++ avidemux_plugins/ADM_scriptEngines/tinyPy/src/ADM_pyTools_win32.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
-   \file ADM_pyTools.cpp
+    \file ADM_pyTools_win32.cpp
     \brief misc. helpers, not strictly related to ADM
     \author szlldm 2023
  ***************************************************************************/
@@ -16,20 +16,80 @@
 #include "ADM_pyTools.h"
 #include "ADM_default.h"
 
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+
+/* Stolen from https://stackoverflow.com/questions/10905892/equivalent-of-gettimeofday-for-windows */
+
 double pyTool_time(IEditor *editor)
 {
+    // This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
+    // until 00:00:00 January 1, 1970
+    static const uint64_t epochOffset = 116444736000000000ULL;
+
+    SYSTEMTIME sysTime;
+    FILETIME fileTime;
+    uint64_t time;
+
+    GetSystemTime(&sysTime);
+    SystemTimeToFileTime(&sysTime, &fileTime);
+
+    time = fileTime.dwLowDateTime;
+    time += ((uint64_t)fileTime.dwHighDateTime) << 32;
+
+    if (epochOffset > time)
         return 0.0;
+    time -= epochOffset;
+    double t = sysTime.wMilliseconds;
+    t /= 1000.0;
+    t += time / 10000000ULL;
+    return t;
 }
 
 char * pyTool_date(IEditor *editor)
 {
-        return ADM_strdup("none");
+    SYSTEMTIME st;
+
+    GetLocalTime(&st); // should the returned struct be validated?
+
+#define TM_BUFLEN 64
+    char tmbuf[TM_BUFLEN];
+    snprintf(tmbuf, TM_BUFLEN, "%04d-%02d-%02d %02d:%02d:%02d", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+    return ADM_strdup(tmbuf);
 }
 
+static uint32_t rng_state = 0;
 
 int pyTool_randint(IEditor *editor, int start, int stop)
 {
-        return start;
+    while (rng_state == 0)
+    {
+        SYSTEMTIME st;
+        GetSystemTime(&st);
+        if (!st.wMilliseconds)
+        {
+            ADM_usleep(1000);
+            continue;
+        }
+        rng_state = st.wMilliseconds;
+        rng_state *= 1000;
+    }
+    if (start == stop) return start;
+    if (stop < start)
+    {
+        int tmp = stop;
+        stop = start;
+        start = tmp;
+    }
+
+    uint64_t rng_product;
+    rng_product = (uint64_t)rng_state * 48271;
+    rng_state = (rng_product & 0x7fffffff) + (rng_product >> 31);
+    rng_state = (rng_state & 0x7fffffff) + (rng_state >> 31);
+    uint32_t rn = rng_state;
+    rn %= (stop + 1 - start);
+    int ri = rn;
+    return ri + start;
 }
 
 int pyTool_isalnum(IEditor *editor, const char * str)
