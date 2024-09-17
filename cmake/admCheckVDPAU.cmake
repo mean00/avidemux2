5023394ade7e6dc7b7d99b6ed11b4f4b64d24d2f
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2024-09-17 02:45:01 +0200

    [cmake] Retain results of checking for VDPAU, improve status messages, remove tabs, unify indentation

--- cmake/admCheckVDPAU.cmake
+++ cmake/admCheckVDPAU.cmake
@@ -5,38 +5,36 @@
 ########################################
 MACRO(checkVDPAU)
     if (UNIX)
-        IF (NOT VDPAU_CHECKED)
-            OPTION(VDPAU "" ON)
+        OPTION(VDPAU "" ON)
+        IF (VDPAU)
+            IF (NOT VDPAU_CHECKED)
+                MESSAGE(STATUS "Checking for VDPAU")
+                MESSAGE(STATUS "******************")
 
-            MESSAGE(STATUS "Checking for VDPAU")
-            MESSAGE(STATUS "*******************")
-
-            IF (VDPAU)
-        
                 FIND_HEADER_AND_LIB(VDPAU vdpau/vdpau.h )
-        
                 IF (VDPAU_FOUND)
-                            # CHECK That it has 
-                            ADM_COMPILE(check_vdpau.cpp "" "${VDPAU_INCLUDE_DIR}" "${VDPAU_LIBRARY_DIR}" VDPAU_COMPILE VDPAU_OUTPUT)
-                            IF(VDPAU_COMPILE)
-                                SET(VDPAU_FOUND 1)
-                                SET(USE_VDPAU 1)
-                                SET(VDPAU_INCLUDE_DIR "${VDPAU_INCLUDE_DIR}")
-                                SET(VDPAU_LIBRARY_DIR "${VDPAU_LIBRARY_DIR}")
-                            ELSE(VDPAU_COMPILE)
-                                MESSAGE(STATUS "The vdpau found does not compile, probably too old ${VDPAU_OUTPUT}.")
-                            ENDIF(VDPAU_COMPILE)
-                                
+                    # CHECK That it has
+                    ADM_COMPILE(check_vdpau.cpp "" "${VDPAU_INCLUDE_DIR}" "${VDPAU_LIBRARY_DIR}" VDPAU_COMPILE VDPAU_OUTPUT)
+                    IF (VDPAU_COMPILE)
+                        SET(USE_VDPAU True CACHE BOOL "")
+                    ELSE (VDPAU_COMPILE)
+                        MESSAGE(STATUS "The vdpau found does not compile, probably too old ${VDPAU_OUTPUT}.")
+                    ENDIF (VDPAU_COMPILE)
                 ENDIF (VDPAU_FOUND)
 
-                PRINT_LIBRARY_INFO("VDPAU" VDPAU_FOUND "${VDPAU_INCLUDE_DIR}" "${VDPAU_LIBRARY_DIR}")
-            ELSE (VDPAU)
-                MESSAGE("${MSG_DISABLE_OPTION}")
-            ENDIF (VDPAU)
+                SET(VDPAU_CHECKED 1 CACHE INTERNAL "")
+                MESSAGE("")
 
-            SET(VDPAU_CHECKED 1)
+            ELSE (NOT VDPAU_CHECKED)
+                MESSAGE(STATUS "Skipping check for VDPAU, using cached results")
+                MESSAGE(STATUS "**********************************************")
+            ENDIF (NOT VDPAU_CHECKED)
+
+            PRINT_LIBRARY_INFO("VDPAU" VDPAU_FOUND "${VDPAU_INCLUDE_DIR}" "${VDPAU_LIBRARY_DIR}")
             MESSAGE("")
-        ENDIF (NOT VDPAU_CHECKED)
+        ELSE (VDPAU)
+            MESSAGE("${MSG_DISABLE_OPTION}")
+        ENDIF (VDPAU)
 
         APPEND_SUMMARY_LIST("Miscellaneous" "VDPAU" "${VDPAU_FOUND}")
     endif (UNIX)
