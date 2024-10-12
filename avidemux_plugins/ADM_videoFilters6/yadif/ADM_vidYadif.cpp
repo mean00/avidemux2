f60ffb00d46963b67e63a6650cfb2c8c6e74d9c8
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2024-10-12 18:14:40 +0200

    [yadif] Fix timebase calculation

--- avidemux_plugins/ADM_videoFilters6/yadif/ADM_vidYadif.cpp
+++ avidemux_plugins/ADM_videoFilters6/yadif/ADM_vidYadif.cpp
@@ -178,7 +178,7 @@ void yadifFilter::updateInfo(void)
         info.frameIncrement/=2;
         if(info.timeBaseNum && info.timeBaseDen)
         {
-            if(info.timeBaseDen<=30000 && (info.timeBaseNum & 1))
+            if(info.timeBaseNum & 1)
                 info.timeBaseDen*=2;
             else
                 info.timeBaseNum/=2;
