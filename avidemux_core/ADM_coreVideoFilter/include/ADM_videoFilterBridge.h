81116ec2df8746aa9b1c7544ea714ede23933b24
mean <fixounet@free.fr>
2024-12-16 07:08:53 +0100

    more core update

--- avidemux_core/ADM_coreVideoFilter/include/ADM_videoFilterBridge.h
+++ avidemux_core/ADM_coreVideoFilter/include/ADM_videoFilterBridge.h
@@ -18,10 +18,9 @@
 #ifndef ADM_VIDEO_FILTER_BRIDGE_H
 #define ADM_VIDEO_FILTER_BRIDGE_H
 
-#include "ADM_coreVideoFilter6_export.h"
 #include "ADM_coreVideoFilter.h"
+#include "ADM_coreVideoFilter6_export.h"
 #include "ADM_editor/include/IEditor.h"
-
 /**
     \class ADM_videoFilterBridge
     \brief Class that transform Editor API to coreVideoFilter API
@@ -29,30 +28,41 @@
 */
 class ADM_COREVIDEOFILTER6_EXPORT ADM_videoFilterBridge : public ADM_coreVideoFilter
 {
-protected:
-        uint64_t            startTime,endTime;
-        FilterInfo          bridgeInfo;
-        bool                firstImage;
-        uint32_t            lastSentImage;
-        bool                getNextFrameBase(uint32_t *frameNumber,ADMImage *image);
-        IEditor*            editor;
-public:
-                            ADM_videoFilterBridge(IEditor *editor, uint64_t startTime, uint64_t endTime);
-       virtual             ~ADM_videoFilterBridge();
-       virtual bool         goToTime(uint64_t usSeek, bool fineSeek = false);
-       virtual bool         getNextFrame(uint32_t *frameNumber,ADMImage *image);
-               bool         getNextFrameAs(ADM_HW_IMAGE type,uint32_t *frameNumber,ADMImage *image);
-       virtual FilterInfo  *getInfo(void);                                      /// Return picture parameters after this filter
-       virtual bool         getCoupledConf(CONFcouple **couples) {*couples=NULL;return true;} ; /// Return the current filter configuration
-       virtual void         setCoupledConf(CONFcouple *couples) {}
-       virtual uint64_t     getAbsoluteStartTime(void)
-                            {
-                                    return startTime;
-                            }
-       virtual bool         getTimeRange(uint64_t *start, uint64_t *end) { *start=startTime; *end=endTime; return true; }
-       virtual void         updateBridge(uint64_t startTime, uint64_t endTime);
-       bool                 rewind(void);
+  protected:
+    uint64_t startTime, endTime;
+    FilterInfo bridgeInfo;
+    bool firstImage;
+    uint32_t lastSentImage;
+    bool getNextFrameBase(uint32_t *frameNumber, ADMImage *image);
+    IEditor *editor;
 
+  public:
+    ADM_videoFilterBridge(IEditor *editor, uint64_t startTime, uint64_t endTime);
+    virtual ~ADM_videoFilterBridge();
+    virtual bool goToTime(uint64_t usSeek, bool fineSeek = false);
+    virtual bool getNextFrame(uint32_t *frameNumber, ADMImage *image);
+    bool getNextFrameAs(ADM_HW_IMAGE type, uint32_t *frameNumber, ADMImage *image);
+    virtual FilterInfo *getInfo(void); /// Return picture parameters after this filter
+    virtual bool getCoupledConf(CONFcouple **couples)
+    {
+        *couples = NULL;
+        return true;
+    }; /// Return the current filter configuration
+    virtual void setCoupledConf(CONFcouple *couples)
+    {
+    }
+    virtual uint64_t getAbsoluteStartTime(void)
+    {
+        return startTime;
+    }
+    virtual bool getTimeRange(uint64_t *start, uint64_t *end)
+    {
+        *start = startTime;
+        *end = endTime;
+        return true;
+    }
+    virtual void updateBridge(uint64_t startTime, uint64_t endTime);
+    bool rewind(void);
 };
 
 #endif
