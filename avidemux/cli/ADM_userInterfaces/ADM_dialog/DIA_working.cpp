6deb4f915e79352fc7a7ab5195f8fdae88d7a62b
mean <fixounet@free.fr>
2024-12-25 15:00:23 +0100

    and even more cleanup

--- avidemux/cli/ADM_userInterfaces/ADM_dialog/DIA_working.cpp
+++ avidemux/cli/ADM_userInterfaces/ADM_dialog/DIA_working.cpp
@@ -5,7 +5,7 @@
     copyright            : (C) 2003 by mean
     email                : fixounet@free.fr
 
-	This class deals with the working window
+    This class deals with the working window
 
  ***************************************************************************/
 
@@ -20,116 +20,113 @@
 
 #include <math.h>
 
-#include "config.h"
 #include "ADM_default.h"
+#include "config.h"
 
 #define aprintf ADM_info
 
-# include "config.h"
 #include "DIA_working.h"
+#include "config.h"
 
 class DIA_workingNone : public DIA_workingBase
 {
-protected:
-    virtual void        postCtor( void );
-public:
-                        DIA_workingNone( const char *title=NULL );
-    virtual             ~DIA_workingNone();
-    // If returns 1 -> Means aborted
-    virtual uint8_t     update(uint32_t percent);
-    virtual uint8_t     update(uint32_t current,uint32_t total);
-    virtual uint8_t     isAlive (void );
+  protected:
+    virtual void postCtor(void);
 
+  public:
+    DIA_workingNone(const char *title = NULL);
+    virtual ~DIA_workingNone();
+    // If returns 1 -> Means aborted
+    virtual uint8_t update(uint32_t percent);
+    virtual uint8_t update(uint32_t current, uint32_t total);
+    virtual uint8_t isAlive(void);
 };
 //**********************************
-DIA_workingNone::DIA_workingNone( const char *title ) : DIA_workingBase(title)
+DIA_workingNone::DIA_workingNone(const char *title) : DIA_workingBase(title)
 {
     this->postCtor();
 }
 
-void DIA_workingNone :: postCtor( void )
+void DIA_workingNone ::postCtor(void)
 {
-    lastper=0;
-    _nextUpdate=0;
+    lastper = 0;
+    _nextUpdate = 0;
 }
 
 uint8_t DIA_workingNone::update(uint32_t percent)
 {
-    if(!percent || percent==lastper)
+    if (!percent || percent == lastper)
         return 0;
 
-    lastper=percent;
-    //aprintf("DIA_working::update(%lu) called\n", percent);
-    elapsed=_clock.getElapsedMS();
-    if(elapsed==_nextUpdate)
+    lastper = percent;
+    // aprintf("DIA_working::update(%lu) called\n", percent);
+    elapsed = _clock.getElapsedMS();
+    if (elapsed == _nextUpdate)
         return 0;
 
-    if(elapsed<1000)
+    if (elapsed < 1000)
     {
-        printf("%*u%% done, elapsed: %*u ms\n",3,percent,4,elapsed);
-        _nextUpdate=elapsed;
+        printf("%*u%% done, elapsed: %*u ms\n", 3, percent, 4, elapsed);
+        _nextUpdate = elapsed;
         return 0;
     }
-    if(elapsed<_nextUpdate)
+    if (elapsed < _nextUpdate)
         return 0;
-    _nextUpdate=elapsed+1000;
+    _nextUpdate = elapsed + 1000;
 
-		// 100/totalMS=percent/elapsed
-		// totalM=100*elapsed/percent
+    // 100/totalMS=percent/elapsed
+    // totalM=100*elapsed/percent
 
     double f;
-    f=100.;
-    f*=elapsed;
-    f/=percent;
+    f = 100.;
+    f *= elapsed;
+    f /= percent;
 
-    f-=elapsed;
-    f/=1000;
+    f -= elapsed;
+    f /= 1000;
 
-    uint32_t sectogo=(uint32_t)floor(f);
+    uint32_t sectogo = (uint32_t)floor(f);
 
     char b[300];
-    int  mm,ss;
-    mm=sectogo/60;
-    ss=sectogo%60;
-    printf("%*u%% done, %d m %*d s left\n",3,percent,mm,2,ss);
+    int mm, ss;
+    mm = sectogo / 60;
+    ss = sectogo % 60;
+    printf("%*u%% done, %d m %*d s left\n", 3, percent, mm, 2, ss);
 
     return 0;
-
 }
 
 uint8_t DIA_workingNone::update(uint32_t cur, uint32_t total)
 {
-    double d,n;
+    double d, n;
     uint32_t percent;
 
-    //aprintf("DIA_working::update(uint32_t %lu,uint32_t %lu) called\n", cur, total);
-    if(!total) return 0;
+    // aprintf("DIA_working::update(uint32_t %lu,uint32_t %lu) called\n", cur, total);
+    if (!total)
+        return 0;
 
-    d=total;
-    n=cur;
-    n=n*100.;
+    d = total;
+    n = cur;
+    n = n * 100.;
 
-    n=n/d;
+    n = n / d;
 
-    percent=(uint32_t )floor(n);
+    percent = (uint32_t)floor(n);
     return update(percent);
-
 }
 
-uint8_t DIA_workingNone::isAlive (void )
+uint8_t DIA_workingNone::isAlive(void)
 {
     return 1;
 }
 
 DIA_workingNone::~DIA_workingNone()
 {
-
 }
 namespace ADM_CliCoreUIToolkit
 {
 DIA_workingBase *createWorking(const char *title)
 {
     return new DIA_workingNone(title);
-
 }
-}
+} // namespace ADM_CliCoreUIToolkit
