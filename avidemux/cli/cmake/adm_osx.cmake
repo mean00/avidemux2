9c3c2173ac5c24b858da5818516aadda3f6d427a
mean <fixounet@free.fr>
2024-12-26 19:32:20 +0100

    split qt4 , cli, plugin, temporary fix (osx)

---
+++ avidemux/cli/cmake/adm_osx.cmake
@@ -0,0 +1,12 @@
+#
+#include(BundleUtilities)
+
+#
+SET(bundle avidemux3_${QT_EXTENSION})
+#
+MACRO(ADM_PREBUILD)
+  LIST(APPEND PlatformLibs   "-lm -lstdc++")
+  LIST(APPEND PlatformLibs  "-framework CoreServices -framework CoreAudio -framework AudioUnit -framework Carbon -framework Accelerate")
+  LIST(APPEND PlatformLibs  "-Wl,-dylib_file,/System/Library/Frameworks/OpenGL.framework/Versions/A/Libraries/libGL.dylib:/System/Library/Frameworks/OpenGL.framework/Versions/A/Libraries/libGL.dylib")
+ENDMACRO(ADM_PREBUILD)
+#
