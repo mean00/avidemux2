9aa5b3ef701d67c048fdbbf78478bf365c3b4db6
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2025-03-22 02:06:46 +0100

    cmake: fix dependencies for CPack-generated Debian packages, fix starter and application icon not packaged

--- avidemux/cli/cliPackageDebian.cmake
+++ avidemux/cli/cliPackageDebian.cmake
@@ -5,7 +5,7 @@ set(CPACK_COMPONENTS_ALL             runtime dev )
 
 SET(CPACK_DEBIAN_PACKAGE_NAME        "avidemux3-cli")
 SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Simple video editor,main program, command line version ")
-SET(DEPS "libc6 (>=2.4),libglib2.0-0 (>=2.14.0),libstdc++6 (>=4.2.1),libx11-6,   zlib1g (>=1:1.1.4), avidemux3-core-runtime (>=${AVIDEMUX_VERSION})")
+SET(DEPS "avidemux3-core-runtime (>=${AVIDEMUX_VERSION})")
 # Add optional DEPS here
 SET(CPACK_DEBIAN_PACKAGE_DEPENDS "${DEPS}")
 #
