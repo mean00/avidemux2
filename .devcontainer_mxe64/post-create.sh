f7bdc7eae4e1952f7672423580cf72dbef684c6c
mean <fixounet@free.fr>
2026-04-05 20:52:04 +0200

    devpod: mxe64, mostly working version except it timeouts easily at build time

--- .devcontainer_mxe64/post-create.sh
+++ .devcontainer_mxe64/post-create.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-apt-get install python3-pefile
+echo "post"
--- devpod_mxe64.sh
+++ devpod_mxe64.sh
@@ -1,1 +1,3 @@
-devpod up . --ide none --id adm-mxe64 --devcontainer-path .devcontainer_mxe64/devcontainer.json
+#
+echo "Dont forget devpod provider set-options --provider docker --option INACTIVITY_TIMEOUT=90m"
+devpod up . --ide none --id adm-mxe64  --devcontainer-path .devcontainer_mxe64/devcontainer.json
