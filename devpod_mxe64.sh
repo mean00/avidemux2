006c8a8fc8bdda5a10a03de7b7ea69e096aae4d6
mean <fixounet@free.fr>
2026-04-15 19:05:27 +0200

    devpod/mxe: dont use post create + add extra arg support

--- devpod_mxe64.sh
+++ devpod_mxe64.sh
@@ -1,3 +1,3 @@
 #
 echo "Dont forget devpod provider set-options --provider docker --option INACTIVITY_TIMEOUT=90m"
-devpod up . --ide none --id adm-mxe64  --devcontainer-path .devcontainer_mxe64/devcontainer.json
+devpod up . --ide none --id adm-mxe64  --devcontainer-path .devcontainer_mxe64/devcontainer.json $@
