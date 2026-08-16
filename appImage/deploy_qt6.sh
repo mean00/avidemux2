c7fd88467347d57c7a204567bd289548325e76fa
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2026-08-16 03:15:29 +0200

    appImage: support Fedora container, add Qt6 Wayland decoration plugin

--- appImage/deploy_qt6.sh
+++ appImage/deploy_qt6.sh
@@ -13,33 +13,50 @@ fail() {
 
 }
 set -x
-export QT6_PLUGINS=/usr/lib/x86_64-linux-gnu/qt6/plugins/
+if [ -e /etc/redhat-release ]; then
+  export LIBRARY_DIR=/usr/lib64
+else
+  export LIBRARY_DIR=/usr/lib/x86_64-linux-gnu
+fi
+export QT6_PLUGINS=$LIBRARY_DIR/qt6/plugins
 # export LD_DEBUG=amm LD_DEBUG_OUTPUT=/tmp/somefile
 export SOURCE=$1
 export DEST=$2
 
-echo "Installing $SOURCE -> $DEST"
+echo "Installing ${SOURCE} -> ${DEST}"
 
-rm -Rf $DEST
-mkdir $DEST
-cp -Rap $SOURCE/* $DEST/
-mv $DEST/usr/lib/x86_64-linux-gnu/* $DEST/usr/lib
-export H=$PWD
-cd $DEST/usr/lib/x86_64-linux-gnu/
-ln -s ../ADM_plugins6 .
-cd $H
-mv $DEST/usr/bin/avidemux3_qt6 $DEST/usr/bin/avidemux3_portable
+rm -Rf ${DEST} || fail "Removing destination directory"
+mkdir ${DEST}
+cp -Rap ${SOURCE}/* ${DEST}/
+
+if [ -e /etc/redhat-release ]; then
+  pushd ${DEST}/usr && mv lib64 lib && ln -sf lib lib64
+  popd
+else
+  mkdir -p ${DEST}/usr/lib && mv ${DEST}/${LIBRARY_DIR}/* ${DEST}/usr/lib/
+  pushd ${DEST}/${LIBRARY_DIR} && ln -s ../ADM_plugins6 .
+  popd
+fi
+mv ${DEST}/usr/bin/avidemux3_qt6 ${DEST}/usr/bin/avidemux3_portable
+mv ${DEST}/usr/bin/avidemux3_jobs_qt6 ${DEST}/usr/bin/avidemux3_jobs_portable
 #
 #
 #
-mkdir -p $DEST/usr/plugins/platforms
+mkdir -p ${DEST}/usr/plugins/platforms
 cp -t ${DEST}/usr/plugins/platforms/ ${QT6_PLUGINS}/platforms/*wayland* ${QT6_PLUGINS}/platforms/*xcb*
 cp -Rap -t ${DEST}/usr/plugins/ ${QT6_PLUGINS}/*integration*
+cp -Rap -t ${DEST}/usr/plugins ${QT6_PLUGINS}/wayland-decoration-client
 #
 #
-cp -t ${DEST}/usr/lib /usr/lib/x86_64-linux-gnu/libwayland-client.so
-cp -t ${DEST}/usr/lib /usr/lib/x86_64-linux-gnu/libwayland-cursor.so
-cp -t ${DEST}/usr/lib /usr/lib/x86_64-linux-gnu/libwayland-egl.so
+if [ -e /etc/redhat-release ]; then
+  cp -t ${DEST}/usr/lib64 -H ${LIBRARY_DIR}/libwayland-client.so.0
+  cp -t ${DEST}/usr/lib64 -H ${LIBRARY_DIR}/libwayland-cursor.so.0
+  cp -t ${DEST}/usr/lib64 -H ${LIBRARY_DIR}/libwayland-egl.so.1
+else
+  cp -t ${DEST}/usr/lib ${LIBRARY_DIR}/libwayland-client.so
+  cp -t ${DEST}/usr/lib ${LIBRARY_DIR}/libwayland-cursor.so
+  cp -t ${DEST}/usr/lib ${LIBRARY_DIR}/libwayland-egl.so
+fi
 python3 appImage/checkDeps.py $DEST
 
 exit 0
