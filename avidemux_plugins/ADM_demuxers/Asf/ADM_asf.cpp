9592331bde39051029ac6e7a91a316fb7ab80b68
eumagga0x2a <eumagga0x2a@users.noreply.github.com>
2026-09-06 00:24:48 +0200

    demuxers/Asf: check frame length to be within bounds, stop before reaching EOF, improve code legibility

--- avidemux_plugins/ADM_demuxers/Asf/ADM_asf.cpp
+++ avidemux_plugins/ADM_demuxers/Asf/ADM_asf.cpp
@@ -264,114 +264,122 @@ bool                       asfHeader::setPtsDts(uint32_t frame,uint64_t pts,uint
 */    
 uint8_t  asfHeader::getFrame(uint32_t framenum,ADMCompressedImage *img)
 {
-  img->dataLength=0;
-  img->flags=AVI_KEY_FRAME;
-  uint32_t len=0;
-  if(framenum>=nbImage)
-  {
-    printf("[ASF] Going out of bound %u %u\n",framenum, nbImage);
-    return 0;
-  }
-  if(!_index[framenum].frameLen)
-  {
-    goto gotcha;
-  }
-  // In case curSeq is stored as one byte..
-  curSeq&=0xff;
-  //
-  
-  aprintf("Framenum %u len: %u curSeq %u frameSeq=%u packetnb=%u \n",
-         framenum,_index[framenum].frameLen,curSeq,
-         _index[framenum].segNb,_index[framenum].packetNb);
-  // Seeking ?
-  if(_index[framenum].segNb!=curSeq || _index[framenum].segNb==1)
-  {
-    aprintf("Seeking.. curseq:%u wanted seq:%u packet=%" PRIu64"\n",curSeq,_index[framenum].segNb,_index[framenum].packetNb);
-    if(!_packet->goToPacket(_index[framenum].packetNb))
+    uint32_t len;
+    img->dataLength = 0;
+    img->flags = AVI_KEY_FRAME;
+    if (framenum >= nbImage)
     {
-      printf("[ASF] Cannot seek to frame %u\n",framenum);
-      return 0; 
+        ADM_warning("Requested frame %u out of bounds, must be < %u\n", framenum, nbImage);
+        return 0;
     }
-    _packet->purge();
-    curSeq=_index[framenum].segNb;
-    printf("Seeking done, starting at seq=%u\n",curSeq);
-  }
-  
-  
-  len=0;
-  uint32_t delta;
-  while(1)
-  {
-    while(readQueue.size())
+    asfIndex *dx = &(_index[framenum]);
+    if (dx->frameLen > ADM_COMPRESSED_MAX_DATA_LENGTH)
     {
-      asfBit *bit;
-      bit=readQueue.front();
-      readQueue.pop_front();
-      aprintf(">found packet of size %d seq %d, while curseq =%d wanted seg=%u current offset=%u, packet=%u\n",
-                    bit->len,bit->sequence,curSeq,_index[framenum].segNb,len,bit->packet);
-      // Here it is tricky
-      // if len==0 we are reading the first part of our frame
-      // The packet may starts with segments from the previous frame
-      // discard them
-      // Delta is just a security as it should be slightly >10
-      delta=256+bit->sequence-_index[framenum].segNb;
-      delta &=0xff;
-      if(!len) // Starting a new frame
-      {
-          if(_index[framenum].segNb != bit->sequence )
-          {
-            aprintf("Dropping seq=%u too old for %u delta %d\n",
-                  bit->sequence,_index[framenum].segNb,delta);
-            storageQueue.push_back(bit);
-            bit=NULL;
-            if(delta<230)
-            {
-              printf("[ASF] Very suspicious delta :%" PRIu32"\n",delta);
-            }
-            continue; 
-          }
-          // We have found our first chunk
-          curSeq=bit->sequence;
-          memcpy(img->data,bit->data,bit->len);
-          len=bit->len;
-          delete[] bit->data;
-          delete bit;
-          continue;
-      }
-      // Continuing a frame
-      // If the seq number is different it is the beginning of a new frame
-      if(bit->sequence!=curSeq )
-      {
-        aprintf("New sequence %u->%u while loading %u frame\n",curSeq,bit->sequence,framenum);
-        img->dataLength=len;
-        readQueue.push_front(bit);
-        curSeq=bit->sequence;
+        ADM_error("Frame %u size %u exceeds max. supported.\n", framenum, dx->frameLen);
+        return 0;
+    }
+    if (!dx->frameLen)
+    {
         goto gotcha;
-      }
-      // still same sequence ...add
-      memcpy(img->data+len,bit->data,bit->len);
-      len+=bit->len;
-      storageQueue.push_back(bit);
-      bit=NULL;
     }
-    if(!_packet->nextPacket(_videoStreamId))
+    // In case curSeq is stored as one byte..
+    curSeq&=0xff;
+    aprintf("Framenum %u len: %u curSeq: %u frameSeq: %u packetnb: %llu\n",
+            framenum, dx->frameLen, curSeq, dx->segNb, dx->packetNb);
+    // Seeking ?
+    if (dx->segNb != curSeq || dx->segNb == 1)
     {
-      printf("[ASF] Packet Error\n");
-      return 0; 
+        aprintf("Seeking... curSeq: %u wanted seq: %u packet: %" PRIu64"\n", curSeq, dx->segNb, dx->packetNb);
+        if(!_packet->goToPacket(dx->packetNb))
+        {
+            ADM_error("Cannot seek to frame %u\n", framenum);
+            return 0;
+        }
+        _packet->purge();
+        curSeq = dx->segNb;
+        ADM_info("Seeking done, starting at seq %u \n", curSeq);
     }
-    _packet->skipPacket();
-  }
+
+    len = 0;
+    uint32_t delta;
+    while(1)
+    {
+        while(readQueue.size())
+        {
+            asfBit *bit;
+            bit = readQueue.front();
+            readQueue.pop_front();
+            aprintf("-> found packet of size %d seq %d while curseq = %d wanted seg = %u, acc. data len: %u, packet: %u\n",
+                    bit->len, bit->sequence, curSeq, dx->segNb, len, bit->packet);
+            // Here it is tricky
+            // if len==0 we are reading the first part of our frame
+            // The packet may starts with segments from the previous frame
+            // discard them
+            // Delta is just a security as it should be slightly >10
+            delta = 256 + bit->sequence - dx->segNb;
+            delta &= 0xff;
+            if (!len) // Starting a new frame
+            {
+                if (dx->segNb != bit->sequence)
+                {
+                    aprintf("Dropping seq %u, too old for seg %u, delta: %d\n", bit->sequence, dx->segNb, delta);
+                    storageQueue.push_back(bit);
+                    bit = NULL;
+                    if (delta < 230)
+                    {
+                        aprintf("[ASF] Very suspicious delta : %" PRIu32"\n", delta);
+                    }
+                    continue;
+                }
+                // We have found our first chunk
+                curSeq = bit->sequence;
+                ADM_assert(bit->len <= dx->frameLen);
+                memcpy(img->data, bit->data, bit->len);
+                len = bit->len;
+                storageQueue.push_back(bit);
+                bit = NULL;
+                continue;
+            }
+            // Continuing a frame
+            // If the seq number is different it is the beginning of a new frame
+            if (bit->sequence != curSeq)
+            {
+                aprintf("New sequence %u -> %u while loading frame %u\n", curSeq, bit->sequence, framenum);
+                readQueue.push_front(bit);
+                curSeq = bit->sequence;
+                goto gotcha;
+            }
+            // still same sequence ...add
+            ADM_assert((bit->len <= dx->frameLen) && (len + bit->len <= dx->frameLen));
+            memcpy(img->data + len, bit->data, bit->len);
+            len += bit->len;
+            storageQueue.push_back(bit);
+            bit = NULL;
+        }
+        if(_packet->finished())
+        {
+            ADM_warning("Too close to EOF.\n");
+            return 0;
+        }
+        if(!_packet->nextPacket(_videoStreamId))
+        {
+            ADM_error("Packet error.\n");
+            return 0;
+        }
+        _packet->skipPacket();
+    }
 gotcha:
-  
-  img->dataLength=len;
-  img->demuxerDts=_index[framenum].dts;
-  img->demuxerPts=_index[framenum].pts;
-  img->flags=_index[framenum].flags;
-  if(len!=_index[framenum].frameLen)
-  {
-    ADM_error("[ASF] Frame=%u :-> Mismatch found len : %u expected %u\n",framenum,len, _index[framenum].frameLen);
-  }
-  aprintf(">>Len %d seq %d\n",len,curSeq);
-  return 1; 
+    aprintf("-> Len %u seq %u\n", len, curSeq);
+    if (len != dx->frameLen)
+    {
+        ADM_error("Frame %u size mismatch: got %u, expected %u\n", framenum, len, dx->frameLen);
+        return 0;
+    }
+    img->dataLength = len;
+    img->demuxerDts = dx->dts;
+    img->demuxerPts = dx->pts;
+    img->flags = dx->flags;
+
+    return 1;
 }
 //EOF
